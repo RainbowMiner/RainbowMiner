4642fcb6b42fdb230878f0c52c993f8451896d7a
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-05-22 15:21:23 +0200

    Active updater

--- Updater.ps1
+++ Updater.ps1
@@ -6,24 +6,23 @@ if ($script:MyInvocation.MyCommand.Path) {Set-Location (Split-Path $script:MyInv
 
 $ProgressPreferenceBackup = $ProgressPreference
 
-if ( $false ) {
-    $Name = "RainbowMiner"
-    try {
-        $ProgressPreference = "SilentlyContinue"
-        $Request = Invoke-RestMethod -Uri "https://api.github.com/repos/rainbowminer/$Name/releases/latest" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop
-        $Version = ($Request.tag_name -replace '^v')
-        $Uri = $Request.assets | Where-Object Name -EQ "$($Name)V$($Version).zip" | Select-Object -ExpandProperty browser_download_url
 
-        if ($Version -ne $MPMVersion) {
-            $ProgressPreference = $ProgressPreferenceBackup
-            Write-Progress -Activity "Updater" -Status $Name -CurrentOperation "Acquiring Online ($URI)"
-            $ProgressPreference = "SilentlyContinue"
-            Write-Log -Level Warn "The software ($Name) is out of date; there is an updated version available at $URI. "
-        }
+$Name = "RainbowMiner"
+try {
+    $ProgressPreference = "SilentlyContinue"
+    $Request = Invoke-RestMethod -Uri "https://api.github.com/repos/rainbowminer/$Name/releases/latest" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop
+    $Version = ($Request.tag_name -replace '^v')
+    $Uri = $Request.assets | Where-Object Name -EQ "$($Name)V$($Version).zip" | Select-Object -ExpandProperty browser_download_url
+
+    if ($Version -ne $MPMVersion) {
+        $ProgressPreference = $ProgressPreferenceBackup
+        Write-Progress -Activity "Updater" -Status $Name -CurrentOperation "Acquiring Online ($URI)"
+        $ProgressPreference = "SilentlyContinue"
+        Write-Log -Level Warn "The software ($Name) is out of date; there is an updated version available at $URI. "
     }
-    catch {
-        Write-Log -Level Warn "The software ($Name) failed to update. "
-    }
+}
+catch {
+    Write-Log -Level Warn "The software ($Name) failed to update. "
 }
 
 $Name = "PowerShell"
