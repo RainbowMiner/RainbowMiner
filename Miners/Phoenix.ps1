ce458414a5665948620444035304f3c259cc18ba
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-06-15 13:18:58 +0200

    Update Phoenix Miner

---
+++ Miners/Phoenix.ps1
@@ -0,0 +1,52 @@
+﻿using module ..\Include.psm1
+
+$Path = ".\Bin\Ethash-Phoenix\PhoenixMiner.exe"
+$URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.0c-phoenixminer/PhoenixMiner_3.0c.zip"
+$Port = "308{0:d2}"
+
+$Commands = [PSCustomObject]@{
+    "ethash" = "" #Ethash
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Devices = @($Devices.NVIDIA) + @($Devices.AMD) 
+if (-not $Devices -and -not $Config.InfoOnly) {return} # No GPU present in system
+
+Select-Device $Devices -MinMemSize 3GB | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Vendor = Get-DeviceVendor $_
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($_.Model)) -join '-'
+
+    $DeviceIDsAll = Get-GPUIDs $Miner_Device -join '' -Offset 1
+
+    switch($Miner_Vendor) {
+        "NVIDIA" {$Miner_Deviceparams = "-nvidia"}
+        "AMD" {$Miner_Deviceparams = "-amd"}
+        Default {$Miner_Deviceparams = ""}
+    }
+    $Miner_Vendor = (Get-Culture).TextInfo.ToTitleCase($Miner_Vendor.ToLower())
+
+    $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_
+
+        $possum    = if ( $Pools.$Algorithm_Norm.Protocol -eq "stratum+tcp" <#temp fix#> ) { "-proto 4 -stales 0" } else { "-proto 1" }
+        $proto     = if ( $Pools.$Algorithm_Norm.Protocol -eq "stratum+tcp" <#temp fix#> ) { "stratum+tcp://" } else { "" }
+
+        [PSCustomObject]@{
+            Name = $Miner_Name
+            DeviceName = $Miner_Device.Name
+            DeviceModel = $Miner_Model
+            Path = $Path
+            Arguments = "-rmode 0 -cdmport $($Miner_Port) -cdm 1 -coin auto -gpus $($DeviceIDsAll) -pool $($proto)$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -wal $($Pools.$Algorithm_Norm.User) -pass $($Pools.$Algorithm_Norm.Pass) $($possum) $($Miner_Deviceparams)$($Commands.$_)"
+            HashRates = [PSCustomObject]@{$Algorithm_Norm = $($Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week)}
+            API = "Claymore"
+            Port = $Miner_Port
+            URI = $Uri
+            DevFee = 0.65
+        }
+    }
+}
