401637348271b58ec786ff94c2816dbbac2c416c
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-06-18 01:18:44 +0200

    Update claymore

--- Miners/Claymore.ps1
+++ Miners/Claymore.ps1
@@ -62,14 +62,12 @@ $Devices = @($Devices.NVIDIA) + @($Devices.AMD)
 if (-not $Devices -and -not $Config.InfoOnly) {return} # No GPU present in system
 
 $Devices | Select-Object Vendor, Model -Unique | ForEach-Object {
-    $Miner_Device = $Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
-    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Device = $Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Device | Select-Object -First 1 -ExpandProperty Index)
     $Miner_Vendor = Get-DeviceVendor $_
     $Miner_Model = $_.Model
     $Miner_Fee = 0
-    if ($Miner_Device | Where-Object {$_.OpenCL.GlobalMemsize -gt 2Gb}) {$Miner_Fee=$DevFee}
-
-    $DeviceIDsAll = Get-GPUIDs $Miner_Device -join '' -ToHex
+    if (Select-Device $Device -MinMemSize 2.1Gb) {$Miner_Fee=$DevFee}
 
     switch($Miner_Vendor) {
         "NVIDIA" {$Arguments_Platform = " -platform 2"}
@@ -81,36 +79,47 @@ $Devices | Select-Object Vendor, Model -Unique | ForEach-Object {
         $MainAlgorithm = $_.MainAlgorithm
         $MainAlgorithm_Norm = Get-Algorithm $MainAlgorithm
 
-        if ($Pools.$MainAlgorithm_Norm.Name -eq 'NiceHash') {$EthereumStratumMode = "3"} else {$EthereumStratumMode = "2"} #Optimize stratum compatibility
+        Switch ($MainAlgorithm_Norm) {
+            # default is all devices, ethash has a 4GB minimum memory limit
+            "Ethash" {$Miner_Device = Select-Device $Device -MinMemSize 4gb}
+            "Ethash3gb" {$Miner_Device = Select-Device $Device -MinMemSize 3gb}
+            default {$Miner_Device = $Device}
+        }
 
-        if ($Arguments_Platform) {
-            if ($_.SecondaryAlgorithm) {
-                $SecondaryAlgorithm = $_.SecondaryAlgorithm
-                $SecondaryAlgorithm_Norm = Get-Algorithm $SecondaryAlgorithm
+        if ($Miner_Device) {
+            $DeviceIDsAll = Get-GPUIDs $Miner_Device -join '' -ToHex
 
-                $Miner_Name = (@("$($Name)$($SecondaryAlgorithm_Norm)$(if ($_.SecondaryIntensity -ge 0) {$_.SecondaryIntensity})") + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-                $Miner_HashRates = [PSCustomObject]@{"$MainAlgorithm_Norm" = $Stats."$($Miner_Name)_$($MainAlgorithm_Norm)_HashRate".Week; "$SecondaryAlgorithm_Norm" = $Stats."$($Miner_Name)_$($SecondaryAlgorithm_Norm)_HashRate".Week}
-                $Arguments_Secondary = " -mode 0 -dcoin $($Coins.$SecondaryAlgorithm) -dpool $($Pools.$SecondaryAlgorithm_Norm.Host):$($Pools.$SecondaryAlgorithm_Norm.Port) -dwal $($Pools.$SecondaryAlgorithm_Norm.User) -dpsw $($Pools.$SecondaryAlgorithm_Norm.Pass)$(if($_.SecondaryIntensity -ge 0){" -dcri=$($_.SecondaryIntensity)"})"
-                if ($Miner_Fee -gt 0) {$Miner_Fee = $DevFeeDual}
+            if ($Pools.$MainAlgorithm_Norm.Name -eq 'NiceHash') {$EthereumStratumMode = "3"} else {$EthereumStratumMode = "2"} #Optimize stratum compatibility
+
+            if ($Arguments_Platform) {
+                if ($_.SecondaryAlgorithm) {
+                    $SecondaryAlgorithm = $_.SecondaryAlgorithm
+                    $SecondaryAlgorithm_Norm = Get-Algorithm $SecondaryAlgorithm
+
+                    $Miner_Name = (@("$($Name)$($MainAlgorithm_Norm -replace '^ethash', '')$($SecondaryAlgorithm_Norm)$(if ($_.SecondaryIntensity -ge 0) {$_.SecondaryIntensity})") + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+                    $Miner_HashRates = [PSCustomObject]@{"$MainAlgorithm_Norm" = $Stats."$($Miner_Name)_$($MainAlgorithm_Norm)_HashRate".Week; "$SecondaryAlgorithm_Norm" = $Stats."$($Miner_Name)_$($SecondaryAlgorithm_Norm)_HashRate".Week}
+                    $Arguments_Secondary = " -mode 0 -dcoin $($Coins.$SecondaryAlgorithm) -dpool $($Pools.$SecondaryAlgorithm_Norm.Host):$($Pools.$SecondaryAlgorithm_Norm.Port) -dwal $($Pools.$SecondaryAlgorithm_Norm.User) -dpsw $($Pools.$SecondaryAlgorithm_Norm.Pass)$(if($_.SecondaryIntensity -ge 0){" -dcri $($_.SecondaryIntensity)"})"
+                    if ($Miner_Fee -gt 0) {$Miner_Fee = $DevFeeDual}
+                }
+                else {
+                    $Miner_Name = (@("$($Name)$($MainAlgorithm_Norm -replace '^ethash', '')") + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+                    $Miner_HashRates = [PSCustomObject]@{"$MainAlgorithm_Norm" = $Stats."$($Miner_Name)_$($MainAlgorithm_Norm)_HashRate".Week}
+                    $Arguments_Secondary = " -mode 1"
+                }
+
+                [PSCustomObject]@{
+                    Name        = $Miner_Name
+                    DeviceName  = $Miner_Device.Name
+                    DeviceModel = $Miner_Model
+                    Path        = $Path               
+                    Arguments   = ("-mport -$($Miner_Port) -epool $($Pools.$MainAlgorithm_Norm.Host):$($Pools.$MainAlgorithm_Norm.Port) -ewal $($Pools.$MainAlgorithm_Norm.User) -epsw $($Pools.$MainAlgorithm_Norm.Pass) -allpools 1 -allcoins exp -esm $($EthereumStratumMode)$($Arguments_Secondary)$($_.Params)$($Arguments_Platform) -di $($DeviceIDsAll)" -replace "\s+", " ").trim()
+                    HashRates   = $Miner_HashRates
+                    API         = "Claymore"
+                    Port        = $Miner_Port
+                    URI         = $Uri
+                    DevFee        = $Miner_Fee 
+                }             
             }
-            else {
-                $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-                $Miner_HashRates = [PSCustomObject]@{"$MainAlgorithm_Norm" = $Stats."$($Miner_Name)_$($MainAlgorithm_Norm)_HashRate".Week}
-                $Arguments_Secondary = " -mode 1"
-            }
-
-            [PSCustomObject]@{
-                Name        = $Miner_Name
-                DeviceName  = $Miner_Device.Name
-                DeviceModel = $Miner_Model
-                Path        = $Path               
-                Arguments   = ("-mport -$($Miner_Port) -epool $($Pools.$MainAlgorithm_Norm.Host):$($Pools.$MainAlgorithm.Port) -ewal $($Pools.$MainAlgorithm_Norm.User) -epsw $($Pools.$MainAlgorithm_Norm.Pass) -allpools 1 -allcoins exp -esm $($EthereumStratumMode)$($Arguments_Secondary)$($_.Params)$($Arguments_Platform) -di $($DeviceIDsAll)" -replace "\s+", " ").trim()
-                HashRates   = $Miner_HashRates
-                API         = "Claymore"
-                Port        = $Miner_Port
-                URI         = $Uri
-                DevFee        = $Miner_Fee 
-            }             
         }
     }
 }
