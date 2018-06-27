615aaa38b77db7430c183f13be57a983985af06d
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-06-27 16:00:05 +0200

    More miner-defaults including Afterburner Profiles

--- Config/MinersConfigDefault.ps1
+++ Config/MinersConfigDefault.ps1
@@ -1,11 +1,17 @@
 ﻿
 [PSCustomObject]@{
+        "Bminer" = [PSCustomObject[]]@(
+            [PSCustomObject]@{MainAlgorithm = "ethash"; SecondaryAlgorithm = "blake14r"; Params = ""; Profile = "5"}
+            [PSCustomObject]@{MainAlgorithm = "ethash"; SecondaryAlgorithm = "blake2s"; Params = ""; Profile = "5"}
+        )
         "CcminerAlexis78" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "c11"; SecondaryAlgorithm = ""; Params = "-i 21"; Profile = ""},
-            [PSCustomObject]@{MainAlgorithm = "keccak"; SecondaryAlgorithm = ""; Params = "-m 2 -i 29"; Profile = ""},
-            [PSCustomObject]@{MainAlgorithm = "keccakc"; SecondaryAlgorithm = ""; Params = "-i 29"; Profile = ""},
-            [PSCustomObject]@{MainAlgorithm = "veltor"; SecondaryAlgorithm = ""; Params = "-i 23"; Profile = ""},
-            [PSCustomObject]@{MainAlgorithm = "x11evo"; SecondaryAlgorithm = ""; Params = "-i 21"; Profile = ""},
+            [PSCustomObject]@{MainAlgorithm = "c11"; SecondaryAlgorithm = ""; Params = "-i 21"; Profile = ""}
+            [PSCustomObject]@{MainAlgorithm = "keccak"; SecondaryAlgorithm = ""; Params = "-m 2 -i 29"; Profile = ""}
+            [PSCustomObject]@{MainAlgorithm = "keccakc"; SecondaryAlgorithm = ""; Params = "-i 29"; Profile = ""}
+            [PSCustomObject]@{MainAlgorithm = "lyra2z"; SecondaryAlgorithm = ""; Params = ""; Profile = "4"}
+            [PSCustomObject]@{MainAlgorithm = "lyra2v2"; SecondaryAlgorithm = ""; Params = ""; Profile = "4"}
+            [PSCustomObject]@{MainAlgorithm = "veltor"; SecondaryAlgorithm = ""; Params = "-i 23"; Profile = ""}
+            [PSCustomObject]@{MainAlgorithm = "x11evo"; SecondaryAlgorithm = ""; Params = "-i 21"; Profile = ""}
             [PSCustomObject]@{MainAlgorithm = "x13"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
             [PSCustomObject]@{MainAlgorithm = "x17"; SecondaryAlgorithm = ""; Params = "-i 20.5"; Profile = ""}
         )
@@ -13,11 +19,19 @@
             [PSCustomObject]@{MainAlgorithm = "lyra2z"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
         )
         "CcminerKlausT" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "neoscrypt"; SecondaryAlgorithm = ""; Params = "-i 17.6"; Profile = ""}
+            [PSCustomObject]@{MainAlgorithm = "neoscrypt"; SecondaryAlgorithm = ""; Params = "-i 17.6"; Profile = "3"}
+        )
+        "CcminerPigeoncoin" = [PSCustomObject[]]@(        
+            [PSCustomObject]@{MainAlgorithm = "x16s"; SecondaryAlgorithm = ""; Params = ""; Profile = "4"}
         )
         "CcminerPolytimos" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "keccak"; SecondaryAlgorithm = ""; Params = "-i auto"; Profile = ""}
+            [PSCustomObject]@{MainAlgorithm = "lyra2v2"; SecondaryAlgorithm = ""; Params = ""; Profile = "4"}
+            [PSCustomObject]@{MainAlgorithm = "nist5"; SecondaryAlgorithm = ""; Params = ""; Profile = "4"}
             [PSCustomObject]@{MainAlgorithm = "x17"; SecondaryAlgorithm = ""; Params = "-i 20.5"; Profile = ""}
+        )
+        "CcminerRavencoin" = [PSCustomObject[]]@(        
+            [PSCustomObject]@{MainAlgorithm = "x16r"; SecondaryAlgorithm = ""; Params = ""; Profile = "4"}
         )
         "CcminerSkunk" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "skunk"; SecondaryAlgorithm = ""; Params = "-i 25"; Profile = ""}
@@ -29,14 +43,24 @@
             [PSCustomObject]@{MainAlgorithm = "tribus"; SecondaryAlgorithm = ""; Params = "-i auto"; Profile = ""}
         )
         "CcminerTpruvot" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "lyra2z"; SecondaryAlgorithm = ""; Params = "-i 21"; Profile = ""}
+            [PSCustomObject]@{MainAlgorithm = "lyra2z"; SecondaryAlgorithm = ""; Params = "-i 21"; Profile = "4"}
             [PSCustomObject]@{MainAlgorithm = "x13"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
         )
         "CcminerTpruvotx64" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "allium"; SecondaryAlgorithm = ""; Params = "-i 22.125"; Profile = ""}
+            [PSCustomObject]@{MainAlgorithm = "keccak"; SecondaryAlgorithm = ""; Params = "-i 29"; Profile = ""}
             [PSCustomObject]@{MainAlgorithm = "keccakc"; SecondaryAlgorithm = ""; Params = "-i 29"; Profile = ""}
-            [PSCustomObject]@{MainAlgorithm = "lyra2z"; SecondaryAlgorithm = ""; Params = "-i 21"; Profile = ""}
+            [PSCustomObject]@{MainAlgorithm = "lyra2z"; SecondaryAlgorithm = ""; Params = "-i 21"; Profile = "4"}
+            [PSCustomObject]@{MainAlgorithm = "sha256t"; SecondaryAlgorithm = ""; Params = "-i 29"; Profile = ""}
             [PSCustomObject]@{MainAlgorithm = "x13"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
+        )
+        "CcminerX16r" = [PSCustomObject[]]@(
+            [PSCustomObject]@{MainAlgorithm = "x16r"; SecondaryAlgorithm = ""; Params = ""; Profile = "4"}
+            [PSCustomObject]@{MainAlgorithm = "x16s"; SecondaryAlgorithm = ""; Params = ""; Profile = "4"}
+        )
+        "CcminerX16s" = [PSCustomObject[]]@(
+            [PSCustomObject]@{MainAlgorithm = "x16r"; SecondaryAlgorithm = ""; Params = ""; Profile = "4"}
+            [PSCustomObject]@{MainAlgorithm = "x16s"; SecondaryAlgorithm = ""; Params = ""; Profile = "4"}
         )
         "CcminerXevan" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "c11"; SecondaryAlgorithm = ""; Params = "-i 21"; Profile = ""}
@@ -45,6 +69,14 @@
         )
         "CcminerXevan75" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "skein"; SecondaryAlgorithm = ""; Params = "-i auto"; Profile = ""}
+        )
+        "ClaymoreEthash" = [PSCustomObject[]]@(
+            [PSCustomObject]@{MainAlgorithm = "ethash"; SecondaryAlgorithm = "pascal"; Params = ""; Profile = "5"}
+            [PSCustomObject]@{MainAlgorithm = "ethash"; SecondaryAlgorithm = "keccak"; Params = ""; Profile = "3"}
+            [PSCustomObject]@{MainAlgorithm = "ethash"; SecondaryAlgorithm = "blake2s"; Params = ""; Profile = "5"}
+        )
+        "ClaymoreEthash9" = [PSCustomObject[]]@(
+            [PSCustomObject]@{MainAlgorithm = "ethash"; SecondaryAlgorithm = "pascal"; Params = ""; Profile = "5"}
         )
         "EnemyZealot" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "aeriumx"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
@@ -56,9 +88,15 @@
             [PSCustomObject]@{MainAlgorithm = "timetravel"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
             [PSCustomObject]@{MainAlgorithm = "tribus"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
             [PSCustomObject]@{MainAlgorithm = "xevan"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
-            [PSCustomObject]@{MainAlgorithm = "x16r"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
-            [PSCustomObject]@{MainAlgorithm = "x16s"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
+            [PSCustomObject]@{MainAlgorithm = "x16r"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = "4"}
+            [PSCustomObject]@{MainAlgorithm = "x16s"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = "4"}
             [PSCustomObject]@{MainAlgorithm = "x17"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
             [PSCustomObject]@{MainAlgorithm = "vitalium"; SecondaryAlgorithm = ""; Params = "-i 20"; Profile = ""}
+        )
+        "Excavator" = [PSCustomObject[]]@(
+            [PSCustomObject]@{MainAlgorithm = "neoscrypt"; SecondaryAlgorithm = ""; Params = ""; Profile = "3"}
+        )
+        "Excavator1.4.4" = [PSCustomObject[]]@(
+            [PSCustomObject]@{MainAlgorithm = "neoscrypt"; SecondaryAlgorithm = ""; Params = ""; Profile = "3"}
         )
     }
