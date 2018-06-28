bee634ec58ef779749415299183e0c2673c70234
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-06-28 12:48:01 +0200

    No pool fee for Ethash on MiningPoolHub until 2018/06/30

--- Pools/MiningPoolHub.ps1
+++ Pools/MiningPoolHub.ps1
@@ -26,7 +26,6 @@ if (($MiningPoolHub_Request.return | Measure-Object).Count -le 1) {
 }
 
 $MiningPoolHub_Regions = "europe", "us-east", "asia"
-$MiningPoolHub_Fee = 0.9
 
 $MiningPoolHub_Request.return | ForEach-Object {
     $MiningPoolHub_Hosts = $_.all_host_list.split(";")
@@ -34,7 +33,9 @@ $MiningPoolHub_Request.return | ForEach-Object {
     $MiningPoolHub_Algorithm = $_.algo
     $MiningPoolHub_Algorithm_Norm = Get-Algorithm $MiningPoolHub_Algorithm
     $MiningPoolHub_Coin = (Get-Culture).TextInfo.ToTitleCase(($_.current_mining_coin -replace "-", " " -replace "_", " ")) -replace " "
+    $MiningPoolHub_Fee = 0.9
     
+    if ($MiningPoolHub_Algorithm_Norm -eq "Ethash" -and (get-date -Format "yyyyMMdd") -le "20180630") {$MiningPoolHub_Fee = 0.0}
     if ($MiningPoolHub_Algorithm_Norm -eq "Sia") {$MiningPoolHub_Algorithm_Norm = "SiaClaymore"} #temp fix
 
     $Divisor = 1000000000
