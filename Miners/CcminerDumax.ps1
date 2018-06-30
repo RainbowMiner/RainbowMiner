daee619efa2f4f5e64e00eeab104fb19bf4e9af0
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-07-01 00:11:06 +0200

    Update ccminerdumax (phi2-miner) to v0.9.3

--- Miners/CcminerDumax.ps1
+++ Miners/CcminerDumax.ps1
@@ -1,7 +1,7 @@
 ﻿using module ..\Include.psm1
 
 $Path = ".\Bin\NVIDIA-ccminerDumax\ccminer.exe"
-$Uri = "https://github.com/DumaxFr/ccminer/releases/download/dumax-0.9.2/ccminer-dumax-0.9.2-win64.zip"
+$Uri = "https://github.com/DumaxFr/ccminer/releases/download/dumax-0.9.3/ccminer-dumax-0.9.3-win64.zip"
 $Port = "120{0:d2}"
 
 $Devices = $Devices.NVIDIA
