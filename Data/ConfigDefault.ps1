aefdb1774a31de55c9e5e2f1239badd282f6ba4a
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-07-08 20:47:50 +0200

    Add EquihashZero (192, Zero_PoW) to default config

--- Data/ConfigDefault.ps1
+++ Data/ConfigDefault.ps1
@@ -1,6 +1,6 @@
 ﻿[PSCustomObject]@{
     PoolName = @("Nicehash","Blazepool","MiningPoolHub","Zergpool")
-    Algorithm = @("blake2s","c11","cryptolight","cryptonightheavy","cryptonightv3","cryptonightv7","cryptonightv8","equihash","equihashbtg","ethash","hmq1725","hsr","keccak","keccakc","lyra2re2","lyra2z","myrgr","neoscrypt","pascal","phi","phi2","skein","skunk","timetravel","tribus","vit","x16r","x16s","x17","xevan","yescrypt")
+    Algorithm = @("blake2s","c11","cryptolight","cryptonightheavy","cryptonightv3","cryptonightv7","cryptonightv8","equihash","equihashbtg","equihashzero","ethash","hmq1725","hsr","keccak","keccakc","lyra2re2","lyra2z","myrgr","neoscrypt","pascal","phi","phi2","skein","skunk","timetravel","tribus","vit","x16r","x16s","x17","xevan","yescrypt")
     FastestMinerOnly = $true 
     ShowPoolBalances = $true
     ShowMinerWindow = $false
