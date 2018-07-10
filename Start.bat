a13d2910af81e7d999af78aef2e929f525efbe16
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-07-10 14:31:52 +0200

    Update start.bat
    Older powershell 5.0 launches, if pwsh 6.0.2 is not found in path

--- Start.bat
+++ Start.bat
@@ -7,9 +7,17 @@
 @if not "%GPU_SINGLE_ALLOC_PERCENT%"=="100" (setx GPU_SINGLE_ALLOC_PERCENT 100) > nul
 @if not "%CUDA_DEVICE_ORDER%"=="PCI_BUS_ID" (setx CUDA_DEVICE_ORDER PCI_BUS_ID) > nul
 
-@set "command=& .\rainbowminer.ps1 -configfile .\Config\config.txt"
+@set "command=& .\rainbowminer.ps1 -configfile .\Config\config.txt -disableautoupdate -remoteapi"
+
+@echo off
 
 start pwsh -noexit -executionpolicy bypass -command "& .\reader.ps1 -log '^(.+)?-\d+_\d\d\d\d-\d\d-\d\d_\d\d-\d\d-\d\d.txt' -sort '^[^_]*_' -quickstart"
 
+where pwsh.exe >nul 2>nul
+if %errorlevel%==1 (
+    powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
+    goto end
+)
 pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
 
+:end
