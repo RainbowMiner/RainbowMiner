94bfa998e3e43dab8848f163cf3e8c215701c78f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-07-16 14:52:57 +0200

    Update miner CryptoDredge

--- Miners/CryptoDredge.ps1
+++ Miners/CryptoDredge.ps1
@@ -8,7 +8,7 @@ param(
 )
 
 $Path = ".\Bin\NVIDIA-CryptoDredge\CryptoDredge.exe"
-$Uri = "https://github.com/CryptoDredge/beta/releases/download/v0.6.1-beta/CryptoDredge_0.6.1.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7.0/CryptoDredge_0.7.0_win_x64.zip"
 $ManualUri = "https://bitcointalk.org/index.php?topic=4129696.0"
 $Port = "313{0:d2}"
 
@@ -18,7 +18,7 @@ if (-not $Devices -or $Config.InfoOnly) {return} # No NVIDIA present in system
 $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "allium"; Params = ""} #Allium
     [PSCustomObject]@{MainAlgorithm = "lyra2v2"; Params = ""} #Lyra2Re2
-    [PSCustomObject]@{MainAlgorithm = "lyra2z"; Params = "--submit-stale"} #Lyra2z
+    [PSCustomObject]@{MainAlgorithm = "lyra2z"; Params = ""} #Lyra2z
     [PSCustomObject]@{MainAlgorithm = "neoscrypt"; Params = ""} #Neoscrypt
     [PSCustomObject]@{MainAlgorithm = "phi"; Params = ""} #PHI
     [PSCustomObject]@{MainAlgorithm = "phi2"; Params = ""} #PHI2
@@ -46,7 +46,7 @@ $Devices | Select-Object Vendor, Model -Unique | ForEach-Object {
                 DeviceName = $Miner_Device.Name
                 DeviceModel = $Miner_Model
                 Path = $Path
-                Arguments = "-R 1 -N 1 -b 127.0.0.1:$($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) --no-watchdog -q -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($_.Params)"
+                Arguments = "--retry-pause 1 -b 127.0.0.1:$($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) --no-watchdog -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($_.Params)"
                 HashRates = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
                 API = "CryptoDredge"
                 Port = $Miner_Port
