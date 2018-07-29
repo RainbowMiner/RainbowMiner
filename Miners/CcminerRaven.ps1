e7c82c6b95660d800148e0dfecb0cc8ad4d31fa8
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-07-30 00:15:25 +0200

    Add miner CcminerRaven v3.0 Cuda 9.2
    Near same speed as CcminerX16r

---
+++ Miners/CcminerRaven.ps1
@@ -0,0 +1,51 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject]$Devices
+)
+
+$Path = ".\Bin\NVIDIA-Raven\ccminer.exe"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.0-ravencoinminer/ccminerRavenx32.zip"
+$Port = "121{0:d2}"
+
+$Devices = $Devices.NVIDIA
+if (-not $Devices -or $Config.InfoOnly) {return} # No NVIDIA present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "x16r"; Params = ""; ExtendInterval = 10; FaultTolerance = 0.5; HashrateDuration = "Day"} #X16R RavenCoin
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Devices | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+
+    $DeviceIDsAll = $Miner_Device.Type_PlatformId_Index -join ','
+
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+
+        if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+            [PSCustomObject]@{
+                Name = $Miner_Name
+                DeviceName = $Miner_Device.Name
+                DeviceModel = $Miner_Model
+                Path = $Path
+                Arguments = "-R 1 -b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -q -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($_.Params)"
+                HashRates = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate"."$(if ($_.HashrateDuration){$_.HashrateDuration}else{"Week"})"}
+                API = "Ccminer"
+                Port = $Miner_Port
+                URI = $Uri
+                FaultTolerance = $_.FaultTolerance
+                ExtendInterval = $_.ExtendInterval
+            }
+        }
+    }
+}
