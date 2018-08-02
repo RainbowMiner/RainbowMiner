dabed4f715facbd1ea0526c1db3243ebd5555f75
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-08-02 12:00:47 +0200

    Update miner Ethminer
    - new minimum memory requirement 4GB
    - beautify code

--- Miners/Ethminer.ps1
+++ Miners/Ethminer.ps1
@@ -12,7 +12,9 @@ $URI = "https://github.com/ethereum-mining/ethminer/releases/download/v0.15.0/et
 $Port = "301{0:d2}"
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{MainAlgorithm = "ethash"; Params = ""} #Ethash
+    [PSCustomObject]@{MainAlgorithm = "ethash2gb"; MinMemGB = 2; Params = @()} #Ethash2GB
+    [PSCustomObject]@{MainAlgorithm = "ethash3gb"; MinMemGB = 3; Params = @()} #Ethash3GB
+    [PSCustomObject]@{MainAlgorithm = "ethash"   ; MinMemGB = 4; Params = @()} #Ethash
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -20,32 +22,36 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 $Devices = @($Devices.NVIDIA) + @($Devices.AMD) 
 if (-not $Devices -and -not $Config.InfoOnly) {return} # No GPU present in system
 
-Select-Device $Devices -MinMemSize 3GB | Select-Object Vendor, Model -Unique | ForEach-Object {
-    $Miner_Device = $Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
+$Devices | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Device = $Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
     $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
     $Miner_Vendor = Get-DeviceVendor $_
     $Miner_Model = $_.Model
-    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
 
     switch($Miner_Vendor) {
-        "NVIDIA" {$Miner_Deviceparams = "--cuda-devices $($Miner_Device.Type_PlatformId_Index -join ' ') --cuda"}
-        "AMD" {$Miner_Deviceparams = "--opencl-devices $($Miner_Device.Type_PlatformId_Index -join ' ') --opencl --opencl-platform $($Miner_Device | select -Property Platformid -Unique -ExpandProperty PlatformId)"}
+        "NVIDIA" {$Miner_Deviceparams = "--cuda --cuda-devices"}
+        "AMD" {$Miner_Deviceparams = "--opencl --opencl-platform $($Device | Select-Object -First 1 -ExpandProperty PlatformId) --opencl-devices"}
         Default {$Miner_Deviceparams = ""}
     }
     $Miner_Vendor = (Get-Culture).TextInfo.ToTitleCase($Miner_Vendor.ToLower())
 
     $Commands | ForEach-Object {
         $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+        $MinMemGB = $_.MinMemGB
 
-        $Miner_Protocol = $Pools.$Algorithm_Norm.Protocol -replace "stratum","stratum$(if ($Pools.$Algorithm_Norm.Name -eq 'NiceHash'){2})"       
+        if ($Miner_Device = @($Device | Where-Object {$_.OpenCL.GlobalMemsize -ge $MinMemGB * 1Gb})) {
+            $Miner_Name = ((@($Name) + @("$($Algorithm_Norm -replace '^ethash', '')") + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-')  -replace "-+", "-"
+            $DeviceIDsAll = ($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join ' '
+
+            $Miner_Protocol = $Pools.$Algorithm_Norm.Protocol
+            if ($Pools.$Algorithm_Norm.Name -eq 'NiceHash') {$Miner_Protocol = $Miner_Protocol -replace "stratum","stratum2"}
         
-        if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
             [PSCustomObject]@{
                 Name = $Miner_Name
                 DeviceName = $Miner_Device.Name
                 DeviceModel = $Miner_Model
                 Path = $Path
-                Arguments = "--api-port $($Miner_Port) $($Miner_Deviceparams) -P $($Miner_Protocol)://$($Pools.$Algorithm_Norm.User):$($Pools.$Algorithm_Norm.Pass)@$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) $($_.Params)"
+                Arguments = "--api-port $($Miner_Port) $($Miner_Deviceparams) $($DeviceIDsAll) -P $($Miner_Protocol)://$([System.Web.HttpUtility]::UrlEncode($Pools.$Algorithm_Norm.User)):$([System.Web.HttpUtility]::UrlEncode($Pools.$Algorithm_Norm.Pass))@$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) $($_.Params)"
                 HashRates = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
                 API = "Claymore"
                 Port = $Miner_Port
