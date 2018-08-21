cba8b548eb6bda7e2e04e9481f9f0f3c48a305e7
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-08-21 12:40:00 +0200

    Update miner Excavator
    - new multicoin handling

--- Miners/Excavator.ps1
+++ Miners/Excavator.ps1
@@ -81,7 +81,7 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
                         [PSCustomObject]@{id = 1; method = "algorithm.add"; params = @("$Main_Algorithm")},`
                         [PSCustomObject]@{id = 1; method = "workers.add"; params = @(@($Miner_Device.Type_Vendor_Index | ForEach-Object {@("alg-$($Algorithm)", "$_") + $Params} | Select-Object) * $Threads)}
                     )
-                    HashRates            = [PSCustomObject]@{$Main_Algorithm_Norm = $Stats."$($Miner_Name)_$($Main_Algorithm_Norm)_HashRate".Week}
+                    HashRates            = [PSCustomObject]@{$Main_Algorithm_Norm = $Stats."$($Miner_Name)_$($Main_Algorithm_Norm -replace '\-.*$')_HashRate".Week}
                     API                  = "Excavator"
                     Port                 = $Miner_Port
                     URI                  = $Uri
@@ -108,7 +108,7 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
                             [PSCustomObject]@{id = 1; method = "algorithm.add"; params = @("$Main_Algorithm")};[PSCustomObject]@{id = 1; method = "algorithm.add"; params = @("$Secondary_Algorithm")},`
                             [PSCustomObject]@{id = 1; method = "workers.add"; params = @(@($Miner_Device.Type_Vendor_Index | ForEach-Object {@("alg-$($Algorithm)", "$_") + $Params} | Select-Object) * $Threads)}
                         )
-                        HashRates            = [PSCustomObject]@{$Main_Algorithm_Norm = $Stats."$($Miner_Name)_$($Main_Algorithm_Norm)_HashRate".Week; $Secondary_Algorithm_Norm = $Stats."$($Miner_Name)_$($Secondary_Algorithm_Norm)_HashRate".Week}
+                        HashRates            = [PSCustomObject]@{$Main_Algorithm_Norm = $Stats."$($Miner_Name)_$($Main_Algorithm_Norm -replace '\-.*$')_HashRate".Week; $Secondary_Algorithm_Norm = $Stats."$($Miner_Name)_$($Secondary_Algorithm_Norm -replace '\-.*$')_HashRate".Week}
                         API                  = "Excavator"
                         Port                 = $Miner_Port
                         URI                  = $Uri
