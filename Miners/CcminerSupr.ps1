74e030d6082cf06650d7bc57761ef5a92ec387f3
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-08-23 16:08:53 +0200

    Update miner CcminerSupr to git9

--- Miners/CcminerSupr.ps1
+++ Miners/CcminerSupr.ps1
@@ -8,15 +8,16 @@ param(
 )
 
 $Path = ".\Bin\NVIDIA-CcminerSupr\ccminer.exe"
-$Uri = "https://github.com/sp-hash/suprminer/releases/download/spmod-git8/raven_spmodgit8.7z"
+$Uri = "https://github.com/sp-hash/suprminer/releases/download/spmod-git9/spmodgit9.7z"
 $Port = "116{0:d2}"
 
 $Devices = $Devices.NVIDIA
 if (-not $Devices -or $Config.InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "c11"; Params = "-N 1"} #c11
     [PSCustomObject]@{MainAlgorithm = "x16r"; Params = "-N 10"; ExtendInterval = 1; FaultTolerance = 0.5} #X16R RavenCoin
-    #[PSCustomObject]@{MainAlgorithm = "x16s"; Params = ""; FaultTolerance = 0.5} #X16S PigeonCoin
+    [PSCustomObject]@{MainAlgorithm = "x16s"; Params = ""; FaultTolerance = 0.5} #X16S PigeonCoin
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
