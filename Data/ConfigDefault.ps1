5f81f689a318c0cbac4baa8ee9f1e28406fed293
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-08-29 15:37:46 +0200

    Update default config
    - added new algorithms

--- Data/ConfigDefault.ps1
+++ Data/ConfigDefault.ps1
@@ -1,6 +1,6 @@
 ﻿[PSCustomObject]@{
     PoolName = @("Nicehash","AHashPool","Blazepool","MiningPoolHub","Zergpool")
-    Algorithm = @("allium","blake2s","c11","cryptonightlite","cryptonightheavy","cryptonightv7","equihash","equihash144","equihash192","ethash","hex","hmq1725","hodl","hsr","keccak","keccakc","lyra2re2","lyra2z","m7m","myrgr","neoscrypt","pascal","phi","phi2","poly","renesis","skein","skunk","sonoa","timetravel","tribus","x16r","x16s","x17","xevan","yescrypt","yescryptr16","yespower")
+    Algorithm = @("aergo","allium","balloon","bitcore","blake2s","c11","cryptonightlite","cryptonightheavy","cryptonightv7","equihash","equihash144","equihash192","ethash","hex","hmq1725","hodl","keccak","keccakc","lyra2re2","lyra2z","m7m","myrgr","neoscrypt","pascal","phi","phi2","poly","renesis","skein","skunk","sonoa","timetravel","tribus","x16r","x16s","x17","xevan","yescrypt","yescryptr16","yespower")
     FastestMinerOnly = $true 
     ShowPoolBalances = $true
     ShowMinerWindow = $false
