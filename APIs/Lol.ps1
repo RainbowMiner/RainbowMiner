a4bd630ad11015724e8c1816e2b60981e6287716
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-08-30 02:48:58 +0200

    Add miner lolMiner v0.43
    - NVIDIA and AMD
    - Equihash 96,5 / 144,5 / 210,9

---
+++ APIs/Lol.ps1
@@ -0,0 +1,47 @@
+﻿using module ..\Include.psm1
+
+class Lol : Miner {
+    [String[]]UpdateMinerData () {
+        if ($this.GetStatus() -ne [MinerStatus]::Running) {return @()}
+
+        $Server = "localhost"
+        $Timeout = 10 #seconds
+
+        $Response = ""
+
+        $HashRate = [PSCustomObject]@{}
+
+        try {
+            $Response = Invoke-TcpRead $Server 31702 $Timeout -ErrorAction Stop
+            $Data = $Response | ConvertFrom-Json -ErrorAction Stop
+        }
+        catch {
+            Write-Log -Level Error "Failed to connect to miner ($($this.Name)). "
+            return @("", $Response)
+        }
+
+        $HashRate_Name = [String]$this.Algorithm[0]
+        try {
+            $HashRate_Value = [Double]$Data.'TotalSpeed(60s)'
+            if (-not $HashRate_Value) {throw}
+        } catch {
+            $HashRate_Value = [double]$Data.'TotalSpeed(5s)'
+        }
+        
+        $HashRate_Value = [Int64]$HashRate_Value
+        if ($HashRate_Name -and $HashRate_Value -gt 0) {
+            $HashRate | Add-Member @{$HashRate_Name = $HashRate_Value}
+            $this.Data += [PSCustomObject]@{
+                Date     = (Get-Date).ToUniversalTime()
+                Raw      = $Response
+                HashRate = $HashRate
+                PowerDraw = Get-DevicePowerDraw -DeviceName $this.DeviceName
+                Device   = @()
+            }
+        }
+
+        $this.Data = @($this.Data | Select-Object -Last 10000)
+
+        return @("", $Data | ConvertTo-Json -Compress)
+    }
+}
