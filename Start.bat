b36e243591dd4cb5e9ac4e34a291b0d498a2ebc7
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-08-31 04:03:23 +0200

    Start back to old format
    - otherwise U will fail for this release

--- Start.bat
+++ Start.bat
@@ -12,6 +12,8 @@
 
 @echo off
 
+start pwsh -noexit -executionpolicy bypass -command "& .\reader.ps1 -log '^(.+)?-\d+_\d\d\d\d-\d\d-\d\d_\d\d-\d\d-\d\d.txt' -sort '^[^_]*_' -quickstart"
+
 :restart
 where pwsh.exe >nul 2>nul
 if %errorlevel%==1 (
