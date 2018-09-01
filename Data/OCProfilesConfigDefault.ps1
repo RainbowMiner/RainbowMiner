1302d7cc3de8f01c439e8b3372285b9e2496489b
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-09-01 16:18:48 +0200

    Update ocprofiles default
    - add LockVoltagePoint

--- Data/OCProfilesConfigDefault.ps1
+++ Data/OCProfilesConfigDefault.ps1
@@ -4,53 +4,62 @@
         ThermalLimit = 0
         MemoryClockBoost = "*"
         CoreClockBoost = "*"
+        LockVoltagePoint = "*"
     }
     'Profile2' = [PSCustomObject]@{
         PowerLimit = 0
         ThermalLimit = 0
         MemoryClockBoost = "*"
         CoreClockBoost = "*"
+        LockVoltagePoint = "*"
     }
     'Profile3' = [PSCustomObject]@{
         PowerLimit = 0
         ThermalLimit = 0
         MemoryClockBoost = "*"
         CoreClockBoost = "*"
+        LockVoltagePoint = "*"
     }
     'Profile4' = [PSCustomObject]@{
         PowerLimit = 0
         ThermalLimit = 0
         MemoryClockBoost = "*"
         CoreClockBoost = "*"
+        LockVoltagePoint = "*"
     }
     'Profile5' = [PSCustomObject]@{
         PowerLimit = 0
         ThermalLimit = 0
         MemoryClockBoost = "*"
         CoreClockBoost = "*"
+        LockVoltagePoint = "*"
     }
     'Profile6' = [PSCustomObject]@{
         PowerLimit = 0
         ThermalLimit = 0
         MemoryClockBoost = "*"
         CoreClockBoost = "*"
+        LockVoltagePoint = "*"
     }
     'Profile7' = [PSCustomObject]@{
         PowerLimit = 0
         ThermalLimit = 0
         MemoryClockBoost = "*"
         CoreClockBoost = "*"
+        LockVoltagePoint = "*"
     }
     'Profile8' = [PSCustomObject]@{
         PowerLimit = 0
         ThermalLimit = 0
         MemoryClockBoost = "*"
         CoreClockBoost = "*"
+        LockVoltagePoint = "*"
     }
     'Profile9' = [PSCustomObject]@{
         PowerLimit = 0
         ThermalLimit = 0
         MemoryClockBoost = "*"
         CoreClockBoost = "*"
+        LockVoltagePoint = "*"
     }
 }
