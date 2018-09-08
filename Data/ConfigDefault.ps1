cc2498a2186b22f63b1fba18ae295fbe4b6baf4c
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-09-08 20:12:39 +0200

    Update config defaults
    - starting exludeminer list for first setup

--- Data/ConfigDefault.ps1
+++ Data/ConfigDefault.ps1
@@ -1,6 +1,7 @@
 ﻿[PSCustomObject]@{
     PoolName = @("Nicehash","AHashPool","Blazepool","MiningPoolHub","Zergpool")
     Algorithm = @("aergo","allium","balloon","bitcore","blake2s","c11","cryptonightlite","cryptonighthaven","cryptonightheavy","cryptonightv7","equihash","equihash144","equihash192","ethash","hex","hmq1725","hodl","keccak","keccakc","lyra2re2","lyra2z","m7m","myrgr","neoscrypt","pascal","phi","phi2","poly","renesis","skein","skunk","sonoa","timetravel","tribus","x16r","x16s","x17","xevan","yescrypt","yescryptr16","yespower")
+    ExcludeMinerName = @("ClaymoreEquihashAmd","lolMiner")
     FastestMinerOnly = $true
     RemoteAPI = $false 
     ShowPoolBalances = $true
