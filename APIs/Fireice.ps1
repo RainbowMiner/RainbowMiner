b4c7d6589aa9586799444ebd1bf4ea311d7cb3ed
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-09-08 14:14:56 +0200

    Update miner FireIce
    - check min. GPU memory
    - miner gets it's own API

---
+++ APIs/Fireice.ps1
@@ -0,0 +1,54 @@
+﻿using module ..\Include.psm1
+
+class Fireice : Miner {
+
+    [String]GetArguments() {
+        $Parameters = $this.Arguments | ConvertFrom-Json        
+        $ConfigFile = "config_$($this.Name)-$($this.Algorithm -join '-')$(if ($Parameters.Config.pool_list[0].use_tls){"-ssl"}).txt"
+        ($Parameters.Config | ConvertTo-Json -Depth 10) -replace "^{" -replace "}$" | Set-Content "$(Split-Path $this.Path)\$ConfigFile" -ErrorAction Ignore -Encoding UTF8 -Force
+
+        return "-C $ConfigFile $($Parameters.Params)".Trim()
+    }
+
+    [String[]]UpdateMinerData () {
+        if ($this.GetStatus() -ne [MinerStatus]::Running) {return @()}
+
+        $Server = "localhost"
+        $Timeout = 10 #seconds
+
+        $Request = ""
+        $Response = ""
+
+        $HashRate = [PSCustomObject]@{}
+
+        try {
+            $Response = Invoke-WebRequest "http://$($Server):$($this.Port)/api.json" -UseBasicParsing -TimeoutSec $Timeout -ErrorAction Stop
+            $Data = $Response | ConvertFrom-Json -ErrorAction Stop
+        }
+        catch {
+            Write-Log -Level Error "Failed to connect to miner ($($this.Name)). "
+            return @($Request, $Response)
+        }
+
+        $HashRate_Name = [String]($this.Algorithm -like (Get-Algorithm $Data.algo))
+        if (-not $HashRate_Name) {$HashRate_Name = [String]($this.Algorithm -like "$(Get-Algorithm $Data.algo)*")} #temp fix
+        if (-not $HashRate_Name) {$HashRate_Name = [String]$this.Algorithm[0]} #fireice fix
+        $HashRate_Value = [Double]$Data.hashrate.total[0]
+        if (-not $HashRate_Value) {$HashRate_Value = [Double]$Data.hashrate.total[1]} #fix
+        if (-not $HashRate_Value) {$HashRate_Value = [Double]$Data.hashrate.total[2]} #fix
+
+        $HashRate | Where-Object {$HashRate_Name} | Add-Member @{$HashRate_Name = [Int64]$HashRate_Value}
+
+        $this.Data += [PSCustomObject]@{
+            Date     = (Get-Date).ToUniversalTime()
+            Raw      = $Response
+            HashRate = $HashRate
+            PowerDraw = Get-DevicePowerDraw -DeviceName $this.DeviceName
+            Device   = @()
+        }
+
+        $this.Data = @($this.Data | Select-Object -Last 10000)
+
+        return @($Request, $Data | ConvertTo-Json -Compress)
+    }
+}
--- Miners/Fireice.ps1
+++ Miners/Fireice.ps1
@@ -10,19 +10,19 @@ param(
 $Path = ".\Bin\CryptoNight-FireIce\xmr-stak.exe"
 $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.4.7-fireice/xmr-stak-win64.zip"
 $Port = "309{0:d2}"
-$DevFee = 0.0
+$DevFee = 2.0
 
 $Commands = [PSCustomObject[]]@(
-    #[PSCustomObject]@{MainAlgorithm = "cryptonight"; Params = ""} #CryptoNight
-    [PSCustomObject]@{MainAlgorithm = "cryptonight_bittube2"; Params = ""} # CryptoNightBittube2
-    [PSCustomObject]@{MainAlgorithm = "cryptonight_haven"; Params = ""} # CryptoNightHaven
-    [PSCustomObject]@{MainAlgorithm = "cryptonight_heavy"; Params = ""} # CryptoNightHeavy
-    [PSCustomObject]@{MainAlgorithm = "cryptonight_lite"; Params = ""} # CryptoNightLite
-    [PSCustomObject]@{MainAlgorithm = "cryptonight_lite_v7"; Params = ""} # CryptoNightLiteV7
-    [PSCustomObject]@{MainAlgorithm = "cryptonight_lite_v7_xor"; Params = ""} # CryptoNightLiteV7Xor
-    [PSCustomObject]@{MainAlgorithm = "cryptonight_masari"; Params = ""} # CryptoNightMasari
-    [PSCustomObject]@{MainAlgorithm = "cryptonight_v7"; Params = ""} #CryptoNightV7
-    [PSCustomObject]@{MainAlgorithm = "cryptonight_v7_stellite"; Params = ""} #CryptoNightV7Stellite
+    #[PSCustomObject]@{MainAlgorithm = "cryptonight";            Threads = 1; MinMemGb = 2; Params = ""} #CryptoNight
+    [PSCustomObject]@{MainAlgorithm = "cryptonight_bittube2";    Threads = 1; MinMemGb = 4; Params = ""} # CryptoNightBittube2
+    [PSCustomObject]@{MainAlgorithm = "cryptonight_haven";       Threads = 1; MinMemGb = 4; Params = ""} # CryptoNightHaven
+    [PSCustomObject]@{MainAlgorithm = "cryptonight_heavy";       Threads = 1; MinMemGb = 4; Params = ""} # CryptoNightHeavy
+    [PSCustomObject]@{MainAlgorithm = "cryptonight_lite";        Threads = 1; MinMemGb = 1; Params = ""} # CryptoNightLite
+    [PSCustomObject]@{MainAlgorithm = "cryptonight_lite_v7";     Threads = 1; MinMemGb = 1; Params = ""} # CryptoNightLiteV7
+    [PSCustomObject]@{MainAlgorithm = "cryptonight_lite_v7_xor"; Threads = 1; MinMemGb = 1; Params = ""} # CryptoNightLiteV7Xor
+    [PSCustomObject]@{MainAlgorithm = "cryptonight_masari";      Threads = 1; MinMemGb = 2; Params = ""} # CryptoNightMasari
+    [PSCustomObject]@{MainAlgorithm = "cryptonight_v7";          Threads = 1; MinMemGb = 2; Params = ""} #CryptoNightV7
+    [PSCustomObject]@{MainAlgorithm = "cryptonight_v7_stellite"; Threads = 1; MinMemGb = 2; Params = ""} #CryptoNightV7Stellite
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -55,11 +55,8 @@ if (-not $Devices.NVIDIA -and -not $Devices.AMD -and -not $Devices.CPU -and -not
 @($Devices.FullComboModels.PSObject.Properties.Name) | Foreach-Object {
     $Miner_Vendor = $_  
     @($Devices.$Miner_Vendor) | Where-Object {$_.Model -eq $Devices.FullComboModels.$Miner_Vendor} | Select-Object Vendor, Model -Unique | ForEach-Object {
-        $Miner_Device = $Devices.$Miner_Vendor | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
-        $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)        
+        $Device = $Devices.$Miner_Vendor | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
         $Miner_Model = $_.Model
-        $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-        $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
             
         switch($Miner_Vendor) {
             "NVIDIA" {$Miner_Deviceparams = "--noUAC --noAMD --noCPU"}
@@ -68,56 +65,66 @@ if (-not $Devices.NVIDIA -and -not $Devices.AMD -and -not $Devices.CPU -and -not
         }
 
         $Commands | ForEach-Object {
-
-            $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+            $Algorithm = $_.MainAlgorithm
+            $Algorithm_Norm = Get-Algorithm $Algorithm
+            $MinMemGb = $_.MinMemGb
+            $Params = $_.Params
+        
+            $Miner_Device = @($Device | Where-Object {$_.OpenCL.GlobalMemsize -ge ($MinMemGb * 1gb)})
 
             if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
-                $Miner_ConfigFileName = "$($Pools.$Algorithm_Norm.Name)_$($Algorithm_Norm)_$($Pools.$Algorithm_Norm.User)_$(if ($Pools.$Algorithm_Norm.SSL){"ssl_"})$($Miner_Port).txt"
+                $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)            
+                $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+                $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
 
-                ([PSCustomObject]@{
-                        pool_list       = @([PSCustomObject]@{
-                                pool_address    = "$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port)"
-                                wallet_address  = "$($Pools.$Algorithm_Norm.User)"
-                                pool_password   = "$($Pools.$Algorithm_Norm.Pass)"
-                                use_nicehash    = $true
-                                use_tls         = $Pools.$Algorithm_Norm.SSL
-                                tls_fingerprint = ""
-                                pool_weight     = 1
-                                rig_id = ""
-                            }
-                        )
-                        currency        = if ($Pools.$Algorithm_Norm.Info) {"$($Pools.$Algorithm_Norm.Info -replace '^monero$', 'monero7' -replace '^aeon$', 'aeon7')"} else {"$($_.MainAlgorithm)"}
-                        call_timeout    = 10
-                        retry_time      = 10
-                        giveup_limit    = 0
-                        verbose_level   = 3
-                        print_motd      = $true
-                        h_print_time    = 60
-                        aes_override    = $null
-                        use_slow_memory = "warn"
-                        tls_secure_algo = $true
-                        daemon_mode     = $false
-                        flush_stdout    = $false
-                        output_file     = ""
-                        httpd_port      = $Miner_Port
-                        http_login      = ""
-                        http_pass       = ""
-                        prefer_ipv4     = $true
-                    } | ConvertTo-Json -Depth 10
-                ) -replace "^{" -replace "}$" | Set-Content "$(Split-Path $Path)\$($Miner_ConfigFileName)" -Force -ErrorAction SilentlyContinue
+                if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+                    $Arguments = [PSCustomObject]@{
+                        Params = "-i $($Miner_Port) $($Miner_Deviceparams) $($_.Params)".Trim()
+                        Config = [PSCustomObject]@{
+                            pool_list       = @([PSCustomObject]@{
+                                    pool_address    = "$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port)"
+                                    wallet_address  = "$($Pools.$Algorithm_Norm.User)"
+                                    pool_password   = "$($Pools.$Algorithm_Norm.Pass)"
+                                    use_nicehash    = $true
+                                    use_tls         = $Pools.$Algorithm_Norm.SSL
+                                    tls_fingerprint = ""
+                                    pool_weight     = 1
+                                    rig_id = "$($Config.Pools."$($Pools.$Algorithm_Norm.Name)".Worker)"
+                                }
+                            )
+                            currency        = if ($Pools.$Algorithm_Norm.Info) {"$($Pools.$Algorithm_Norm.Info -replace '^monero$', 'monero7' -replace '^aeon$', 'aeon7')"} else {$Algorithm}
+                            call_timeout    = 10
+                            retry_time      = 10
+                            giveup_limit    = 0
+                            verbose_level   = 3
+                            print_motd      = $true
+                            h_print_time    = 60
+                            aes_override    = $null
+                            use_slow_memory = "warn"
+                            tls_secure_algo = $true
+                            daemon_mode     = $false
+                            flush_stdout    = $false
+                            output_file     = ""
+                            httpd_port      = $Miner_Port
+                            http_login      = ""
+                            http_pass       = ""
+                            prefer_ipv4     = $true
+                        }
+                    }
 
-                [PSCustomObject]@{
-                    Name      = $Miner_Name
-                    DeviceName= $Miner_Device.Name
-                    DeviceModel=$Miner_Model
-                    Path      = $Path
-                    Arguments = "-C $($Miner_ConfigFileName) $($Miner_Deviceparams) -i $($Miner_Port) $($_.Params)"
-                    HashRates = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
-                    API       = "XMRig"
-                    Port      = $Miner_Port
-                    Uri       = $Uri
-                    DevFee    = $DevFee
-                    ManualUri = $ManualUri
+                    [PSCustomObject]@{
+                        Name      = $Miner_Name
+                        DeviceName= $Miner_Device.Name
+                        DeviceModel=$Miner_Model
+                        Path      = $Path
+                        Arguments = $Arguments
+                        HashRates = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                        API       = "Fireice"
+                        Port      = $Miner_Port
+                        Uri       = $Uri
+                        DevFee    = $DevFee
+                        ManualUri = $ManualUri
+                    }
                 }
             }
         }
