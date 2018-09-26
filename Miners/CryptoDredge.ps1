13c05623352973db54149a4cf406deaf4efadc72
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-09-26 17:33:06 +0200

    Update miner CryptoDredge
    - fix memory / devices

--- Miners/CryptoDredge.ps1
+++ Miners/CryptoDredge.ps1
@@ -61,9 +61,7 @@ $Devices = $Devices.NVIDIA
 
 $Devices | Select-Object Vendor, Model -Unique | ForEach-Object {
     $Device = $Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
-    $Miner_Model = $_.Model
-
-    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
+    $Miner_Model = $_.Model    
 
     $Commands | ForEach-Object {
         $MinMemGb = $_.MinMemGb
@@ -76,6 +74,8 @@ $Devices | Select-Object Vendor, Model -Unique | ForEach-Object {
             $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
             $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
             $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+            $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
 
             [PSCustomObject]@{
                 Name = $Miner_Name
