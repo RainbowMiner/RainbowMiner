9b1bb7c66d5022fdc9d6ac09981ea6100317427e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-09-26 17:29:51 +0200

    Update miner CryptoDredge to v0.9.2
    - New CryptoNightSaber (BitTube) algorithm
    - Improve Allium and Phi2 for 1070, 1070 Ti, 1080, 1080 Ti
    - Allow to set intensity per GPU
    - Fix failover mechanism
    - Disable miner to auto-start after system restart
    - Add crash reporting mechanism. Check Readme for more information
    
    Attention! This miner needs CUDA 10.0 - please use uptodate Nvidia drivers

--- Miners/CryptoDredge.ps1
+++ Miners/CryptoDredge.ps1
@@ -8,33 +8,34 @@ param(
 )
 
 $Path = ".\Bin\NVIDIA-CryptoDredge\CryptoDredge.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.9.1-cryptodredge/CryptoDredge_0.9.1_cuda_9.2_windows.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.9.2-cryptodredge/CryptoDredge_0.9.2_cuda_10.0_windows.zip"
 $ManualUri = "https://bitcointalk.org/index.php?topic=4807821"
 $Port = "313{0:d2}"
 $DevFee = 1.0
-$Cuda = "9.2"
+$Cuda = "10.0"
 
 if (-not $Devices.NVIDIA -and -not $Config.InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{MainAlgorithm = "aeon"; Params = ""} #Cryptolightv7 / Aeon
-    [PSCustomObject]@{MainAlgorithm = "allium"; Params = ""} #Allium
-    #[PSCustomObject]@{MainAlgorithm = "blake2s"; Params = ""} #Blake2s, ASIC domain. no longer profitable
-    [PSCustomObject]@{MainAlgorithm = "cnfast"; Params = ""} #CryptonightFast / Masari
-    [PSCustomObject]@{MainAlgorithm = "cnhaven"; Params = ""} #Cryptonighthaven
-    [PSCustomObject]@{MainAlgorithm = "cnheavy"; Params = ""} #Cryptonightheavy
-    [PSCustomObject]@{MainAlgorithm = "cnv7"; Params = ""; ExtendInterval = 2} #CryptonightV7 / Monero
-    [PSCustomObject]@{MainAlgorithm = "lbk3"; Params = ""} #LBK3
-    [PSCustomObject]@{MainAlgorithm = "lyra2v2"; Params = ""} #Lyra2Re2
-    [PSCustomObject]@{MainAlgorithm = "lyra2z"; Params = ""} #Lyra2z
-    #[PSCustomObject]@{MainAlgorithm = "masari"; Params = ""} #Cryptonightfast / Masari
-    [PSCustomObject]@{MainAlgorithm = "neoscrypt"; Params = ""} #Neoscrypt
-    [PSCustomObject]@{MainAlgorithm = "phi"; Params = ""} #PHI
-    [PSCustomObject]@{MainAlgorithm = "phi2"; Params = ""} #PHI2
-    #[PSCustomObject]@{MainAlgorithm = "skein"; Params = ""} #Skein
-    #[PSCustomObject]@{MainAlgorithm = "skunk"; Params = ""} #Skunk
-    [PSCustomObject]@{MainAlgorithm = "stellite"; Params = ""} #Stellite
-    [PSCustomObject]@{MainAlgorithm = "tribus"; Params = ""; ExtendInterval = 2} #Tribus
+    [PSCustomObject]@{MainAlgorithm = "aeon";      MinMemGb = 1; Params = ""} #Cryptolightv7 / Aeon
+    [PSCustomObject]@{MainAlgorithm = "allium";    MinMemGb = 1; Params = ""} #Allium
+    #[PSCustomObject]@{MainAlgorithm = "blake2s";   MinMemGb = 1; Params = ""} #Blake2s, ASIC domain. no longer profitable
+    [PSCustomObject]@{MainAlgorithm = "cnfast";    MinMemGb = 2; Params = ""} #CryptonightFast / Masari
+    [PSCustomObject]@{MainAlgorithm = "cnhaven";   MinMemGb = 4; Params = ""} #Cryptonighthaven
+    [PSCustomObject]@{MainAlgorithm = "cnheavy";   MinMemGb = 4; Params = ""} #Cryptonightheavy
+    [PSCustomObject]@{MainAlgorithm = "cnsaber";   MinMemGb = 4; Params = ""} #Cryptonightheavytube
+    [PSCustomObject]@{MainAlgorithm = "cnv7";      MinMemGb = 2; Params = ""; ExtendInterval = 2} #CryptonightV7 / Monero
+    [PSCustomObject]@{MainAlgorithm = "lbk3";      MinMemGb = 1; Params = ""} #LBK3
+    [PSCustomObject]@{MainAlgorithm = "lyra2v2";   MinMemGb = 1; Params = ""} #Lyra2Re2
+    [PSCustomObject]@{MainAlgorithm = "lyra2z";    MinMemGb = 1; Params = ""} #Lyra2z
+    #[PSCustomObject]@{MainAlgorithm = "masari";    MinMemGb = 2; Params = ""} #Cryptonightfast / Masari
+    [PSCustomObject]@{MainAlgorithm = "neoscrypt"; MinMemGb = 1; Params = ""} #Neoscrypt
+    [PSCustomObject]@{MainAlgorithm = "phi";       MinMemGb = 1; Params = ""} #PHI
+    [PSCustomObject]@{MainAlgorithm = "phi2";      MinMemGb = 1; Params = ""} #PHI2
+    #[PSCustomObject]@{MainAlgorithm = "skein";     MinMemGb = 1; Params = ""} #Skein
+    #[PSCustomObject]@{MainAlgorithm = "skunk";     MinMemGb = 1; Params = ""} #Skunk
+    [PSCustomObject]@{MainAlgorithm = "stellite";  MinMemGb = 1; Params = ""} #Stellite
+    [PSCustomObject]@{MainAlgorithm = "tribus";    MinMemGb = 1; Params = ""; ExtendInterval = 2} #Tribus
 )
 
 
@@ -59,19 +60,23 @@ if (-not (Confirm-Cuda -ActualVersion $Config.CUDAVersion -RequiredVersion $Cuda
 $Devices = $Devices.NVIDIA
 
 $Devices | Select-Object Vendor, Model -Unique | ForEach-Object {
-    $Miner_Device = $Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
-    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Device = $Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
     $Miner_Model = $_.Model
-    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
 
     $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
 
     $Commands | ForEach-Object {
+        $MinMemGb = $_.MinMemGb
+        
+        $Miner_Device = @($Device | Where-Object {$_.OpenCL.GlobalMemsize -ge ($MinMemGb * 1gb)})
 
         $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
         
         if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+            $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+            $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+            $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
             [PSCustomObject]@{
                 Name = $Miner_Name
                 DeviceName = $Miner_Device.Name
