a95321669a8c2018683ce41a0557f9d7dc1cbcb8
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-09-30 00:08:32 +0200

    Update default config
    - add nlpool

--- Data/ConfigDefault.ps1
+++ Data/ConfigDefault.ps1
@@ -1,5 +1,5 @@
 ﻿[PSCustomObject]@{
-    PoolName = @("Nicehash","AHashPool","Blazepool","MiningPoolHub","Zergpool")
+    PoolName = @("Nicehash","Blazepool","MiningPoolHub","NLpool","Zergpool")
     Algorithm = @("aergo","allium","balloon","bitcore","blake2s","c11","cryptonightlite","cryptonighthaven","cryptonightheavy","cryptonightv7","equihash","equihash144","equihash192","ethash","hex","hmq1725","hodl","keccak","keccakc","lyra2re2","lyra2z","m7m","myrgr","neoscrypt","pascal","phi","phi2","poly","renesis","skein","skunk","sonoa","timetravel","tribus","x16r","x16s","x17","xevan","yescrypt","yescryptr16","yespower")
     ExcludeMinerName = @("ClaymoreEquihashAmd")
     FastestMinerOnly = $true
