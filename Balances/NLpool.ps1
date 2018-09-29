964e394692416013f3cfb332b2adcd654d765300
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-09-29 22:47:11 +0200

    Add pool NLpool
    - balances
    - pool api

---
+++ Balances/NLpool.ps1
@@ -0,0 +1,39 @@
+﻿param(
+    $Config
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Request = [PSCustomObject]@{}
+
+$Payout_Currencies = @($Config.Pools.$Name.Wallets.PSObject.Properties.Name | Select-Object Name,Value -Unique | Sort-Object Name,Value)
+
+if (-not $Payout_Currencies) {
+    Write-Log -Level Verbose "Cannot get balance on pool ($Name) - no wallet address specified. "
+    return
+}
+
+$Payout_Currencies | Foreach-Object {
+    try {
+        $Request = Invoke-RestMethod "http://nlpool.nl/api/walletEx?address=$($_.Value)" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop
+        if (($Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Measure-Object Name).Count -le 1) {
+            Write-Log -Level Warn "Pool Balance API ($Name) for $($_.Name) returned nothing. "
+        } else {
+            [PSCustomObject]@{
+                Caption     = "$($Name) ($($Request.currency))"
+                Currency    = $Request.currency
+                Balance     = $Request.balance
+                Pending     = $Request.unsold
+                Total       = $Request.unpaid
+                Payed       = $Request.total - $Request.unpaid
+                Earned      = $Request.total
+                Payouts     = @($Request.payouts | Select-Object)
+                LastUpdated = (Get-Date).ToUniversalTime()
+            }
+        }
+    }
+    catch {
+        if ($Error.Count){$Error.RemoveAt(0)}
+        Write-Log -Level Warn "Pool Balance API ($Name) for $($_.Name) has failed. "
+    }
+}
---
+++ Pools/NLpool.ps1
@@ -0,0 +1,100 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Wallets,
+    [alias("WorkerName")]
+    [String]$Worker, 
+    [TimeSpan]$StatSpan,
+    [String]$DataWindow = "estimate_current",
+    [Bool]$InfoOnly = $false
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Pool_Request = [PSCustomObject]@{}
+$PoolCoins_Request = [PSCustomObject]@{}
+
+try {
+    $Pool_Request = Invoke-RestMethodAsync "http://www.nlpool.nl/api/status" -tag $Name
+}
+catch {
+    if ($Error.Count){$Error.RemoveAt(0)}
+    Write-Log -Level Warn "Pool API ($Name) has failed. "
+    return
+}
+
+if (($Pool_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Measure-Object Name).Count -le 1) {
+    Write-Log -Level Warn "Pool API ($Name) returned nothing. "
+    return
+}
+
+try {
+    $PoolCoins_Request = Invoke-RestMethodAsync "http://www.nlpool.nl/api/currencies" -tag $Name
+}
+catch {
+    if ($Error.Count){$Error.RemoveAt(0)}
+    Write-Log -Level Warn "Pool currencies API ($Name) has failed. "
+}
+
+[hashtable]$Pool_Algorithms = @{}
+[hashtable]$Pool_Coins = @{}
+[hashtable]$Pool_RegionsTable = @{}
+
+$Pool_Regions = @("us")
+$Pool_Regions | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
+$Pool_Currencies = @("BTC") + @($Wallets.PSObject.Properties.Name | Select-Object) + @($PoolCoins_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name) | Select-Object -Unique | Where-Object {$Wallets.$_ -or $InfoOnly}
+if ($PoolCoins_Request) {
+    $PoolCoins_Algorithms = @($Pool_Request.PSObject.Properties.Value | Where-Object coins -eq 1 | Select-Object -ExpandProperty name -Unique)
+    if ($PoolCoins_Algorithms.Count) {foreach($p in $PoolCoins_Request.PSObject.Properties.Name) {if ($PoolCoins_Algorithms -contains $PoolCoins_Request.$p.algo) {$Pool_Coins[$PoolCoins_Request.$p.algo] = [hashtable]@{Name = $PoolCoins_Request.$p.name; Symbol = $p -replace '-.+$'}}}}
+}
+
+$Pool_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | Where-Object {$Pool_Request.$_.hashrate -gt 0} |ForEach-Object {
+    $Pool_Host = "mine.zpool.ca"
+    $Pool_Port = $Pool_Request.$_.port
+    $Pool_Algorithm = $Pool_Request.$_.name
+    if (-not $Pool_Algorithms.ContainsKey($Pool_Algorithm)) {$Pool_Algorithms.$Pool_Algorithm = Get-Algorithm $Pool_Algorithm}
+    $Pool_Algorithm_Norm = $Pool_Algorithms.$Pool_Algorithm
+    $Pool_Coin = $Pool_Coins.$Pool_Algorithm.Name
+    $Pool_Symbol = $Pool_Coins.$Pool_Algorithm.Symbol
+    $Pool_PoolFee = [Double]$Pool_Request.$_.fees
+    if ($Pool_Coin -and -not $Pool_Symbol) {$Pool_Symbol = Get-CoinSymbol $Pool_Coin}
+
+    if ($Pool_Symbol -and $Pool_Algorithm_Norm -ne "Equihash" -and $Pool_Algorithm_Norm -like "Equihash*") {$Pool_Algorithm_All = @($Pool_Algorithm_Norm,"$Pool_Algorithm_Norm-$Pool_Symbol")} else {$Pool_Algorithm_All = @($Pool_Algorithm_Norm)}
+
+    $Pool_Factor = [double]$Pool_Request.$_.mbtc_mh_factor
+    if ($Pool_Factor -le 0) {
+        Write-Log -Level Info "$($Name): Unable to determine divisor for algorithm $Pool_Algorithm. "
+        return
+    }
+    
+    if (-not $InfoOnly) {
+        if (-not (Test-Path "Stats\Pools\$($Name)_$($Pool_Algorithm_Norm)_Profit.txt")) {$Stat = Set-Stat -Name "$($Name)_$($Pool_Algorithm_Norm)_Profit" -Value (Get-YiiMPValue $Pool_Request.$_ -DataWindow "estimate_last24h" -Factor $Pool_Factor) -Duration (New-TimeSpan -Days 1)}
+        else {$Stat = Set-Stat -Name "$($Name)_$($Pool_Algorithm_Norm)_Profit" -Value (Get-YiiMPValue $Pool_Request.$_ -DataWindow $DataWindow -Factor $Pool_Factor) -Duration $StatSpan -ChangeDetection $true}
+    }
+
+    foreach($Pool_Region in $Pool_Regions) {
+        foreach($Pool_Currency in $Pool_Currencies) {
+            foreach($Pool_Algorithm_Norm in $Pool_Algorithm_All) {
+                [PSCustomObject]@{
+                    Algorithm     = $Pool_Algorithm_Norm
+                    CoinName      = $Pool_Coin
+                    CoinSymbol    = $Pool_Symbol
+                    Currency      = $Pool_Currency
+                    Price         = $Stat.Hour #instead of .Live
+                    StablePrice   = $Stat.Week
+                    MarginOfError = $Stat.Week_Fluctuation
+                    Protocol      = "stratum+tcp"
+                    Host          = "$Pool_Algorithm.$Pool_Host"
+                    Port          = $Pool_Port
+                    User          = $Wallets.$Pool_Currency
+                    Pass          = "$Worker,c=$Pool_Currency"
+                    Region        = $Pool_RegionsTable.$Pool_Region
+                    SSL           = $false
+                    Updated       = $Stat.Updated
+                    PoolFee       = $Pool_PoolFee
+                    DataWindow    = $DataWindow
+                }
+            }
+        }
+    }
+}
