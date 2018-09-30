0111656d7bd6e659a59d61a48020d6fd5f8b352d
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-09-30 21:09:19 +0200

    Update defaults

--- Data/ConfigDefault.ps1
+++ Data/ConfigDefault.ps1
@@ -2,6 +2,7 @@
     PoolName = @("Nicehash","Blazepool","MiningPoolHub","NLpool","Zergpool")
     Algorithm = @("aergo","allium","balloon","bitcore","blake2s","c11","cryptonightlite","cryptonighthaven","cryptonightheavy","cryptonightv7","equihash","equihash144","equihash192","ethash","hex","hmq1725","hodl","keccak","keccakc","lyra2re2","lyra2z","m7m","myrgr","neoscrypt","pascal","phi","phi2","poly","renesis","skein","skunk","sonoa","timetravel","tribus","x16r","x16s","x17","xevan","yescrypt","yescryptr16","yespower")
     ExcludeMinerName = @("ClaymoreEquihashAmd")
+    MinerStatusURL = "https://rbminer.net"
     FastestMinerOnly = $true
     RemoteAPI = $false 
     ShowPoolBalances = $true
@@ -13,6 +14,7 @@
     EnableOCProfiles = $false
     EnableOCVoltage = $false
     EnableAutoUpdate = $false
+    EnableMinerStatus = $true
     CPUMiningThreads = $Global:GlobalCPUInfo.Cores
     CPUMiningAffinity = Get-CPUAffinity $Global:GlobalCPUInfo.RealCores.Count -Hex
     Delay = 1
