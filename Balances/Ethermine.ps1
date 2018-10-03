0155daff824b60585893913e7c94c1ae47c582db
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-03 20:25:13 +0200

    Add balance for Ethermine

---
+++ Balances/Ethermine.ps1
@@ -0,0 +1,46 @@
+﻿param(
+    $Config
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Request = [PSCustomObject]@{}
+
+$Payout_Currencies = @($Config.Pools.$Name.Wallets.PSObject.Properties | Select-Object Name,Value -Unique | Sort-Object Name,Value)
+
+if (-not $Payout_Currencies) {
+    Write-Log -Level Verbose "Cannot get balance on pool ($Name) - no wallet address specified. "
+    return
+}
+
+$API_Hosts = @{
+    "ETH" = "https://api.ethermine.org"
+    "ETC" = "https://api-etc.ethermine.org"
+    "ZEC" = "https://api-zcash.flypool.org"
+}
+
+$Payout_Currencies | Foreach-Object {
+    try {
+        "$($API_Hosts."$($_.Name)")/miner/$($_.Value)/dashboard"
+        $Request = Invoke-RestMethod "$($API_Hosts."$($_.Name)")/miner/$($_.Value)/dashboard" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop
+        if ($Request.status -ne "OK") {
+            Write-Log -Level Warn "Pool Balance API ($Name) for $($_.Name) returned nothing. "            
+        } else {
+            [PSCustomObject]@{
+                Caption     = "$($Name) ($($_.Name))"
+                Currency    = $_.Name
+                Balance     = [double]$Request.data.currentStatistics.unpaid/1e18
+                Pending     = [double]$Request.data.currentStatistics.unconfirmed/1e18
+                Total       = [double]$Request.data.currentStatistics.unpaid/1e18 + [double]$Request.data.currentStatistics.unconfirmed/1e18
+                Payed       = 0
+                Earned      = 0
+                Payouts     = @()
+                LastUpdated = (Get-Date).ToUniversalTime()
+            }
+        }
+    }
+    catch {
+        if ($Error.Count){$Error.RemoveAt(0)}
+        Write-Log -Level Warn "Pool Balance API ($Name) for $($_.Name) has failed. "
+    }
+}
