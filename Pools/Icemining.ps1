107a9c17a8fbad182786a8e931affb5788a69ced
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-07 12:47:46 +0200

    Add pool Icemining
    - no autoexchange
    - good BCD performance

---
+++ Pools/Icemining.ps1
@@ -0,0 +1,94 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Wallets,
+    [alias("WorkerName")]
+    [String]$Worker, 
+    [TimeSpan]$StatSpan,
+    [String]$DataWindow = "estimate_current",
+    [Bool]$InfoOnly = $false
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Pool_Fee = 1
+
+$Pool_Request = [PSCustomObject]@{}
+$PoolCoins_Request = [PSCustomObject]@{}
+
+try {
+    $PoolCoins_Request = Invoke-RestMethodAsync "https://icemining.ca/api/currencies" -tag $Name
+}
+catch {
+    if ($Error.Count){$Error.RemoveAt(0)}
+    Write-Log -Level Warn "Pool API ($Name) has failed. "
+    return
+}
+
+if (($PoolCoins_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Measure-Object Name).Count -le 1) {
+    Write-Log -Level Warn "Pool API ($Name) returned nothing. "
+    return
+}
+
+try {
+    $Pool_Request = Invoke-RestMethodAsync "https://icemining.ca/api/status" -tag $Name
+}
+catch {
+    if ($Error.Count){$Error.RemoveAt(0)}
+    Write-Log -Level Warn "Pool status API ($Name) has failed. "
+}
+
+[hashtable]$Pool_Algorithms = @{}
+[hashtable]$Pool_RegionsTable = @{}
+
+$Pool_Regions = @("asia","eu","us")
+$Pool_Regions | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
+
+$PoolCoins_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | Where-Object {$PoolCoins_Request.$_.hashrate -gt 0 -or $InfoOnly} | ForEach-Object {
+    $Pool_CoinSymbol = $_
+
+    $Pool_Host = "icemining.ca"
+
+    $Pool_Port = $PoolCoins_Request.$Pool_CoinSymbol.port
+    $Pool_Algorithm = $PoolCoins_Request.$Pool_CoinSymbol.algo
+    if (-not $Pool_Algorithms.ContainsKey($Pool_Algorithm)) {$Pool_Algorithms.$Pool_Algorithm = Get-Algorithm $Pool_Algorithm}
+    $Pool_Algorithm_Norm = $Pool_Algorithms.$Pool_Algorithm
+    $Pool_Coin = $PoolCoins_Request.$Pool_CoinSymbol.name
+    $Pool_PoolFee = if ($Pool_Request.$Pool_Algorithm) {$Pool_Request.$Pool_Algorithm.fees} else {$Pool_Fee}
+    $Pool_Currency = if ($PoolCoins_Request.$Pool_CoinSymbol.symbol) {$PoolCoins_Request.$Pool_CoinSymbol.symbol} else {$Pool_CoinSymbol}
+    $Pool_User = $Wallets.$Pool_Currency
+
+    if ($Pool_Algorithm_Norm -ne "Equihash" -and $Pool_Algorithm_Norm -like "Equihash*") {$Pool_Algorithm_All = @($Pool_Algorithm_Norm,"$Pool_Algorithm_Norm-$Pool_Currency")} else {$Pool_Algorithm_All = @($Pool_Algorithm_Norm)}
+
+    $Divisor = 1e9
+
+    if (-not $InfoOnly) {
+        $Stat = Set-Stat -Name "$($Name)_$($Pool_CoinSymbol)_Profit" -Value ([Double]$PoolCoins_Request.$Pool_CoinSymbol.estimate / $Divisor) -Duration $StatSpan -ChangeDetection $false
+    }
+
+    foreach($Pool_Region in $Pool_Regions) {
+        $Pool_RegionHost = "$(if ($Pool_Region -eq "us"){"mine"}else{$Pool_Region}).$Pool_Host"
+        foreach($Pool_Algorithm_Norm in $Pool_Algorithm_All) {
+            if (($Pool_Region -ne "asia" -or $Pool_CoinSymbol -eq "BCD") -and ($Pool_User -or $InfoOnly)) {
+                [PSCustomObject]@{
+                    Algorithm     = $Pool_Algorithm_Norm
+                    CoinName      = $Pool_Coin
+                    CoinSymbol    = $Pool_CoinSymbol
+                    Currency      = $Pool_Currency
+                    Price         = $Stat.Hour #instead of .Live
+                    StablePrice   = $Stat.Week
+                    MarginOfError = $Stat.Week_Fluctuation
+                    Protocol      = "stratum+tcp"
+                    Host          = $Pool_RegionHost
+                    Port          = $Pool_Port
+                    User          = $Pool_User
+                    Pass          = "$Worker,c=$Pool_Currency"
+                    Region        = $Pool_RegionsTable.$Pool_Region
+                    SSL           = $false
+                    Updated       = $Stat.Updated
+                    PoolFee       = $Pool_PoolFee
+                }
+            }
+        }
+    }
+}
