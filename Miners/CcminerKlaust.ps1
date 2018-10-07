490bf8b6018381b4395be8b9f7ad7cdd250f84f4
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-07 13:29:02 +0200

    Update miner CcminerKlausT
    - add fallback to cuda9.2

--- Miners/CcminerKlaust.ps1
+++ Miners/CcminerKlaust.ps1
@@ -6,10 +6,19 @@ param(
 )
 
 $Path = ".\Bin\NVIDIA-KlausT\ccminer.exe"
-$Uri = "https://github.com/KlausT/ccminer/releases/download/8.23/ccminer-823-cuda10-x64.zip"
 $Port = "106{0:d2}"
 $DevFee = 0.0
-$Cuda = "10.0"
+
+$UriCuda = @(
+    [PSCustomObject]@{
+        Uri = "https://github.com/KlausT/ccminer/releases/download/8.23/ccminer-823-cuda10-x64.zip"
+        Cuda = "10.0"
+    },
+    [PSCustomObject]@{
+        Uri = "https://github.com/KlausT/ccminer/releases/download/8.23/ccminer-823-cuda92-x64.zip"
+        Cuda = "9.2"
+    }        
+)
 
 if (-not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
@@ -73,7 +82,14 @@ if ($InfoOnly) {
     return
 }
 
-if (-not (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name)) {return}
+$Uri = ""
+for($i=0;$i -le $UriCuda.Count -and -not $Uri;$i++) {
+    if (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $UriCuda[$i].Cuda -Warning $(if ($i -lt $UriCuda.Count-1) {""}else{$Name})) {
+        $Uri = $UriCuda[$i].Uri
+        $Cuda= $UriCuda[$i].Cuda
+    }
+}
+if (-not $Uri) {return}
 
 $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
     $Miner_Device = $Session.Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
