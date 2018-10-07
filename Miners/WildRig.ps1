f600801a8886bf214aefe8e206bf0c2567a977ff
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-07 18:36:21 +0200

    Add miner WildRig v0.12.0 beta AMD
    - bcd, c11, geek, hmq1725, phi, renesis, sonoa, timetravel, tribus, x16r, x16s, x17
    - untested. Please report, if issues arise.

---
+++ Miners/WildRig.ps1
@@ -0,0 +1,77 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+$Path = ".\Bin\AMD-WildRig\wildrig.exe"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.12.0b-wildrig/wildrig-multi-0.12.0-beta.7z"
+$ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
+$Port = "407{0:d2}"
+$DevFee = 1.0
+
+if (-not $Session.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No AMD present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "bcd"; Params = ""} #BCD
+    [PSCustomObject]@{MainAlgorithm = "c11"; Params = ""} #C11
+    [PSCustomObject]@{MainAlgorithm = "geek"; Params = ""} #Geek
+    [PSCustomObject]@{MainAlgorithm = "hmq1725"; Params = ""} #HMQ1725
+    [PSCustomObject]@{MainAlgorithm = "phi"; Params = ""} #PHI
+    [PSCustomObject]@{MainAlgorithm = "renesis"; Params = ""} #Renesis
+    [PSCustomObject]@{MainAlgorithm = "sonoa"; Params = ""} #Sonoa
+    [PSCustomObject]@{MainAlgorithm = "timetravel"; Params = ""} #Timetravel
+    [PSCustomObject]@{MainAlgorithm = "tribus"; Params = ""} #Tribus
+    [PSCustomObject]@{MainAlgorithm = "x16r"; Params = ""} #X16r
+    [PSCustomObject]@{MainAlgorithm = "x16s"; Params = ""} #X16s
+    [PSCustomObject]@{MainAlgorithm = "x17"; Params = ""} #X17
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("AMD")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Session.DevicesByTypes.AMD | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Session.Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
+    $Miner_PlatformId = $Miner_Device | Select -Unique -ExpandProperty PlatformId
+
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+
+        if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+            [PSCustomObject]@{
+                Name = $Miner_Name
+                DeviceName = $Miner_Device.Name
+                DeviceModel = $Miner_Model
+                Path = $Path
+                Arguments = "--api-port $($Miner_Port) --algo $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) -r 4 -R 10 --send-stale --donate-level 1 --opencl-devices $($DeviceIDsAll) --opencl-platform $($Miner_PlatformId) $($_.Params)"
+                HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                API = "XMRig"
+                Port = $Miner_Port
+                Uri = $Uri
+                DevFee = $DevFee
+                ManualUri = $ManualUri
+            }
+        }
+    }
+}
