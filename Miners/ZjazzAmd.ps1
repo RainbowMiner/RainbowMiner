3566332d5087d42d696d71b663265abc5f1ced6e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-11 10:40:13 +0200

    Add miner ZjazzAmd
    - support X22i / SUQA mining for AMD

---
+++ Miners/ZjazzAmd.ps1
@@ -0,0 +1,67 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+$Path = ".\Bin\AMD-Zjazz\zjazz_amd.exe"
+$Uri = "https://github.com/zjazz/zjazz_cuda_miner_experimental/releases/download/x22i_0993_amd/zjazz_amd_win64_0.993.zip"
+$Port = "408{0:d2}"
+$DevFee = 2.0
+
+if (-not $Session.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No NVIDIA present in system
+
+$Commands = [PSCustomObject[]]@(
+    #[PSCustomObject]@{MainAlgorithm = "phi2"; CoinSymbol = ""; Params = ""} #Phi2
+    [PSCustomObject]@{MainAlgorithm = "x22i"; CoinSymbol = ""; Params = ""} #X22i
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("AMD")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Session.DevicesByTypes.AMD | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Session.Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ' -d '
+
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+        
+        if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and ($_.CoinSymbol -eq "" -or $_.CoinSymbol -eq $Pools.$Algorithm_Norm.CoinSymbol)) {
+            [PSCustomObject]@{
+                Name = $Miner_Name
+                DeviceName = $Miner_Device.Name
+                DeviceModel = $Miner_Model
+                Path = $Path
+                Arguments = "-b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($_.Params)"
+                HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                API = "Ccminer"
+                Port = $Miner_Port
+                Uri = $Uri
+                FaultTolerance = $_.FaultTolerance
+                ExtendInterval = $_.ExtendInterval
+                DevFee = $DevFee
+                ManualUri = $ManualUri
+            }
+        }
+    }
+}
