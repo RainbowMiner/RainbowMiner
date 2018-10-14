088ae3d9e31fc13ff744bf46c13c397ab160113a
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-14 16:53:37 +0200

    Update Data/ConfigDefault.ps1

--- Data/ConfigDefault.ps1
+++ Data/ConfigDefault.ps1
@@ -1,5 +1,5 @@
 ﻿[PSCustomObject]@{
-    PoolName = @("Nicehash","Blazepool","MiningPoolHub","NLpool","Zergpool")
+    PoolName = @("Nicehash","Blazepool","MiningPoolHub","NLpool","Zpool")
     Algorithm = @("aergo","allium","balloon","bcd","bitcore","blake2s","c11","cryptonightlite","cryptonighthaven","cryptonightheavy","cryptonightv7","equihash","equihash144","equihash192","ethash","hex","hmq1725","hodl","keccak","keccakc","lyra2re2","lyra2z","m7m","myrgr","neoscrypt","pascal","phi","phi2","poly","renesis","skein","skunk","sonoa","timetravel","tribus","x16r","x16s","x17","xevan","yescrypt","yescryptr16","yespower")
     ExcludeMinerName = @("ClaymoreEquihashAmd")
     MinerStatusURL = "https://rbminer.net"
