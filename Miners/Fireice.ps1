5a152869920e9aeaa11f4370950b0898a2ea7b82
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-18 08:57:41 +0200

    Update miner FireIce to v2.5.1

--- Miners/Fireice.ps1
+++ Miners/Fireice.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\CryptoNight-FireIce250\xmr-stak.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.5.0-fireice/xmr-stak-2.5.0-nodevfee.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.5.1-fireice/xmr-stak-win64-2.5.1-nodevfee.7z"
 $Port = "309{0:d2}"
 $ManualUri = "https://github.com/RainbowMiner/xmr-stak/releases"
 $DevFee = 0.0
