9f2a4271d4be72a62d216bda4c55e35d106b57cb
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-20 11:31:38 +0200

    Update miner SrbMiner to v1.6.8-fix

--- Miners/SrbMiner.ps1
+++ Miners/SrbMiner.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\CryptoNight-SRBMiner\srbminer-cn.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.6.8-srbminer/SRBMiner-CN-V1-6-8.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.6.8-srbminer/SRBMiner-CN-V1-6-8-fix.7z"
 $ManualUri = "https://bitcointalk.org/index.php?topic=3167363.0"
 $Port = "315{0:d2}"
 $DevFee = 0.85
