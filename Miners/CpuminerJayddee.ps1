67b166c67dd41428419f14e72afb1fe614d114c4
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-25 13:56:36 +0200

    Add miner CpuminerJayddee
    - autodetect best exe
    - attention: this miner will be the new CpuminerJayddee and will replace the separate miners. All user using the Jayddee miner should move their indivial settings to this miner. The siblings will be removed in november.

---
+++ Miners/CpuminerJayddee.ps1
@@ -0,0 +1,148 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+$Path = ".\Bin\CPU-JayDDee\cpuminer-$($f=$Global:GlobalCPUInfo.Features;$(if($f.avx2 -and $f.sha){'avx2-sha'}elseif($f.avx2){'avx2'}elseif($f.avx){'avx'}elseif($f.aes -and $f.sse42){'aes-sse42'}else{'sse2'})).exe"
+$Uri = "https://github.com/JayDDee/cpuminer-opt/files/1996977/cpuminer-opt-3.8.8.1-windows.zip"
+$ManualUri = "https://github.com/JayDDee/cpuminer-opt/releases"
+$Port = "500{0:d2}"
+$DevFee = 0.0
+
+if (-not $Session.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No CPU present in system
+
+$Commands = [PSCustomObject[]]@(
+    ### CPU PROFITABLE ALGOS AS OF 06/03/2018
+    ### these algorithms are profitable algorithms on supported pools
+    [PSCustomObject]@{MainAlgorithm = "allium"; Params = ""} #Garlicoin
+    [PSCustomObject]@{MainAlgorithm = "cryptonightv7"; Params = ""} #CryptoNightV7XMR
+    [PSCustomObject]@{MainAlgorithm = "hmq1725"; Params = ""} #HMQ1725
+    [PSCustomObject]@{MainAlgorithm = "hodl"; Params = ""} #HODL
+    [PSCustomObject]@{MainAlgorithm = "lyra2z"; Params = ""} #Lyra2z, ZCoin
+    [PSCustomObject]@{MainAlgorithm = "m7m"; Params = ""} #m7m
+    [PSCustomObject]@{MainAlgorithm = "x12"; Params = ""} #x12
+    [PSCustomObject]@{MainAlgorithm = "yescrypt"; Params = ""} #Yescrypt
+    [PSCustomObject]@{MainAlgorithm = "yescryptr16"; Params = ""} #yescryptr16, YENTEN
+
+    ### MAYBE PROFITABLE ALGORITHMS - NOT MINEABLE IN SUPPORTED POOLS AS OF 06/03/2018
+    ### these algorithms are not mineable on supported pools but may be profitable
+    ### once/if support begins. They should be classified accordingly when or if
+    ### an algo becomes supported by one of the pools.
+    [PSCustomObject]@{MainAlgorithm = "anime"; Params = ""} #Anime
+    [PSCustomObject]@{MainAlgorithm = "argon2"; Params = ""} #Argon2
+    [PSCustomObject]@{MainAlgorithm = "argon2d-crds"; Params = ""} #Argon2Credits
+    [PSCustomObject]@{MainAlgorithm = "argon2d-dyn"; Params = ""} #Argon2Dynamic
+    [PSCustomObject]@{MainAlgorithm = "argon2d-uis"; Params = ""} #Argon2Unitus
+    #[PSCustomObject]@{MainAlgorithm = "axiom"; Params = ""} #axiom
+    [PSCustomObject]@{MainAlgorithm = "bastion"; Params = ""} #bastion
+    [PSCustomObject]@{MainAlgorithm = "bmw"; Params = ""} #bmw
+    [PSCustomObject]@{MainAlgorithm = "deep"; Params = ""} #deep
+    [PSCustomObject]@{MainAlgorithm = "drop"; Params = ""} #drop
+    [PSCustomObject]@{MainAlgorithm = "fresh"; Params = ""} #fresh
+    [PSCustomObject]@{MainAlgorithm = "heavy"; Params = ""} #heavy
+    [PSCustomObject]@{MainAlgorithm = "jha"; Params = ""} #JHA
+    [PSCustomObject]@{MainAlgorithm = "lyra2z330"; Params = ""} #lyra2z330
+    [PSCustomObject]@{MainAlgorithm = "pentablake"; Params = ""} #pentablake
+    [PSCustomObject]@{MainAlgorithm = "pluck"; Params = ""} #pluck
+    [PSCustomObject]@{MainAlgorithm = "scryptjane:nf"; Params = ""} #scryptjane:nf
+    [PSCustomObject]@{MainAlgorithm = "shavite3"; Params = ""} #shavite3
+    [PSCustomObject]@{MainAlgorithm = "skein2"; Params = ""} #skein2
+    [PSCustomObject]@{MainAlgorithm = "veltor"; Params = ""} #Veltor
+    [PSCustomObject]@{MainAlgorithm = "yescryptr8"; Params = ""} #yescryptr8
+    [PSCustomObject]@{MainAlgorithm = "yescryptr32"; Params = ""} #yescryptr32, WAVI
+    [PSCustomObject]@{MainAlgorithm = "zr5"; Params = ""} #zr5
+
+    #GPU or ASIC - never profitable 23/04/2018
+    #[PSCustomObject]@{MainAlgorithm = "bitcore"; Params = ""} #Bitcore
+    #[PSCustomObject]@{MainAlgorithm = "blake"; Params = ""} #blake
+    #[PSCustomObject]@{MainAlgorithm = "blakecoin"; Params = ""} #Blakecoin
+    #[PSCustomObject]@{MainAlgorithm = "blake2s"; Params = ""} #Blake2s
+    #[PSCustomObject]@{MainAlgorithm = "cryptolight"; Params = ""} #cryptolight
+    #[PSCustomObject]@{MainAlgorithm = "cryptonight"; Params = ""} #CryptoNight
+    #[PSCustomObject]@{MainAlgorithm = "c11"; Params = ""} #C11
+    #[PSCustomObject]@{MainAlgorithm = "decred"; Params = ""} #Decred
+    #[PSCustomObject]@{MainAlgorithm = "dmd-gr"; Params = ""} #dmd-gr
+    #[PSCustomObject]@{MainAlgorithm = "equihash"; Params = ""} #Equihash
+    #[PSCustomObject]@{MainAlgorithm = "ethash"; Params = ""} #Ethash
+    #[PSCustomObject]@{MainAlgorithm = "groestl"; Params = ""} #Groestl
+    #[PSCustomObject]@{MainAlgorithm = "keccak"; Params = ""} #Keccak
+    #[PSCustomObject]@{MainAlgorithm = "keccakc"; Params = ""} #keccakc
+    #[PSCustomObject]@{MainAlgorithm = "lbry"; Params = ""} #Lbry
+    #[PSCustomObject]@{MainAlgorithm = "lyra2v2"; Params = ""} #Lyra2RE2
+    #[PSCustomObject]@{MainAlgorithm = "lyra2h"; Params = ""} #lyra2h
+    #[PSCustomObject]@{MainAlgorithm = "lyra2re"; Params = ""} #lyra2re
+    #[PSCustomObject]@{MainAlgorithm = "myr-gr"; Params = ""} #MyriadGroestl
+    #[PSCustomObject]@{MainAlgorithm = "neoscrypt"; Params = ""} #NeoScrypt
+    #[PSCustomObject]@{MainAlgorithm = "nist5"; Params = ""} #Nist5
+    #[PSCustomObject]@{MainAlgorithm = "pascal"; Params = ""} #Pascal
+    #[PSCustomObject]@{MainAlgorithm = "phi1612"; Params = ""} #phi1612
+    #[PSCustomObject]@{MainAlgorithm = "scrypt:N"; Params = ""} #scrypt:N
+    #[PSCustomObject]@{MainAlgorithm = "sha256d"; Params = ""} #sha256d
+    #[PSCustomObject]@{MainAlgorithm = "sha256t"; Params = ""} #sha256t
+    #[PSCustomObject]@{MainAlgorithm = "sib"; Params = ""} #Sib
+    #[PSCustomObject]@{MainAlgorithm = "skunk"; Params = ""} #Skunk
+    #[PSCustomObject]@{MainAlgorithm = "skein"; Params = ""} #Skein
+    #[PSCustomObject]@{MainAlgorithm = "timetravel"; Params = ""} #Timetravel
+    #[PSCustomObject]@{MainAlgorithm = "tribus"; Params = ""} #Tribus
+    #[PSCustomObject]@{MainAlgorithm = "vanilla"; Params = ""} #BlakeVanilla
+    #[PSCustomObject]@{MainAlgorithm = "whirlpoolx"; Params = ""} #whirlpoolx
+    #[PSCustomObject]@{MainAlgorithm = "x11evo"; Params = ""} #X11evo
+    #[PSCustomObject]@{MainAlgorithm = "x13"; Params = ""} #x13
+    #[PSCustomObject]@{MainAlgorithm = "x13sm3"; Params = ""} #x13sm3
+    #[PSCustomObject]@{MainAlgorithm = "x14"; Params = ""} #x14
+    #[PSCustomObject]@{MainAlgorithm = "x15"; Params = ""} #x15
+    #[PSCustomObject]@{MainAlgorithm = "x16r"; Params = ""} #x16r
+    #[PSCustomObject]@{MainAlgorithm = "x16s"; Params = ""} #X16s
+    #[PSCustomObject]@{MainAlgorithm = "x17"; Params = ""} #X17
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("CPU")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Session.DevicesByTypes.CPU | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Session.DevicesByTypes.CPU | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+    $DeviceParams = "$(if ($Session.Config.CPUMiningThreads){"-t $($Session.Config.CPUMiningThreads)"}) $(if ($Session.Config.CPUMiningAffinity -ne ''){"--cpu-affinity $($Session.Config.CPUMiningAffinity)"})"
+
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+
+        if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+            [PSCustomObject]@{
+                Name = $Miner_Name
+                DeviceName = $Miner_Device.Name
+                DeviceModel = $Miner_Model
+                Path = $Path
+                Arguments = "-b $($Miner_Port) -a $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($DeviceParams) $($_.Params)"
+                HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                API = "Ccminer"
+                Port = $Miner_Port
+                Uri = $Uri
+                FaultTolerance = 0.5 #$_.FaultTolerance
+                ExtendInterval = $_.ExtendInterval
+				DevFee = $DevFee
+                ManualUri = $ManualUri
+            }
+        }
+    }
+}
