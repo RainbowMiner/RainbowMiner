b629a9efdc4b36358b37e07b8d2fcbcefae6108e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-26 01:38:23 +0200

    Add miner Trex 0.7.0 to MinersOldVersions
    - you may safely copy those miner files into your MIners folder, if you want to use them. These copies will not be deleted during a release update.

---
+++ MinersOldVersions/Trex070.ps1
@@ -0,0 +1,106 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+$Path = ".\Bin\NVIDIA-Trex\t-rex.exe"
+$ManualUri = "https://bitcointalk.org/index.php?topic=4432704.0"
+$Port = "326{0:d2}"
+$DevFee = 1.0
+
+$UriCuda = @(
+    [PSCustomObject]@{
+        Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7.0-trex/t-rex-0.7.0-win-cuda10.0.zip"
+        Cuda = "10.0"
+    },
+    [PSCustomObject]@{
+        Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7.0-trex/t-rex-0.7.0-win-cuda9.2.zip"
+        Cuda = "9.2"
+    },
+    [PSCustomObject]@{
+        Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7.0-trex/t-rex-0.7.0-win-cuda9.1.zip"
+        Cuda = "9.1"
+    }
+)
+
+if (-not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "balloon"; Params = ""} #Balloon
+    [PSCustomObject]@{MainAlgorithm = "bcd"; Params = ""} #Bcd
+    [PSCustomObject]@{MainAlgorithm = "bitcore"; Params = ""} #BitCore
+    [PSCustomObject]@{MainAlgorithm = "c11"; Params = ""} #C11
+    [PSCustomObject]@{MainAlgorithm = "hmq1725"; Params = ""} #HMQ1725 (new with v0.6.4)
+    [PSCustomObject]@{MainAlgorithm = "hsr"; Params = ""} #HSR
+    [PSCustomObject]@{MainAlgorithm = "lyra2z"; Params = ""} #Lyra2z
+    [PSCustomObject]@{MainAlgorithm = "phi"; Params = ""} #PHI
+    #[PSCustomObject]@{MainAlgorithm = "phi2"; Params = ""} #PHI2
+    [PSCustomObject]@{MainAlgorithm = "polytimos"; Params = ""} #Polytimos
+    [PSCustomObject]@{MainAlgorithm = "renesis"; Params = ""} #Renesis
+    [PSCustomObject]@{MainAlgorithm = "skunk"; Params = ""} #Skunk
+    [PSCustomObject]@{MainAlgorithm = "sonoa"; Params = ""} #Sonoa
+    [PSCustomObject]@{MainAlgorithm = "tribus"; Params = ""} #Tribus
+    [PSCustomObject]@{MainAlgorithm = "x16r"; Params = ""; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"} #X16r (fastest)
+    [PSCustomObject]@{MainAlgorithm = "x16s"; Params = ""; FaultTolerance = 0.5} #X16s
+    [PSCustomObject]@{MainAlgorithm = "x17"; Params = ""} #X17
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("NVIDIA")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $UriCuda[0].Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Uri = ""
+for($i=0;$i -le $UriCuda.Count -and -not $Uri;$i++) {
+    if (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $UriCuda[$i].Cuda -Warning $(if ($i -lt $UriCuda.Count-1) {""}else{$Name})) {
+        $Uri = $UriCuda[$i].Uri
+        $Cuda= $UriCuda[$i].Cuda
+    }
+}
+if (-not $Uri) {return}
+
+$Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
+
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+        
+        if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+            [PSCustomObject]@{
+                Name = $Miner_Name
+                DeviceName = $Miner_Device.Name
+                DeviceModel = $Miner_Model
+                Path = $Path
+                Arguments = "-b 127.0.0.1:$($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $(if (-not $Session.Config.ShowMinerWindow){"--no-color"}) --quiet --api-bind-http 0 $($_.Params)"
+                HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                API = "Ccminer"
+                Port = $Miner_Port
+                Uri = $Uri
+                FaultTolerance = $_.FaultTolerance
+                ExtendInterval = $_.ExtendInterval
+                DevFee = $DevFee
+                ManualUri = $ManualUri
+            }
+        }
+    }
+}
