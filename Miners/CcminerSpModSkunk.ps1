11240e5b1aa2febed0af7da38b92b0c4ce33fc0e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-28 15:21:27 +0100

    Update miner CcminerSpModSkunk
    - simplify

--- Miners/CcminerSpModSkunk.ps1
+++ Miners/CcminerSpModSkunk.ps1
@@ -35,37 +35,34 @@ if ($InfoOnly) {
 
 if (-not (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name)) {return}
 
-$Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
-    $Miner_Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model | Where-Object Model -notmatch "GTX20\d{2}"
+$Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | Where-Object Model -notmatch "GTX20\d{2}" | ForEach-Object {
+    $Miner_Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
 
-    if ($Miner_Device) {
-        $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
-        $Miner_Model = $_.Model
-        $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-        $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
 
-        $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
+    $Commands | ForEach-Object {
 
-        $Commands | ForEach-Object {
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
 
-            $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
-
-            if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
-                [PSCustomObject]@{
-                    Name = $Miner_Name
-                    DeviceName = $Miner_Device.Name
-                    DeviceModel = $Miner_Model
-                    Path = $Path
-                    Arguments = "-R 1 -b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -q -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($_.Params)"                        
-                    HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
-                    API = "Ccminer"
-                    Port = $Miner_Port
-                    Uri = $Uri
-				    DevFee = $DevFee
-                    FaultTolerance = $_.FaultTolerance
-                    ExtendInterval = $_.ExtendInterval
-                    ManualUri = $ManualUri
-                }
+        if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+            [PSCustomObject]@{
+                Name = $Miner_Name
+                DeviceName = $Miner_Device.Name
+                DeviceModel = $Miner_Model
+                Path = $Path
+                Arguments = "-R 1 -b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -q -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($_.Params)"                        
+                HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                API = "Ccminer"
+                Port = $Miner_Port
+                Uri = $Uri
+				DevFee = $DevFee
+                FaultTolerance = $_.FaultTolerance
+                ExtendInterval = $_.ExtendInterval
+                ManualUri = $ManualUri
             }
         }
     }
