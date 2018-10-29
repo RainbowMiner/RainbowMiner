433ece7bb22f20032ecc834cda327df7ea06abc3
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-29 15:07:30 +0100

    Update miner XmrigNvidia to v2.8.4
    - Rainbowminer edition nodevfee (0%)
    - Improved cn/2 performance especially for old GPUs.
    - Better cn/2 autoconfig for old GPUs if variant -1 or 2 selected.

--- Miners/XmrigNvidia.ps1
+++ Miners/XmrigNvidia.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\NVIDIA-Xmrig\xmrig-nvidia.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.8.3-xmrignvidia/xmrig-nvidia-2.8.3-cuda-9_2-win64-rbm.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.8.4-xmrignvidia/xmrig-nvidia-2.8.4-cuda-9_2-win64-rbm.zip"
 $ManualUri = "https://github.com/xmrig/xmrig-nvidia/releases"
 $Port = "303{0:d2}"
 $DevFee = 0.0
