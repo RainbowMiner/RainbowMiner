535d11541997dd30012d494b0ba663128c2a9b05
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-10-31 19:55:11 +0100

    Update miner Excavator1.4.4
    - fix resolve error on nicehash downtime

--- Miners/Excavator1.4.4.ps1
+++ Miners/Excavator1.4.4.ps1
@@ -73,38 +73,20 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
         $Params = $_.Params
 
         if ($Pools.$Main_Algorithm_Norm.Host) {
-            if (-not $Secondary_Algorithm) {
-                #Single algo mining
-                $Miner_Name = (@($Name) + @($Threads) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-
-                [PSCustomObject]@{
-                    Name             = $Miner_Name
-                    DeviceName       = $Miner_Device.Name
-                    DeviceModel      = $Miner_Model
-                    Path             = $Path
-                    Arguments        = @([PSCustomObject]@{id = 1; method = "algorithm.add"; params = @("$Main_Algorithm", "$([Net.DNS]::Resolve($Pools.$Main_Algorithm_Norm.Host).AddressList.IPAddressToString | Select-Object -First 1):$($Pools.$Main_Algorithm_Norm.Port)", "$($Pools.$Main_Algorithm_Norm.User):$($Pools.$Main_Algorithm_Norm.Pass)")}) + @([PSCustomObject]@{id = 1; method = "workers.add"; params = @(@($Miner_Device.Type_Vendor_Index | ForEach-Object {@("alg-0", "$_")} | Select-Object) * $Threads) + $Params})
-                    HashRates        = [PSCustomObject]@{$Main_Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Main_Algorithm_Norm)_HashRate".Week}
-                    API              = "Excavator144"
-                    Port             = $Miner_Port
-                    Uri              = $Uri
-                    ShowMinerWindow  = $True
-                    PrerequisitePath = "$env:SystemRoot\System32\msvcr120.dll"
-                    PrerequisiteURI  = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe"
-                    ManualUri        = $ManualUri
-                }
-            }
-            else {
-                #Dual algo mining
-                if ($Pools.$Secondary_Algorithm_Norm.Host ) {
-                    $Miner_Name = (@($Name) + @("$Secondary_Algorithm_Norm") + @($Threads) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+            $Pool_Host = $null
+            try {$Pool_Host = [Net.DNS]::Resolve($Pools.$Main_Algorithm_Norm.Host).AddressList.IPAddressToString | Select-Object -First 1} catch {}
+            if ($Pool_Host) {
+                if (-not $Secondary_Algorithm) {
+                    #Single algo mining
+                    $Miner_Name = (@($Name) + @($Threads) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
 
                     [PSCustomObject]@{
                         Name             = $Miner_Name
                         DeviceName       = $Miner_Device.Name
                         DeviceModel      = $Miner_Model
                         Path             = $Path
-                        Arguments        = @([PSCustomObject]@{id = 1; method = "algorithm.add"; params = @("$($Main_Algorithm)_$($Secondary_Algorithm)", "$([Net.DNS]::Resolve($Pools.$Main_Algorithm_Norm.Host).AddressList.IPAddressToString | Select-Object -First 1):$($Pools.$Main_Algorithm_Norm.Port)", "$($Pools.$Main_Algorithm_Norm.User):$($Pools.$Main_Algorithm_Norm.Pass)", "$([Net.DNS]::Resolve($Pools.$Secondary_Algorithm_Norm.Host).AddressList.IPAddressToString | Select-Object -First 1):$($Pools.$Secondary_Algorithm_Norm.Port)", "$($Pools.$Secondary_Algorithm_Norm.User):$($Pools.$Secondary_Algorithm_Norm.Pass)")}) + @([PSCustomObject]@{id = 1; method = "workers.add"; params = @(@($Miner_Device.Type_Vendor_Index | ForEach-Object {@("alg-0", "$_")} | Select-Object) * $Threads) + $Params})
-                        HashRates        = [PSCustomObject]@{$Main_Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Main_Algorithm_Norm)_HashRate".Week; $Secondary_Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Secondary_Algorithm_Norm)_HashRate".Week}
+                        Arguments        = @([PSCustomObject]@{id = 1; method = "algorithm.add"; params = @("$Main_Algorithm", "$($Pool_Host):$($Pools.$Main_Algorithm_Norm.Port)", "$($Pools.$Main_Algorithm_Norm.User):$($Pools.$Main_Algorithm_Norm.Pass)")}) + @([PSCustomObject]@{id = 1; method = "workers.add"; params = @(@($Miner_Device.Type_Vendor_Index | ForEach-Object {@("alg-0", "$_")} | Select-Object) * $Threads) + $Params})
+                        HashRates        = [PSCustomObject]@{$Main_Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Main_Algorithm_Norm)_HashRate".Week}
                         API              = "Excavator144"
                         Port             = $Miner_Port
                         Uri              = $Uri
@@ -112,6 +94,28 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
                         PrerequisitePath = "$env:SystemRoot\System32\msvcr120.dll"
                         PrerequisiteURI  = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe"
                         ManualUri        = $ManualUri
+                    }
+                }
+                else {
+                    #Dual algo mining
+                    if ($Pools.$Secondary_Algorithm_Norm.Host ) {
+                        $Miner_Name = (@($Name) + @("$Secondary_Algorithm_Norm") + @($Threads) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+
+                        [PSCustomObject]@{
+                            Name             = $Miner_Name
+                            DeviceName       = $Miner_Device.Name
+                            DeviceModel      = $Miner_Model
+                            Path             = $Path
+                            Arguments        = @([PSCustomObject]@{id = 1; method = "algorithm.add"; params = @("$($Main_Algorithm)_$($Secondary_Algorithm)", "$($Pool_Host):$($Pools.$Main_Algorithm_Norm.Port)", "$($Pools.$Main_Algorithm_Norm.User):$($Pools.$Main_Algorithm_Norm.Pass)", "$([Net.DNS]::Resolve($Pools.$Secondary_Algorithm_Norm.Host).AddressList.IPAddressToString | Select-Object -First 1):$($Pools.$Secondary_Algorithm_Norm.Port)", "$($Pools.$Secondary_Algorithm_Norm.User):$($Pools.$Secondary_Algorithm_Norm.Pass)")}) + @([PSCustomObject]@{id = 1; method = "workers.add"; params = @(@($Miner_Device.Type_Vendor_Index | ForEach-Object {@("alg-0", "$_")} | Select-Object) * $Threads) + $Params})
+                            HashRates        = [PSCustomObject]@{$Main_Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Main_Algorithm_Norm)_HashRate".Week; $Secondary_Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Secondary_Algorithm_Norm)_HashRate".Week}
+                            API              = "Excavator144"
+                            Port             = $Miner_Port
+                            Uri              = $Uri
+                            ShowMinerWindow  = $True
+                            PrerequisitePath = "$env:SystemRoot\System32\msvcr120.dll"
+                            PrerequisiteURI  = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe"
+                            ManualUri        = $ManualUri
+                        }
                     }
                 }
             }
