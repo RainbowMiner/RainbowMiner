4179af4b2177eba4634dee91b4a17e85d653afa7
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-11-18 19:33:48 +0100

    Update miner FireIce/Xmr-Stak to v2.6.0 (nodevfee)
    RainbowMiner version 0% devfee (nodevfee)
    
    Cryptonight_heavy optimisation (+ 5-10%)
    
    This release contains cryptonight_heavy and small OpenCL-AMD optimisations.

--- Miners/Fireice.ps1
+++ Miners/Fireice.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\CryptoNight-FireIce250\xmr-stak.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.5.2-fireice/xmr-stak-win64-2.5.2-nodevfee.7z"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.6.0-fireice/xmr-stak-win64-2.6.0-nodevfee.7z"
 $Port = "309{0:d2}"
 $ManualUri = "https://github.com/RainbowMiner/xmr-stak/releases"
 $DevFee = 0.0
