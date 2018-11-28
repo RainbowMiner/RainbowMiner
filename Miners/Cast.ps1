3a3a4fa2a2885b3409439b8038c5fff211bd90e7
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-11-28 11:29:12 +0100

    Update miner Cast to v1.6.5
    - support for RX 590 GPUs added
    - fixes for detecting Compute Mode on Polaris based GPUs
    - support for CryptoNight-SuperFast (--algo=11) for mining Free Haven Protocol (XFH)
    - updated switch-radeon-gpu to version 1.2.0, which includes bugfixes for newer versions of the AMD Radeon drivers

--- Miners/Cast.ps1
+++ Miners/Cast.ps1
@@ -6,23 +6,24 @@ param(
 )
 
 $Path = ".\Bin\CryptoNight-Cast\cast_xmr-vega.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.50-cast/cast_xmr-vega-win64_150.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.6.5-cast/cast_xmr-vega-win64_165.zip"
 $Port = "306{0:d2}"
-$DevFee = 1.5
+$DevFee = 1.0
 
 if (-not $Session.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No AMD present in system
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{MainAlgorithm = "cryptonightfast"; Params = "--algo=8 --intensity=8"}
-    [PSCustomObject]@{MainAlgorithm = "cryptonightfest"; Params = "--algo=9 --intensity=8"}
-    [PSCustomObject]@{MainAlgorithm = "cryptonightheavy"; Params = "--algo=2 --intensity=8"}
-    [PSCustomObject]@{MainAlgorithm = "cryptonightlite"; Params = "--algo=3 --intensity=8"}
-    [PSCustomObject]@{MainAlgorithm = "cryptonighttubeheavy"; Params = "--algo=5 --intensity=8"}
-    [PSCustomObject]@{MainAlgorithm = "cryptonightv7"; Params = "--algo=1 --intensity=8"}
-    [PSCustomObject]@{MainAlgorithm = "cryptonightv7lite"; Params = "--algo=4 --intensity=8"}
-    [PSCustomObject]@{MainAlgorithm = "cryptonightv7stellitev4"; Params = "--algo=6 --intensity=8"}
-    [PSCustomObject]@{MainAlgorithm = "cryptonightv8"; Params = "--algo=10 --intensity=8"}
-    [PSCustomObject]@{MainAlgorithm = "cryptonightxhvheavy"; Params = "--algo=7 --intensity=8"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonightfast"; Params = "--algo=8"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonightfest"; Params = "--algo=9"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonightheavy"; Params = "--algo=2"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonightlite"; Params = "--algo=3"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonightxfh"; Params = "--algo=11"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonighttubeheavy"; Params = "--algo=5"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonightv7"; Params = "--algo=1"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonightv7lite"; Params = "--algo=4"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonightv7stellitev4"; Params = "--algo=6"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonightv8"; Params = "--algo=10"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonightxhvheavy"; Params = "--algo=7"}
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -42,26 +43,28 @@ if ($InfoOnly) {
 }
 
 $Session.DevicesByTypes.AMD | Select-Object Vendor, Model -Unique | ForEach-Object {
-    $Miner_Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
-    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Devices = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
     $Miner_Model = $_.Model
-    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
-
-    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
 
     $Commands | ForEach-Object {
-
         $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+        $MinMemGb = $_.MinMemGb
+        $Params = $_.Params
+
+        $Miner_Device = $Devices | Where-Object {$_.OpenCL.GlobalMemsize -ge ($MinMemGb * 1gb) -and $_.OpenCL.Name -match "^(Ellesmere|Polaris|Vega|gfx900)"}
 
         if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+            $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+            $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+            $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
             $Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
+
             [PSCustomObject]@{
                 Name = $Miner_Name
                 DeviceName = $Miner_Device.Name
                 DeviceModel = $Miner_Model
                 Path      = $Path
-                Arguments = "--remoteaccess --remoteport $($Miner_Port) -S $($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) --forcecompute --fastjobswitch -G $($DeviceIDsAll) $($_.Params)" 
+                Arguments = "--remoteaccess --remoteport $($Miner_Port) -S $($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) --opencl $($Miner_Device | Select-Object -First 1 -ExpandProperty PlatformId) -G $(($Miner_Device | ForEach-Object {'{0:x}' -f $_.Type_Vendor_Index}) -join ',') --fastjobswitch --intensity -1$(if ($Pools.$Algorithm_Norm.Name -ne "NiceHash") {" --nonicehash"}) $($_.Params)" 
                 HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
                 API       = "Cast"
                 Port      = $Miner_Port
