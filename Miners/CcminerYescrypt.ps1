9e0e6edace88816c058e1f0c1642c1267b76ecd6
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-11-28 11:45:37 +0100

    Update miner CcminerYescrypt to v9
    - attempt to improve accepted shares (YescryptR32)
    - Reduce Job not found rejects on yiimp based pools.

--- Miners/CcminerYescrypt.ps1
+++ Miners/CcminerYescrypt.ps1
@@ -7,7 +7,7 @@ param(
 
 $Path = ".\Bin\NVIDIA-YesCrypt\ccminer.exe"
 #$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v8.21k-ccminerklaust/ccminerklaust_v8.21k.7z"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v7-ccmineryescrypt/ccminerKlausTyescrypt.7z"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v9-ccmineryescrypt/ccminerKlausTyescryptv9.7z"
 $Port = "106{0:d2}"
 $DevFee = 0.0
 $Cuda = "10.0"
@@ -89,8 +89,6 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
 
         $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
 
-        $Miner_Params = $_.Params
-
         if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
             $Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
             [PSCustomObject]@{
@@ -98,7 +96,7 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
                 DeviceName = $Miner_Device.Name
                 DeviceModel = $Miner_Model
                 Path = $Path
-                Arguments = "-N 1 -R 1 -b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -q -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($MinerParams)"
+                Arguments = "-N 1 -R 1 -b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -q -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($_.Params)"
                 HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Day}
                 API = "Ccminer"
                 Port = $Miner_Port
