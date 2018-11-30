2bcda551ae4da62d615ba86d33e1850219509e40
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-11-30 18:22:23 +0100

    Update miner Cast to v1.6.6
    - support for RX 590 GPUs added
    - fixes for detecting Compute Mode on Polaris based GPUs
    - support for CryptoNight-SuperFast (--algo=11) for mining Free Haven Protocol (XFH)
    - updated switch-radeon-gpu to version 1.2.0, which includes bugfixes for newer versions of the AMD Radeon drivers

--- Miners/Cast.ps1
+++ Miners/Cast.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\CryptoNight-Cast\cast_xmr-vega.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.6.5-cast/cast_xmr-vega-win64_165.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.6.6-cast/cast_xmr-vega-win64_166.zip"
 $Port = "306{0:d2}"
 $DevFee = 1.0
 
