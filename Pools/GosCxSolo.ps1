0a9d7d90249f7f2991f1ac890ea45a3d35939870
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-12-01 00:52:11 +0100

    Update pool GosCx
    - add asia stratum
    - update parameter set

--- Pools/GosCxSolo.ps1
+++ Pools/GosCxSolo.ps1
@@ -43,7 +43,7 @@ catch {
 [hashtable]$Pool_Algorithms = @{}
 [hashtable]$Pool_RegionsTable = @{}
 
-$Pool_Regions = @("ru","eu","us")
+$Pool_Regions = @("ru","eu","us","asia")
 $Pool_Regions | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
 
 $PoolCoins_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | Where-Object {$PoolCoins_Request.$_.hashrate_solo -gt 0 -or $InfoOnly -or $AllowZero} | ForEach-Object {
@@ -90,8 +90,8 @@ $PoolCoins_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | S
                     Protocol      = "stratum+tcp"
                     Host          = "$($Pool_Region).$($Pool_Host)"
                     Port          = $Pool_Port
-                    User          = $Pool_User
-                    Pass          = "{workername:$Worker},c=$Pool_Currency,m=solo{diff:,d=`$difficulty}"
+                    User          = "$($Pool_User).{workername:$Worker}"
+                    Pass          = "m=solo{diff:,d=`$difficulty}"
                     Region        = $Pool_RegionsTable.$Pool_Region
                     SSL           = $false
                     Updated       = $Stat.Updated
