4c3016a0957806a398a77a6243ddcfedcaa3c1d9
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-12-11 00:38:37 +0100

    Update algorithms
    - add algorithm Lyra2RE3

--- Cleanup.ps1
+++ Cleanup.ps1
@@ -268,6 +268,10 @@ try {
         }
     }
 
+    if ($Version -le (Get-Version "3.9.0.3")) {
+        $AddAlgorithm += @("lyra2v3")
+    }
+
     if ($AddAlgorithm.Count -gt 0) {
         $ConfigActual = Get-Content "$ConfigFile" -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
         if ($ConfigActual.EnableAutoAlgorithmAdd -ne "`$EnableAutoAlgorithmAdd" -and (Get-Yes $ConfigActual.EnableAutoAlgorithmAdd)) {
