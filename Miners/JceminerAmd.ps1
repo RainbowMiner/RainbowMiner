dffb7e5ddb74d8d6297bdd1a9a80df8f2f9732f7
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-12-17 15:00:29 +0100

    Update miner JceMinerAmd to v0.33b13 (amd)
    New mode: --legacy, fix regression on old algos

--- Miners/JceminerAmd.ps1
+++ Miners/JceminerAmd.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\AMD-Jceminer\jce_cn_gpu_miner64.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.33b12-jceminer/jce_cn_gpu_miner.033b12.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.33b13-jceminer/jce_cn_gpu_miner.033b13.zip"
 $Port = "321{0:d2}"
 $ManualUri = "https://bitcointalk.org/index.php?topic=3281187.0"
 $DevFee = 0.9
