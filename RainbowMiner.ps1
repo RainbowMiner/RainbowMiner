a5f5a911da2bf46bbb16fb947fc244d1540cc946
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-12-21 16:25:58 +0100

    Bump version to v3.9.1.2

--- RainbowMiner.ps1
+++ RainbowMiner.ps1
@@ -173,7 +173,7 @@ $EnableMinerStatus = $true
 
 $Global:Session = [hashtable]::Synchronized(@{}) 
 
-$Session.Version = "3.9.1.1"
+$Session.Version = "3.9.1.2"
 
 $Session.Strikes           = 3
 $Session.SyncWindow        = 10 #minutes, after that time, the pools bias price will start to decay
