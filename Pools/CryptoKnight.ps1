189f732a1fdd775bed1153a971f2777ff3bb8b4e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-12-21 20:14:10 +0100

    Update pool CryptoKnight
    - add Bittorium
    - remove Lines, Qwerty

--- Pools/CryptoKnight.ps1
+++ Pools/CryptoKnight.ps1
@@ -37,6 +37,7 @@ $Pools_Data = @(
     [PSCustomObject]@{coin = "Arto"; symbol = "RTO"; algo = "CnArto"; port = 51201; fee = 0.0; walletSymbol = "arto"; host = "arto.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "BBS"; symbol = "BBS"; algo = "CnLiteV7"; port = 19931; fee = 0.0; walletSymbol = "bbs"; host = "bbs.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "BitcoinNote"; symbol = "BTCN"; algo = "CnLiteV7"; port = 4461; fee = 0.0; walletSymbol = "btcn"; host = "btcn.ingest.cryptoknight.cc"}
+    [PSCustomObject]@{coin = "Bittorium"; symbol = "BTOR"; algo = "CnLiteV7"; port = 10401; fee = 0.0; walletSymbol = "bittorium"; host = "bittorium.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "BitTube"; symbol = "TUBE"; algo = "CnSaber"; port = 4461; fee = 0.0; walletSymbol = "ipbc"; host = "tube.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "Caliber"; symbol = "CAL"; algo = "CnV8"; port = 14101; fee = 0.0; walletSymbol = "caliber"; host = "caliber.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "CitiCash"; symbol = "CCH"; algo = "CnHeavy"; port = 4461; fee = 0.0; walletSymbol = "citi"; host = "citi.ingest.cryptoknight.cc"}
@@ -47,14 +48,14 @@ $Pools_Data = @(
     [PSCustomObject]@{coin = "Iridium"; symbol = "IRD"; algo = "CnLiteV7"; port = 50501; fee = 0.0; walletSymbol = "iridium"; host = "iridium.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "Italo"; symbol = "ITA"; algo = "CnHaven"; port = 50701; fee = 0.0; walletSymbol = "italo"; host = "italo.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "Lethean"; symbol = "LTHN"; algo = "CnV8"; port = 8881; fee = 0.0; walletSymbol = "lethean"; host = "lethean.ingest.cryptoknight.cc"}
-    [PSCustomObject]@{coin = "Lines"; symbol = "LNS"; algo = "CnV7"; port = 50401; fee = 0.0; walletSymbol = "lines"; host = "lines.ingest.cryptoknight.cc"}
+    #[PSCustomObject]@{coin = "Lines"; symbol = "LNS"; algo = "CnV7"; port = 50401; fee = 0.0; walletSymbol = "lines"; host = "lines.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "Loki"; symbol = "LOKI"; algo = "CnHeavy"; port = 7731; fee = 0.0; walletSymbol = "loki"; host = "loki.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "Masari"; symbol = "MSR"; algo = "CnFast"; port = 3333; fee = 0.0; walletSymbol = "msr"; host = "masari.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "Monero"; symbol = "XMR"; algo = "CnV8"; port = 4441; fee = 0.0; walletSymbol = "monero"; host = "monero.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "MoneroV"; symbol = "XMV"; algo = "CnV7"; port = 9221; fee = 0.0; walletSymbol = "monerov"; host = "monerov.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "Niobio"; symbol = "NBR"; algo = "CnHeavy"; port = 50101; fee = 0.0; walletSymbol = "niobio"; host = "niobio.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "Ombre"; symbol = "OMB"; algo = "CnHeavy"; port = 5571; fee = 0.0; walletSymbol = "ombre"; host = "ombre.ingest.cryptoknight.cc"}
-    [PSCustomObject]@{coin = "Qwerty"; symbol = "QWC"; algo = "CnHeavy"; port = 8261; fee = 0.0; walletSymbol = "qwerty"; host = "qwerty.ingest.cryptoknight.cc"}
+    #[PSCustomObject]@{coin = "Qwerty"; symbol = "QWC"; algo = "CnHeavy"; port = 8261; fee = 0.0; walletSymbol = "qwerty"; host = "qwerty.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "Ryo"; symbol = "RYO"; algo = "CnHeavy"; port = 52901; fee = 0.0; walletSymbol = "ryo"; host = "ryo.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "SafeX"; symbol = "SAFE"; algo = "CnV7"; port = 13701; fee = 0.0; walletSymbol = "safex"; host = "safex.ingest.cryptoknight.cc"}
     [PSCustomObject]@{coin = "Saronite"; symbol = "XRN"; algo = "CnHeavy"; port = 5531; fee = 0.0; walletSymbol = "saronite"; host = "saronite.ingest.cryptoknight.cc"}
