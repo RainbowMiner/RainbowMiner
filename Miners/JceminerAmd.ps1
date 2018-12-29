ddb0b75c0ca52f4852406bc38ff42ce05b042dd4
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2018-12-29 12:18:08 +0100

    Update miner JceMinerAmd to v0.33b15 (amd)
    Turtle-v2, fixed IGP config, remote managment

--- Miners/JceminerAmd.ps1
+++ Miners/JceminerAmd.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\AMD-Jceminer\jce_cn_gpu_miner64.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.33b14-jceminer/jce_cn_gpu_miner.033b14.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.33b15-jceminer/jce_cn_gpu_miner.033b15.zip"
 $Port = "321{0:d2}"
 $ManualUri = "https://bitcointalk.org/index.php?topic=3281187.0"
 $DevFee = 0.9
@@ -27,6 +27,7 @@ $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "cryptonight-lite/ipbc";  Threads = 1; MinMemGb = 1; Params = "--variation 6"}
     [PSCustomObject]@{MainAlgorithm = "cryptonight-lite/dark";  Threads = 1; MinMemGb = 1; Params = "--variation 17"}
     [PSCustomObject]@{MainAlgorithm = "cryptonight-lite/red";   Threads = 1; MinMemGb = 1; Params = "--variation 14"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight-lite/turtle";Threads = 1; MinMemGb = 1; Params = "--variation 20"}
     [PSCustomObject]@{MainAlgorithm = "cryptonight-lite/upx";   Threads = 1; MinMemGb = 1; Params = "--variation 19"}
     [PSCustomObject]@{MainAlgorithm = "cryptonight-heavy";      Threads = 1; MinMemGb = 4; Params = "--variation 5"}
     [PSCustomObject]@{MainAlgorithm = "cryptonight-heavy/tube"; Threads = 1; MinMemGb = 4; Params = "--variation 13"}
@@ -52,6 +53,7 @@ $Commands = [PSCustomObject[]]@(
 #N=17 Cryptolight-Dark
 #N=18 Cryptonight-Swap
 #N=19 Cryptolight-Uplexa
+#N=20 Cryptolight-Turtle v2
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 
