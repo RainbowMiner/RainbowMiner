bed3b681812ea8ba68b1f465b302941ba9b78f25
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-01-18 11:00:03 +0100

    Update miner Gminer to v1.19 (nvidia)
    + added cuckaroo29 algorithm (Grin)

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\Equihash-Gminer\miner.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.18-gminer/gminer_1_18_minimal_windows64.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.19-gminer/gminer_1_19_minimal_windows64.zip"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5034735.0"
 $Port = "329{0:d2}"
 $DevFee = 2.0
@@ -15,7 +15,8 @@ $Cuda = "9.0"
 if (-not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{MainAlgorithm = "Equihash965"; MinMemGB = 2; Params = "--algo 96_5"} #Equihash 96,5
+    [PSCustomObject]@{MainAlgorithm = "Cuckaroo29";   MinMemGB = 8; Params = "--algo grin29"} #Equihash Cuckaroo29/GRIN
+    [PSCustomObject]@{MainAlgorithm = "Equihash965";  MinMemGB = 2; Params = "--algo 96_5"} #Equihash 96,5
     [PSCustomObject]@{MainAlgorithm = "Equihash1445"; MinMemGB = 2; Params = "--algo 144_5"} #Equihash 144,5
     [PSCustomObject]@{MainAlgorithm = "Equihash1505"; MinMemGB = 3.9; Params = "--algo 150_5"} #Equihash 150,5
     [PSCustomObject]@{MainAlgorithm = "Equihash1927"; MinMemGB = 3.0; Params = "--algo 192_7"} #Equihash 192,7
