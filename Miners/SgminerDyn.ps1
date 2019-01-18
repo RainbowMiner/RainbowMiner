d8e4e938c273c6fab2a058d5cbcfb3048bec04ae
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-01-18 12:39:34 +0100

    Add miner SgminerDyn v1.0.0 (amd)
    - GPU mining Argon2d-DYN

---
+++ Miners/SgminerDyn.ps1
@@ -0,0 +1,68 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+$Path = ".\Bin\AMD-Dyn\sgminer.exe"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.0.0-sgminerdyn/sgminer-dyn-1.0.0-windows-x64.zip"
+$ManualUri = "https://github.com/duality-solutions/Dynamic-GPU-Miner-AMD/releases"
+$Port = "410{0:d2}"
+$DevFee = 0.0
+
+if (-not $Session.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No AMD present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "argon2d-dyn"; Params = "-k argon2d"}
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("AMD")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Session.DevicesByTypes.AMD | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
+    $Miner_PlatformId = $Miner_Device | Select -Property Platformid -Unique -ExpandProperty PlatformId
+
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+
+        if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+            $Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
+            [PSCustomObject]@{
+                Name        = $Miner_Name
+                DeviceName  = $Miner_Device.Name
+                DeviceModel = $Miner_Model
+                Path        = $Path
+                Arguments   = "--device $($DeviceIDsAll) --api-port $($Miner_Port) --api-listen -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) --text-only --gpu-platform $($Miner_PlatformId) $($_.Params)"
+                HashRates   = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                API         = "Xgminer"
+                Port        = $Miner_Port
+                Uri         = $Uri
+                DevFee      = $DevFee
+                ManualUri   = $ManualUri
+                EnvVars     = @("GPU_FORCE_64BIT_PTR=0")
+            }
+        }
+    }
+}
