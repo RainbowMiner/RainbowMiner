c315e18406f15f03e5e96ee66dd6d68f9c7f8c62
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-01-19 22:26:38 +0100

    Update AlgorithmsConfigDefault.ps1

--- Data/AlgorithmsConfigDefault.ps1
+++ Data/AlgorithmsConfigDefault.ps1
@@ -1,6 +1,10 @@
 ﻿[PSCustomObject]@{
     "Balloon" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile=""}
     "Dedal" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile=""}
+    "GLTAstralHash" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile=""}
+    "GLTJeongHash" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile=""}
+    "GLTPadiHash" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile=""}
+    "GLTPawelHash" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile=""}
     "Lyra2RE2" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile=""}
     "Lyra2RE3" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile=""}
     "Lyra2z" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile=""}
