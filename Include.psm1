a3006cf85911db5dccee26cacba860b3178c0491
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-01-26 13:12:56 +0100

    Update core
    - fix urlencode function

--- Include.psm1
+++ Include.psm1
@@ -4124,8 +4124,14 @@ param(
     [Parameter(Mandatory = $false)]
     [switch]$ConvertDot = $false
 )
-    $Uri = if ($Uri -match "^(.*?)({[^}]+})(.*?)$") {"$([System.Web.HttpUtility]::UrlEncode($Matches[1]))$($Matches[2])$([System.Web.HttpUtility]::UrlEncode($Matches[3]))"} else {[System.Web.HttpUtility]::UrlEncode($Uri)}
-    if ($ConvertDot) {$Uri -replace "\.","%2e"} else {$Uri}
+    $Uri2 = @()
+    while ($Uri -match "^(.*?)({[^}]+})(.*?)$") {
+        if ($Matches[1].Length) {$s = [System.Web.HttpUtility]::UrlEncode($Matches[1]); $Uri2+=if ($ConvertDot) {$s -replace "\.","%2e"} else {$s}}
+        $Uri2+=$Matches[2]
+        $Uri=$Matches[3]
+    }
+    if ($Uri.Length) {$Uri = [System.Web.HttpUtility]::UrlEncode($Uri); $Uri2+=if ($ConvertDot) {$Uri -replace "\.","%2e"} else {$Uri}}
+    $Uri2 -join ''    
 }
 
 function Get-LastDrun {
