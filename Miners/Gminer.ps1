498ff67329bdd68521985c60475603887858f90b
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-01-28 22:31:19 +0100

    Update miner Gminer (nvidia)
    - re-enable Beam on nicehash

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -15,7 +15,7 @@ $Cuda = "9.0"
 if (-not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{MainAlgorithm = "Aeternity";    MinMemGB = 8; Params = "--algo aeternity"; ExtendInterval = 2; Penalty = 0} #Equihash Cuckoo29/Aeternity
+    [PSCustomObject]@{MainAlgorithm = "Aeternity";    MinMemGB = 8; Params = "--algo aeternity"; ExtendInterval = 2; Penalty = 0} #Equihash Cuckaroo29/Aeternity
     [PSCustomObject]@{MainAlgorithm = "Cuckaroo29";   MinMemGB = 8; Params = "--algo grin29"; ExtendInterval = 2; Penalty = 0} #Equihash Cuckaroo29/GRIN    
     [PSCustomObject]@{MainAlgorithm = "Equihash965";  MinMemGB = 2; Params = "--algo 96_5"} #Equihash 96,5
     [PSCustomObject]@{MainAlgorithm = "Equihash1445"; MinMemGB = 2; Params = "--algo 144_5"} #Equihash 144,5
@@ -57,7 +57,7 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
 
         $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ' '
         
-        if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and ($Algorithm_Norm -ne "Equihash25x5" -or $Pools.$Algorithm_Norm.Name -notmatch "nicehash")) {
+        if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
             $Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
             [PSCustomObject]@{
                 Name = $Miner_Name
