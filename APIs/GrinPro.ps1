4fd8d5d0650fa662189a69ada5bbf322c6c69154
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-01-31 14:15:10 +0100

    Add miner GrinProMiner v1.1 (amd,nvidia)
    - happy GRIN mining for AMD

---
+++ APIs/GrinPro.ps1
@@ -0,0 +1,86 @@
+﻿using module ..\Include.psm1
+
+class GrinPro : Miner {
+
+    [String]GetArguments() {
+        $Parameters = $this.Arguments | ConvertFrom-Json
+
+        $ConfigPath = "$([IO.Path]::GetFullPath($this.Path) | Split-Path)\$($this.Pool -join '-')-$($this.DeviceModel)$(if ($Parameters.SSL){"-ssl"})"
+
+        if (Test-Path $this.Path) {
+            if (-not (Test-Path $ConfigPath)) {New-Item $ConfigPath -ItemType "directory" > $null}
+            "<?xml version=`"1.0`" encoding=`"utf-8`"?>
+<Config xmlns:xsi=`"http://www.w3.org/2001/XMLSchema-instance`" xmlns:xsd=`"http://www.w3.org/2001/XMLSchema`">
+  <PrimaryConnection>
+    <ConnectionAddress>$($Parameters.Config.Host)</ConnectionAddress>
+    <ConnectionPort>$($Parameters.Config.Port)</ConnectionPort>
+    <Ssl>$(if ($Parameters.Config.SSL) {"true"} else {"false"})</Ssl>
+    <Login>$($Parameters.Config.User)</Login>
+    <Password>$($Parameters.Config.Pass)</Password>  
+  </PrimaryConnection>
+  <SecondaryConnection>
+  </SecondaryConnection>
+  <LogOptions>
+    <FileMinimumLogLevel>WARNING</FileMinimumLogLevel>
+    <ConsoleMinimumLogLevel>INFO</ConsoleMinimumLogLevel>
+    <KeepDays>1</KeepDays>
+    <DisableLogging>false</DisableLogging>
+  </LogOptions>
+  <CPUOffloadValue>0</CPUOffloadValue>
+  <GPUOptions>$($Parameters.Device | Foreach-Object {"
+    <GPUOption>
+        <GPUName>$($_.Name)</GPUName>
+        <GPUType>$($_.Vendor)</GPUType>
+        <DeviceID>$($_.Index)</DeviceID>
+        <PlatformID>$($_.PlatformId)</PlatformID>
+        <Enabled>true</Enabled>
+    </GPUOption>"})
+  </GPUOptions>
+</Config>" | Out-File "$($ConfigPath)\config.xml" -Encoding utf8
+        }
+
+        return "configpath=$ConfigPath $($Parameters.Params)".Trim()
+    }
+
+    [String[]]UpdateMinerData () {
+        if ($this.GetStatus() -ne [MinerStatus]::Running) {return @()}
+
+        $Server = "localhost"
+        $Timeout = 10 #seconds
+
+        $Request = ""
+        $Response = ""
+
+        $HashRate = [PSCustomObject]@{}
+
+        $oldProgressPreference = $Global:ProgressPreference
+        $Global:ProgressPreference = "SilentlyContinue"
+        try {
+            $Response = Invoke-WebRequest "http://$($Server):$($this.Port)/api/status" -UseBasicParsing -TimeoutSec $Timeout -ErrorAction Stop
+            if ($Response.StatusCode -ne 200) {throw}
+            $Data = $Response.Content | ConvertFrom-Json -ErrorAction Stop
+        }
+        catch {
+            Write-Log -Level Error "Failed to connect to miner ($($this.Name)). "
+            return @($Request, $Response)
+        }
+        $Global:ProgressPreference = $oldProgressPreference
+
+        $HashRate_Name = [String]$this.Algorithm[0]
+        $HashRate_Value = [Double]($Data.workers | Where-Object status -eq "ONLINE" | Select-Object -ExpandProperty graphsPerSecond | Measure-Object -Sum).Sum
+
+        $HashRate | Where-Object {$HashRate_Name} | Add-Member @{$HashRate_Name = $HashRate_Value}
+
+        $this.AddMinerData([PSCustomObject]@{
+            Date     = (Get-Date).ToUniversalTime()
+            Raw      = $Response
+            HashRate = $HashRate
+            PowerDraw = Get-DevicePowerDraw -DeviceName $this.DeviceName
+            Device   = @()
+        })
+
+        $this.CleanupMinerData()
+
+        return @($Request, $Data | ConvertTo-Json -Compress)
+    }
+}
---
+++ Miners/GrinProMiner.ps1
@@ -0,0 +1,89 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+$Path = ".\Bin\GRIN-GrinPro\GrinProMiner.exe"
+$URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1-grinprominer/GrinPro_1_1.zip"
+$ManualURI = "https://grinpro.io"
+$Port = "335{0:d2}"
+$DevFee = 2.0
+$Cuda = "10.0"
+
+if (-not $Session.DevicesByTypes.NVIDIA -and -not $Session.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No GPU present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "cuckaroo29"; MinMemGb = 6; Params = ""; DevFee = 2.0; ExtendInterval = 2; FaultTolerance = 0.3; Penalty = 0; Vendor = @("AMD")} #GRIN/Cuckaroo29
+    #[PSCustomObject]@{MainAlgorithm = "cuckaroo29"; MinMemGb = 6; Params = ""; DevFee = 2.0; ExtendInterval = 2; FaultTolerance = 0.3; Penalty = 0; Vendor = @("NVIDIA")} #GRIN/Cuckaroo29
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("AMD","NVIDIA")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+if ($Session.DevicesByTypes.NVIDIA) {$Cuda = Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name}
+
+foreach ($Miner_Vendor in @("AMD","NVIDIA")) {
+	$Session.DevicesByTypes.$Miner_Vendor | Where-Object Type -eq "GPU" | Where-Object {$_.Vendor -ne "NVIDIA" -or $Cuda} | Select-Object Vendor, Model -Unique | ForEach-Object {
+        $Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
+        $Miner_Model = $_.Model
+
+        $Commands | Where-Object {$_.Vendor -icontains $Miner_Vendor} | ForEach-Object {
+            $MinMemGb = $_.MinMemGb
+            $MainAlgorithm = $_.MainAlgorithm
+            $MainAlgorithm_Norm = Get-Algorithm $MainAlgorithm
+
+            $Miner_Device = $Device | Where-Object {$_.OpenCL.GlobalMemsize -ge ($MinMemGb * 1gb)}
+
+            if ($Pools.$MainAlgorithm_Norm.Host -and $Miner_Device) {
+                $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+                $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+                
+                $Arguments = [PSCustomObject]@{
+                    Params = "api-port=$($Miner_Port) $($_.Params)".Trim()
+                    Config = [PSCustomObject]@{
+                        Host = $Pools.$MainAlgorithm_Norm.Host
+                        Port = $Pools.$MainAlgorithm_Norm.Port
+                        SSL  = $Pools.$MainAlgorithm_Norm.SSL
+                        User = $Pools.$MainAlgorithm_Norm.User
+                        Pass = $Pools.$MainAlgorithm_Norm.Pass
+                    }
+                    Device = @($Miner_Device | Foreach-Object {[PSCustomObject]@{Name=$_.Model_Name;Vendor=$_.Vendor;Index=$_.Type_Vendor_Index;PlatformId=$_.PlatformId}})
+                }
+
+                $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+                [PSCustomObject]@{
+                    Name = $Miner_Name
+                    DeviceName = $Miner_Device.Name
+                    DeviceModel = $Miner_Model
+                    Path = $Path
+                    #Arguments = "ignore-config=true $($DeviceIDsAll) api-port=$($Miner_Port) stratum-address=$($Pools.$MainAlgorithm_Norm.Host) stratum-port=$($Pools.$MainAlgorithm_Norm.Port) stratum-login=$($Pools.$MainAlgorithm_Norm.User) $(if ($Pools.$MainAlgorithm_Norm.Pass) {"stratum-password=$($Pools.$MainAlgorithm_Norm.Pass)"}) $($_.Params)"
+                    Arguments = $Arguments
+                    HashRates = [PSCustomObject]@{$MainAlgorithm_Norm = $Session.Stats."$($Miner_Name)_$($MainAlgorithm_Norm)_HashRate".Week * $(if ($_.Penalty) {1-$_.Penalty/100} else {1})}
+                    API = "GrinPro"
+                    Port = $Miner_Port
+                    Uri = $Uri
+                    DevFee = $_.DevFee
+                    FaultTolerance = $_.FaultTolerance
+                    ExtendInterval = $_.ExtendInterval
+                    ManualUri = $ManualUri
+                    StopCommand = "Sleep 15; Get-CIMInstance CIM_Process | Where-Object ExecutablePath | Where-Object {`$_.ExecutablePath -like `"$([IO.Path]::GetFullPath($Path) | Split-Path)\*`"} | Select-Object ProcessId,ProcessName | Foreach-Object {Stop-Process -Id `$_.ProcessId -Force -ErrorAction Ignore}"
+                }
+            }
+        }
+    }
+}
