162978a4e0d9f1a7b3ed509f6f81948a9886d17a
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-02-01 23:39:54 +0100

    Update miner Teamred to v0.3.9 (amd)
    - add support for lyra2re3

--- Miners/Teamred.ps1
+++ Miners/Teamred.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\AMD-Teamred\teamredminer.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.3.8-teamred/teamredminer-v0.3.8-win.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.3.9-teamred/teamredminer-v0.3.9-win.zip"
 $Port = "409{0:d2}"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5059817.0"
 $DevFee = 3.0
@@ -15,6 +15,7 @@ if (-not $Session.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No NVIDIA p
 
 $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "cnv8";   MinMemGb = 2; Params = ""; DevFee = 2.5}
+    [PSCustomObject]@{MainAlgorithm = "lyra2rev3"; MinMemGb = 1; Params = ""; DevFee = 2.5}
     [PSCustomObject]@{MainAlgorithm = "lyra2z"; MinMemGb = 1; Params = ""; DevFee = 3.0}
     [PSCustomObject]@{MainAlgorithm = "phi2";   MinMemGb = 1; Params = ""; DevFee = 3.0}
 )
