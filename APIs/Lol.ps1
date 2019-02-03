7f10a9e8e97e61baca528960c3fb7a5acc37d7b3
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-02-03 18:43:23 +0100

    Add miner LoLminer v0.6 & v0.7a4 (amd,nvidia)

--- APIs/Lol.ps1
+++ APIs/Lol.ps1
@@ -12,32 +12,29 @@ class Lol : Miner {
         $HashRate = [PSCustomObject]@{}
 
         try {
-            $Response = Invoke-TcpRead $Server 31702 $Timeout -ErrorAction Stop
-            $Data = $Response | ConvertFrom-Json -ErrorAction Stop
+            $Response = Invoke-WebRequest "http://$($Server):$($this.Port)/summary" -UseBasicParsing -TimeoutSec $Timeout -ErrorAction Stop
+            if ($Response.StatusCode -ne 200) {throw}
+            $Data = $Response.Content | ConvertFrom-Json -ErrorAction Stop
         }
         catch {
             Write-Log -Level Error "Failed to connect to miner ($($this.Name)). "
             return @("", $Response)
         }
 
-        $HashRate_Name = [String]$this.Algorithm[0]
-        try {
-            $HashRate_Value = [Double]$Data.'TotalSpeed(60s)'
-            if (-not $HashRate_Value) {throw}
-        } catch {
-            $HashRate_Value = [double]$Data.'TotalSpeed(5s)'
-        }
-        
+        $HashRate_Name = Get-Algorithm($Data.Mining.Algorithm -replace "/")
+        $HashRate_Value = [Double]$data.Session.Performance_Summary
+
         if ($HashRate_Name -and $HashRate_Value -gt 0) {
             $HashRate | Add-Member @{$HashRate_Name = $HashRate_Value}
-            $this.AddMinerData([PSCustomObject]@{
-                Date     = (Get-Date).ToUniversalTime()
-                Raw      = $Response
-                HashRate = $HashRate
-                PowerDraw = Get-DevicePowerDraw -DeviceName $this.DeviceName
-                Device   = @()
-            })
         }
+
+        $this.AddMinerData([PSCustomObject]@{
+            Date     = (Get-Date).ToUniversalTime()
+            Raw      = $Response.Content
+            HashRate = $HashRate
+            PowerDraw = Get-DevicePowerDraw -DeviceName $this.DeviceName
+            Device   = @()
+        })
 
         $this.CleanupMinerData()
 
---
+++ Miners/lolminer.ps1
@@ -0,0 +1,75 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+$Path = ".\Bin\Equihash-lolMiner\lolMiner.exe"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.6-lolminer/lolMiner_v06_Win64.zip"
+$ManualUri = "https://bitcointalk.org/index.php?topic=4724735.0"
+$Port = "317{0:d2}"
+$Cuda = 10.0
+$DevFee = 2.0
+
+if (-not $Session.DevicesByTypes.NVIDIA -and -not $Session.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No GPU present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "Equihash16x5"; MinMemGB = 1; Params = "--coin MNX --workbatch VERYHIGH"; Fee=1}  #Equihash 96,5
+    [PSCustomObject]@{MainAlgorithm = "Equihash21x9"; MinMemGB = 1; Params = "--coin AION"; Fee=2} #Equihash 210,9
+    [PSCustomObject]@{MainAlgorithm = "Equihash24x5"; MinMemGB = 2; Params = "--coin AUTO144_5"; Fee=1.5} #Equihash 144,5
+    [PSCustomObject]@{MainAlgorithm = "Equihash24x7"; MinMemGB = 3; Params = "--coin AUTO192_7"; Fee=2} #Equihash 192,7
+    #[PSCustomObject]@{MainAlgorithm = "Equihash25x5"; MinMemGB = 4; Params = "--coin BEAM"; Fee=2} #Equihash 150,5
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("AMD","NVIDIA")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+if ($Session.DevicesByTypes.NVIDIA) {$Cuda = Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name}
+
+foreach ($Miner_Vendor in @("AMD","NVIDIA")) {
+    $Session.DevicesByTypes.$Miner_Vendor | Where-Object Type -eq "GPU" | Where-Object {$_.Vendor -ne "NVIDIA" -or $Cuda} | Select-Object Vendor, Model -Unique | ForEach-Object {
+        $Device = $Session.DevicesByTypes.$Miner_Vendor | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model
+        $Miner_Model = $_.Model
+
+        $Commands | ForEach-Object {            
+            $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+            $MinMemGB = $_.MinMemGB
+            $Miner_Device = $Device | Where-Object {$_.OpenCL.GlobalMemsize -ge ($MinMemGB * 1gb)}
+
+            if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and ($Algorithm_Norm -ne "Equihash25x5" -or $Pools.$Algorithm_Norm.Name -ne "Nicehash")) {
+                $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+                $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+                $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+                
+                [PSCustomObject]@{
+                    Name        = $Miner_Name
+                    DeviceName  = $Miner_Device.Name
+                    DeviceModel = $Miner_Model
+                    Path        = $Path
+                    Arguments   = "--pool $($Pools.$Algorithm_Norm.Host) --port $($Pools.$Algorithm_Norm.Port) --user $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" --pass $($Pools.$Algorithm_Norm.Pass)"}) --devices $($Miner_Device.Type_Vendor_Index -join ',') --apiport $($Miner_Port) --digits 2 --longstats 60 --shortstats 5 --connectattempts 3 $($_.Params)"
+                    HashRates   = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                    API         = "Lol"
+                    Port        = $Miner_Port
+                    DevFee      = $_.Fee
+                    Uri         = $Uri
+                    ExtendInterval = $_.ExtendInterval
+                    ManualUri   = $ManualUri
+                }
+            }
+        }
+    }
+}
