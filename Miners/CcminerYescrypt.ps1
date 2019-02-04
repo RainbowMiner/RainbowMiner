22e3ed2124b48fa5ce7fc108a7fb399ffbba97bc
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-02-04 02:06:43 +0100

    Update CcminerYescrypt.ps1

--- Miners/CcminerYescrypt.ps1
+++ Miners/CcminerYescrypt.ps1
@@ -6,58 +6,26 @@ param(
 )
 
 $Path = ".\Bin\NVIDIA-YesCrypt\ccminer.exe"
-#$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v8.21k-ccminerklaust/ccminerklaust_v8.21k.7z"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v10-ccmineryescrypt/ccminerKlausTyescryptv10.7z"
-$Port = "106{0:d2}"
+$ManualUri = "https://github.com/KlausT/ccminer/releases"
+$Port = "129{0:d2}"
 $DevFee = 0.0
-$Cuda = "10.0"
+
+$UriCuda = @(
+    [PSCustomObject]@{
+        Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v8.21r18-ccminerklaust/ccminer-8.21-r18.7z"
+        Cuda = "10.0"
+    }        
+)
 
 if (-not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
-    #GPU - profitable 20/04/2018
-    #[PSCustomObject]@{MainAlgorithm = "c11"; Params = ""} #C11
-    #[PSCustomObject]@{MainAlgorithm = "deep"; Params = ""} #deep
-    #[PSCustomObject]@{MainAlgorithm = "dmd-gr"; Params = ""} #dmd-gr
-    #[PSCustomObject]@{MainAlgorithm = "fresh"; Params = ""} #fresh
-    #[PSCustomObject]@{MainAlgorithm = "fugue256"; Params = ""} #Fugue256
-    #[PSCustomObject]@{MainAlgorithm = "groestl"; Params = ""} #Groestl
-    #[PSCustomObject]@{MainAlgorithm = "jackpot"; Params = ""} #Jackpot
-    #[PSCustomObject]@{MainAlgorithm = "keccak"; Params = ""} #Keccak
-    #[PSCustomObject]@{MainAlgorithm = "luffa"; Params = ""} #Luffa
-    #[PSCustomObject]@{MainAlgorithm = "lyra2v2"; Params = ""} #Lyra2RE2
-    #[PSCustomObject]@{MainAlgorithm = "lyra2z"; Params = "-N 1"} #lyra2z
-    [PSCustomObject]@{MainAlgorithm = "neoscrypt"; Params = "-N 1"} #NeoScrypt
-    #[PSCustomObject]@{MainAlgorithm = "penta"; Params = ""} #Pentablake
-    #[PSCustomObject]@{MainAlgorithm = "skein"; Params = ""} #Skein
-    #[PSCustomObject]@{MainAlgorithm = "s3"; Params = ""} #S3
-    #[PSCustomObject]@{MainAlgorithm = "tribus"; Params = ""} #Tribus
-    #[PSCustomObject]@{MainAlgorithm = "veltor"; Params = ""} #Veltor
-    #[PSCustomObject]@{MainAlgorithm = "whirlpool"; Params = ""} #Whirlpool
-    #[PSCustomObject]@{MainAlgorithm = "whirlpoolx"; Params = ""} #whirlpoolx
-    #[PSCustomObject]@{MainAlgorithm = "X17"; Params = ""} #X17 Verge
     [PSCustomObject]@{MainAlgorithm = "yescrypt"; Params = ""} #yescrypt
     [PSCustomObject]@{MainAlgorithm = "yescryptR8"; Params = ""}, #YesctyptR8
     [PSCustomObject]@{MainAlgorithm = "yescryptR16"; Params = ""} #YescryptR16 #Yenten
     [PSCustomObject]@{MainAlgorithm = "yescryptR16v2"; Params = ""} #PPN
     [PSCustomObject]@{MainAlgorithm = "yescryptR24"; Params = ""} #YescryptR24
-    [PSCustomObject]@{MainAlgorithm = "yescryptR32"; Params = ""; ExtendInterval = 2; Penalty = 0.86} #YescryptR32
- 
-    # ASIC - never profitable 20/04/2018
-    #[PSCustomObject]@{MainAlgorithm = "blake"; Params = ""} #blake
-    #[PSCustomObject]@{MainAlgorithm = "blakecoin"; Params = ""} #Blakecoin
-    #[PSCustomObject]@{MainAlgorithm = "blake2s"; Params = ""} #Blake2s
-    #[PSCustomObject]@{MainAlgorithm = "myr-gr"; Params = ""} #MyriadGroestl
-    #[PSCustomObject]@{MainAlgorithm = "nist5"; Params = ""} #Nist5
-    #[PSCustomObject]@{MainAlgorithm = "quark"; Params = ""} #Quark
-    #[PSCustomObject]@{MainAlgorithm = "qubit"; Params = ""} #Qubit
-    #[PSCustomObject]@{MainAlgorithm = "vanilla"; Params = ""} #BlakeVanilla
-    #[PSCustomObject]@{MainAlgorithm = "sha256d"; Params = ""} #sha256d
-    #[PSCustomObject]@{MainAlgorithm = "sia"; Params = ""} #SiaCoin
-    #[PSCustomObject]@{MainAlgorithm = "x11"; Params = ""} #X11
-    #[PSCustomObject]@{MainAlgorithm = "x13"; Params = ""} #x13
-    #[PSCustomObject]@{MainAlgorithm = "x14"; Params = ""} #x14
-    #[PSCustomObject]@{MainAlgorithm = "x15"; Params = ""} #x15
+    [PSCustomObject]@{MainAlgorithm = "yescryptR32"; Params = ""; ExtendInterval = 2; Penalty = 1} #YescryptR32
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -68,7 +36,7 @@ if ($InfoOnly) {
         Name      = $Name
         Path      = $Path
         Port      = $Miner_Port
-        Uri       = $Uri
+        Uri       = $UriCuda.Uri
         DevFee    = $DevFee
         ManualUri = $ManualUri
         Commands  = $Commands
@@ -76,7 +44,14 @@ if ($InfoOnly) {
     return
 }
 
-if (-not (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name)) {return}
+$Uri = ""
+for($i=0;$i -le $UriCuda.Count -and -not $Uri;$i++) {
+    if (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $UriCuda[$i].Cuda -Warning $(if ($i -lt $UriCuda.Count-1) {""}else{$Name})) {
+        $Uri = $UriCuda[$i].Uri
+        $Cuda= $UriCuda[$i].Cuda
+    }
+}
+if (-not $Uri) {return}
 
 $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
     $Miner_Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
