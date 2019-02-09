5c95817feff6d0c2b22101348f73780a32956fe3
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-02-09 12:44:14 +0100

    Update miner TTminer to v2.1.9 (nvidia)
       - new algo Myriad-Groestl (Myriad, DigiByte, Verge, etc)
       - fixes a 'duplicate share' bug on very fast systems
       - add new command line options (-PRT & -PRS) to control performance information output on the screen
       - add core & mem clock information to the performance report
    
    Sample for the new options:
    -PRT INT      Performance-Report & information after INT multiple of 15 seconds
             Set INT to 0 to disable output after a fixed timeframe
             sample -RPT 24 shows the performance report after 24 * 5 sec = 2 minutes
             Default: -PRT 3
    
    -PRS INT      Performance-Report & information after a INT shares found
             Set INT to 0 to disable output after a fixed number of shares
             sample -RPS 10 shows the performance report after 10 shares were found
             Default: -PRS 0
    
             Mixed sample:
                - You want to see the performance report all 25 shares and all 30 secs:
                   -PRS 25 -PRT 6
    
                - You do not want to see any performance report:
                   -PRT 0

--- Miners/TTminer.ps1
+++ Miners/TTminer.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\NVIDIA-TTminer\TT-Miner.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.1.8-ttminer/TT-Miner-2.1.8.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.1.9-ttminer/TT-Miner-2.1.9.zip"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5025783.0"
 $Port = "333{0:d2}"
 $DevFee = 1.0
@@ -18,7 +18,9 @@ $Commands = [PSCustomObject[]]@(
     #[PSCustomObject]@{MainAlgorithm = "ETHASH2gb"  ; MinMemGB = 2; Params = ""} #Ethash2GB 
     #[PSCustomObject]@{MainAlgorithm = "ETHASH3gb"  ; MinMemGB = 3; Params = ""} #Ethash3GB 
     #[PSCustomObject]@{MainAlgorithm = "ETHASH"     ; MinMemGB = 4; Params = ""} #Ethash 
-    [PSCustomObject]@{MainAlgorithm = "MTP"        ; MinMemGB = 6; Params = ""} #MTP 
+    #[PSCustomObject]@{MainAlgorithm = "LYRA2V3"    ; MinMemGB = 4; Params = ""} #LYRA2V3
+    [PSCustomObject]@{MainAlgorithm = "MTP"        ; MinMemGB = 6; Params = ""} #MTP
+    [PSCustomObject]@{MainAlgorithm = "MYRGR"      ; MinMemGB = 2; Params = ""} #MYRGR    
     [PSCustomObject]@{MainAlgorithm = "PROGPOW2gb" ; MinMemGB = 2; Params = ""; ExtendInterval = 2} #ProgPoW2gb 
     [PSCustomObject]@{MainAlgorithm = "PROGPOW3gb" ; MinMemGB = 3; Params = ""; ExtendInterval = 2} #ProgPoW3gb 
     [PSCustomObject]@{MainAlgorithm = "PROGPOW"    ; MinMemGB = 4; Params = ""; ExtendInterval = 2} #ProgPoW 
