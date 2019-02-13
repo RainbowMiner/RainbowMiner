b451d0cd3f3ea801a3ddc1314de99d4d14f58490
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-02-13 11:18:11 +0100

    Update miner Wildrig to v0.15.3.8 beta (amd)
    tuned auto parameters for Vega, fixed support for Bonaire and Tonga

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\AMD-WildRig\wildrig.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.3.7b-wildrig/wildrig-multi-windows-0.15.3.7-beta.7z"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.3.8b-wildrig/wildrig-multi-windows-0.15.3.8-beta.7z"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 1.0
