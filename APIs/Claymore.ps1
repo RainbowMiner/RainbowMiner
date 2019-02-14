01cb439c026b4438a75ce0e5a1adb17aa0a749fc
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-02-14 15:07:44 +0100

    Update API Claymore
    - fix ethminer 0.17.0

--- APIs/Claymore.ps1
+++ APIs/Claymore.ps1
@@ -21,12 +21,11 @@ class Claymore : Miner {
             return @($Request, $Response)
         }
 
+        $HashRate_Name = [String]$this.Algorithm[0]
+
         $Accepted_Shares = [Int64]($Data.result[2] -split ";")[1]
         $Rejected_Shares = [Int64]($Data.result[2] -split ";")[2]
-
-        $HashRate_Name = [String]($this.Algorithm -like (Get-Algorithm ($Data.result[0] -split " - ")[1]))
-        if (-not $HashRate_Name) {$HashRate_Name = [String]($this.Algorithm -like "$(Get-Algorithm ($Data.result[0] -split " - ")[1])*")} #temp fix
-        if (-not $HashRate_Name) {$HashRate_Name = [String]$this.Algorithm[0]}
+        
         $HashRate_Value = [Double]($Data.result[2] -split ";")[0]
         if ($this.Algorithm -like "ethash*" -and $Data.result[0] -notmatch "^TT-Miner") {$HashRate_Value *= 1000}
         if ($this.Algorithm -like "progpow*" -and $Data.result[0] -notmatch "^TT-Miner") {$HashRate_Value *= 1000}
@@ -39,18 +38,22 @@ class Claymore : Miner {
             $this.UpdateShares($HashRate_Name,$Accepted_Shares,$Rejected_Shares)
         }
 
-        $Accepted_Shares = [Int64]($Data.result[4] -split ";")[1]
-        $Rejected_Shares = [Int64]($Data.result[4] -split ";")[2]
+        if ($this.Algorithm[1]) {
+            $HashRate_Name = [String]$this.Algorithm[1]
 
-        $HashRate_Name = if ($HashRate_Name) {[String]($this.Algorithm -notlike $HashRate_Name)}
-        $HashRate_Value = [Double]($Data.result[4] -split ";")[0]
-        if ($this.Algorithm -like "ethash*" -and $Data.result[0] -notmatch "^TT-Miner") {$HashRate_Value *= 1000}
-        if ($this.Algorithm -like "progpow*" -and $Data.result[0] -notmatch "^TT-Miner") {$HashRate_Value *= 1000}
-        if ($this.Algorithm -eq "neoscrypt") {$HashRate_Value *= 1000}
+            $Accepted_Shares = [Int64]($Data.result[4] -split ";")[1]
+            $Rejected_Shares = [Int64]($Data.result[4] -split ";")[2]
 
-        if ($HashRate_Name -and $HashRate_Value -gt 0) {
-            $HashRate | Add-Member @{$HashRate_Name = $HashRate_Value}
-            $this.UpdateShares($HashRate_Name,$Accepted_Shares,$Rejected_Shares)
+            $HashRate_Name = if ($HashRate_Name) {[String]($this.Algorithm -notlike $HashRate_Name)}
+            $HashRate_Value = [Double]($Data.result[4] -split ";")[0]
+            if ($this.Algorithm -like "ethash*" -and $Data.result[0] -notmatch "^TT-Miner") {$HashRate_Value *= 1000}
+            if ($this.Algorithm -like "progpow*" -and $Data.result[0] -notmatch "^TT-Miner") {$HashRate_Value *= 1000}
+            if ($this.Algorithm -eq "neoscrypt") {$HashRate_Value *= 1000}
+
+            if ($HashRate_Name -and $HashRate_Value -gt 0) {
+                $HashRate | Add-Member @{$HashRate_Name = $HashRate_Value}
+                $this.UpdateShares($HashRate_Name,$Accepted_Shares,$Rejected_Shares)
+            }
         }
 
         $this.AddMinerData([PSCustomObject]@{
