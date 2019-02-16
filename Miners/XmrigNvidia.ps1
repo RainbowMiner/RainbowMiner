3ba1908bd9b6b22212f786d66c8640e49f67ea9e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-02-16 12:13:57 +0100

    Update miner XmrigNvidia to v2.12.0-beta (nvidia)
    - add cryptonight/wow

--- Miners/XmrigNvidia.ps1
+++ Miners/XmrigNvidia.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\NVIDIA-Xmrig\xmrig-nvidia.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.11.0-xmrig/xmrig-nvidia-2.11.0-msvc-win64-rbm.7z"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.12.0-xmrig/xmrig-nvidia-2.12.0b-msvc-win64-rbm.7z"
 $ManualUri = "https://github.com/xmrig/xmrig-nvidia/releases"
 $Port = "303{0:d2}"
 $DevFee = 0.0
@@ -15,21 +15,22 @@ $Cuda = "9.2"
 if (-not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{MainAlgorithm = "cryptonight/1";          Params = ""}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight/2";          Params = "--bfactor=12"}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight/gpu";        Params = ""}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight/half";       Params = ""}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight/fast";       Params = ""; Algorithm = "cryptonight/msr"}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight/rto";        Params = ""}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight/xao";        Params = ""}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight/xtl";        Params = ""}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight-lite/0";     Params = ""}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight-lite/1";     Params = ""}
-    #[PSCustomObject]@{MainAlgorithm = "cryptonight-lite/ipbc";  Params = ""}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight-heavy";      Params = ""}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight-heavy/tube"; Params = ""}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight-heavy/xhv";  Params = ""}
-    [PSCustomObject]@{MainAlgorithm = "cryptonight-turtle";     Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight/1";          MinMemGb = 2; Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight/2";          MinMemGb = 2; Params = "--bfactor=12"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight/gpu";        MinMemGb = 4; Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight/half";       MinMemGb = 2; Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight/fast";       MinMemGb = 2; Params = ""; Algorithm = "cryptonight/msr"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight/r";          MinMemGb = 2; Params = ""; Algorithm = "cryptonight/wow"}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight/rto";        MinMemGb = 2; Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight/xao";        MinMemGb = 2; Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight/xtl";        MinMemGb = 2; Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight-lite/0";     MinMemGb = 1; Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight-lite/1";     MinMemGb = 1; Params = ""}
+    #[PSCustomObject]@{MainAlgorithm = "cryptonight-lite/ipbc";  MinMemGb = 2; Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight-heavy";      MinMemGb = 4; Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight-heavy/tube"; MinMemGb = 4; Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight-heavy/xhv";  MinMemGb = 4; Params = ""}
+    [PSCustomObject]@{MainAlgorithm = "cryptonight-turtle";     MinMemGb = 4; Params = ""}
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -51,19 +52,23 @@ if ($InfoOnly) {
 if (-not (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name)) {return}
 
 $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
-    $Miner_Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
-    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
     $Miner_Model = $_.Model
-    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
-
-    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
 
     $Commands | ForEach-Object {
-        
         $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+        $MinMemGb = $_.MinMemGb
+        $Params = $_.Params
+
+        $Miner_Device = $Device | Where-Object {$_.OpenCL.GlobalMemsize -ge ($MinMemGb * 1gb)}
 
         if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+            $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+            $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+            $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+
+            $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
+
             $xmrig_algo = if ($_.Algorithm) {$_.Algorithm} else {$_.MainAlgorithm}
             $Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
             [PSCustomObject]@{
@@ -71,7 +76,7 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
                 DeviceName = $Miner_Device.Name
                 DeviceModel = $Miner_Model
                 Path      = $Path
-                Arguments = "-R 1 --cuda-devices=$($DeviceIDsAll) --api-port $($Miner_Port) -a $($xmrig_algo) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) --keepalive $(if ($Pools.$Algorithm_Norm.Name -eq "NiceHash") {"--nicehash"}) --donate-level=0 $($_.Params)"
+                Arguments = "-R 1 --cuda-devices=$($DeviceIDsAll) --api-port $($Miner_Port) -a $($xmrig_algo) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) --keepalive $(if ($Pools.$Algorithm_Norm.Name -eq "NiceHash") {"--nicehash"}) --donate-level=0 $($Params)"
                 HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
                 API       = "XMRig"
                 Port      = $Miner_Port
