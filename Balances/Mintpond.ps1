a5bad50a88607ff2c4f5f321c275260879e030d5
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-02-16 22:12:56 +0100

    Add balances for Mintpond

---
+++ Balances/Mintpond.ps1
@@ -0,0 +1,41 @@
+﻿param(
+    $Config
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Request = [PSCustomObject]@{}
+
+$Pools_Data = @(
+    [PSCustomObject]@{coin = "Zcoin"; algo = "MTP"; symbol = "XZC"; url = "zcoin"; port = 3000; fee = 0.9; ssl = $false; protocol = "stratum+tcp"}
+)
+
+$Count = 0
+$Pools_Data | Where-Object {$Config.Pools.$Name.Wallets."$($_.symbol)"} | Foreach-Object {
+    $Pool_Currency = $_.symbol
+    $Pool_Url = "https://api.mintpond.com/v1/$($_.url)"
+
+    try {
+        $Request = Invoke-RestMethodAsync "$($Pool_Url)/miner/balances/$($Config.Pools.$Name.Wallets.$Pool_Currency)" -delay $(if ($Count){500} else {0}) -cycletime ($Config.BalanceUpdateMinutes*60)
+        $Count++
+        if (-not $Request.miner.balances) {
+            Write-Log -Level Info "Pool Balance API ($Name) for $($_.Name) returned nothing. "            
+        } else {
+            [PSCustomObject]@{
+                Caption     = "$($Name) ($($_.Name))"
+                Currency    = $Pool_Currency
+                Balance     = [double]$Request.miner.balances.confirmed
+                Pending     = [double]$Request.miner.balances.unconfirmed
+                Total       = [double]$Request.miner.balances.confirmed + [double]$Request.miner.balances.unconfirmed
+                Payed       = [double]$Request.miner.balances.paid
+                Earned      = 0
+                Payouts     = @()
+                LastUpdated = (Get-Date).ToUniversalTime()
+            }
+        }
+    }
+    catch {
+        if ($Error.Count){$Error.RemoveAt(0)}
+        Write-Log -Level Warn "Pool Balance API ($Name) for $($_.Name) has failed. "
+    }
+}
