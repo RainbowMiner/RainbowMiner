8fe04690438d282dec5a5bbd10b174347b01d18e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-02-22 11:49:26 +0100

    Update miner CcminerMTP to v1.1.15 (nvidia)
    VC++2015 cuda 10.0
    Supports Windows 7,8,10
    
    Supports NiceHash MTP
    Reduce Job not found rejects on yiimp based pools
    add Per gpu statistics EWBF style
    Display gpu model on startup
    add compute support (compute_75,compute_61,compute_60,compute_52,compute_50,compute_35)
    increase default intensity small speed increase
    
    Thank you nemo!

--- Miners/CcminerMtp.ps1
+++ Miners/CcminerMtp.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\NVIDIA-CcminerMTP\ccminer.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1.13-ccminermtp/ccminermtp-1.1.13p.7z"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1.15-ccminermtp/ccminermtp-v1.1.15.7z"
 $ManualUri = "https://github.com/zcoinofficial/ccminer/releases"
 $Port = "126{0:d2}"
 $DevFee = 0.0
@@ -57,7 +57,7 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
                 Name = $Miner_Name
                 DeviceName = $Miner_Device.Name
                 DeviceModel = $Miner_Model
-                Path = "$(if ($Pools.$Algorithm_Norm.Name -eq "Nicehash") {$Path -replace "ccminer.exe","ccminer-nh.exe"} else {$Path})"
+                Path = $Path
                 Arguments = "-R 1 -b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) $($_.Params)"
                 HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
                 API = "Ccminer"
