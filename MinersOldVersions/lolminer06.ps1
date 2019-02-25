99f3abb0243e03ce33787fb9ecd0d02e6466d52e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-02-25 15:59:04 +0100

    Update miner lolMiner to v0.7 (amd,nvidia)
    - Added support for Beam (BEAM, modified Equihash 150/5), only tuned for AMD cards
    - Added TLS support for stratum. The default is off for all coins except Beam but on for Beam. This adds a new parameter --tls to control TLS on / off (see manual on usage)
    - Some bugfixes and reworks in the stratum code. It is more stable now
    - Complete rework of the mining back end. Lower CPU load for AMD graphic cards.
    - GPU sorting changed. Its now sorted by PCIE bus address. This addresses are also shown at startup and in API.
    - Integrated all kernel files to the executable
    - Lowered fee of ALL algorithms / coins to 1%
    - Added --help parameter (needs formating)
    - API update interval is now fixed to 10 seconds
    - API now smooths the performance data over approx one minute
    
    Coin specific changes:
    - Removed workbatch parameter for MNX, its obsolete now
    - Removed 96/5 Nvidia specific kernel
    - Changed Safecoin (SAFE) from Equihash 144/5 to 192/7
    - Added Vidulum (VDL), Equihash 192/7

--- MinersOldVersions/lolminer06.ps1
+++ MinersOldVersions/lolminer06.ps1
@@ -5,8 +5,8 @@ param(
     [Bool]$InfoOnly
 )
 
-$Path = ".\Bin\Equihash-lolMiner07\lolMiner.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7alpha5-lolminer/lolMiner_v07_alpha5_Win64.zip"
+$Path = ".\Bin\Equihash-lolMiner06\lolMiner.exe"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.6-lolminer/lolMiner_v06_Win64.zip"
 $ManualUri = "https://bitcointalk.org/index.php?topic=4724735.0"
 $Port = "336{0:d2}"
 $Cuda = "10.0"
@@ -15,11 +15,11 @@ $DevFee = 2.0
 if (-not $Session.DevicesByTypes.NVIDIA -and -not $Session.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No GPU present in system
 
 $Commands = [PSCustomObject[]]@(
-    #[PSCustomObject]@{MainAlgorithm = "Equihash16x5"; MinMemGB = 1; Params = "--coin MNX --workbatch VERYHIGH"; Fee=1}  #Equihash 96,5
-    #[PSCustomObject]@{MainAlgorithm = "Equihash21x9"; MinMemGB = 1; Params = "--coin AION"; Fee=2} #Equihash 210,9
-    #[PSCustomObject]@{MainAlgorithm = "Equihash24x5"; MinMemGB = 2; Params = "--coin AUTO144_5"; Fee=1.5} #Equihash 144,5
-    #[PSCustomObject]@{MainAlgorithm = "Equihash24x7"; MinMemGB = 3; Params = "--coin AUTO192_7"; Fee=2} #Equihash 192,7
-    [PSCustomObject]@{MainAlgorithm = "Equihash25x5"; MinMemGB = 4; Params = "--coin BEAM"; Fee=2} #Equihash 150,5
+    [PSCustomObject]@{MainAlgorithm = "Equihash16x5"; MinMemGB = 1; Params = "--coin MNX --workbatch VERYHIGH"; Fee=1}  #Equihash 96,5
+    [PSCustomObject]@{MainAlgorithm = "Equihash21x9"; MinMemGB = 1; Params = "--coin AION"; Fee=2} #Equihash 210,9
+    [PSCustomObject]@{MainAlgorithm = "Equihash24x5"; MinMemGB = 2; Params = "--coin AUTO144_5"; Fee=1.5} #Equihash 144,5
+    [PSCustomObject]@{MainAlgorithm = "Equihash24x7"; MinMemGB = 3; Params = "--coin AUTO192_7"; Fee=2} #Equihash 192,7
+    #[PSCustomObject]@{MainAlgorithm = "Equihash25x5"; MinMemGB = 4; Params = "--coin BEAM"; Fee=2} #Equihash 150,5
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -51,7 +51,7 @@ foreach ($Miner_Vendor in @("AMD","NVIDIA")) {
             $Miner_Device = $Device | Where-Object {$_.OpenCL.GlobalMemsize -ge ($MinMemGB * 1gb)}
 
 			foreach($Algorithm_Norm in @($Algorithm_Norm,"$($Algorithm_Norm)-$($Miner_Model)")) {
-				if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+				if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and ($Algorithm_Norm -notmatch "Equihash25x5" -or $Pools.$Algorithm_Norm.Name -ne "Nicehash")) {
 					$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
 					$Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
 					$Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
@@ -62,7 +62,7 @@ foreach ($Miner_Vendor in @("AMD","NVIDIA")) {
 						DeviceName  = $Miner_Device.Name
 						DeviceModel = $Miner_Model
 						Path        = $Path
-						Arguments   = "--pool $($Pools.$Algorithm_Norm.Host) --port $($Pool_Port) --user $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" --pass $($Pools.$Algorithm_Norm.Pass)"}) --devices $($Miner_Device.Type_Vendor_Index -join ',') --apiport $($Miner_Port) --tls $(if ($Pools.$Algorithm_Norm.SSL) {1} else {0}) --digits 2 --longstats 60 --shortstats 5 --connectattempts 3 $($_.Params)"
+						Arguments   = "--pool $($Pools.$Algorithm_Norm.Host) --port $($Pool_Port) --user $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" --pass $($Pools.$Algorithm_Norm.Pass)"}) --devices $($Miner_Device.Type_Vendor_Index -join ',') --apiport $($Miner_Port) --digits 2 --longstats 60 --shortstats 5 --connectattempts 3 $($_.Params)"
 						HashRates   = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week}
 						API         = "Lol"
 						Port        = $Miner_Port
