b03131fa7aa6873e7fe5283ab12d900a6730f522
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-02-26 13:06:38 +0100

    Update miner CcminerMTP (nvidia)
    - give this miner more miningpriority (set to high)

--- Miners/CcminerMtp.ps1
+++ Miners/CcminerMtp.ps1
@@ -55,19 +55,20 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
 			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
 				$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
 				[PSCustomObject]@{
-					Name = $Miner_Name
-					DeviceName = $Miner_Device.Name
-					DeviceModel = $Miner_Model
-					Path = "$(if ($Pools.$Algorithm_Norm.Name -eq "NiceHash") {$Path -replace "ccminer.exe","ccminer-nh.exe"} else {$Path})"
-					Arguments = "-R 1 -N 3 -b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) $($_.Params)"
-					HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week}
-					API = "Ccminer"
-					Port = $Miner_Port
-					Uri = $Uri
-					DevFee = 0.0
+					Name           = $Miner_Name
+					DeviceName     = $Miner_Device.Name
+					DeviceModel    = $Miner_Model
+					Path           = "$(if ($Pools.$Algorithm_Norm.Name -eq "NiceHash") {$Path -replace "ccminer.exe","ccminer-nh.exe"} else {$Path})"
+					Arguments      = "-R 1 -N 3 -b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) $($_.Params)"
+					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week}
+					API            = "Ccminer"
+					Port           = $Miner_Port
+					Uri            = $Uri
+					DevFee         = 0.0
 					FaultTolerance = $_.FaultTolerance
 					ExtendInterval = $_.ExtendInterval
-					ManualUri = $ManualUri
+					ManualUri      = $ManualUri
+                    MiningPriority = 2
 				}
 			}
 		}
