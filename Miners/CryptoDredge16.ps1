be843d770bed38100ca9afee8e344f92ca9295c9
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-03-04 18:45:14 +0100

    Update miner CryptoDredge
    - re-add Exosis for ZergPool

---
+++ Miners/CryptoDredge16.ps1
@@ -0,0 +1,95 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+$Path = ".\Bin\NVIDIA-CryptoDredge16\CryptoDredge.exe"
+$ManualUri = "https://bitcointalk.org/index.php?topic=4807821"
+$Port = "338{0:d2}"
+$DevFee = 1.0
+
+$UriCuda = @(
+    [PSCustomObject]@{
+        Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.16.0-cryptodredge/CryptoDredge_0.16.0_cuda_10.0_windows.zip"
+        Cuda = "10.0"
+    },
+    [PSCustomObject]@{
+        Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.16.0-cryptodredge/CryptoDredge_0.16.0_cuda_9.2_windows.zip"
+        Cuda = "9.2"
+    }
+)
+
+if (-not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "exosis";    MinMemGb = 1; Params = ""} #Exosis
+)
+
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("NVIDIA")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $UriCuda.Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Uri = ""
+for($i=0;$i -le $UriCuda.Count -and -not $Uri;$i++) {
+    if (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $UriCuda[$i].Cuda -Warning $(if ($i -lt $UriCuda.Count-1) {""}else{$Name})) {
+        $Uri = $UriCuda[$i].Uri
+        $Cuda= $UriCuda[$i].Cuda
+    }
+}
+if (-not $Uri) {return}
+
+$Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
+    $Miner_Model = $_.Model    
+
+    $Commands | ForEach-Object {
+        $MinMemGb = $_.MinMemGb
+        
+        $Miner_Device = $Device | Where-Object {$_.OpenCL.GlobalMemsize -ge ($MinMemGb * 1gb)}
+
+        $Algorithm = if ($_.Algorithm) {$_.Algorithm} else {$_.MainAlgorithm}
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+        
+		foreach($Algorithm_Norm in @($Algorithm_Norm,"$($Algorithm_Norm)-$($Miner_Model)")) {
+			if ($Pools.$Algorithm_Norm.Host -and ($Algorithm_Norm -notmatch "Cuckaroo29" -or $Pools.$Algorithm_Norm.Name -ne "NiceHash") -and $Miner_Device) {
+				$Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+				$Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+				$Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+				$DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
+
+				$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
+				[PSCustomObject]@{
+					Name = $Miner_Name
+					DeviceName = $Miner_Device.Name
+					DeviceModel = $Miner_Model
+					Path = $Path
+					Arguments = "-r 10 -R 1 -b 127.0.0.1:$($Miner_Port) -d $($DeviceIDsAll) -a $($Algorithm) --no-watchdog -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) --log log_$($Miner_Port).txt $($_.Params) --no-nvml"
+					HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week}
+					API = "Ccminer"
+					Port = $Miner_Port
+					Uri = $Uri
+					FaultTolerance = $_.FaultTolerance
+					ExtendInterval = $_.ExtendInterval
+					DevFee = if ($_.DevFee -ne $null) {$_.DevFee} else {$DevFee}
+					ManualUri = $ManualUri
+				}
+			}
+		}
+    }
+}
