3d65c41eb89e231e3b7604354851207511cc9dc9
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-03-10 18:26:41 +0100

    Add miner RhminerCpu v1.4 (cpu)
    - mining randomhash (f2pool and nanopool) on CPU

---
+++ APIs/RH.ps1
@@ -0,0 +1,45 @@
+﻿using module ..\Include.psm1
+
+class RH : Miner {
+    [String[]]UpdateMinerData () {
+        if ($this.GetStatus() -ne [MinerStatus]::Running) {return @()}
+
+        $Server = "localhost"
+        $Timeout = 10 #seconds
+
+        $Request = " "
+        $Response = ""
+
+        $HashRate = [PSCustomObject]@{}
+
+        try {
+            $Response = Invoke-TcpRequest $Server $this.Port $Request $Timeout -ErrorAction Stop
+            $Data = $Response | ConvertFrom-Json -ErrorAction Stop
+        }
+        catch {
+            Write-Log -Level Error "Failed to connect to miner ($($this.Name)). "
+            return @($Request, $Response)
+        }
+
+        $Accepted_Shares = [Double]($Data.infos.accepted | Measure-Object -Sum).Sum
+        $Rejected_Shares = [Double]($Data.infos.rejected | Measure-Object -Sum).Sum
+
+        $HashRate_Name = [String]$this.Algorithm[0]
+        $HashRate_Value = [Double]($Data.infos.speed | Measure-Object -Sum).Sum
+        
+        if ($HashRate_Name -and $HashRate_Value -gt 0) {
+            $HashRate | Add-Member @{$HashRate_Name = $HashRate_Value}
+            $this.UpdateShares(0,$Accepted_Shares,$Rejected_Shares)
+        }
+
+        $this.AddMinerData([PSCustomObject]@{
+            Raw      = $Response
+            HashRate = $HashRate
+            Device   = @()
+        })
+
+        $this.CleanupMinerData()
+
+        return @($Request, $Data | ConvertTo-Json -Compress)
+    }
+}
---
+++ Miners/RhminerCpu.ps1
@@ -0,0 +1,71 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+$Path = ".\Bin\CPU-RHminer\rhminer.exe"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.4-rhminer/rhminer.1.4.Windows.CPU.7z"
+$ManualUri = "https://github.com/polyminer1/rhminer/releases"
+$Port = "533{0:d2}"
+$DevFee = 1.0
+
+if (-not $Session.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No CPU present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "randomhash"; Params = ""; ExtendInterval = 2} #RandomHash/PASCcoin
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("CPU")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Session.DevicesByTypes.CPU | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Session.DevicesByTypes.CPU | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    #$Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+    $Miner_Port = 4048
+
+    $DeviceParams = "$(if ($Session.Config.CPUMiningThreads){"-cputhreads $($Session.Config.CPUMiningThreads)"})"
+
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+
+		foreach($Algorithm_Norm in @($Algorithm_Norm,"$($Algorithm_Norm)-$($Miner_Model)")) {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+				[PSCustomObject]@{
+					Name = $Miner_Name
+					DeviceName = $Miner_Device.Name
+					DeviceModel = $Miner_Model
+					Path = $Path
+					Arguments = "-apiport $($Miner_Port) -s $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -su $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -pw $($Pools.$Algorithm_Norm.Pass)"}) -cpu $($DeviceParams) $($_.Params)"
+					HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week}
+					API = "RH"
+					Port = $Miner_Port
+					Uri = $Uri
+					FaultTolerance = $_.FaultTolerance
+					ExtendInterval = $_.ExtendInterval
+					DevFee = $DevFee
+					ManualUri = $ManualUri
+				}
+			}
+		}
+    }
+}
