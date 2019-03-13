e49fdd5cde639321edaf41fd0fbe5d8af7c5df3d
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-03-13 01:42:57 +0100

    Add miner Nanominer v1.1.0 (cpu)
    - RandomHash miner

---
+++ APIs/Nanominer.ps1
@@ -0,0 +1,74 @@
+﻿using module ..\Include.psm1
+
+class Nanominer : Miner {
+
+    [String]GetArguments() {
+        $Miner_Path = Split-Path $this.Path
+        $Parameters = $this.Arguments | ConvertFrom-Json
+        $ConfigFile = "config_$($this.Pool -join '-')-$($this.BaseAlgorithm -join '-')-$($this.DeviceModel)$(if ($Parameters.SSL){"-ssl"}).txt"
+
+        if (Test-Path $this.Path) {
+            "mport=-$($this.Port)
+webPort = 0
+Watchdog = false
+
+[$($Parameters.Algo)]
+wallet=$($Parameters.Wallet -split '\.' | Select-Object -First 1)
+paymentId=$(if ($pid = $Parameters.Wallet -split '\.' | Select-Object -Index 1) {$pid} else {0})
+rigName=$($Parameters.Worker)
+rigPassword=$($Parameters.Pass)
+email=$($Parameters.Email)
+pool1 = $($Parameters.Host):$($Parameters.Port)
+devices =
+$(if ($Parameters.Threads) {"cpuThreads = $($Parameters.Threads)"})
+" | Out-File "$($Miner_Path)\$($ConfigFile)" -Encoding utf8
+        }
+
+        return $ConfigFile
+    }
+
+    [String[]]UpdateMinerData () {
+        if ($this.GetStatus() -ne [MinerStatus]::Running) {return @()}
+
+        $Server = "localhost"
+        $Timeout = 10 #seconds
+
+        $Request = @{id = 1; jsonrpc = "2.0"; method = "miner_getstat1"} | ConvertTo-Json -Compress
+        $Response = ""
+
+        $HashRate = [PSCustomObject]@{}
+
+        try {
+            $Response = Invoke-TcpRequest $Server $this.Port $Request $Timeout -ErrorAction Stop -Quiet
+            $Data = $Response | ConvertFrom-Json -ErrorAction Stop
+        }
+        catch {
+            Write-Log -Level Info "Failed to connect to miner ($($this.Name)). "
+            return @($Request, $Response)
+        }
+
+        $HashRate_Name = [String]$this.Algorithm[0]
+
+        $HashRate_Value = [Double]($Data.result[2] -split ";")[0]
+        $Accepted_Shares = [Int64]($Data.result[2] -split ";")[1]
+        $Rejected_Shares = [Int64]($Data.result[2] -split ";")[2]
+        $Accepted_Shares -= $Rejected_Shares
+
+        $HashRate_Value = [Int64]$HashRate_Value
+
+        if ($HashRate_Name -and $HashRate_Value -gt 0) {
+            $HashRate | Add-Member @{$HashRate_Name = $HashRate_Value}
+            $this.UpdateShares(0,$Accepted_Shares,$Rejected_Shares)
+        }
+
+        $this.AddMinerData([PSCustomObject]@{
+            Raw      = $Response
+            HashRate = $HashRate
+            Device   = @()
+        })
+
+        $this.CleanupMinerData()
+
+        return @($Request, $Data | ConvertTo-Json -Compress)
+    }
+}
---
+++ Miners/Nanominer.ps1
@@ -0,0 +1,85 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+$Path = ".\Bin\CPU-Nanominer\nanominer.exe"
+$URI = "https://github.com/nanopool/nanominer/releases/download/v1.1.0/nanominer-windows-1.1.0.zip"
+$ManualURI = "https://github.com/nanopool/nanominer/releases"
+$Port = "534{0:d2}"
+$DevFee = 3.0
+
+if (-not $Session.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No GPU present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "RandomHash"; Params = ""; ExtendInterval = 2} #RandomHash/PASCcoin
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("CPU")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Session.DevicesByTypes.CPU | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Session.DevicesByTypes.CPU | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+    #$Miner_Port = 4048
+
+    $DeviceParams = "$(if ($Session.Config.CPUMiningThreads){" -cputhreads $($Session.Config.CPUMiningThreads)"})$(if ($Session.Config.CPUMiningAffinity -ne ''){" -processorsaffinity $((ConvertFrom-CPUAffinity $Session.Config.CPUMiningAffinity) -join ",")"})"
+    
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+
+		foreach($Algorithm_Norm in @($Algorithm_Norm,"$($Algorithm_Norm)-$($Miner_Model)")) {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+
+				$Arguments = [PSCustomObject]@{
+                    Algo   = $_.MainAlgorithm
+					Host   = $Pools.$Algorithm_Norm.Host
+					Port   = $Pools.$Algorithm_Norm.Port
+					SSL    = $Pools.$Algorithm_Norm.SSL
+					Wallet = $Pools.$Algorithm_Norm.Wallet
+                    Worker = "{workername:$($Pools.$Algorithm_Norm.Worker)}"
+                    Pass   = $Pools.$Algorithm_Norm.Pass
+                    Email  = $Pools.$Algorithm_Norm.Email
+                    Threads= $Session.Config.CPUMiningThreads
+				}
+
+				[PSCustomObject]@{
+					Name           = $Miner_Name
+					DeviceName     = $Miner_Device.Name
+					DeviceModel    = $Miner_Model
+					Path           = $Path
+					Arguments      = $Arguments
+					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week}
+					API            = "Nanominer"
+					Port           = $Miner_Port
+					Uri            = $Uri
+					FaultTolerance = $_.FaultTolerance
+					ExtendInterval = $_.ExtendInterval
+					DevFee         = $DevFee
+					ManualUri      = $ManualUri
+                    MiningAffinity = $Session.Config.CPUMiningAffinity
+				}
+			}
+		}
+    }
+}
