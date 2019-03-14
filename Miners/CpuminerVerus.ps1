ee79f1b28aff0a18f451a3163d4aba4dd7aee12e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-03-15 00:03:59 +0100

    Add CpuminerVerus v3.3 (cpu)
    - cpu mining VerusHash

---
+++ Miners/CpuminerVerus.ps1
@@ -0,0 +1,69 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+$Path = ".\Bin\CPU-Verus\ccminer.exe"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.3-ccminerverus/CCMinerVerus-3.3CPU.7z"
+$ManualUri = "https://github.com/monkins1010/ccminer/releases"
+$Port = "535{0:d2}"
+$DevFee = 0.0
+
+if (-not $Session.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No CPU present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "verus"; Params = ""; ExtendInterval = 2} #VerusHash
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("CPU")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Session.DevicesByTypes.CPU | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Session.DevicesByTypes.CPU | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+    $DeviceParams = "$(if ($Session.Config.CPUMiningThreads){" -t $($Session.Config.CPUMiningThreads)"})$(if ($Session.Config.CPUMiningAffinity -ne ''){" --cpu-affinity $($Session.Config.CPUMiningAffinity)"})"
+
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+
+		foreach($Algorithm_Norm in @($Algorithm_Norm,"$($Algorithm_Norm)-$($Miner_Model)")) {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+				[PSCustomObject]@{
+					Name = $Miner_Name
+					DeviceName = $Miner_Device.Name
+					DeviceModel = $Miner_Model
+					Path = $Path
+					Arguments = "-N 1 -R 2 -r 10 -b $($Miner_Port) -a $($_.MainAlgorithm) -o stratum+tcp://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"})$($DeviceParams) $($_.Params)"
+					HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week}
+					API = "Ccminer"
+					Port = $Miner_Port
+					Uri = $Uri
+					FaultTolerance = $_.FaultTolerance
+					ExtendInterval = $_.ExtendInterval
+					DevFee = $DevFee
+					ManualUri = $ManualUri
+				}
+			}
+		}
+    }
+}
