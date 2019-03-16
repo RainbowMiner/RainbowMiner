8cc1b5df84875a8277d2aa73989d3ff961b6199d
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-03-16 16:59:05 +0100

    Add balance for pool HashPool

---
+++ Balances/Hashpool.ps1
@@ -0,0 +1,59 @@
+﻿param(
+    $Config
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Request = [PSCustomObject]@{}
+
+$Payout_Currencies = $Config.Pools.$Name.Wallets.PSObject.Properties | Select-Object Name,Value -Unique | Sort-Object Name,Value
+
+if (-not $Payout_Currencies) {
+    Write-Log -Level Verbose "Cannot get balance on pool ($Name) - no wallet address specified. "
+    return
+}
+
+$PoolCoins_Request = [PSCustomObject]@{}
+try {
+    $PoolCoins_Request = Invoke-RestMethodAsync "https://hashpool.eu/api/currencies" -tag $Name -cycletime 120
+}
+catch {
+    if ($Error.Count){$Error.RemoveAt(0)}
+    Write-Log -Level Warn "Pool API ($Name) has failed. "
+    return
+}
+
+$Pool_Xlat = [PSCustomObject]@{
+    "DGBM" = "DGB"
+    "DGBQ" = "DGB"
+    "DGBS" = "DGB"
+    "DGBSK" = "DGB"
+    "XVGG" = "XVG"
+}
+
+$Count = 0
+$Payout_Currencies | Where-Object {@($PoolCoins_Request.PSObject.Properties | Foreach-Object {$Pool_CoinSymbol = $_.Name;if ($Pool_Xlat.$Pool_CoinSymbol) {$Pool_Xlat.$Pool_CoinSymbol} else {$Pool_CoinSymbol}} | Select-Object -Unique) -icontains $_.Name} | Foreach-Object {
+    try {
+        $Request = Invoke-RestMethodAsync "https://hashpool.eu/api/walletEx?address=$($_.Value)" -delay $(if ($Count){500} else {0}) -cycletime ($Config.BalanceUpdateMinutes*60)
+        $Count++
+        if (($Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Measure-Object Name).Count -le 1) {
+            Write-Log -Level Info "Pool Balance API ($Name) for $($_.Name) returned nothing. "
+        } else {
+            [PSCustomObject]@{
+                Caption     = "$($Name) ($($Request.currency))"
+                Currency    = $Request.currency
+                Balance     = $Request.balance
+                Pending     = $Request.unsold
+                Total       = $Request.unpaid
+                Payed       = $Request.total - $Request.unpaid
+                Earned      = $Request.total
+                Payouts     = @($Request.payouts | Select-Object)
+                LastUpdated = (Get-Date).ToUniversalTime()
+            }
+        }
+    }
+    catch {
+        if ($Error.Count){$Error.RemoveAt(0)}
+        Write-Log -Level Verbose "Pool Balance API ($Name) for $($_.Name) has failed. "
+    }
+}
