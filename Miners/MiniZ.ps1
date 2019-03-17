b0a6fa8c56481bae11892905cc8fce4019e15632
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-03-17 20:16:41 +0100

    Update miner MiniZ to v1.2l (nvidia)
    * Support for Equihash 192,7 (ZER, SAFE, GENX, VDL, ZERC).
     * miniZ GUI Launcher v1.0. (Windows)
     * Minor speed improvements for 144,5.
     * Fixed --intensity option. Express in percentage [0,100].
     * Other bug fixes.

--- Miners/MiniZ.ps1
+++ Miners/MiniZ.ps1
@@ -12,11 +12,11 @@ $DevFee = 2.0
 
 $UriCuda = @(
     [PSCustomObject]@{
-        Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1k-miniz/miniZ_v1.1k_cuda10_win-x64.zip"
+        Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.2l-miniz/miniZ_v1.2l_cuda10_win-x64.zip"
         Cuda = "10.0"
     },
     [PSCustomObject]@{
-        Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1k-miniz/miniZ_v1.1k_win-x64.zip"
+        Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.2l-miniz/miniZ_v1.2l_win-x64.zip"
         Cuda = "8.0"
     }
 )
@@ -26,6 +26,7 @@ if (-not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDI
 $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "Equihash16x5"; MinMemGB = 1; Params = "--par=96,5"}  #Equihash 96,5
     [PSCustomObject]@{MainAlgorithm = "Equihash24x5"; MinMemGB = 2; Params = "--par=144,5"} #Equihash 144,5
+    [PSCustomObject]@{MainAlgorithm = "Equihash24x7"; MinMemGB = 2; Params = "--par=192,7"} #Equihash 192,7
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
