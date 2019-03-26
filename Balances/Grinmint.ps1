50fb911462e0bd45e46fea6cffc57b741381aa8b
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-03-26 14:48:51 +0100

    Update balance for Grinmint
    - unified api

--- Balances/Grinmint.ps1
+++ Balances/Grinmint.ps1
@@ -12,33 +12,26 @@ if (!$PoolConfig.GRIN) {
 
 $Request = [PSCustomObject]@{}
 
-$Pools_Data = @(
-    [PSCustomObject]@{region = "eu"; host = "eu-west-api.grinmint.com"}
-    [PSCustomObject]@{region = "us"; host = "us-east-api.grinmint.com"}
-)
-
-$Pools_Data | Foreach-Object {
-    try {
-        if ($Request = Invoke-RestMethodAsync "https://$($_.host)/v1/user/$($PoolConfig.GRIN)/userStats" -cycletime ($Config.BalanceUpdateMinutes*60)) {
-            if ($Request.status) {
-                $Unpaid   = $Request.unpaid_balance / 1e9
-                $Immature = $Request.immature_balance / 1e9
-                [PSCustomObject]@{
-                        Caption     = "$($Name) $(Get-Region $_.region)"
-                        Info        = Get-Region $_.region
-                        Currency    = "GRIN"
-                        Balance     = $Unpaid
-                        Pending     = $Immature
-                        Total       = $Unpaid + $Immature
-                        Paid        = 0
-                        Earned      = 0
-                        Payouts     = @()
-                        LastUpdated = (Get-Date).ToUniversalTime()
-                }
+try {
+    if ($Request = Invoke-RestMethodAsync "https://api.grinmint.com/v1/user/$($PoolConfig.GRIN)/userStats" -cycletime ($Config.BalanceUpdateMinutes*60)) {
+        if ($Request.status) {
+            $Unpaid   = $Request.unpaid_balance / 1e9
+            $Immature = $Request.immature_balance / 1e9
+            [PSCustomObject]@{
+                    Caption     = $Name
+                    Info        = ""
+                    Currency    = "GRIN"
+                    Balance     = $Unpaid
+                    Pending     = $Immature
+                    Total       = $Unpaid + $Immature
+                    Paid        = 0
+                    Earned      = 0
+                    Payouts     = @()
+                    LastUpdated = (Get-Date).ToUniversalTime()
             }
         }
     }
-    catch {
-        if ($Error.Count){$Error.RemoveAt(0)}
-    }
+}
+catch {
+    if ($Error.Count){$Error.RemoveAt(0)}
 }
