fc9c0cafc8c7e8b53f557a8a1d1de3ae086933a3
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-03-28 11:55:12 +0100

    Update miner WildRigPreview to v0.15.4p11 (amd)
    385GH/s per RX570

--- Miners/WildRigPreview.ps1
+++ Miners/WildRigPreview.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\AMD-WildRigPreview\wildrig.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.4p10-wildrig/wildrig-multi-windows-0.15.4-preview10.7z"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.4p11-wildrig/wildrig-multi-windows-0.15.4-preview11.7z"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "412{0:d2}"
 $DevFee = 1.0
