b1a986209a09d5b962cbe857b1b3a7400ebb45e3
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-03-29 19:18:00 +0100

    Update miner WildRig to v0.15.4p12 (amd)
    up to 1.1TH/s per RX570 on rainforest(if use good CPU)

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\AMD-WildRig\wildrig.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.4p11-wildrig/wildrig-multi-windows-0.15.4-preview11.7z"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.4p12-wildrig/wildrig-multi-windows-0.15.4-preview12.7z"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 1.0
