2e6b91579c68b5a68297aace842ccd4b6ce3a10a
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-03-30 19:47:26 +0100

    Create FixDPS.bat

---
+++ FixDPS.bat
@@ -0,0 +1,35 @@
+﻿@echo off
+echo ... Set DPS service start type to manual ...
+echo.
+sc config DPS start= demand
+
+echo.
+echo ... Find PID of DPS service ...
+
+for /f "tokens=2 delims=[:]" %%f in ('sc queryex dps ^|find /i "PID"') do set PID=%%f
+
+echo.
+echo ... Kill DPS service
+echo.
+
+taskkill /f /pid %PID%
+
+
+echo.
+echo ... Delete sru Folder ...
+echo.
+
+rd /s "%windir%\system32\sru"
+
+echo.
+echo ... Set DPS service start type to auto ...
+echo.
+sc config DPS start= auto
+
+echo.
+echo ... Start DPS service ...
+
+sc start DPS
+echo.
+
+pause
