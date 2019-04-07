c28be578fe06e8af3775c10f9f70d0426c46b19c
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-04-07 19:20:13 +0200

    Update miner CcminerYescrypt
    - some more tolerance for YescryptR32 rejected shares. Specifically at startup.

--- Miners/CcminerYescrypt.ps1
+++ Miners/CcminerYescrypt.ps1
@@ -25,7 +25,7 @@ $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "yescryptR16"; Params = ""; ExtendInterval = 2; FaultTolerance = 0.3} #YescryptR16 #Yenten
     [PSCustomObject]@{MainAlgorithm = "yescryptR16v2"; Params = ""; ExtendInterval = 2; FaultTolerance = 0.3} #PPN
     [PSCustomObject]@{MainAlgorithm = "yescryptR24"; Params = ""; ExtendInterval = 2; FaultTolerance = 0.3} #YescryptR24
-    [PSCustomObject]@{MainAlgorithm = "yescryptR32"; Params = "-i 12.5"; ExtendInterval = 2; FaultTolerance = 0.3} #YescryptR32
+    [PSCustomObject]@{MainAlgorithm = "yescryptR32"; Params = "-i 12.5"; ExtendInterval = 2; FaultTolerance = 0.3; MaxRejectedShareRatio = 0.9} #YescryptR32
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -82,7 +82,7 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
 					ExtendInterval = $_.ExtendInterval
 					ManualUri      = $ManualUri
 					MiningPriority = 2
-					MaxRejectedShareRatio = 0.5
+					MaxRejectedShareRatio = if ($_.MaxRejectedShareRatio) {$_.MaxRejectedShareRatio} else {0.5}
 				}
 			}
 		}
