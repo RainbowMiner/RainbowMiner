22cd9009990794c01dfa162ac74f717dfc318235
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-04-11 15:36:01 +0200

    Update pool GrinMint
    - add Cuckatoo31
    - fix price calculation

--- Pools/Grinmint.ps1
+++ Pools/Grinmint.ps1
@@ -19,16 +19,14 @@ $Pool_Fee = 2.5
 
 if (-not $Wallets.$Pool_Currency -and -not $InfoOnly) {return}
 
-$Pool_Algorithm_Norm = Get-Algorithm "Cuckaroo29"
-
 $Pool_Request = [PSCustomObject]@{}
 $Pool_NetworkRequest = [PSCustomObject]@{}
 
 try {
     $Pool_Request = Invoke-RestMethodAsync "https://api.grinmint.com/v1/poolStats" -tag $Name -retry 3 -retrywait 1000 -cycletime 120
     if (-not $Pool_Request.status) {throw}
-    #$Pool_NetworkRequest = Invoke-RestMethodAsync "https://api.grinmint.com/v1/networkStats" -tag $Name -retry 3 -retrywait 1000 -delay 500 -cycletime 120
-    #if (-not $Pool_NetworkRequest.status) {throw}
+    $Pool_NetworkRequest = Invoke-RestMethodAsync "https://api.grinmint.com/v1/networkStats" -tag $Name -retry 3 -retrywait 1000 -delay 500 -cycletime 120
+    if (-not $Pool_NetworkRequest.status) {throw}
 }
 catch {
     if ($Error.Count){$Error.RemoveAt(0)}
@@ -40,27 +38,35 @@ catch {
 @("eu","us") | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
 
 $Pools_Data = @(
-    [PSCustomObject]@{port = 3416; region = "eu"; host = "eu-west-stratum.grinmint.com"; ssl = $false}
-    [PSCustomObject]@{port = 4416; region = "eu"; host = "eu-west-stratum.grinmint.com"; ssl = $true}
-    [PSCustomObject]@{port = 3416; region = "us"; host = "us-east-stratum.grinmint.com"; ssl = $false}
-    [PSCustomObject]@{port = 4416; region = "us"; host = "us-east-stratum.grinmint.com"; ssl = $true}
+    [PSCustomObject]@{algo = "Cuckaroo29"; port = 3416; region = "eu"; host = "eu-west-stratum.grinmint.com"; ssl = $false}
+    [PSCustomObject]@{algo = "Cuckaroo29"; port = 4416; region = "eu"; host = "eu-west-stratum.grinmint.com"; ssl = $true}
+    [PSCustomObject]@{algo = "Cuckaroo29"; port = 3416; region = "us"; host = "us-east-stratum.grinmint.com"; ssl = $false}
+    [PSCustomObject]@{algo = "Cuckaroo29"; port = 4416; region = "us"; host = "us-east-stratum.grinmint.com"; ssl = $true}
+    [PSCustomObject]@{algo = "Cuckatoo31"; port = 3416; region = "eu"; host = "eu-west-stratum.grinmint.com"; ssl = $false}
+    [PSCustomObject]@{algo = "Cuckatoo31"; port = 4416; region = "eu"; host = "eu-west-stratum.grinmint.com"; ssl = $true}
+    [PSCustomObject]@{algo = "Cuckatoo31"; port = 3416; region = "us"; host = "us-east-stratum.grinmint.com"; ssl = $false}
+    [PSCustomObject]@{algo = "Cuckatoo31"; port = 4416; region = "us"; host = "us-east-stratum.grinmint.com"; ssl = $true}
 )
+
+$reward = 60
+$diff   = $Pool_NetworkRequest.target_difficulty
+$PBR29  = (86400 / 42) * ($Pool_NetworkRequest.secondary_scaling/$diff)
+$PBR31  = (86400 / 42) * (7936/$diff) #31*2^8
 
 $lastBlock     = $Pool_Request.mined_blocks | Sort-Object height | Select-Object -last 1
 $Pool_BLK      = $Pool_Request.pool_stats.blocks_found_last_24_hours
 $Pool_TSL      = if ($lastBlock) {((Get-Date).ToUniversalTime() - (Get-Date $lastBlock.time).ToUniversalTime()).TotalSeconds}
-$reward        = 60
 $btcPrice      = if ($Session.Rates.$Pool_Currency) {1/[double]$Session.Rates.$Pool_Currency} else {0}
-$btcRewardLive = if ($Pool_Request.pool_stats.secondary_hashrate -gt 0) {$btcPrice * $reward * $Pool_BLK / $Pool_Request.pool_stats.secondary_hashrate} else {0}
-$Divisor       = 1
     
 if (-not $InfoOnly) {
-    $Stat = Set-Stat -Name "$($Name)_$($Pool_Currency)_Profit" -Value ($btcRewardLive/$Divisor) -Duration $StatSpan -ChangeDetection $true -HashRate $Pool_Request.pool_stats.secondary_hashrate -BlockRate $Pool_BLK
+    $Stat29 = Set-Stat -Name "$($Name)_$($Pool_Currency)29_Profit" -Value ($PBR29 * $reward * $btcPrice) -Duration $StatSpan -ChangeDetection $true -HashRate $Pool_Request.pool_stats.secondary_hashrate -BlockRate $Pool_BLK
+    $Stat31 = Set-Stat -Name "$($Name)_$($Pool_Currency)31_Profit" -Value ($PBR31 * $reward * $btcPrice) -Duration $StatSpan -ChangeDetection $true -HashRate $Pool_Request.pool_stats.primary_hashrate -BlockRate $Pool_BLK
 }
 
 $Pools_Data | ForEach-Object {
+    $Stat = if ($_.algo -eq "Cuckaroo29") {$Stat29} else {$Stat31}
     [PSCustomObject]@{
-        Algorithm     = $Pool_Algorithm_Norm
+        Algorithm     = Get-Algorithm $_.algo
         CoinName      = $Pool_Currency
         CoinSymbol    = $Pool_Currency
         Currency      = $Pool_Currency
