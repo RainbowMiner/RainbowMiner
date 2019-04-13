5ed53fd732508226b8a8238061b4b4aeaea28ae3
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-04-13 13:26:48 +0200

    Update pool Ravenminer
    - no more RavenminerEu

--- Pools/Ravenminer.ps1
+++ Pools/Ravenminer.ps1
@@ -16,81 +16,42 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 
 $Pool_Request = [PSCustomObject]@{}
 
-$Success = $true
 try {
-    if (-not ($Pool_Request = Invoke-RestMethodAsync "https://ravenminer.com/api/status" -tag $Name -cycletime 120)){throw}
+    $Pool_Request = Invoke-RestMethodAsync "https://ravenminer.com/api/status" -tag $Name -cycletime 120
 }
 catch {
     if ($Error.Count){$Error.RemoveAt(0)}
-    $Success = $false
-}
-
-if (-not $Success) {
-    $Success = $true
-    try {
-        $Pool_Request = Invoke-GetUrl "https://ravenminer.com/site/current_results" -method "WEB"
-        $Value_Group = ([regex]'data="([\d\.]+?)"').Matches($Pool_Request.Content).Groups | Where-Object Name -eq 1
-        if (-not ($Value = $Value_Group | Select-Object -Last 1 -ExpandProperty Value)){throw}
-        $Hashrate = $Value_Group | Select-Object -First 1 -ExpandProperty Value
-        $Pool_Request = [PSCustomObject]@{'x16r'=[PSCustomObject]@{actual_last24h = $Value;hashrate = $Hashrate;fees = 0.5;name = "x16r";port = 1111}}
-        $DataWindow = "actual_last24h"
-    }
-    catch {
-        if ($Error.Count){$Error.RemoveAt(0)}
-        $Success = $false
-    }
-}
-
-if (-not $Success) {
     Write-Log -Level Warn "Pool API ($Name) has failed. "
     return
 }
 
-if (($Pool_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Measure-Object Name).Count -lt 1) {
-    Write-Log -Level Warn "Pool API ($Name) returned nothing. "
-    return
-}
-
 try {
-    if (-not ($PoolCoins_Request = Invoke-RestMethodAsync "https://ravenminer.com/api/currencies" -tag $Name -cycletime 120)){throw}
+    $PoolCoins_Request = Invoke-RestMethodAsync "https://ravenminer.com/api/currencies" -tag $Name -cycletime 120
 }
 catch {
     if ($Error.Count){$Error.RemoveAt(0)}
-    $Success = $false
-}
-
-if (-not $Success) {
-    $Success = $true
-    try {
-        $PoolCoins_Request = Invoke-GetUrl "https://ravenminer.com/site/history_results" -method "WEB"
-        $Value_Content = $PoolCoins_Request.Content -split "</thead>" | Select-Object -Last 1
-        $Value_Content = $Value_Content -split "</tr>" | Select-Object -First 1        
-        $Value_Content = ([regex]">([\d\.]+?)<").Matches($Value_Content)
-        if ($Value_Content.Count -ge 2) {
-            $PoolCoins_Request = [PSCustomObject]@{RVN=[PSCustomObject]@{"24h_blocks" = $Value_Content[1].Groups[1].Value}}
-        }
-    }
-    catch {
-        if ($Error.Count){$Error.RemoveAt(0)}
-        $Success = $false
-    }
+    Write-Log -Level Info "Pool Currency API ($Name) has failed. "
 }
 
 $Pool_Coin = "Ravencoin"
 $Pool_Currency = "RVN"
 $Pool_Host = "ravenminer.com"
-$Pool_Region = Get-Region "us"
+
+[hashtable]$Pool_RegionsTable = @{}
+
+$Pool_Regions = @("us","eu","asia")
+$Pool_Regions | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
 
 $Pool_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | Where-Object {$Pool_Request.$_.actual_last24h -gt 0 -or $InfoOnly -or $AllowZero} | ForEach-Object {
     $Pool_Algorithm = $Pool_Request.$_.name
     $Pool_Algorithm_Norm = Get-Algorithm $Pool_Algorithm
     $Pool_PoolFee = [Double]$Pool_Request.$_.fees
     $Pool_User = $Wallets.$Pool_Currency
-    $Pool_Port = 6666
+    $Pool_Port = $Pool_Request.$_.port
 
-    $Pool_Factor = 1
+    $Pool_Factor = $Pool_Request.$_.mbtc_mh_factor
 
-    $Pool_TSL = if ($PoolCoins_Request) {$PoolCoins_Request.$Pool_CoinSymbol.timesincelast}else{$null}
+    $Pool_TSL = if ($PoolCoins_Request) {$PoolCoins_Request.$Pool_Currency.timesincelast}else{$null}
     $Pool_BLK = $PoolCoins_Request.$Pool_Currency."24h_blocks"
 
     if (-not $InfoOnly) {
@@ -102,29 +63,31 @@ $Pool_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select
     $Pool_Params = if ($Params.$Pool_Currency) {",$($Params.$Pool_Currency)"}
 
     if ($Pool_User -or $InfoOnly) {
-        [PSCustomObject]@{
-            Algorithm     = $Pool_Algorithm_Norm
-            CoinName      = $Pool_Coin
-            CoinSymbol    = $Pool_Currency
-            Currency      = $Pool_Currency
-            Price         = $Stat.$StatAverage #instead of .Live
-            StablePrice   = $Stat.Week
-            MarginOfError = $Stat.Week_Fluctuation
-            Protocol      = "stratum+tcp"
-            Host          = $Pool_Host
-            Port          = $Pool_Port
-            User          = $Pool_User
-            Pass          = "{workername:$Worker},c=$Pool_Currency{diff:,d=`$difficulty}$Pool_Params"
-            Region        = $Pool_Region
-            SSL           = $false
-            Updated       = $Stat.Updated
-            PoolFee       = $Pool_PoolFee
-            DataWindow    = $DataWindow
-            Workers       = $Pool_Request.$_.workers
-            Hashrate      = $Stat.HashRate_Live
-            BLK           = $Stat.BlockRate_Average
-            TSL           = $Pool_TSL
-            ErrorRatio    = $Stat.ErrorRatio_Average
+        foreach($Pool_Region in $Pool_Regions) {
+            [PSCustomObject]@{
+                Algorithm     = $Pool_Algorithm_Norm
+                CoinName      = $Pool_Coin
+                CoinSymbol    = $Pool_Currency
+                Currency      = $Pool_Currency
+                Price         = $Stat.$StatAverage #instead of .Live
+                StablePrice   = $Stat.Week
+                MarginOfError = $Stat.Week_Fluctuation
+                Protocol      = "stratum+tcp"
+                Host          = "$($Pool_Region).$($Pool_Host)"
+                Port          = $Pool_Port
+                User          = $Pool_User
+                Pass          = "{workername:$Worker},c=$Pool_Currency{diff:,d=`$difficulty}$Pool_Params"
+                Region        = $Pool_Regions.$Pool_Region
+                SSL           = $false
+                Updated       = $Stat.Updated
+                PoolFee       = $Pool_PoolFee
+                DataWindow    = $DataWindow
+                Workers       = $Pool_Request.$_.workers
+                Hashrate      = $Stat.HashRate_Live
+                BLK           = $Stat.BlockRate_Average
+                TSL           = $Pool_TSL
+                ErrorRatio    = $Stat.ErrorRatio_Average
+            }
         }
     }
 }
