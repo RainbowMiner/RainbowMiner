1d98613c5f24f0a65a0f0e28c52520a7773e3b86
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-04-14 19:14:41 +0200

    Update miner Srbminer to v1.8.3 (amd)
    - maxnosharesent parameter is not used anymore by default, also it's max is moved to 24h
    - In guided setup mode password can now be 120 characters long instead of 80 that was previously
    - Adl2 is now auto set to older gpus
    - Removed Webchain algo
    - heavy_mode 3 is now the default
    - Fixed stratum+ssl:// in pool url, now it recognises and sets tls to true if used
    - Gpu id and bus id now displayed with every accepted/rejected share
    - preparedata parameter now gives a small boost on Vegas for turtle, mox, heavy, gpu algos
    - Added fork info for sumokoin from cn to cn/r
    - Possible fix of random miner crash
    - SRB-Restarter now shows date and is logging to a file

--- Miners/SrbMiner.ps1
+++ Miners/SrbMiner.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $Path = ".\Bin\CryptoNight-SRBMiner\srbminer-cn.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.8.2-srbminer/SRBMiner-CN-V1-8-2.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.8.3-srbminer/SRBMiner-CN-V1-8-3.zip"
 $ManualUri = "https://bitcointalk.org/index.php?topic=3167363.0"
 $Port = "315{0:d2}"
 $DevFee = 0.85
@@ -44,7 +44,7 @@ $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "swap"       ; Threads = 2; MinMemGb = 2; Params = ""} # CryptoNight-Swap 2 thread
     [PSCustomObject]@{MainAlgorithm = "turtle"     ; Threads = 2; MinMemGb = 2; Params = ""} # CryptoNight-Turtle 2 thread
     [PSCustomObject]@{MainAlgorithm = "upx"        ; Threads = 2; MinMemGb = 1; Params = ""} # CryptoNight-Uplexa 2 threads
-    [PSCustomObject]@{MainAlgorithm = "webchain"   ; Threads = 2; MinMemGb = 1; Params = ""} # CryptoNight-Webchain 2 threads    
+    #[PSCustomObject]@{MainAlgorithm = "webchain"   ; Threads = 2; MinMemGb = 1; Params = ""} # CryptoNight-Webchain 2 threads    
     [PSCustomObject]@{MainAlgorithm = "xcash"      ; Threads = 2; MinMemGb = 2; Params = ""} # CryptoNight-HeavyX/Xcash 2 thread
     [PSCustomObject]@{MainAlgorithm = "zelerius"   ; Threads = 2; MinMemGb = 2; Params = ""} # CryptoNight-Zelerius thread
 )
