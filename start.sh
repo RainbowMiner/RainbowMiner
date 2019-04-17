ec454052ce03788f6df2cb54bfec34ac503a0bbe
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-04-17 14:43:05 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+
+command="& {./RainbowMiner.ps1 -configfile ./Config/config.txt; exit \$lastexitcode}"
+
+while true; do
+
+  pwsh -ExecutionPolicy bypass -Command ${command}
+
+  if [ "$?" != "99" ]; then
+    break
+  fi
+
+done
