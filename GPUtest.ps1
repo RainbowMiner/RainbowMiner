faef09e47eb78e56d3a9ee7f8d7284ffad03d21e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-04-20 16:29:45 +0200

    Add gputest for linux

--- GPUtest.ps1
+++ GPUtest.ps1
@@ -4,53 +4,89 @@ if ($MyInvocation.MyCommand.Path) {Set-Location (Split-Path $MyInvocation.MyComm
 
 $TestFileName = "gputestresult.txt"
 
-Start-Afterburner
+if ($IsWindows -eq $null) {
+    if ([System.Environment]::OSVersion.Platform -eq "Win32NT") {
+        $Global:IsWindows = $true
+        $Global:IsLinux = $false
+        $Global:IsMacOS = $false
+    }
+}
 
 "GPU-TEST $((Get-Date).ToUniversalTime())" | Out-File $TestFileName -Encoding utf8
 "="*80 | Out-File $TestFileName -Append
 " " | Out-File $TestFileName -Append
-"1. Afterburner NVIDIA" | Out-File $TestFileName -Append
-"-"*80 | Out-File $TestFileName -Append
-" " | Out-File $TestFileName -Append
 
-Get-AfterburnerDevices "NVIDIA" | Out-File $TestFileName -Encoding utf8 -Append
+if ($IsWindows) {
+    Start-Afterburner
 
-" " | Out-File $TestFileName -Append
-"2. Afterburner AMD" | Out-File $TestFileName -Append
-"-"*80 | Out-File $TestFileName -Append
-" " | Out-File $TestFileName -Append
+    "1. Afterburner NVIDIA" | Out-File $TestFileName -Append
+    "-"*80 | Out-File $TestFileName -Append
+    " " | Out-File $TestFileName -Append
 
-Get-AfterburnerDevices "AMD" | Out-File $TestFileName -Encoding utf8 -Append
+    Get-AfterburnerDevices "NVIDIA" | Out-File $TestFileName -Encoding utf8 -Append
 
-" " | Out-File $TestFileName -Append
-"3. OverdriveN" | Out-File $TestFileName -Append
-"-"*80 | Out-File $TestFileName -Append
-" " | Out-File $TestFileName -Append
+    " " | Out-File $TestFileName -Append
+    "2. Afterburner AMD" | Out-File $TestFileName -Append
+    "-"*80 | Out-File $TestFileName -Append
+    " " | Out-File $TestFileName -Append
 
-Invoke-Exe '.\Includes\OverdriveN.exe' -WorkingDirectory $Pwd -ExpandLines -ExcludeEmptyLines | Out-File $TestFileName -Encoding utf8 -Append
+    Get-AfterburnerDevices "AMD" | Out-File $TestFileName -Encoding utf8 -Append
+
+    " " | Out-File $TestFileName -Append
+    "3. OverdriveN" | Out-File $TestFileName -Append
+    "-"*80 | Out-File $TestFileName -Append
+    " " | Out-File $TestFileName -Append
+
+    Invoke-Exe '.\Includes\OverdriveN.exe' -WorkingDirectory $Pwd -ExpandLines -ExcludeEmptyLines | Out-File $TestFileName -Encoding utf8 -Append
 
 
-" " | Out-File $TestFileName -Append
-"4. nvidia-smi" | Out-File $TestFileName -Append
-"-"*80 | Out-File $TestFileName -Append
-" " | Out-File $TestFileName -Append
+    " " | Out-File $TestFileName -Append
+    "4. nvidia-smi" | Out-File $TestFileName -Append
+    "-"*80 | Out-File $TestFileName -Append
+    " " | Out-File $TestFileName -Append
 
-$Arguments = @(
-    '--query-gpu=gpu_name,utilization.gpu,utilization.memory,temperature.gpu,power.draw,power.limit,fan.speed,pstate,clocks.current.graphics,clocks.current.memory,power.max_limit,power.default_limit'
-    '--format=csv,noheader'
-)
+    $Arguments = @(
+        '--query-gpu=gpu_name,utilization.gpu,utilization.memory,temperature.gpu,power.draw,power.limit,fan.speed,pstate,clocks.current.graphics,clocks.current.memory,power.max_limit,power.default_limit'
+        '--format=csv,noheader'
+    )
 
-Invoke-Exe ".\Includes\nvidia-smi.exe" -ArgumentList ($Arguments -join ' ') -WorkingDirectory $Pwd -ExpandLines -ExcludeEmptyLines  | Out-File $TestFileName -Encoding utf8 -Append
+    Invoke-Exe ".\Includes\nvidia-smi.exe" -ArgumentList ($Arguments -join ' ') -WorkingDirectory $Pwd -ExpandLines -ExcludeEmptyLines  | Out-File $TestFileName -Encoding utf8 -Append
 
-" " | Out-File $TestFileName -Append
-"5. OpenCL" | Out-File $TestFileName -Append
-"-"*80 | Out-File $TestFileName -Append
-" " | Out-File $TestFileName -Append
+    " " | Out-File $TestFileName -Append
+    "5. OpenCL" | Out-File $TestFileName -Append
+    "-"*80 | Out-File $TestFileName -Append
+    " " | Out-File $TestFileName -Append
 
-[OpenCl.Platform]::GetPlatformIDs() | ForEach-Object {
-    $_ | Out-File $TestFileName -Encoding utf8 -Append
-    [OpenCl.Device]::GetDeviceIDs($_, [OpenCl.DeviceType]::All) | Out-File $TestFileName -Encoding utf8 -Append
-    "."*80 | Out-File $TestFileName -Append
+    [OpenCl.Platform]::GetPlatformIDs() | ForEach-Object {
+        $_ | Out-File $TestFileName -Encoding utf8 -Append
+        [OpenCl.Device]::GetDeviceIDs($_, [OpenCl.DeviceType]::All) | Out-File $TestFileName -Encoding utf8 -Append
+        "."*80 | Out-File $TestFileName -Append
+    }
+}
+
+if ($IsLinux) {
+    " " | Out-File $TestFileName -Append
+    "1. nvidia-smi" | Out-File $TestFileName -Append
+    "-"*80 | Out-File $TestFileName -Append
+    " " | Out-File $TestFileName -Append
+
+    $Arguments = @(
+        '--query-gpu=gpu_name,utilization.gpu,utilization.memory,temperature.gpu,power.draw,power.limit,fan.speed,pstate,clocks.current.graphics,clocks.current.memory,power.max_limit,power.default_limit'
+        '--format=csv,noheader'
+    )
+
+    Invoke-Exe "nvidia-smi" -ArgumentList ($Arguments -join ' ') -WorkingDirectory $Pwd -ExpandLines -ExcludeEmptyLines  | Out-File $TestFileName -Encoding utf8 -Append
+
+    " " | Out-File $TestFileName -Append
+    "2. OpenCL" | Out-File $TestFileName -Append
+    "-"*80 | Out-File $TestFileName -Append
+    " " | Out-File $TestFileName -Append
+
+    [OpenCl.Platform]::GetPlatformIDs() | ForEach-Object {
+        $_ | Out-File $TestFileName -Encoding utf8 -Append
+        [OpenCl.Device]::GetDeviceIDs($_, [OpenCl.DeviceType]::All) | Out-File $TestFileName -Encoding utf8 -Append
+        "."*80 | Out-File $TestFileName -Append
+    }
 }
 
 Write-Host "Done! Now please drop the file"
---
+++ gputest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+command="& ./GPUtest.ps1"
+
+pwsh -ExecutionPolicy bypass -Command ${command}
+
+read -n 1 -s -r -p "Press any key to continue"
