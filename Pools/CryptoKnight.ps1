f21e9a968b515363129f6c3890bb6c2c1df3d1b4
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-04-22 20:35:35 +0200

    Update pool CryptoKnight
    - optimize ssl/ports

--- Pools/CryptoKnight.ps1
+++ Pools/CryptoKnight.ps1
@@ -93,24 +93,9 @@ $Pools_Data | Where-Object {$Pool_Algorithms -icontains $_.rpc} | Where-Object {
 
     $ok = $true
     if (-not $InfoOnly) {
-        $Pool_Ports_Ok = $false
         try {
             $Pool_Request = Invoke-RestMethodAsync "https://cryptoknight.cc/rpc/$($Pool_RpcPath)/stats" -tag $Name -timeout 15 -cycletime 120
-            @("CPU","GPU","RIG","CPU-SSL","GPU-SSL","RIG-SSL") | Foreach-Object {
-                $PortType = $_ -replace '-.*$'
-                $Ports = if ($_ -match 'SSL') {$Pool_Request.config.ports | Where-Object {$_.ssl}} else {$Pool_Request.config.ports | Where-Object {-not $_.ssl}}
-                if ($Ports) {
-                    $PortIndex = if ($_ -match 'SSL') {1} else {0}
-                    $Port = Switch ($PortType) {                        
-                        "GPU" {$Ports | Where-Object desc -match 'GPU' | Select-Object -First 1}
-                        "RIG" {$Ports | Where-Object desc -match 'RIG' | Select-Object -First 1}
-                    }
-                    if (-not $Port) {$Port = $Ports | Select-Object -First 1}
-                    if ($Pool_Ports.Count -eq 1 -and $PortIndex -eq 1) {$Pool_Ports += [PSCustomObject]@{}}
-                    $Pool_Ports[$PortIndex] | Add-Member $PortType $Port.port -Force
-                    $Pool_Ports_Ok = $true
-                }
-            }
+            $Pool_Ports   = Get-PoolPortsFromRequest $Pool_Request -mCPU "" -mGPU "GPU" -mRIG "RIG"
             if ($Pool_Currency2) {
                 $Pool_Request2 = Invoke-RestMethodAsync "https://cryptoknight.cc/rpc/$($Pools_Data | Where-Object {$_.symbol -eq $Pool_Currency2 -and -not $_.symbol2} | Select-Object -ExpandProperty rpc)/stats" -tag $Name -timeout 15 -cycletime 120
             }
@@ -120,7 +105,7 @@ $Pools_Data | Where-Object {$Pool_Algorithms -icontains $_.rpc} | Where-Object {
             Write-Log -Level Warn "Pool API ($Name) for $Pool_Currency has failed. "
             $ok = $false
         }
-        if (-not $Pool_Ports_Ok) {$ok = $false}
+        if (-not ($Pool_Ports | Where-Object {$_} | Measure-Object).Count) {$ok = $false}
     }
 
     if ($ok -and -not $InfoOnly) {
@@ -144,29 +129,31 @@ $Pools_Data | Where-Object {$Pool_Algorithms -icontains $_.rpc} | Where-Object {
     if (($ok -and ($AllowZero -or $Pool_Data.Live.hashrate -gt 0)) -or $InfoOnly) {
         $Pool_SSL = $false
         foreach ($Pool_Port in $Pool_Ports) {
-            foreach($Pool_Region in $Pool_Regions) {
-                [PSCustomObject]@{
-                    Algorithm     = $Pool_Algorithm_Norm
-                    CoinName      = $_.coin
-                    CoinSymbol    = $Pool_Currency
-                    Currency      = $Pool_Currency
-                    Price         = $Stat.$StatAverage #instead of .Live
-                    StablePrice   = $Stat.Week
-                    MarginOfError = $Stat.Week_Fluctuation
-                    Protocol      = "stratum+$(if ($Pool_SSL) {"ssl"} else {"tcp"})"
-                    Host          = "$($Pool_HostPath).ingest$(if ($Pool_Region -ne $Pool_Region_Default) {"-$Pool_Region"}).cryptoknight.cc"
-                    Port          = $Pool_Port.CPU
-                    Ports         = $Pool_Port
-                    User          = "$($Wallets.$Pool_Currency){diff:$(if ($_.diffdot) {$_.diffdot} else {"."})`$difficulty}"
-                    Pass          = "{workername:$Worker}"
-                    Region        = $Pool_RegionsTable.$Pool_Region
-                    SSL           = $Pool_SSL
-                    Updated       = $Stat.Updated
-                    PoolFee       = $Pool_Fee
-                    Workers       = $Pool_Data.Workers
-                    Hashrate      = $Stat.HashRate_Live
-                    TSL           = $Pool_Data.TSL
-                    BLK           = $Stat.BlockRate_Average
+            if ($Pool_Port) {
+                foreach($Pool_Region in $Pool_Regions) {
+                    [PSCustomObject]@{
+                        Algorithm     = $Pool_Algorithm_Norm
+                        CoinName      = $_.coin
+                        CoinSymbol    = $Pool_Currency
+                        Currency      = $Pool_Currency
+                        Price         = $Stat.$StatAverage #instead of .Live
+                        StablePrice   = $Stat.Week
+                        MarginOfError = $Stat.Week_Fluctuation
+                        Protocol      = "stratum+$(if ($Pool_SSL) {"ssl"} else {"tcp"})"
+                        Host          = "$($Pool_HostPath).ingest$(if ($Pool_Region -ne $Pool_Region_Default) {"-$Pool_Region"}).cryptoknight.cc"
+                        Port          = $Pool_Port.CPU
+                        Ports         = $Pool_Port
+                        User          = "$($Wallets.$Pool_Currency){diff:$(if ($_.diffdot) {$_.diffdot} else {"."})`$difficulty}"
+                        Pass          = "{workername:$Worker}"
+                        Region        = $Pool_RegionsTable.$Pool_Region
+                        SSL           = $Pool_SSL
+                        Updated       = $Stat.Updated
+                        PoolFee       = $Pool_Fee
+                        Workers       = $Pool_Data.Workers
+                        Hashrate      = $Stat.HashRate_Live
+                        TSL           = $Pool_Data.TSL
+                        BLK           = $Stat.BlockRate_Average
+                    }
                 }
             }
             $Pool_SSL = $true
