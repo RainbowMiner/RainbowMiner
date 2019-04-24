c1509bde96ec6486e2287e1596b4c42d2eef4a9b
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-04-24 12:02:17 +0200

    Update core
    - fix ethpill enable

--- Include.psm1
+++ Include.psm1
@@ -2710,7 +2710,7 @@ class Miner {
 
         if (-not $this.Process) {
             if ($this.StartCommand) {try {Invoke-Expression $this.StartCommand} catch {if ($Error.Count){$Error.RemoveAt(0)};Write-Log -Level Warn "StartCommand failed for miner $($this.Name)"}}
-            if ($Global:IsWindows -and $this.EthPillEnable -ne "disable" -and (Compare-Object $this.BaseAlgorithm @("Ethash","MTP") | Measure-Object).Count) {
+            if ($Global:IsWindows -and $this.EthPillEnable -ne "disable" -and (Compare-Object $this.BaseAlgorithm @("Ethash","MTP") -IncludeEqual -ExcludeDifferent | Measure-Object).Count) {
                 $Prescription_Device = @(Get-Device $this.DeviceName) | Where-Object Model -in @("GTX1080","GTX1080Ti","TITANXP")
                 $Prescription = ""
                 switch ($this.EthPillEnable) {
