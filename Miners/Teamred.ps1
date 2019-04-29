989710b8764e8a2b123f6ea79059e5f74dd06398
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-04-29 21:17:35 +0200

    Update miner TeamRed to v0.4.5 (amd)
    - Added cryptonight v8 upx2 for the uPlexa coin fork.
    - Reworked init procedure, added retry logic on comm errors.
    - Added section on temps to the CN_MAX_YOUR_VEGA guide.
    - Added a new howto MAP_YOUR_GPUS describing how to map gpus between miner/tools/registry.

--- Miners/Teamred.ps1
+++ Miners/Teamred.ps1
@@ -9,10 +9,10 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\AMD-Teamred\teamredminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.4.4-teamred/teamredminer-v0.4.4-linux.tgz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.4.5-teamred/teamredminer-v0.4.5-linux.tgz"
 } else {
     $Path = ".\Bin\AMD-Teamred\teamredminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.4.4-teamred/teamredminer-v0.4.4-win.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.4.5-teamred/teamredminer-v0.4.5-win.zip"
 }
 $Port = "409{0:d2}"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5059817.0"
@@ -27,6 +27,7 @@ $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "cnv8_half"; MinMemGb = 2; Params = ""; DevFee = 2.5}
     [PSCustomObject]@{MainAlgorithm = "cnv8_rwz";  MinMemGb = 2; Params = ""; DevFee = 2.5}
     [PSCustomObject]@{MainAlgorithm = "cnv8_trtl"; MinMemGb = 2; Params = ""; DevFee = 2.5}
+    [PSCustomObject]@{MainAlgorithm = "cnv8_upx2"; MinMemGb = 2; Params = ""; DevFee = 2.5}
     [PSCustomObject]@{MainAlgorithm = "lyra2rev3"; MinMemGb = 1; Params = ""; DevFee = 2.5}
     [PSCustomObject]@{MainAlgorithm = "lyra2z";    MinMemGb = 1; Params = ""; DevFee = 3.0}
     [PSCustomObject]@{MainAlgorithm = "phi2";      MinMemGb = 1; Params = ""; DevFee = 3.0}
