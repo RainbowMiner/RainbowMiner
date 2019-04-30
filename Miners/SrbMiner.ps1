4bbb32eef6f89febc67c451f5fb41e6eba45605f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-04-30 13:11:40 +0200

    Update miner SrbMiner to v1.8.7 (amd)
    - Improvements with tweak profile 10 on Vega64 with Samsung memory
    - Fixed a bug with pool reconnect on job timeout
    - job_timeout is now disabled by default, to enable it set a value > 0
    - Fixed min_rig_speed bug that triggered restart with V4 algos
    - Added info in api/web stats about tweak profile
    - Made hashrate display on web stats page bigger
    - In guided setup mode changed max wallet and password length to 200 so address+payment id can be used

--- Miners/SrbMiner.ps1
+++ Miners/SrbMiner.ps1
@@ -8,7 +8,7 @@ param(
 if (-not $IsWindows) {return}
 
 $Path = ".\Bin\CryptoNight-SRBMiner\srbminer-cn.exe"
-$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.8.6-srbminer/SRBMiner-CN-V1-8-6.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.8.7-srbminer/SRBMiner-CN-V1-8-7.zip"
 $ManualUri = "https://bitcointalk.org/index.php?topic=3167363.0"
 $Port = "315{0:d2}"
 $DevFee = 0.85
