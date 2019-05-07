dc95c22bf6d1431a7379e1dd9692a5a4357ebe77
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-05-07 16:16:40 +0200

    Update InitClient.ps1
    - add centralized server config files

--- InitClient.ps1
+++ InitClient.ps1
@@ -31,6 +31,8 @@ try {
     exit
 }
 
+$ConfigSetup  = Get-ChildItemContent ".\Data\ConfigDefault.ps1" | Select-Object -ExpandProperty Content
+
 $Config | Add-Member RunMode $(if ($Config.RunMode -eq "`$RunMode") {"Standalone"} else {$Config.RunMode}) -Force
 $Config | Add-Member APIport $(if ($Config.APIport -eq "`$APIport" -or -not $Config.APIport) {4000} else {[int]$Config.APIport}) -Force
 $Config | Add-Member ServerName $(if ($Config.ServerName -eq "`$ServerName") {""} else {$Config.ServerName}) -Force
@@ -38,6 +40,8 @@ $Config | Add-Member ServerPort $(if ($Config.ServerPort -eq "`$ServerPort" -or 
 $Config | Add-Member ServerUser $(if ($Config.ServerUser -eq "`$ServerUser") {""} else {$Config.ServerUser}) -Force
 $Config | Add-Member ServerPassword $(if ($Config.ServerPassword -eq "`$ServerPassword") {""} else {$Config.ServerPassword}) -Force
 $Config | Add-Member StartPaused $(if ($Config.StartPaused -eq "`$StartPaused") {$false} else {Get-Yes $Config.StartPaused}) -Force
+$Config | Add-Member EnableServerConfig $(if ($Config.EnableServerConfig -eq "`$EnableServerConfig") {$ConfigSetup.EnableServerConfig} else {$Config.EnableServerConfig}) -Force
+$Config | Add-Member ServerConfigName $(if ($Config.ServerConfigName -eq "`$ServerConfigName") {$ConfigSetup.ServerConfigName} else {@(Get-ConfigArray $Config.ServerConfigName)}) -Force
 
 do {
     if (-not (Read-HostBool "Setup $([System.Environment]::MachineName) as CLIENT?" -default ($Config.RunMode -eq "Client"))) {exit}
@@ -51,11 +55,16 @@ do {
     $Config.ServerPort = Read-HostInt -Prompt "Enter the server's API port " -Default $Config.ServerPort -Min 0 -Max 9999
     $Config.ServerUser = Read-HostString -Prompt "If you have auth enabled on your server's API, enter the username " -Default $Config.ServerUser -Characters "A-Z0-9"
     $Config.ServerPassword = Read-HostString -Prompt "If you have auth enabled on your server's API, enter the password " -Default $Config.ServerPassword -Characters ""
+    $Config.EnableServerConfig = Read-HostBool -Prompt "Enable automatic download of selected server config files? " -Default $Config.EnableServerConfig
+    if (Get-Yes $Config.EnableServerConfig) {
+        $Config.ServerConfigName = Read-HostArray -Prompt "Enter the config files to be copied to this machine" -Default $Config.ServerConfigName -Characters "A-Z" -Valid @("algorithms","coins","config","miners","ocprofiles","pools")
+    }
 
     if (Get-Yes $Config.StartPaused) {
         $Config.StartPaused = -not (Read-HostBool -Prompt "RainbowMiner is currently configured to start in pause/no-mining mode. Do you want to disable that?" -Default $true)
     }
     $Config.StartPaused = if (Get-Yes $Config.StartPaused) {"1"} else {"0"}
+    $Config.ServerConfigName = $Config.ServerConfigName -join ','
 
     Write-Host " "
     Write-Host "Check your data:"
@@ -63,6 +72,9 @@ do {
     Write-Host " ServerPort = $($Config.ServerPort)"
     Write-Host " ServerUser = $($Config.ServerUser)"
     Write-Host " ServerPassword = $($Config.ServerPassword)"
+    if (Get-Yes $Config.EnableServerConfig) {
+        Write-Host " ServerConfigName = $($Config.ServerConfigName -join ',') (automatic download from server enabled)"
+    }
     Write-Host " "
     if ($Config.ServerName -and $Config.ServerPort -and (Test-TcpServer -Server $Config.ServerName -Port $Config.ServerPort -Timeout 2)) {
         Write-Host "Server connected successfully!" -ForegroundColor Green
@@ -76,7 +88,7 @@ Write-Host " "
 Write-Host "Initializing Client"
 Write-Host " "
 if ($IsWindows -and -not (Test-IsElevated)) {
-    Write-Host " Please select `"Yes`" for all UACL prompts! " -BackgroundColor Yellow -ForegroundColor Black
+    Write-Host " Please select `"Yes`" for all UAC prompts! " -BackgroundColor Yellow -ForegroundColor Black
     Write-Host " "
 }
 
