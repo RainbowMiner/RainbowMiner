0e93708c3ae48a508bafe7ed3cbeefc6e7840cb2
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-05-08 11:56:39 +0200

    Update PresetDefault.ps1

--- Data/PresetDefault.ps1
+++ Data/PresetDefault.ps1
@@ -1,3 +1,11 @@
 ﻿[PSCustomObject]@{
     EnableCheckMiningConflict = $Global:GlobalCPUInfo.Cores -le 2
+    ExcludeServerConfigVars = @(
+        "WorkerName","DeviceName","Proxy",
+        "APIPort","APIUser","APIPassword","APIAuth",
+        "MSIApath","NVSMIpath",
+        "CPUMiningThreads","CPUMiningAffinity","GPUMiningAffinity",
+        "ServerName","ServerPort","ServerUser","ServerPassword","EnableServerConfig","ServerConfigName","ExcludeServerConfigVars",
+        "RunMode","StartPaused"
+    )
 }
