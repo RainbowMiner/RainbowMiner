ac5774dd2272f629f0af4ddc7818d64110cc9326
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-05-13 16:15:23 +0200

    Add miner ProgPOWZNvidia v1.1.2 (nvidia)
    - hyle-team zano reference miner

---
+++ APIs/EthminerWrapper.ps1
@@ -0,0 +1,48 @@
+﻿using module ..\Include.psm1
+
+class EthminerWrapper : Miner {
+
+    [String[]]UpdateMinerData () {
+        if ($this.Process.HasMoreData) {
+            $HashRate_Name = $this.Algorithm[0]
+
+            $this.Process | Receive-Job | ForEach-Object {
+                $Line = $_ -replace "`n|`r", ""
+                $Line_Simple = $Line -replace "\x1B\[[0-?]*[ -/]*[@-~]", ""
+                if ($Line_Simple) {
+
+                    if ($Line_Simple -match "^\s+m\s+(.+)$") {
+                        $Line_Cols = $Matches[1] -split '\s+'
+                        $HashRate = [PSCustomObject]@{}
+
+                        $HashRate_Value  = [double]($Line_Cols[4] -replace ',','.') * $(Switch ($Line_Cols[5]) {
+                            "Th" {1e12}
+                            "Gh" {1e9}
+                            "Mh" {1e6}
+                            "kh" {1e3}
+                            default {1}
+                        })
+
+                        $Accepted_Shares = [Int64]$(if ($Line_Cols[3] -match "A(\d+)") {$Matches[1]})
+                        $Rejected_Shares = [Int64]0
+
+                        if ($HashRate_Value -gt 0) {
+                            $HashRate | Add-Member @{$HashRate_Name = $HashRate_Value}
+                            $this.UpdateShares(0,$Accepted_Shares,$Rejected_Shares)
+                        }
+
+                        $this.AddMinerData([PSCustomObject]@{
+                            Raw = $Line_Simple
+                            HashRate = $HashRate                          
+                            Device = @()
+                        })
+                    }
+                }
+            }
+
+            $this.CleanupMinerData()
+        }
+
+        return @()
+    }
+}
---
+++ Miners/ProgPOWZNvidia.ps1
@@ -0,0 +1,87 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+if (-not $IsWindows) {return}
+
+$Port = "340{0:d2}"
+$ManualURI = "https://github.com/hyle-team/zano/releases"
+$DevFee = 0.0
+
+$UriCuda = @(
+    [PSCustomObject]@{
+        Path = ".\Bin\NVIDIA-ProgPOWZ\progminer-zano-cuda.exe"
+        Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1.2-progminerzano/progminer-zano-win-1.1.2.7z"
+        Cuda = "10.1"
+    }
+)
+
+if (-not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "progpowz"; Params = ""; ExtendInterval = 2} #ProgPOW
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("NVIDIA")
+        Name      = $Name
+        Path      = $UriCuda.Path | Select-Object -First 1
+        Port      = $Miner_Port
+        Uri       = $UriCuda.Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Uri = $Path = ""
+for($i=0;$i -le $UriCuda.Count -and -not $Uri;$i++) {
+    if (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $UriCuda[$i].Cuda -Warning $(if ($i -lt $UriCuda.Count-1) {""}else{$Name})) {
+        $Path= $UriCuda[$i].Path
+        $Uri = $UriCuda[$i].Uri
+        $Cuda= $UriCuda[$i].Cuda
+    }
+}
+if (-not $Uri) {return}
+
+$Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ' '
+
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+
+		foreach($Algorithm_Norm in @($Algorithm_Norm,"$($Algorithm_Norm)-$($Miner_Model)")) {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+				$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
+				[PSCustomObject]@{
+					Name        = $Miner_Name
+					DeviceName  = $Miner_Device.Name
+					DeviceModel = $Miner_Model
+					Path        = $Path
+					Arguments   = "--api-port -$($Miner_Port) -P stratum$(if ($Pools.$Algorithm_Norm.SSL) {"s"})://$(Get-UrlEncode $Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {":$(Get-UrlEncode $Pools.$Algorithm_Norm.Pass)"})@$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) --cuda --cu-devices $($DeviceIDsAll) --exit $($_.Params)"
+					HashRates   = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week }
+					API         = "EthminerWrapper"
+					Port        = $Miner_Port
+					Uri         = $Uri
+					DevFee      = $DevFee
+					ManualUri   = $ManualUri
+					ExtendInterval = $_.ExtendInterval
+				}
+			}
+		}
+    }
+}
