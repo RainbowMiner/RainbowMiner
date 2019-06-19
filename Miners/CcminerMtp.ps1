157846bf1a150d2b5936a8fcb2278711ba4a3a08
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-06-19 10:13:25 +0200

    Update miner CcminerMTP (nvidia)
    - no linux nicehash variant

--- Miners/CcminerMtp.ps1
+++ Miners/CcminerMtp.ps1
@@ -12,6 +12,7 @@ if ($IsLinux) {
     $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1.20t-ccminermtp/ccminermtp-v1.1.20tr-linux.7z"
 } else {
     $Path = ".\Bin\NVIDIA-CcminerMTP\ccminer.exe"
+    $PathNh = ".\Bin\NVIDIA-CcminerMTP\ccminer-nh.exe"
     $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1.20t-ccminermtp/ccminermtp-v1.1.20tr-win.7z"
 }
 $ManualUri = "https://github.com/zcoinofficial/ccminer/releases"
@@ -61,13 +62,13 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
         $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
 
 		foreach($Algorithm_Norm in @($Algorithm_Norm,"$($Algorithm_Norm)-$($Miner_Model)")) {
-			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and (-not $IsNh -or ($IsNh -and $PathNh))) {
 				$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
 				[PSCustomObject]@{
 					Name           = $Miner_Name
 					DeviceName     = $Miner_Device.Name
 					DeviceModel    = $Miner_Model
-					Path           = "$(if ($IsNh) {$Path -replace "ccminer.exe","ccminer-nh.exe"} else {$Path})"
+					Path           = "$(if ($IsNh -and $PathNh) {$PathNh} else {$Path})"
 					Arguments      = "-R 1 -N 3 -b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) $($_.Params)"
 					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week}
 					API            = "Ccminer"
