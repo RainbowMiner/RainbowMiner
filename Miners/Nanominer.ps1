ac8932e150fb62df9ea72555bc4675a47984a740
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-06-28 11:31:59 +0200

    Update miner Nanominer to v1.4.0 (amd.cpu,nvidia)
    - New memTweak common option to control AMD GPU memory timings on Ethash algorithm. Supports integer values from zero (disabled) to 10 (most intense). Default value is 1 which gives up to 6%-10% performance increase compared to stock settings. Miner must be launched using admin/root privileges in order to change timings.
    - Faster Ethash kernel tuning on AMD Vega and Radeon VII GPUs.

--- Miners/Nanominer.ps1
+++ Miners/Nanominer.ps1
@@ -9,10 +9,10 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\ANY-Nanominer\nanominer"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.3.4-nanominer/nanominer-linux-1.3.4.tar.gz"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.4.0-nanominer/nanominer-linux-1.4.0.tar.gz"
 } else {
     $Path = ".\Bin\ANY-Nanominer\nanominer.exe"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.3.4-nanominer/nanominer-windows-1.3.4.zip"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.4.0-nanominer/nanominer-windows-1.4.0.zip"
 }
 $ManualURI = "https://github.com/nanopool/nanominer/releases"
 $Port = "534{0:d2}"
