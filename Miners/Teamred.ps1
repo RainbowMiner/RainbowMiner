c140d307607ecabdbb3ebd87cf14a282c60b35db
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-07-04 00:51:44 +0200

    Update miner Teamred to v0.5.3 (amd)
    - Added MTP algo for Zcoin/XZC (please read MTP_MINING.txt before mining).
    - Further small stabilization fixes for CN variants, primarily 4MB algos.

--- Miners/Teamred.ps1
+++ Miners/Teamred.ps1
@@ -9,10 +9,10 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\AMD-Teamred\teamredminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.5.2-teamred/teamredminer-v0.5.2-linux.tgz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.5.3-teamred/teamredminer-v0.5.3-linux.tgz"
 } else {
     $Path = ".\Bin\AMD-Teamred\teamredminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.5.2-teamred/teamredminer-v0.5.2-win.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.5.3-teamred/teamredminer-v0.5.3-win.zip"
 }
 $Port = "409{0:d2}"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5059817.0"
