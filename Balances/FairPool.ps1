8c09feb673f09c868ef177f1979a17f3a29da748
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-07-17 11:36:50 +0200

    Update balance FairPool
    - fix

--- Balances/FairPool.ps1
+++ Balances/FairPool.ps1
@@ -5,32 +5,35 @@
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 
 $Pools_Data = @(
-    [PSCustomObject]@{coin = "BitTube"; symbol = "TUBE"; algo = "CnSaber"; port = 6040; fee = 1.0; walletSymbol = "tube"; host = "mine.tube.fairpool.xyz"; user="%wallet%+%worker%"}
-    [PSCustomObject]@{coin = "Swap"; symbol = "XWP"; algo = "CnSwap"; port = 6080; fee = 1.0; walletSymbol = "xfh"; host = "mine.xfh.fairpool.xyz"; user="%wallet%+%worker%"}
-    [PSCustomObject]@{coin = "Haven"; symbol = "XHV"; algo = "CnHaven"; port = 5566; fee = 1.0; walletSymbol = "xhv"; host = "mine.xhv.fairpool.xyz"; user="%wallet%+%worker%"}
-    [PSCustomObject]@{coin = "Lethean"; symbol = "LTHN"; algo = "CnV8"; port = 6070; fee = 1.0; walletSymbol = "lethean"; host = "mine.lethean.fairpool.xyz"; user="%wallet%+%worker%"}
-    [PSCustomObject]@{coin = "Loki"; symbol = "LOKI"; algo = "CnHeavy"; port = 5577; fee = 1.0; walletSymbol = "loki"; host = "mine.loki.fairpool.xyz"; user="%wallet%+%worker%"}
-    [PSCustomObject]@{coin = "Masari"; symbol = "MSR"; algo = "CnHalf"; port = 6060; fee = 1.0; walletSymbol = "msr"; host = "mine.msr.fairpool.xyz"; user="%wallet%+%worker%"}
-    [PSCustomObject]@{coin = "PrivatePay"; symbol = "XPP"; algo = "CnFast"; port = 6050; fee = 1.0; walletSymbol = "xpp"; host = "mine.xpp.fairpool.xyz"; user="%wallet%+%worker%"}
-    [PSCustomObject]@{coin = "QuantumResistantLedger"; symbol = "QRL"; algo = "CnV7"; port = 7000; fee = 1.0; walletSymbol = "qrl"; host = "mine.qrl.fairpool.xyz"; user="%wallet%+%worker%"}
-    [PSCustomObject]@{coin = "Ryo"; symbol = "RYO"; algo = "CnGpu"; port = 5555; fee = 1.0; walletSymbol = "ryo"; host = "mine.ryo.fairpool.xyz"; user="%wallet%+%worker%"}
-    [PSCustomObject]@{coin = "Saronite"; symbol = "XRN"; algo = "CnHaven"; port = 5599; fee = 1.0; walletSymbol = "xrn"; host = "mine.xrn.fairpool.xyz"; user="%wallet%+%worker%"}
-    [PSCustomObject]@{coin = "Solace"; symbol = "XPP"; algo = "CnHeavy"; port = 5588; fee = 1.0; walletSymbol = "solace"; host = "mine.solace.fairpool.xyz"; user="%wallet%+%worker%"}
-    [PSCustomObject]@{coin = "Swap"; symbol = "XWP"; algo = "Cuckaroo29s"; port = 5588; fee = 1.0; walletSymbol = "xfh"; host = "mine.xfh.fairpool.xyz"; user="%wallet%+%worker%"}
+    [PSCustomObject]@{coin = "BitTube";         symbol = "TUBE"; algo = "CnSaber";     port = 6040; fee = 1.0; rpc = "tube";    user="%wallet%+%worker%"}
+    [PSCustomObject]@{coin = "Haven";           symbol = "XHV";  algo = "CnHaven";     port = 5566; fee = 1.0; rpc = "xhv";     user="%wallet%+%worker%"}
+    [PSCustomObject]@{coin = "Lethean";         symbol = "LTHN"; algo = "CnR";         port = 6070; fee = 1.0; rpc = "lethean"; user="%wallet%+%worker%"}
+    [PSCustomObject]@{coin = "Loki";            symbol = "LOKI"; algo = "CnHeavy";     port = 5577; fee = 1.0; rpc = "loki";    user="%wallet%+%worker%"}
+    [PSCustomObject]@{coin = "Masari";          symbol = "MSR";  algo = "CnHalf";      port = 6060; fee = 1.0; rpc = "msr";     user="%wallet%+%worker%"}
+    #[PSCustomObject]@{coin = "PrivatePay";      symbol = "XPP";  algo = "CnFast";      port = 6050; fee = 1.0; rpc = "xpp";     user="%wallet%+%worker%"}
+    [PSCustomObject]@{coin = "Qrl";             symbol = "QRL";  algo = "CnV7";        port = 7000; fee = 1.0; rpc = "qrl";     user="%wallet%+%worker%"}
+    [PSCustomObject]@{coin = "Ryo";             symbol = "RYO";  algo = "CnGpu";       port = 5555; fee = 1.0; rpc = "ryo";     user="%wallet%+%worker%"}
+    #[PSCustomObject]@{coin = "Saronite";        symbol = "XRN";  algo = "CnHaven";     port = 5599; fee = 1.0; rpc = "xrn";     user="%wallet%+%worker%"}
+    #[PSCustomObject]@{coin = "Solace";          symbol = "XPP";  algo = "CnHeavy";     port = 5588; fee = 1.0; rpc = "solace";  user="%wallet%+%worker%"}
+    [PSCustomObject]@{coin = "Swap";            symbol = "XWP";  algo = "Cuckaroo29s"; port = 6080; fee = 1.0; rpc = "xfh";     user="%wallet%+%worker%"; divisor = 32}
+    [PSCustomObject]@{coin = "Wow";             symbol = "WOW";  algo = "RandomWow";   port = 6090; fee = 1.0; rpc = "wow";     user="%wallet%+%worker%"}
+    [PSCustomObject]@{coin = "Xtend";           symbol = "XTNC"; algo = "CnTurtle";    port = 7010; fee = 1.0; rpc = "xtnc";    user="%wallet%+%worker%"}
 
-    [PSCustomObject]@{coin = "Akroma"; symbol = "AKA"; algo = "Ethash"; port = 2222; fee = 1.0; walletSymbol = "aka"; host = "mine.aka.fairpool.xyz"; user="%wallet%.%worker%"}
-    [PSCustomObject]@{coin = "DogEthereum"; symbol = "DOGX"; algo = "Ethash"; port = 7788; fee = 1.0; walletSymbol = "dogx"; host = "mine.dogx.fairpool.xyz"; user="%wallet%.%worker%"}
-    [PSCustomObject]@{coin = "EthereumClassic"; symbol = "ETC"; algo = "Ethash"; port = 4444; fee = 1.0; walletSymbol = "etc"; host = "mine.etc.fairpool.xyz"; user="%wallet%.%worker%"}
-    [PSCustomObject]@{coin = "Metaverse"; symbol = "ETP"; algo = "Ethash"; port = 6666; fee = 1.0; walletSymbol = "etp"; host = "mine.etp.fairpool.xyz"; user="%wallet%.%worker%"}
-    [PSCustomObject]@{coin = "Nekonium"; symbol = "NUKO"; algo = "Ethash"; port = 7777; fee = 1.0; walletSymbol = "nuko"; host = "mine.nuko.fairpool.xyz"; user="%wallet%.%worker%"}
-    [PSCustomObject]@{coin = "Pegascoin"; symbol = "PGC"; algo = "Ethash"; port = 1111; fee = 1.0; walletSymbol = "pgc"; host = "mine.pgc.fairpool.xyz"; user="%wallet%.%worker%"}
+    #[PSCustomObject]@{coin = "Akroma";          symbol = "AKA";  algo = "Ethash";      port = 2222; fee = 1.0; rpc = "aka";     user="%wallet%.%worker%"}
+    [PSCustomObject]@{coin = "DogEthereum";     symbol = "DOGX"; algo = "Ethash";      port = 7788; fee = 1.0; rpc = "dogx";    user="%wallet%.%worker%"}
+    [PSCustomObject]@{coin = "EthereumClassic"; symbol = "ETC";  algo = "Ethash";      port = 4444; fee = 1.0; rpc = "etc";     user="%wallet%.%worker%"}
+    [PSCustomObject]@{coin = "Metaverse";       symbol = "ETP";  algo = "Ethash";      port = 6666; fee = 1.0; rpc = "etp";     user="%wallet%.%worker%"}
+    [PSCustomObject]@{coin = "Nekonium";        symbol = "NUKO"; algo = "Ethash";      port = 7777; fee = 1.0; rpc = "nuko";    user="%wallet%.%worker%"}
+    [PSCustomObject]@{coin = "Pegascoin";       symbol = "PGC";  algo = "Ethash";      port = 1111; fee = 1.0; rpc = "pgc";     user="%wallet%.%worker%"}
 
-    [PSCustomObject]@{coin = "Purk"; symbol = "PURK"; algo = "WildKeccak"; port = 2244; fee = 1.0; walletSymbol = "purk"; host = "mine.purk.fairpool.xyz"}
+    [PSCustomObject]@{coin = "Zano";            symbol = "ZANO"; algo = "ProgPowZ";    port = 7020; fee = 1.0; rpc = "zano";    user="%wallet%.%worker%"}
+
+    #[PSCustomObject]@{coin = "Purk";            symbol = "PURK"; algo = "WildKeccak";  port = 2244; fee = 1.0; rpc = "purk";    user="%wallet%+%worker%"}
 )
 
 $Pools_Data | Where-Object {$Config.Pools.$Name.Wallets."$($_.symbol)"} | Foreach-Object {
     $Pool_Currency = $_.symbol
-    $Pool_RpcPath = $_.walletSymbol.ToLower()
+    $Pool_RpcPath = $_.rpc.ToLower()
 
     $Pool_Request = [PSCustomObject]@{}
     $Request = [PSCustomObject]@{}
@@ -41,7 +44,7 @@ $Pools_Data | Where-Object {$Config.Pools.$Name.Wallets."$($_.symbol)"} | Foreac
         $Divisor = 1e12
 
         $Request = Invoke-RestMethodAsync "https://$($Pool_RpcPath).fairpool.xyz/api/stats?login=$($Config.Pools.$Name.Wallets.$Pool_Currency)" -delay 100 -cycletime ($Config.BalanceUpdateMinutes*60)
-        if (-not $Request.stats -or -not $Divisor) {
+        if ($Request.method -ne "stats" -or -not $Divisor) {
             Write-Log -Level Info "Pool Balance API ($Name) for $($Pool_Currency) returned nothing. "
         } else {            
             [PSCustomObject]@{
