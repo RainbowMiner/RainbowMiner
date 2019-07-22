d3ad9f3b56fc1abe7224306604a86cb13037f62a
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-07-22 16:44:04 +0200

    Update miner Teamred to v0.5.6 (amd)

--- Miners/Teamred.ps1
+++ Miners/Teamred.ps1
@@ -9,10 +9,10 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\AMD-Teamred\teamredminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.5.5-teamred/teamredminer-v0.5.5-linux.tgz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.5.6-teamred/teamredminer-v0.5.6-linux.tgz"
 } else {
     $Path = ".\Bin\AMD-Teamred\teamredminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.5.5-teamred/teamredminer-v0.5.5-win.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.5.6-teamred/teamredminer-v0.5.6-win.zip"
 }
 $Port = "409{0:d2}"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5059817.0"
