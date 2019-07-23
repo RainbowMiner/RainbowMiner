65de01989765eca23772097fb0507b82e229e2ad
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-07-23 13:48:43 +0200

    Split off CcminerMtpNh

--- Miners/CcminerMtpNh.ps1
+++ Miners/CcminerMtpNh.ps1
@@ -5,19 +5,14 @@ param(
     [Bool]$InfoOnly
 )
 
-if (-not $IsWindows -and -not $IsLinux) {return}
+if (-not $IsWindows) {return}
 
-if ($IsLinux) {
-    $Path = ".\Bin\NVIDIA-CcminerMTP\ccminer-linux-cuda"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1.24-ccminermtp/ccminermtp-v1.1.24-linux.7z"
-    $Cuda = "9.2"
-} else {
-    $Path = ".\Bin\NVIDIA-CcminerMTP\ccminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1.24-ccminermtp/ccminermtp-v1.1.24-win.7z"
-    $Cuda = "10.1"
-}
+$Path = ".\Bin\NVIDIA-CcminerMTPNh\ccminer-nh.exe"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1.24-ccminermtp/ccminermtp-v1.1.24-win.7z"
+$Cuda = "10.0"
+
 $ManualUri = "https://github.com/zcoinofficial/ccminer/releases"
-$Port = "126{0:d2}"
+$Port = "134{0:d2}"
 $DevFee = 0.0
 
 if (-not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
@@ -44,16 +39,6 @@ if ($InfoOnly) {
 
 if (-not (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name)) {return}
 
-if ($IsLinux) {
-    if (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion "10.1") {
-        $Path += "_101"
-    } elseif (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion "10.0") {
-        $Path += "_100"
-    } else {
-        $Path += "_92"
-    }
-}
-
 $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
     $Device = $Session.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
     $Miner_Model = $_.Model
@@ -70,14 +55,14 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
         $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
 
 		foreach($Algorithm_Norm in @($Algorithm_Norm,"$($Algorithm_Norm)-$($Miner_Model)")) {
-			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and $Pools.$Algorithm_Norm.Name -notmatch "NiceHash") {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
 				$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
 				[PSCustomObject]@{
 					Name           = $Miner_Name
 					DeviceName     = $Miner_Device.Name
 					DeviceModel    = $Miner_Model
 					Path           = $Path
-					Arguments      = "-R 1 -N 3 -b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) --no-donation $($_.Params)"
+					Arguments      = "-R 1 -N 3 -b $($Miner_Port) -d $($DeviceIDsAll) -a $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) $($_.Params)"
 					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week}
 					API            = "Ccminer"
 					Port           = $Miner_Port
