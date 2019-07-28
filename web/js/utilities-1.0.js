db005580567a21a94908c59a48b26e9c2e625808
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-07-28 19:32:44 +0200

    Update utilities-1.0.js

--- web/js/utilities-1.0.js
+++ web/js/utilities-1.0.js
@@ -117,10 +117,13 @@ function formatVersion(version) {
   
 
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + ((exdays==-1? 3650 : exdays) * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    var expires = "";
+    if (exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + ((exdays==-1? 3650:exdays) * 24 * 60 * 60 * 1000));
+        expires = "expires=" + d.toUTCString() + ";";
+    }
+    document.cookie = cname + "=" + cvalue + ";" + expires + "path=/";
 }
 
 function getCookie(cname) {
