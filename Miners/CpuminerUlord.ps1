281e3fc781a8b29f2023ad8d7f9829cf7d618949
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-07-29 10:58:30 +0200

    Add miner CpuminerUlord v1.2.1 (cpu)
    - ULORD=UL mining on UUpool

---
+++ Miners/CpuminerUlord.ps1
@@ -0,0 +1,76 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+if (-not $IsWindows -and -not $IsLinux) {return}
+
+if ($IsLinux) {
+    $Path = ".\Bin\CPU-Ulord\ulordrig"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.2.1-ulordrig/ulordrig-linux-v1.2.1.tar.gz"
+} else {
+    $Path = ".\Bin\CPU-Ulord\ulordrig.exe"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.2.1-ulordrig/ulordrig-win-v1.2.1.zip"
+}
+$ManualUri = "https://uupool.cn/tutorial/26"
+$Port = "538{0:d2}"
+$DevFee = 0.0
+
+if (-not $Session.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No CPU present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "cryptohello"; Params = ""; ExtendInterval = 2} #CryptoHello
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("CPU")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Session.DevicesByTypes.CPU | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Session.DevicesByTypes.CPU | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+    $DeviceParams = "$(if ($Session.Config.CPUMiningThreads){" -t $($Session.Config.CPUMiningThreads)"})$(if ($Session.Config.CPUMiningAffinity -ne ''){" --cpu-affinity $($Session.Config.CPUMiningAffinity)"})"
+
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+
+		foreach($Algorithm_Norm in @($Algorithm_Norm,"$($Algorithm_Norm)-$($Miner_Model)")) {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+				[PSCustomObject]@{
+					Name = $Miner_Name
+					DeviceName = $Miner_Device.Name
+					DeviceModel = $Miner_Model
+					Path = $Path
+					Arguments = "-R 2 -r 10 --api-port $($Miner_Port) -o stratum+tcp://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"})$(if ($Pools.$Algorithm_Norm.Name -match "NiceHash") {" --nicehash"})$($DeviceParams) $($_.Params)"
+					HashRates = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week}
+					API = "Ccminer"
+					Port = $Miner_Port
+					Uri = $Uri
+					FaultTolerance = $_.FaultTolerance
+					ExtendInterval = $_.ExtendInterval
+					DevFee = $DevFee
+					ManualUri = $ManualUri
+				}
+			}
+		}
+    }
+}
