6703345deb3c4afe43056106d610a71889ce43d3
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-08-01 11:42:47 +0200

    Bump version to v4.3.7.3

--- RainbowMiner.ps1
+++ RainbowMiner.ps1
@@ -213,7 +213,7 @@ Set-OsFlags
 
 $Global:Session = [hashtable]::Synchronized(@{}) 
 
-$Session.Version         = "4.3.7.2"
+$Session.Version         = "4.3.7.3"
 $Session.MainWindowTitle = "RainbowMiner v$($Session.Version)"
 $Session.SetupOnly       = $SetupOnly
 
