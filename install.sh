b92a571f8905e36ea02d5591ccbb86abf752a17a
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-08-04 23:17:05 +0200

    Update install.sh
    - update pwsh core version to v6.2.2

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 command="& {./Install.ps1; exit \$lastexitcode}"
 
 if ! [ -x "$(command -v pwsh)" ]; then
-wget https://github.com/PowerShell/PowerShell/releases/download/v6.2.1/powershell-6.2.1-linux-x64.tar.gz -O /tmp/powershell.tar.gz
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.2.2/powershell-6.2.2-linux-x64.tar.gz -O /tmp/powershell.tar.gz
 sudo mkdir -p /opt/microsoft/powershell/6
 sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/6
 sudo chmod +x /opt/microsoft/powershell/6/pwsh
