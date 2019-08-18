6da7cc8a1d5fa412c430a0b29e69cb2ed056558f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-08-19 00:03:07 +0200

    Update Cleanup.ps1

--- Cleanup.ps1
+++ Cleanup.ps1
@@ -568,6 +568,10 @@ try {
         }
     }
 
+    if ($Version -le (Get-Version "4.3.9.7")) {
+        $AddAlgorithm += @("Argon2Chukwa","Argon2Wkrz")
+    }
+
     if ($OverridePoolPenalties) {
         if (Test-Path "Data\PoolsConfigDefault.ps1") {
             $PoolsDefault = Get-ChildItemContent "Data\PoolsConfigDefault.ps1" -Quick
