e6234057524e3a43a31794064e28938f8d99f18b
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-08-19 17:59:19 +0200

    Update miner Gminer to v1.57 (amd,nvidia)
    improved BeamHashII performance on RTX cards

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -9,10 +9,10 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.56-gminer/gminer_1_56_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.57-gminer/gminer_1_57_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.56-gminer/gminer_1_56_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.57-gminer/gminer_1_57_windows64.zip"
 }
 $ManualUri = "https://bitcointalk.org/index.php?topic=5034735.0"
 $Port = "329{0:d2}"
