f2a9c4a430c8fa0cdea3b1b5919d956d1fe6b7d8
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-08-23 23:51:04 +0200

    Add miner XmrigUpx v0.2.0 (cpu)
    - add UPX2 mining

---
+++ Miners/XmrigUpx.ps1
@@ -0,0 +1,78 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+if (-not $IsWindows -and -not $IsLinux) {return}
+
+if ($IsLinux) {
+    $Path = ".\Bin\CPU-XmrigUpx\xmrig"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.2.0-xmrigupx/xmrig-upx-v0.2.0-lin64.tar.gz"
+    $DevFee = 1.0
+} else {
+    $Path = ".\Bin\CPU-XmrigUpx\xmrig.exe"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.2.0-xmrigupx/xmrig-upx-v0.2.0-win64.zip"
+    $DevFee = 1.0
+}
+$ManualUri = "https://github.com/uPlexa/xmrig-upx/releases"
+$Port = "540{0:d2}"
+$Version = "0.2.0"
+
+if (-not $Session.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No CPU present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "cryptonight-upx/2";      Params = ""; ExtendInterval = 2}
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("CPU")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Session.DevicesByTypes.CPU | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Device = $Session.DevicesByTypes.CPU | Where-Object Model -EQ $_.Model
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+    $Miner_Model = $_.Model
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Port = Get-MinerPort -MinerName $Name -DeviceName @($Miner_Device.Name) -Port $Miner_Port
+
+    $DeviceParams = "$(if ($Session.Config.CPUMiningThreads){"-t $($Session.Config.CPUMiningThreads)"}) $(if ($Session.Config.CPUMiningAffinity -ne ''){"--cpu-affinity $($Session.Config.CPUMiningAffinity)"})"
+
+    $Commands | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_.MainAlgorithm
+
+		foreach($Algorithm_Norm in @($Algorithm_Norm,"$($Algorithm_Norm)-$($Miner_Model)")) {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+
+				[PSCustomObject]@{
+					Name           = $Miner_Name
+					DeviceName     = $Miner_Device.Name
+					DeviceModel    = $Miner_Model
+					Path           = $Path
+                    Arguments      = "-R 1 --api-port $($Miner_Port) -a $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($DeviceParams) --keepalive$(if ($Pools.$Algorithm_Name.Name -eq "NiceHash") {" --nicehash"}) --donate-level=1 --print-time=10 $($_.Params)"
+					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Session.Stats."$($Miner_Name)_$($Algorithm_Norm -replace '\-.*$')_HashRate".Week}
+					API            = "XMRig"
+					Port           = $Miner_Port
+					Uri            = $Uri
+					DevFee         = $DevFee
+					ManualUri      = $ManualUri
+                    ExtendInterval = $_.ExtendInterval
+				}
+			}
+		}
+    }
+}
