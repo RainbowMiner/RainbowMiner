f3143dec4010ea1d0f846487774cc885efca4162
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-08-28 18:06:55 +0200

    Update core
    - optimize autobenchmark detection II

--- web/scripts/resetneededbenchmarks.ps1
+++ web/scripts/resetneededbenchmarks.ps1
@@ -18,18 +18,18 @@ $count = 0
         $SecondAlgo = $_.HashRates.PSObject.Properties.Name | Select -Index 1
     }
 
-    $Miner_Name = $_.Name                            
+    $Miner_Name = $_.Name                  
     $Miners_Key  = "$($Miner_Name)-$($Algo)"
     if ($JsonUri_Dates[$_.BaseName] -ne $null -and -not $Miners_List.ContainsKey($Miners_Key)) {
-        $Miners_List[$Miners_Key] = $true                            
+        $Miners_List[$Miners_Key] = $true
         $Miners_Path = Get-ChildItem ".\Stats\Miners\*-$($Miner_Name)_$($Algo)_HashRate.txt" | Where-Object -FilterScript {$_.Name -match "^(AMD|CPU|NVIDIA)-$($Miner_Name)_$($Algo)_HashRate.txt$"}
 
         if ($Miners_Path -and $Miners_Path.LastWriteTime.ToUniversalTime() -lt $JsonUri_Dates[$_.BaseName]) {
-            Get-ChildItem ".\Stats\Miners\*-$($Miner_Name)_$($Algo)_HashRate.txt" | Remove-Item -ErrorAction Ignore
+            Get-ChildItem ".\Stats\Miners\*-$($Miner_Name -replace '-.+')-$($Miner_Name -replace '^.+?-' -replace '-','*')*_$($Algo)_HashRate.txt" | Remove-Item -ErrorAction Ignore
             $text += "$($Miner_Name -replace '-(C|G)PU.+$')/$($_.DeviceModel)/$($Algo)`n"
             $count++
             if ($SecondAlgo -ne '') {
-                Get-ChildItem ".\Stats\Miners\*-$($Miner_Name)_$($SecondAlgo)_HashRate.txt" | Remove-Item -ErrorAction Ignore
+                Get-ChildItem ".\Stats\Miners\*-$($Miner_Name -replace '-.+')-$($Miner_Name -replace '^.+?-' -replace '-','*')*_$($SecondAlgo)_HashRate.txt" | Remove-Item -ErrorAction Ignore
             }
         }
     }
