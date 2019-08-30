33dfca61e82b97a41b2629958ad588250f3666df
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-08-30 12:15:21 +0200

    Update miner Gminer to v1.61 (amd,nvidia)
    - improved compatibility with large rigs (>= 13 GPUs)
    - improved launch time of miner

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -9,16 +9,16 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.60-gminer/gminer_1_60_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.61-gminer/gminer_1_61_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.60-gminer/gminer_1_60_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.61-gminer/gminer_1_61_windows64.zip"
 }
 $ManualUri = "https://bitcointalk.org/index.php?topic=5034735.0"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "1.59"
+$Version = "1.61"
 
 if (-not $Session.DevicesByTypes.AMD -and -not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
 
