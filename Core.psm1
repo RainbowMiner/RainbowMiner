0461c81e57f58dcb935202a8fb3eb35fd3601180
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-09-03 14:31:44 +0200

    Update regions
    - allow finer region selection
    - added Southeast-Asia (India, Singapore, etc.)

--- Core.psm1
+++ Core.psm1
@@ -399,8 +399,12 @@ function Invoke-Core {
         }
         $Session.Config.Algorithm = @($Session.Config.Algorithm | ForEach-Object {Get-Algorithm $_} | Where-Object {$_} | Select-Object -Unique)
         $Session.Config.ExcludeAlgorithm = @($Session.Config.ExcludeAlgorithm | ForEach-Object {Get-Algorithm $_} | Where-Object {$_} | Select-Object -Unique)
-        $Session.Config.Region = $Session.Config.Region | ForEach-Object {Get-Region $_}
+        $Session.Config.Region = $Session.Config.Region | ForEach-Object {Get-Region $_} | Select-Object -First 1
         $Session.Config.DefaultPoolRegion = @($Session.Config.DefaultPoolRegion | ForEach-Object {Get-Region $_} | Where-Object {$_} | Select-Object -Unique)
+        if ($Session.Config.Region -match '-') {
+            $WiderRegion = $Session.Config.Region -replace "^.+-"
+            $Session.Config.DefaultPoolRegion = @($WiderRegion) + @($Session.Config.DefaultPoolRegion | Where-Object {$_ -ne $WiderRegion} | Select-Object)
+        }
         $Session.Config.Currency = @($Session.Config.Currency | ForEach-Object {$_.ToUpper()} | Where-Object {$_})
         $Session.Config.UIstyle = if ($Session.Config.UIstyle -ne "full" -and $Session.Config.UIstyle -ne "lite") {"full"} else {$Session.Config.UIstyle}
         $Session.Config.PowerPriceCurrency = $Session.Config.PowerPriceCurrency | ForEach-Object {$_.ToUpper()}
