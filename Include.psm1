c898bcbea0a0256d63fdeb612576c495e80604fa
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-09-11 14:51:20 +0200

    Update core
    - fix line endings

--- Include.psm1
+++ Include.psm1
@@ -1722,9 +1722,9 @@ function Start-SubProcessInScreen {
     $Cmd += "  done"
     $Cmd += ")"
     $Cmd += "screen -S $($ScreenName) -d -m", "sleep .1"
-    $Cmd += "screen -S $($ScreenName) -X stuff $`"cd /`"\n", "sleep .1"
-    $Cmd += "screen -S $($ScreenName) -X stuff $`"cd '$WorkingDirectory'`"\n", "sleep .1"
-    $Cmd += "screen -S $($ScreenName) -X stuff $`"start-stop-daemon --start --make-pidfile --chdir '$WorkingDirectory' --pidfile '$PIDPath' --exec '$FilePath' -- $ArgumentList`"\n", "sleep .1"
+    $Cmd += "screen -S $($ScreenName) -X stuff $`"cd /\n`"", "sleep .1"
+    $Cmd += "screen -S $($ScreenName) -X stuff $`"cd '$WorkingDirectory'\n`"", "sleep .1"
+    $Cmd += "screen -S $($ScreenName) -X stuff $`"start-stop-daemon --start --make-pidfile --chdir '$WorkingDirectory' --pidfile '$PIDPath' --exec '$FilePath' -- $ArgumentList\n`"", "sleep .1"
 
     Set-BashFile -FilePath $PIDbash -Cmd $Cmd
     & chmod +x $PIDBash > $null
