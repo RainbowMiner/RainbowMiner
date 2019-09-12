10f74ed707997742331b4b603025fd51e449196f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-09-12 15:10:47 +0200

    Update start.sh
    - fix CUDA device order

--- start.sh
+++ start.sh
@@ -2,6 +2,14 @@
 
 cd "$(dirname "$0")"
 
+export GPU_FORCE_64BIT_PTR=1
+export GPU_MAX_HEAP_SIZE=100
+export GPU_USE_SYNC_OBJECTS=1
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_WORKGROUP_SIZE=256
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
+
 command="& {./RainbowMiner.ps1 -configfile ./Config/config.txt; exit \$lastexitcode}"
 
 while true; do
