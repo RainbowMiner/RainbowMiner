0c015ee2d62ce5f976d9888acfc4e6de91ad2443
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-09-12 13:40:34 +0200

    Update Include.psm1

--- Include.psm1
+++ Include.psm1
@@ -1924,7 +1924,6 @@ function Start-SubProcessInScreen {
                 ArgumentList     = ""
                 WorkingDirectory = $WorkingDirectory
                 PassThru         = $true
-                RedirectStandardOutput  = "/dev/null"
                 RedirectStandardError = "/dev/null"
             }
             if ($null -ne (Start-Process @ProcessParams)) {$started=$true}
