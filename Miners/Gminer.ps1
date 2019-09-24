3fc779b471dd6583acd09cdfc595c4997993dbb8
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-09-24 23:42:58 +0200

    Update miner Gminer to v1.66 (amd,nvidia)
    - significant performance improvements for cuckarood29 (+10%)

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -9,16 +9,16 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.65-gminer/gminer_1_65_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.66-gminer/gminer_1_66_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.65-gminer/gminer_1_65_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.66-gminer/gminer_1_66_windows64.zip"
 }
 $ManualUri = "https://bitcointalk.org/index.php?topic=5034735.0"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "1.65"
+$Version = "1.66"
 
 if (-not $Session.DevicesByTypes.AMD -and -not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
 
