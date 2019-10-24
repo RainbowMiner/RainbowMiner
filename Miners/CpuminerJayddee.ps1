d56ee4aafd907dfe1a53657a1ab6b9eb0485469f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-10-24 21:59:35 +0200

    Update miner CpuminerJayddee to v3.9.9.1 (cpu)
    Fixed a day1 bug that could cause the miner to idle for up to 2 minutes
    under certain circumstances.
    Redesigned summary stats report now includes session statistics.
    More robust handling of statistics to reduce corruption.
    Removed --hide-diff option.
    Better handling of cpu-affinity with more than 64 CPUs.

--- Miners/CpuminerJayddee.ps1
+++ Miners/CpuminerJayddee.ps1
@@ -9,15 +9,15 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\CPU-JayDDee\cpuminer-$($f=$Global:GlobalCPUInfo.Features;$(if($f.avx2 -and $f.sha -and $f.aes){'zen'}elseif($f.avx2 -and $f.aes){'avx2'}elseif($f.sse42 -and $f.aes){'aes-sse42'}elseif($f.sse42){'sse42'}else{'sse2'}))"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.9.9-jayddee/cpuminer-opt-3.9.9-linux.7z"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.9.9.1-jayddee/cpuminer-opt-3.9.9.1-linux.7z"
 } else {
     $Path = ".\Bin\CPU-JayDDee\cpuminer-$($f=$Global:GlobalCPUInfo.Features;$(if($f.avx2 -and $f.sha -and $f.aes){'zen'}elseif($f.avx2 -and $f.aes){'avx2'}elseif($f.avx -and $f.aes){'avx'}elseif($f.sse42 -and $f.aes){'aes-sse42'}else{'sse2'})).exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.9.9-jayddee/cpuminer-opt-3.9.9-windows.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.9.9.1-jayddee/cpuminer-opt-3.9.9.1-windows.zip"
 }
 $ManualUri = "https://github.com/JayDDee/cpuminer-opt/releases"
 $Port = "500{0:d2}"
 $DevFee = 0.0
-$Version = "3.9.9"
+$Version = "3.9.9.1"
 
 if (-not $Session.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No CPU present in system
 
