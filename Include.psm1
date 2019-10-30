1a38e4ab219e747c87f2552e28de9833aaee3714
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-10-30 10:56:36 +0100

    Update core
    - some RV

--- Include.psm1
+++ Include.psm1
@@ -111,6 +111,7 @@ function Get-PoolPayoutCurrencies {
     if (-not (Test-Path Variable:Global:PoolFields)) {
         $Setup = Get-ChildItemContent ".\Data\PoolsConfigDefault.ps1" | Select-Object -ExpandProperty Content
         $Global:PoolFields = @($Setup.PSObject.Properties.Value | Where-Object {$_.Fields} | Foreach-Object {$_.Fields.PSObject.Properties.Name} | Select-Object -Unique) + @("Worker","DataWindow","Penalty","Algorithm","ExcludeAlgorithm","CoinName","ExcludeCoin","CoinSymbol","ExcludeCoinSymbol","MinerName","ExcludeMinerName","FocusWallet","Wallets","EnableAutoCoin","EnablePostBlockMining") | Select-Object -Unique | Sort-Object
+        Remove-Variable "Setup"
     }
     @($Pool.PSObject.Properties) | Where-Object Membertype -eq "NoteProperty" | Where-Object {$_.Value -is [string] -and ($_.Value.Length -gt 2 -or $_.Value -eq "`$Wallet" -or $_.Value -eq "`$$($_.Name)") -and $Global:PoolFields -inotcontains $_.Name -and $_.Name -notmatch "-Params$"} | Select-Object Name,Value -Unique | Sort-Object Name,Value | Foreach-Object{$Payout_Currencies | Add-Member $_.Name $_.Value}
     $Payout_Currencies
@@ -6031,6 +6032,7 @@ Param(
     if (-not (Test-Path Variable:Global:Asyncloader)) {
         if ($delay) {Start-Sleep -Milliseconds $delay}
         Invoke-GetUrl -JobData $JobData -JobKey $JobKey
+        Remove-Variable "JobData"
         return
     }
     
