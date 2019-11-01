67568b6a37899639d8fdbd944ac7ae4b350cdc16
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-11-01 13:24:01 +0100

    Update miner CcminerMTP
    - maximum number of GPU is 6 for this miner

--- Miners/CcminerMtp.ps1
+++ Miners/CcminerMtp.ps1
@@ -84,7 +84,7 @@ $Session.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-O
         $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
 
 		foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)")) {
-			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and $Pools.$Algorithm_Norm.Name -notmatch "(NiceHash|MiningRigRentals)") {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and $Miner_Device.Count -le 6 -and $Pools.$Algorithm_Norm.Name -notmatch "(NiceHash|MiningRigRentals)") {
 				$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
 				[PSCustomObject]@{
 					Name           = $Miner_Name
