c181afde5e699f8e2b31b6c1a6d4adf030cbc0d1
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-11-14 13:53:37 +0100

    Update miner Gminer to v1.74 (amd,nvidia)
    - significant performance improvements for Cuckatoo31 algorithm

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -9,16 +9,16 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.73-gminer/gminer_1_73_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.74-gminer/gminer_1_74_linux64.tar.gz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.73-gminer/gminer_1_73_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.74-gminer/gminer_1_74_windows64.zip"
 }
 $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "1.73"
+$Version = "1.74"
 
 if (-not $Session.DevicesByTypes.AMD -and -not $Session.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
 
