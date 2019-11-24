e0927ae9b80fe5b739f8b54d8311122ba190e785
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-11-24 13:31:12 +0100

    Update AlgorithmsConfigDefault.ps1
    - BMW512 is a good notch faster on Profile4

--- Data/AlgorithmsConfigDefault.ps1
+++ Data/AlgorithmsConfigDefault.ps1
@@ -2,6 +2,7 @@
     "Allium" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile="Profile4"}
     "Balloon" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile="Profile4"}
     "Bitcore" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile="Profile4"}
+    "BMW512" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile="Profile4"}
     "Dedal" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile="Profile4"}
     "Equihash24x7" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="5";OCprofile="Profile5"}
     "GLTAstralHash" = [PSCustomObject]@{Penalty = "0";MinHashrate = "0";MinWorkers = "0";MaxTimeToFind="0";MSIAprofile="4";OCprofile="Profile4"}
