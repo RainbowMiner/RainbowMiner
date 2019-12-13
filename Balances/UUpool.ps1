11e7f9093fe8673a831b1faeb183fad0918d289d
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-12-13 19:32:03 +0100

    Update balances UUpool
    - Rates from Session to Global

--- Balances/UUpool.ps1
+++ Balances/UUpool.ps1
@@ -30,7 +30,7 @@ $Pool_Request | Where-Object {$Pool_Currency = $_.coin -replace "(29|31)" -repla
             [PSCustomObject]@{
                 Caption     = "$($Name) ($($_.Name))"
 				BaseName    = $Name
-                Currency    = if ($Session.Rates."$($_.coin)") {$_.coin} else {$Pool_Currency}
+                Currency    = if ($Global:Rates."$($_.coin)") {$_.coin} else {$Pool_Currency}
                 Balance     = [Decimal]$Request.balance / 1e8
                 Pending     = [Decimal]0
                 Total       = [Decimal]$Request.balance / 1e8
