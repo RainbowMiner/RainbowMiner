b0ca2fe5adc29735494b1f6e0f9c3a51ea228e4e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-12-14 18:27:11 +0100

    Update pool WTM
    - get rid of expand-property

--- Pools/WhatToMine.ps1
+++ Pools/WhatToMine.ps1
@@ -10,7 +10,7 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 
 $Pool_Request = [PSCustomObject]@{}
 try {
-    $Pool_Request = Invoke-RestMethodAsync (Get-WhatToMineUrl) -tag $Name -cycletime 120 | Select-Object -ExpandProperty coins
+    $Pool_Request = Invoke-RestMethodAsync (Get-WhatToMineUrl) -tag $Name -cycletime 120
 }
 catch {
     if ($Error.Count){$Error.RemoveAt(0)}
@@ -18,7 +18,7 @@ catch {
     return
 }
 
-if (-not $Pool_Request -or ($Pool_Request.PSObject.Properties.Name | Measure-Object).Count -lt 10) {
+if (-not $Pool_Request.coins -or ($Pool_Request.coins.PSObject.Properties.Name | Measure-Object).Count -lt 10) {
     Write-Log -Level Warn "Pool API1 ($Name) returned nothing. "
     return
 }
@@ -29,9 +29,9 @@ $WTMWallets = $Pools | Where-Object {$_.Algorithm -notmatch '-'} | Foreach-Objec
 
 $Pool_Coins = @($WTMWallets.CoinSymbol | Select-Object)
 
-$Pool_Request.PSObject.Properties.Name | Where-Object {$Pool_Coins -icontains $Pool_Request.$_.tag} | ForEach-Object {
-    $Pool_Currency   = $Pool_Request.$_.tag
-    $Pool_Algorithm  = $Pool_Request.$_.algorithm -replace "[^a-z0-9]+"
+$Pool_Request.coins.PSObject.Properties.Name | Where-Object {$Pool_Coins -icontains $Pool_Request.coins.$_.tag} | ForEach-Object {
+    $Pool_Currency   = $Pool_Request.coins.$_.tag
+    $Pool_Algorithm  = $Pool_Request.coins.$_.algorithm -replace "[^a-z0-9]+"
     if (-not $Pool_Algorithms.ContainsKey($Pool_Algorithm)) {$Pool_Algorithms.$Pool_Algorithm = Get-Algorithm $Pool_Algorithm}
     $Pool_Algorithm_Norm = $Pool_Algorithms.$Pool_Algorithm
 
@@ -42,9 +42,9 @@ $Pool_Request.PSObject.Properties.Name | Where-Object {$Pool_Coins -icontains $P
         $WTMWallets = $WTMWallets | Where-Object {$_.Algorithm -ne $Pool_Algorithm_Norm -or $_.CoinSymbol -ne $Pool_Currency}
 
         if (Test-Path ".\Stats\Pools\$($Name)_$($Pool_Algorithm_Norm)_$($Pool_Currency)_Profit") {
-            $Stat = Set-Stat -Name "$($Name)_$($Pool_Algorithm_Norm)_$($Pool_Currency)_Profit" -Value ([Double]$Pool_Request.$_.btc_revenue / $Divisor) -Duration $StatSpan -ChangeDetection $true -Quiet
+            $Stat = Set-Stat -Name "$($Name)_$($Pool_Algorithm_Norm)_$($Pool_Currency)_Profit" -Value ([Double]$Pool_Request.coins.$_.btc_revenue / $Divisor) -Duration $StatSpan -ChangeDetection $true -Quiet
         } else {
-            $Stat = Set-Stat -Name "$($Name)_$($Pool_Algorithm_Norm)_$($Pool_Currency)_Profit" -Value ([Double]$Pool_Request.$_.btc_revenue24 / $Divisor) -Duration (New-TimeSpan -Days 1) -ChangeDetection $false -Quiet
+            $Stat = Set-Stat -Name "$($Name)_$($Pool_Algorithm_Norm)_$($Pool_Currency)_Profit" -Value ([Double]$Pool_Request.coins.$_.btc_revenue24 / $Divisor) -Duration (New-TimeSpan -Days 1) -ChangeDetection $false -Quiet
         }
 
         [PSCustomObject]@{
@@ -62,7 +62,7 @@ if (-not ($WTMWallets | Measure-Object).Count) {return}
 
 $Pool_Request = [PSCustomObject]@{}
 try {
-    $Pool_Request = Invoke-RestMethodAsync "https://whattomine.com/calculators.json" -tag $Name -cycletime (12*3600) | Select-Object -ExpandProperty coins
+    $Pool_Request = Invoke-RestMethodAsync "https://whattomine.com/calculators.json" -tag $Name -cycletime (12*3600)
 }
 catch {
     if ($Error.Count){$Error.RemoveAt(0)}
@@ -70,16 +70,16 @@ catch {
     return
 }
 
-if (-not $Pool_Request -or ($Pool_Request.PSObject.Properties.Name | Measure-Object).Count -lt 10) {
+if (-not $Pool_Request.coins -or ($Pool_Request.coins.PSObject.Properties.Name | Measure-Object).Count -lt 10) {
     Write-Log -Level Warn "Pool API2 ($Name) returned nothing. "
     return
 }
 
 $Pool_Coins = @($WTMWallets.CoinSymbol | Select-Object)
 
-$Pool_Request.PSObject.Properties.Name | Where-Object {$Pool_Coins -icontains $Pool_Request.$_.tag} | ForEach-Object {
-    $Pool_Currency   = $Pool_Request.$_.tag
-    $Pool_Algorithm  = $Pool_Request.$_.algorithm -replace "[^a-z0-9]+"
+$Pool_Request.coins.PSObject.Properties.Name | Where-Object {$Pool_Coins -icontains $Pool_Request.coins.$_.tag} | ForEach-Object {
+    $Pool_Currency   = $Pool_Request.coins.$_.tag
+    $Pool_Algorithm  = $Pool_Request.coins.$_.algorithm -replace "[^a-z0-9]+"
     if (-not $Pool_Algorithms.ContainsKey($Pool_Algorithm)) {$Pool_Algorithms.$Pool_Algorithm = Get-Algorithm $Pool_Algorithm}
     $Pool_Algorithm_Norm = $Pool_Algorithms.$Pool_Algorithm
 
@@ -91,7 +91,7 @@ $Pool_Request.PSObject.Properties.Name | Where-Object {$Pool_Coins -icontains $P
 
         $Pool_CoinRequest = [PSCustomObject]@{}
         try {
-            $Pool_CoinRequest = Invoke-RestMethodAsync "https://whattomine.com/coins/$($Pool_Request.$_.id).json?hr=10&p=0&fee=0.0&cost=0.0&hcost=0.0" -tag $Name -cycletime 120
+            $Pool_CoinRequest = Invoke-RestMethodAsync "https://whattomine.com/coins/$($Pool_Request.coins.$_.id).json?hr=10&p=0&fee=0.0&cost=0.0&hcost=0.0" -tag $Name -cycletime 120
         } catch {
            if ($Error.Count){$Error.RemoveAt(0)} 
         }
