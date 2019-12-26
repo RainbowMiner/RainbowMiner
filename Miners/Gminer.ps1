3a04885510992d886ebf19fbf93c2e3ceed4a724
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-12-26 18:39:58 +0100

    Update miner Gminer to v1.91 (amd,nvidia)
    - improved performance for CryptoNightBBC on some GPUs (1080, 1080ti, mining edition cards)
    - improved cleanJobs behavior for BBC stratum, should improve hashrate on pool side

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -9,16 +9,16 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.90-gminer/gminer_1_90_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.91-gminer/gminer_1_91_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.90-gminer/gminer_1_90_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.91-gminer/gminer_1_91_windows64.zip"
 }
 $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "1.90"
+$Version = "1.91"
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
 
