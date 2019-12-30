2fdd15d94b1336c18a74049c73970735c911c4b6
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-12-30 23:56:15 +0100

    Update miner Gminer to v1.92 (amd,nvidia)
    - fixed miner id for BBC (mining will work on bbcpool.io)
    
    NOTE: we cannot collect devfee for BBC
    if you like miner you can donate BTC on follow address:
    1F81grpyFVZx4gotHZems1gDcnrvxnScwm

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -9,16 +9,16 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.91-gminer/gminer_1_91_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.92-gminer/gminer_1_92_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.91-gminer/gminer_1_91_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.92-gminer/gminer_1_92_windows64.zip"
 }
 $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "1.91"
+$Version = "1.92"
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
 
