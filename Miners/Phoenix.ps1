91eb68243e157bde4bd680d4a5b9f0281170fb68
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-01-09 12:27:08 +0100

    Update miner Phoenix to v4.9c (amd,nvidia)
    - Adding option -rate 2 to use the command name "eth_submitHashRate" instead of "eth_submitHashrate" when solo mining
    - Adding support for AMD Linux drivers 19.50-967956
    - Other small fixes and improvements

--- Miners/Phoenix.ps1
+++ Miners/Phoenix.ps1
@@ -9,16 +9,16 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Phoenix\PhoenixMiner"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v4.9b-phoenix/PhoenixMiner_4.9b_Linux.tar.gz"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v4.9c-phoenix/PhoenixMiner_4.9c_Linux.tar.gz"
 } else {
     $Path = ".\Bin\GPU-Phoenix\PhoenixMiner.exe"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v4.9b-phoenix/PhoenixMiner_4.9b_Windows.7z"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v4.9c-phoenix/PhoenixMiner_4.9c_Windows.7z"
 }
 $ManualURI = "https://bitcointalk.org/index.php?topic=2647654.0"
 $Port = "308{0:d2}"
 $DevFee = 0.65
 $Cuda = "8.0"
-$Version = "4.9b"
+$Version = "4.9c"
 
 if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $Global:DeviceCache.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No GPU present in system
 
