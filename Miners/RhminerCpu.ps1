1fe2da10f659fbc8ad0666f30afc5faed4d901c1
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-01-22 15:18:01 +0100

    Update miner RHminer to v2.1 (cpu)
    - Optimized RandomHash2 with up to 80% more speed. (On some system rhminer 2.1 is up to 350% faster than official miner)
    - Fixed linux binary where main thread was exiting after miner startup.
    - Linux zip files now contain rhminer as an executable and not a simple file.
    - Fixed windows packages and removed extra folders

--- Miners/RhminerCpu.ps1
+++ Miners/RhminerCpu.ps1
@@ -9,15 +9,15 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\CPU-RHminer\rhminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.0-rhminer/rhminer.2.0.Linux.CPU.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.1-rhminer/rhminer.2.1.Linux.CPU.zip"
 } else {
     $Path = ".\Bin\CPU-RHminer\rhminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.0-rhminer/rhminer.2.0.Windows.CPU.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.1-rhminer/rhminer.2.1.Windows.CPU.zip"
 }
 $ManualUri = "https://github.com/polyminer1/rhminer/releases"
 $Port = "131{0:d2}"
 $DevFee = 1.0
-$Version = "2.0"
+$Version = "2.1"
 
 if (-not $Global:DeviceCache.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No CPU present in system
 
