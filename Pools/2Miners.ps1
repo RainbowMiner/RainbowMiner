5180e4d5028e69b1362ce7778aab61f642efb5d5
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-01-26 20:43:58 +0100

    Update pool 2Miners
    - fix issue #848 : all ports follow the direction, lower number equals lower difficulty, except for XZC it's the other way round.

--- Pools/2Miners.ps1
+++ Pools/2Miners.ps1
@@ -138,7 +138,7 @@ $Pools_Data | Where-Object {$Wallets."$($_.symbol -replace "\d")" -or $InfoOnly}
     if ($ok) {
         $Pool_Hosts = @()
         $Pool_Wallet = Get-WalletWithPaymentId $Wallets.$Pool_Currency -pidchar '.'
-        $Pool_HostStatus | Where-Object {$_.host -notmatch 'solo-' -and $_.host -match "$($Pool_Host)" -and $Pool_Hosts -notcontains "$($_.host)$([int]$_.port -ge 10000)"} | Select-Object host,port | Foreach-Object {
+        $Pool_HostStatus | Where-Object {$_.host -notmatch 'solo-' -and $_.host -match "$($Pool_Host)" -and $Pool_Hosts -notcontains "$($_.host)$([int]$_.port -ge 10000)"} | Select-Object host,port | Sort-Object -Descending:$($Pool_Currency -eq "XZC") {[int]$_.port} | Foreach-Object {
             $SSL = [int]$_.port -ge 10000
             $Pool_Hosts += "$($_.host)$($SSL)"
             [PSCustomObject]@{
