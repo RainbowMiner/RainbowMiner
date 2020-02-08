2bdb0ef96ce165a7d0ee6f39b290a3ccaf7e7442
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-02-08 15:40:22 +0100

    Update Cleanup.ps1

--- Cleanup.ps1
+++ Cleanup.ps1
@@ -697,6 +697,9 @@ try {
         $AddAlgorithm += @("Cuckaroo30","ScryptN2")
         Get-ChildItem "Cache" -Filter "9DFA752C6FD0FF15B7E1F4A10E54B228.asy" -File | Foreach-Object {Remove-Item $_.FullName -Force;$ChangesTotal++}
         Get-ChildItem "Cache" -Filter "AB88C0C3CF2AD655BE82469CE6957F2B.asy" -File | Foreach-Object {Remove-Item $_.FullName -Force;$ChangesTotal++}
+    }
+
+    if ($Version -le (Get-Version "4.5.2.8")) {
         if ($IsLinux) {
             $RemoveMinerStats += @("*-Xmrig-*_HashRate.txt")
         }
