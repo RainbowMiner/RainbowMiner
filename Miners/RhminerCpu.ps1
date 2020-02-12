26dd8c43e49d7d47ab2feeb9de0964457f7ed62b
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-02-12 23:38:45 +0100

    Update miner RHminer to v2.2b (cpu)
    - Improved pool connection
    - code cleanup and minor improvements (linux and macos)

--- Miners/RhminerCpu.ps1
+++ Miners/RhminerCpu.ps1
@@ -9,15 +9,15 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\CPU-RHminer\rhminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.2-rhminer/rhminer.2.2.Linux.CPU.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.2b-rhminer/rhminer.2.2b.Linux.CPU.zip"
 } else {
     $Path = ".\Bin\CPU-RHminer\rhminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.2-rhminer/rhminer.2.2.Windows.CPU.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.2b-rhminer/rhminer.2.2b.Windows.CPU.zip"
 }
 $ManualUri = "https://github.com/polyminer1/rhminer/releases"
 $Port = "131{0:d2}"
 $DevFee = 1.0
-$Version = "2.2"
+$Version = "2.2b"
 
 if (-not $Global:DeviceCache.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No CPU present in system
 
