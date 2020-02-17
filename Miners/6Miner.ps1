8d06e926cf51bfd7c0c627df84243c144ce76774
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-02-17 18:13:15 +0100

    Update miner 6Miner
    - enable for CPU, as well

--- Miners/6Miner.ps1
+++ Miners/6Miner.ps1
@@ -8,10 +8,10 @@ param(
 if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
-    $Path = ".\Bin\GPU-6miner\6miner"
+    $Path = ".\Bin\ANY-6miner\6miner"
     $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.0.5-6miner/6miner-v0.0.5-amd64-linux.tar.gz"
 } else {
-    $Path = ".\Bin\GPU-6miner\6miner.exe"
+    $Path = ".\Bin\ANY-6miner\6miner.exe"
     $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.0.5-6miner/6miner-v0.0.5-x64-windows.zip"
 }
 $Port = "356{0:d2}"
@@ -19,7 +19,7 @@ $ManualURI = "https://github.com/6block/6miner/releases"
 $DevFee = 3.0
 $Version = "0.0.5"
 
-if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
+if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.CPU -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "bl2bsha3"; Params = ""; ExtendInterval = 2} #Blake2b+SHA3 / HNS
@@ -29,7 +29,7 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 
 if ($InfoOnly) {
     [PSCustomObject]@{
-        Type      = @("AMD","NVIDIA")
+        Type      = @("AMD","CPU","NVIDIA")
         Name      = $Name
         Path      = $Path
         Port      = $Miner_Port
@@ -41,15 +41,22 @@ if ($InfoOnly) {
     return
 }
 
-foreach ($Vendor in @("AMD","NVIDIA")) {
-    $Global:DeviceCache.DevicesByTypes.$Vendor | Select-Object Vendor, Model -Unique | ForEach-Object {
+foreach ($Miner_Vendor in @("AMD","CPU","NVIDIA")) {
+    $Global:DeviceCache.DevicesByTypes.$Miner_Vendor | Select-Object Vendor, Model -Unique | ForEach-Object {
         $First = $true
-        $Miner_Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)" | Where-Object Model -EQ $_.Model
+        $Miner_Device = $Global:DeviceCache.DevicesByTypes.$Miner_Vendor | Where-Object Model -EQ $_.Model
         $Miner_Model = $_.Model
 
         $Commands | ForEach-Object {
 
             $Algorithm_Norm_0 = Get-Algorithm $_.MainAlgorithm
+
+            if ($Miner_Vendor -eq "CPU") {
+                $CPUThreads = if ($Session.Config.Miners."$Name-CPU-$Algorithm_Norm_0".Threads)  {$Session.Config.Miners."$Name-CPU-$Algorithm_Norm_0".Threads}  elseif ($Session.Config.Miners."$Name-CPU".Threads)  {$Session.Config.Miners."$Name-CPU".Threads}  elseif ($Session.Config.CPUMiningThreads)  {$Session.Config.CPUMiningThreads}
+                $CPUAffinity= if ($Session.Config.Miners."$Name-CPU-$Algorithm_Norm_0".Affinity) {$Session.Config.Miners."$Name-CPU-$Algorithm_Norm_0".Affinity} elseif ($Session.Config.Miners."$Name-CPU".Affinity) {$Session.Config.Miners."$Name-CPU".Affinity} elseif ($Session.Config.CPUMiningAffinity) {$Session.Config.CPUMiningAffinity}
+
+                $DeviceParams = "$(if ($CPUThreads){" --threads=$CPUThreads"})$(if ($false -and $CPUAffinity){" --cpu-affinity $CPUAffinity"})"
+            }
 
 		    foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)")) {
 			    if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
@@ -65,7 +72,7 @@ foreach ($Vendor in @("AMD","NVIDIA")) {
 					    DeviceName     = $Miner_Device.Name
 					    DeviceModel    = $Miner_Model
 					    Path           = $Path
-                        Arguments      = "-a hns/bl2bsha3 -o $($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) -m opencl --opcl-vendor=$($Vendor.ToLower()) --opcl-no-cuda-fix --devices=$DeviceIDsAll"
+                        Arguments      = "-a hns/bl2bsha3 -o $($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $(if ($Miner_Vendor -eq "CPU") {"-m cpu$DeviceParams"} else {"-m opencl --opcl-vendor=$($Miner_Vendor.ToLower()) --opcl-no-cuda-fix --devices=$DeviceIDsAll"})"
 					    HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Week }
 					    API            = "SixMinerWrapper"
 					    Port           = $Miner_Port
