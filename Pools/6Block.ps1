ee8b7d674eeec5492dd6a94ee99e8ae178fa96a2
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-02-24 19:05:27 +0100

    Update pool 6Block
    - just to be sure

--- Pools/6Block.ps1
+++ Pools/6Block.ps1
@@ -39,7 +39,7 @@ $Pools_Data | Where-Object {$Wallets."$($_.symbol)" -or $InfoOnly} | ForEach-Obj
     if (-not $InfoOnly) {
         try {
             $WebRequest = Invoke-RestMethodAsync "https://6block.com/en" -tag $Name -timeout 15 -cycletime 120
-            $WebParams = if ($WebRequest -match "}}\(([^\)]+)\)") {$Matches[1] -split ',' | Foreach-Object {$_  -replace '^"' -replace '"$'}}
+            $WebParams = if ($WebRequest -match "}}\(([^\)]+)\)") {$Matches[1] -split ',' | Foreach-Object {$_  -replace '^"' -replace '"$'}} else {@()}
             foreach ($c in @("statPool","found24H","activeMiners")) {
                 if ($WebRequest -match "$($c):(.+?)[,}]") {
                     $Pool_Request | Add-Member $c ($Matches[1] -replace '^"' -replace '"$') -Force
