98c7d976c7e4e4a7bbf649d00cdc2e707c734453
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-02-28 14:05:09 +0100

    Bump version to v4.5.4.2

--- RainbowMiner.ps1
+++ RainbowMiner.ps1
@@ -269,7 +269,7 @@ $EnableMinerStatus = $true
 
 Initialize-Session
 
-$Session.Version         = "4.5.4.1"
+$Session.Version         = "4.5.4.2"
 $Session.MainWindowTitle = "RainbowMiner v$($Session.Version)"
 $Session.SetupOnly       = $SetupOnly
 $Session.LogLevel        = $LogLevel
