cbd55a010516035380246be96f0384ce32a6c9e5
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-03-30 17:49:03 +0200

    Update miner NBminer to v28.1 (amd,nvidia)
    - Support HNS & HNS+ETH mining on NiceHash
    - 添加HNS、HNS+ETH在NiceHash挖矿的支持

--- Miners/NBminer.ps1
+++ Miners/NBminer.ps1
@@ -9,16 +9,16 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-NBMiner\nbminer"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v28.0-nbminer/NBMiner_28.0_Linux.tgz"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v28.1-nbminer/NBMiner_28.1_Linux.tgz"
 } else {
     $Path = ".\Bin\GPU-NBMiner\nbminer.exe"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v28.0-nbminer/NBMiner_28.0_Win.zip"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v28.1-nbminer/NBMiner_28.1_Win.zip"
 }
 $ManualURI = "https://github.com/NebuTech/NBMiner/releases"
 $Port = "340{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.1"
-$Version = "28.0"
+$Version = "28.1"
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
 
