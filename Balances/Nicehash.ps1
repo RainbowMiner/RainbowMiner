8a33b7cf5e48c8e043f388fea0170c7e4844f7d9
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-04-19 14:57:43 +0200

    Update balance Nicehash
    - add fallback for users with external wallets or no API-credentials

--- Balances/Nicehash.ps1
+++ Balances/Nicehash.ps1
@@ -5,20 +5,23 @@ param(
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 $PoolConfig = $Config.Pools.$Name
 
-if (-not ($PoolConfig.API_Key -and $PoolConfig.API_Secret -and $PoolConfig.OrganizationID)) {
-    Write-Log -Level Verbose "Cannot get balance on pool ($Name) - no API credentials specified. "
+if (!$PoolConfig.BTC) {
+    Write-Log -Level Verbose "Cannot get balance on pool ($Name) - no wallet address specified. "
     return
 }
 
 $Request_Balance = [PSCustomObject]@{}
 
-try {
-    $Request_Balance = Invoke-NHRequest "/main/api/v2/accounting/account2/BTC/" $PoolConfig.API_Key $PoolConfig.API_Secret $PoolConfig.OrganizationID
+if ($PoolConfig.API_Key -and $PoolConfig.API_Secret -and $PoolConfig.OrganizationID) {
+    try {
+        $Request_Balance = Invoke-NHRequest "/main/api/v2/accounting/account2/BTC/" $PoolConfig.API_Key $PoolConfig.API_Secret $PoolConfig.OrganizationID
+    }
+    catch {
+        if ($Error.Count){$Error.RemoveAt(0)}
+        Write-Log -Level Warn "Pool Accounts API ($Name) has failed. "
+    }
 }
-catch {
-    if ($Error.Count){$Error.RemoveAt(0)}
-    Write-Log -Level Warn "Pool Accounts API ($Name) has failed. "
-}
+
 if ($Request_Balance.active) {
     [PSCustomObject]@{
         Caption     = "$($Name) ($($Request_Balance.currency))"
@@ -30,5 +33,26 @@ if ($Request_Balance.active) {
         Payouts     = @()
         LastUpdated = (Get-Date).ToUniversalTime()
     }        
+} else {
+    $Request = [PSCustomObject]@{}
+    try {
+        $Request = Invoke-RestMethodAsync "https://api2.nicehash.com/main/api/v2/mining/external/$($PoolConfig.BTC)/rigs2/" -cycletime ($Config.BalanceUpdateMinutes*60)
+    }
+    catch {
+        if ($Error.Count){$Error.RemoveAt(0)}
+        Write-Log -Level Warn "Pool Mining API ($Name) has failed. "
+        return
+    }
+
+    [PSCustomObject]@{
+        Caption     = "$($Name) (BTC)"
+        BaseName    = $Name
+        Currency    = "BTC"
+        Balance     = [Decimal]$Request.externalBalance
+        Pending     = [Decimal]$Request.unpaidAmount
+        Total       = [Decimal]$Request.externalBalance + [Decimal]$Request.unpaidAmount
+        Payouts     = @()
+        LastUpdated = (Get-Date).ToUniversalTime()
+    }
 }
 
