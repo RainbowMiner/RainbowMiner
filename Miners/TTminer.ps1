8981fc94a39eec9f3ffda442e560b3d321bf0c7d
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-05-07 15:46:37 +0200

    Update miner TTminer to v5.0.1 (nvidia)
    I added a new error message that will help linux user to detect incomplete or missing cuda installation. Older versions said that the algo information is missing. That message is misleading on linux systems. The reason is in most cases that the nvidia builtins couldn't be loaded. They are required for TT.

--- Miners/TTminer.ps1
+++ Miners/TTminer.ps1
@@ -9,16 +9,16 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-TTminer\TT-Miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.0-ttminer/TT-Miner-5.0.0.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.1-ttminer/TT-Miner-5.0.1.tar.xz"
 } else {
     $Path = ".\Bin\NVIDIA-TTminer\TT-Miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.0-ttminer/TT-Miner-5.0.0.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.1-ttminer/TT-Miner-5.0.1.zip"
 }
 $ManualUri = "https://bitcointalk.org/index.php?topic=5025783.0"
 $Port = "333{0:d2}"
 $DevFee = 0.0
 $Cuda = "9.2"
-$Version = "5.0.0"
+$Version = "5.0.1"
 
 if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
