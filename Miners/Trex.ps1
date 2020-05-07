0924691789fffd8ff64df1e09952cb1460241210
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-05-07 22:02:09 +0200

    Update miner Trex v0.15.5 (nvidia)
    - Less memory consumption. Possible crash fix for "out of memory" error for GPUs with 3GB memory onboard.
    - Unblock feature of intensity setting.

--- Miners/Trex.ps1
+++ Miners/Trex.ps1
@@ -10,21 +10,21 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://github.com/trexminer/T-Rex/releases"
 $Port = "316{0:d2}"
 $DevFee = 1.0
-$Version = "0.15.4"
+$Version = "0.15.5"
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Trex\t-rex"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.4-trex/t-rex-0.15.4-linux-cuda10.0.tar.gz"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.5-trex/t-rex-0.15.5-linux-cuda10.0.tar.gz"
             Cuda = "10.0"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.4-trex/t-rex-0.15.4-linux-cuda9.2.tar.gz"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.5-trex/t-rex-0.15.5-linux-cuda9.2.tar.gz"
             Cuda = "9.2"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.4-trex/t-rex-0.15.4-linux-cuda9.1.tar.gz"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.5-trex/t-rex-0.15.5-linux-cuda9.1.tar.gz"
             Cuda = "9.1"
         }
     )
@@ -32,15 +32,15 @@ if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Trex\t-rex.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.4-trex/t-rex-0.15.4-win-cuda10.0.zip"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.5-trex/t-rex-0.15.5-win-cuda10.0.zip"
             Cuda = "10.0"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.4-trex/t-rex-0.15.4-win-cuda9.2.zip"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.5-trex/t-rex-0.15.5-win-cuda9.2.zip"
             Cuda = "9.2"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.4-trex/t-rex-0.15.4-win-cuda9.1.zip"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.15.5-trex/t-rex-0.15.5-win-cuda9.1.zip"
             Cuda = "9.1"
         }
     )
