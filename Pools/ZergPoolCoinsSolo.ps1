1297498965d95e8e515db4b865ae5224ddba35e6
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-05-09 15:06:37 +0200

    Update pool ZergPool
    - ProgPow BCI uses "stratum" connection

--- Pools/ZergPoolCoinsSolo.ps1
+++ Pools/ZergPoolCoinsSolo.ps1
@@ -166,7 +166,7 @@ $PoolCoins_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | S
                 Hashrate      = $Stat.HashRate_Live
                 BLK           = $Stat.BlockRate_Average
                 TSL           = $Pool_TSL
-                EthMode       = if ($Pool_Algorithm_Norm -match "^(Ethash|ProgPow)") {"ethproxy"} elseif ($Pool_Algorithm_Norm -match "^(KawPOW)") {"stratum"} else {$null}
+                EthMode       = if ($Pool_Algorithm_Norm -eq "Ethash") {"ethproxy"} elseif ($Pool_Algorithm_Norm -match "^(ProgPow|KawPOW)") {"stratum"} else {$null}
                 ErrorRatio    = $Stat.ErrorRatio
                 Name          = $Name
                 Penalty       = 0
