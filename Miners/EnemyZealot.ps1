b5122c45b2e18f0fd655c63071ef47fa06181743
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-05-09 16:30:45 +0200

    Update miner EnemyZealot
    - disable KawPOW on Nicehash

--- Miners/EnemyZealot.ps1
+++ Miners/EnemyZealot.ps1
@@ -57,25 +57,25 @@ if ($IsLinux) {
 if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{MainAlgorithm = "aergo"; Params = "-N 1"} #AeriumX, new in 1.11
-    #[PSCustomObject]@{MainAlgorithm = "bcd"; Params = "-N 1"} #Bcd, new in 1.20
-    #[PSCustomObject]@{MainAlgorithm = "bitcore"; Params = "-N 1"} #Bitcore
-    #[PSCustomObject]@{MainAlgorithm = "c11"; Params = "-N 1"} # New in 1.11
-    [PSCustomObject]@{MainAlgorithm = "hex"; Params = "-N 1"; ExtendInterval = 3; FaultTolerance = 0.5; HashrateDuration = "Day"} #HEX/XDNA, new in 1.15a
-    #[PSCustomObject]@{MainAlgorithm = "hsr"; Params = "-N 1"} #HSR
-    [PSCustomObject]@{MainAlgorithm = "kawpow"; Params = "-N 1"; ExtendInterval = 3; MinMemGB=3} #KawPOW/RVN, new in 2.5
-    #[PSCustomObject]@{MainAlgorithm = "phi"; Params = "-N 1"; ExtendInterval = 2} #PHI
-    #[PSCustomObject]@{MainAlgorithm = "phi2"; Params = "-N 1"} #PHI2, new in 1.12
-    #[PSCustomObject]@{MainAlgorithm = "poly"; Params = "-N 1"} #Polytimos
-    #[PSCustomObject]@{MainAlgorithm = "skunk"; Params = "-N 1"} #Skunk, new in 1.11
-    #[PSCustomObject]@{MainAlgorithm = "sonoa"; Params = "-N 1"} #Sonoa, new in 1.12 (testing)
-    #[PSCustomObject]@{MainAlgorithm = "timetravel"; Params = "-N 1"} #Timetravel8
-    #[PSCustomObject]@{MainAlgorithm = "tribus"; Params = "-N 1"} #Tribus, new in 1.10
-    #[PSCustomObject]@{MainAlgorithm = "x16r"; Params = "-N 10"; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"} #X16R
-    [PSCustomObject]@{MainAlgorithm = "x16rv2"; Params = "-N 10"; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"} #X16Rv2
-    #[PSCustomObject]@{MainAlgorithm = "x16s"; Params = "-N 1"; FaultTolerance = 0.5} #X16S (T-Rex faster)
-    #[PSCustomObject]@{MainAlgorithm = "x17"; Params = "-N 1"} #X17 (T-Rex has better numbers at the pool)
-    [PSCustomObject]@{MainAlgorithm = "xevan"; Params = "-N 1"} #Xevan, new in 1.09a
+    [PSCustomObject]@{MainAlgorithm = "aergo"; Params = "-N 1"; NH = $true} #AeriumX, new in 1.11
+    #[PSCustomObject]@{MainAlgorithm = "bcd"; Params = "-N 1"; NH = $true} #Bcd, new in 1.20
+    #[PSCustomObject]@{MainAlgorithm = "bitcore"; Params = "-N 1"; NH = $true} #Bitcore
+    #[PSCustomObject]@{MainAlgorithm = "c11"; Params = "-N 1"; NH = $true} # New in 1.11
+    [PSCustomObject]@{MainAlgorithm = "hex"; Params = "-N 1"; ExtendInterval = 3; FaultTolerance = 0.5; HashrateDuration = "Day"; NH = $true} #HEX/XDNA, new in 1.15a
+    #[PSCustomObject]@{MainAlgorithm = "hsr"; Params = "-N 1"; NH = $true} #HSR
+    [PSCustomObject]@{MainAlgorithm = "kawpow"; Params = "-N 1"; ExtendInterval = 3; MinMemGB=3; NH = $false} #KawPOW/RVN, new in 2.5
+    #[PSCustomObject]@{MainAlgorithm = "phi"; Params = "-N 1"; ExtendInterval = 2; NH = $true} #PHI
+    #[PSCustomObject]@{MainAlgorithm = "phi2"; Params = "-N 1"; NH = $true} #PHI2, new in 1.12
+    #[PSCustomObject]@{MainAlgorithm = "poly"; Params = "-N 1"; NH = $true} #Polytimos
+    #[PSCustomObject]@{MainAlgorithm = "skunk"; Params = "-N 1"; NH = $true} #Skunk, new in 1.11
+    #[PSCustomObject]@{MainAlgorithm = "sonoa"; Params = "-N 1"; NH = $true} #Sonoa, new in 1.12 (testing)
+    #[PSCustomObject]@{MainAlgorithm = "timetravel"; Params = "-N 1"; NH = $true} #Timetravel8
+    #[PSCustomObject]@{MainAlgorithm = "tribus"; Params = "-N 1"; NH = $true} #Tribus, new in 1.10
+    #[PSCustomObject]@{MainAlgorithm = "x16r"; Params = "-N 10"; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"; NH = $true} #X16R
+    [PSCustomObject]@{MainAlgorithm = "x16rv2"; Params = "-N 10"; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"; NH = $true} #X16Rv2
+    #[PSCustomObject]@{MainAlgorithm = "x16s"; Params = "-N 1"; FaultTolerance = 0.5; NH = $true} #X16S (T-Rex faster)
+    #[PSCustomObject]@{MainAlgorithm = "x17"; Params = "-N 1"; NH = $true} #X17 (T-Rex has better numbers at the pool)
+    [PSCustomObject]@{MainAlgorithm = "xevan"; Params = "-N 1"; NH = $true} #Xevan, new in 1.09a
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -119,7 +119,7 @@ $Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique 
         $Miner_Device = $Device | Where-Object {Test-VRAM $_ $MinMemGb}
 
 		foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)")) {
-			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and ($_.NH -or $Pools.$Algorithm_Norm.Name -ne "Nicehash")) {
                 if ($First) {
                     $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
                     $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
