3a93080f97724b64b8a5cb019486a4136374cb7a
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-05-14 14:35:57 +0200

    Update miner EnemyZealot v2.6 (nvidia)
    - disable hex/xevan/x16rv2 (added v2.5 to take over these algorithms, until it's fixed in the next Z-Enemy version)

--- Miners/EnemyZealot.ps1
+++ Miners/EnemyZealot.ps1
@@ -57,13 +57,13 @@ if ($IsLinux) {
 if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{MainAlgorithm = "aergo"; Params = "-N 1"; NH = $true} #AeriumX, new in 1.11
+    #[PSCustomObject]@{MainAlgorithm = "aergo"; Params = "-N 1"; NH = $true} #AeriumX, new in 1.11
     #[PSCustomObject]@{MainAlgorithm = "bcd"; Params = "-N 1"; NH = $true} #Bcd, new in 1.20
     #[PSCustomObject]@{MainAlgorithm = "bitcore"; Params = "-N 1"; NH = $true} #Bitcore
     #[PSCustomObject]@{MainAlgorithm = "c11"; Params = "-N 1"; NH = $true} # New in 1.11
-    [PSCustomObject]@{MainAlgorithm = "hex"; Params = "-N 1"; ExtendInterval = 3; FaultTolerance = 0.5; HashrateDuration = "Day"; NH = $true} #HEX/XDNA, new in 1.15a
+    #[PSCustomObject]@{MainAlgorithm = "hex"; Params = "-N 1"; ExtendInterval = 3; FaultTolerance = 0.5; HashrateDuration = "Day"; NH = $true} #HEX/XDNA, new in 1.15a
     #[PSCustomObject]@{MainAlgorithm = "hsr"; Params = "-N 1"; NH = $true} #HSR
-    [PSCustomObject]@{MainAlgorithm = "kawpow"; Params = "-N 1"; ExtendInterval = 3; MinMemGB=3; NH = $false} #KawPOW/RVN, new in 2.5
+    [PSCustomObject]@{MainAlgorithm = "kawpow"; Params = "-N 1"; ExtendInterval = 3; MinMemGB=3; NH = $true} #KawPOW/RVN, new in 2.5
     #[PSCustomObject]@{MainAlgorithm = "phi"; Params = "-N 1"; ExtendInterval = 2; NH = $true} #PHI
     #[PSCustomObject]@{MainAlgorithm = "phi2"; Params = "-N 1"; NH = $true} #PHI2, new in 1.12
     #[PSCustomObject]@{MainAlgorithm = "poly"; Params = "-N 1"; NH = $true} #Polytimos
@@ -72,10 +72,10 @@ $Commands = [PSCustomObject[]]@(
     #[PSCustomObject]@{MainAlgorithm = "timetravel"; Params = "-N 1"; NH = $true} #Timetravel8
     #[PSCustomObject]@{MainAlgorithm = "tribus"; Params = "-N 1"; NH = $true} #Tribus, new in 1.10
     #[PSCustomObject]@{MainAlgorithm = "x16r"; Params = "-N 10"; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"; NH = $true} #X16R
-    [PSCustomObject]@{MainAlgorithm = "x16rv2"; Params = "-N 10"; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"; NH = $true} #X16Rv2
+    #[PSCustomObject]@{MainAlgorithm = "x16rv2"; Params = "-N 10"; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"; NH = $true} #X16Rv2
     #[PSCustomObject]@{MainAlgorithm = "x16s"; Params = "-N 1"; FaultTolerance = 0.5; NH = $true} #X16S (T-Rex faster)
     #[PSCustomObject]@{MainAlgorithm = "x17"; Params = "-N 1"; NH = $true} #X17 (T-Rex has better numbers at the pool)
-    [PSCustomObject]@{MainAlgorithm = "xevan"; Params = "-N 1"; NH = $true} #Xevan, new in 1.09a
+    #[PSCustomObject]@{MainAlgorithm = "xevan"; Params = "-N 1"; NH = $true} #Xevan, new in 1.09a
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
