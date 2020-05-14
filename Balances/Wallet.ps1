a6004a00ad04684fce7ad600f01b9f6ef5fef332
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-05-14 16:50:06 +0200

    Add balance Wallet
    - add BTC wallet to balances (issue #989)

---
+++ Balances/Wallet.ps1
@@ -0,0 +1,39 @@
+﻿param(
+    $Config
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($Config.Wallet -notmatch "^[13]" -or $Config.Currency -notcontains "BTC") {
+    Write-Log -Level Verbose "Pool Balance API ($Name) has failed - no wallet address specified."
+    return
+}
+
+$Request = [PSCustomObject]@{}
+
+$Success = $true
+try {
+    $Request = Invoke-RestMethodAsync "https://blockchain.info/balance?active=$($Config.Wallet)" -cycletime ($Config.BalanceUpdateMinutes*60)
+    if ($Request."$($Config.Wallet)" -eq $null){$Success = $false}
+}
+catch {
+    if ($Error.Count){$Error.RemoveAt(0)}
+    $Success=$false
+}
+
+if (-not $Success) {
+    Write-Log -Level Warn "Pool Balance API ($Name) has failed. "
+    return
+}
+
+[PSCustomObject]@{
+        Caption     = "$($Name) (BTC)"
+		BaseName    = $Name
+        Currency    = "BTC"
+        Balance     = [Decimal]$Request."$($Config.Wallet)".final_balance / 1e8
+        Pending     = 0
+        Total       = [Decimal]$Request."$($Config.Wallet)".final_balance / 1e8
+        Earned      = [Decimal]$Request."$($Config.Wallet)".total_received / 1e8
+        Payouts     = @()
+        LastUpdated = (Get-Date).ToUniversalTime()
+}
--- Include.psm1
+++ Include.psm1
@@ -1527,7 +1527,7 @@ function Get-BalancesContent {
     }
 
     $UsePools = Get-ChildItem "Pools" -File -ErrorAction Ignore | Select-Object -ExpandProperty BaseName | Where-Object {($Config.PoolName.Count -eq 0 -or $Config.PoolName -icontains $_) -and ($Config.ExcludePoolName -eq 0 -or $Config.ExcludePoolName -inotcontains $_)}
-    foreach($Balance in @(Get-ChildItem "Balances" -File -ErrorAction Ignore | Where-Object {$UsePools -match "^$($_.BaseName)`(Coins|Party|Solo`)?$" -or $Config.ShowPoolBalancesExcludedPools})) {
+    foreach($Balance in @(Get-ChildItem "Balances" -File -ErrorAction Ignore | Where-Object {$UsePools -match "^$($_.BaseName)`(Coins|Party|Solo`)?$" -or $Config.ShowPoolBalancesExcludedPools -or $_.BaseName -eq "Wallet"})) {
         $Name = $Balance.BaseName 
         foreach($c in @(& $Balance.FullName @Parameters)) {
             $c | Add-Member Name "$(if ($c.Name) {$c.Name} else {$Name})$(if ($c.Info) {$c.Info})" -Force -PassThru
