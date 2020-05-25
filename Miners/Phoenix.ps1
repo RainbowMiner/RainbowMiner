c5eeab069d064322e076f9b9e60240afa9157015
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-05-25 17:18:41 +0200

    Update miner Phoenix to v5.0b (amd,nvidia)

--- Miners/Phoenix.ps1
+++ Miners/Phoenix.ps1
@@ -9,16 +9,16 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Phoenix\PhoenixMiner"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v4.9c-phoenix/PhoenixMiner_4.9c_Linux.tar.gz"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0b-phoenix/PhoenixMiner_5.0b_Linux.tar.gz"
 } else {
     $Path = ".\Bin\GPU-Phoenix\PhoenixMiner.exe"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v4.9c-phoenix/PhoenixMiner_4.9c_Windows.7z"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0b-phoenix/PhoenixMiner_5.0b_Windows.7z"
 }
 $ManualURI = "https://bitcointalk.org/index.php?topic=2647654.0"
 $Port = "308{0:d2}"
 $DevFee = 0.65
 $Cuda = "8.0"
-$Version = "4.9c"
+$Version = "5.0b"
 
 if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $Global:DeviceCache.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No GPU present in system
 
@@ -113,7 +113,10 @@ foreach ($Miner_Vendor in @("AMD","NVIDIA")) {
                         "minerproxy"       {"-proto 1"}
                         "ethproxy"         {"-proto 2"}
                         "qtminer"          {"-proto 3"}
+                        "ethstratum"       {"-proto 4"}
+                        "ethstratum1"      {"-proto 4"}
 						"ethstratumnh"     {"-proto 4 -stales 0"}
+                        "ethstratum2"      {"-proto 5"}
 						default            {"-proto 1"}
 					}
 
