5df3aabc193fc6db6fad80bad9e4c10f5af93f8b
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-06-04 18:10:36 +0200

    Update balance RPlant
    - deactivate: currently out-of-order

--- Balances/RPlant.ps1
+++ Balances/RPlant.ps1
@@ -6,6 +6,8 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 
 $Payout_Currencies = $Config.Pools.$Name.Wallets.PSObject.Properties | Where-Object Value | Select-Object Name,Value -Unique | Sort-Object Name,Value
 
+return # currently out-of-order
+
 if (-not $Payout_Currencies) {
     Write-Log -Level Verbose "Cannot get balance on pool ($Name) - no wallet address specified. "
     return
@@ -22,28 +24,32 @@ catch {
 
 $Count = 0
 $Payout_Currencies | Where-Object {@($Pools_Request.pools.PSObject.Properties.Value | Select-Object -ExpandProperty symbol -Unique) -icontains $_.Name -and (-not $Config.ExcludeCoinsymbolBalances.Count -or $Config.ExcludeCoinsymbolBalances -notcontains $_.Name)} | Foreach-Object {
-    $Request = [PSCustomObject]@{}
-    try {
-        $Request = Invoke-RestMethodAsync "https://pool.rplant.xyz/api/worker_stats?$($_.Value)" -delay $(if ($Count){500} else {0}) -cycletime ($Config.BalanceUpdateMinutes*60)
-        $Count++
-        if ($Request.miner -ne $_.Value) {
-            Write-Log -Level Info "Pool Balance API ($Name) for $($_.Name) returned nothing. "
-        } else {
-            [PSCustomObject]@{
-                Caption     = "$($Name) ($($_.Name))"
-				BaseName    = $Name
-                Currency    = $_.Name
-                Balance     = [Decimal]$Request.balance
-                Pending     = [Decimal]$Request.immature
-                Total       = [Decimal]$Request.balance + [Decimal]$Request.immature
-                Paid        = [Decimal]$Request.paid
-                Payouts     = @()
-                LastUpdated = (Get-Date).ToUniversalTime()
+    $Pool_Currency = $_.Name
+    $Pool_Name = "$($Pools_Request.pools.PSObject.Properties | Where-Object {$_.Value.symbol -eq $Pool_Currency} | Foreach-Object {$_.Name} | Select-Object -First 1)"
+    if ($Pool_Name) {
+        $Request = [PSCustomObject]@{}
+        try {
+            $Request = Invoke-RestMethodAsync "https://pool.rplant.xyz/api/wallet/$($Pool_Name)/$($_.Value)" -delay $(if ($Count){500} else {0}) -cycletime ($Config.BalanceUpdateMinutes*60)
+            $Count++
+            if (-not $Request.address) {
+                Write-Log -Level Info "Pool Balance API ($Name) for $($Pool_Currency) returned nothing. "
+            } else {
+                [PSCustomObject]@{
+                    Caption     = "$($Name) ($($Pool_Currency))"
+				    BaseName    = $Name
+                    Currency    = $Pool_Currency
+                    Balance     = [Decimal]$Request.balance
+                    Pending     = [Decimal]$Request.unsold
+                    Total       = [Decimal]$Request.unpaid
+                    Paid        = [Decimal]$Request.total
+                    Payouts     = @()
+                    LastUpdated = (Get-Date).ToUniversalTime()
+                }
             }
         }
-    }
-    catch {
-        if ($Error.Count){$Error.RemoveAt(0)}
-        Write-Log -Level Verbose "Pool Balance API ($Name) for $($_.Name) has failed. "
+        catch {
+            if ($Error.Count){$Error.RemoveAt(0)}
+            Write-Log -Level Verbose "Pool Balance API ($Name) for $($_.Name) has failed. "
+        }
     }
 }
