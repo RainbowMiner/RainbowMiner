84ce8727e3c0ef5b207d72789cc55a4c7cefe52d
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-06-18 11:32:21 +0200

    Update miner MiniZ to v1.5u2 (nvidia)
    - 144,5 (BTG, XSG, BTCZ, BTH, BZE,…): major improvements for various GPUs. Up to 6.5% depending on GPU.
    - 125,4 (ZEL): minor improvements. Up to 1% depending on GPU.
    - Fixed connection latency issues.
    - Added --ocX option for automatic tunning of the best kernel for your GPU and OC settings, for all algos.
    - Added --show-mode option to show miniZ kernel that each GPU is using.
    - Reduced memory utilization on 144,5 for all GPUs.
    - Reduced memory usage on 3GB GPUs for 125,4, on Windows 10 (work still in progress).
    - Fixed bug that on ZEL mining was not submitting shares on some pools ex: 2miners.
    - Adjusted default kernels for GTX1070, on stock settings, for all algorithms.

--- Miners/MiniZ.ps1
+++ Miners/MiniZ.ps1
@@ -10,30 +10,22 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://bitcointalk.org/index.php?topic=4767892.0"
 $Port = "330{0:d2}"
 $DevFee = 2.0
-$Version = "1.5t3"
+$Version = "1.5u2"
 
 if ($IsLinux) {
     $Path = ".\Bin\Equihash-MiniZ\miniZ"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.5t3-miniz/miniZ_v1.5t3_cuda10_linux-x64.tar.gz"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.5u2-miniz/miniZ_v1.5u2_cuda10_linux-x64.7z"
             Cuda = "10.0"
-        },
-        [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.5t3-miniz/miniZ_v1.5t3_cuda8_linux-x64.tar.gz"
-            Cuda = "8.0"
         }
     )
 } else {
     $Path = ".\Bin\Equihash-MiniZ\miniZ.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.5t3-miniz/miniZ_v1.5t3_cuda10_win-x64.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.5u2-miniz/miniZ_v1.5u2_cuda10_win-x64.7z"
             Cuda = "10.0"
-        },
-        [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.5t3-miniz/miniZ_v1.5t3_cuda8_win-x64.7z"
-            Cuda = "8.0"
         }
     )
 }
@@ -101,7 +93,7 @@ $Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique 
 					DeviceName     = $Miner_Device.Name
 					DeviceModel    = $Miner_Model
 					Path           = $Path
-					Arguments      = "--telemetry `$mport -cd $($DeviceIDsAll) --server $(if ($Pools.$Algorithm_Norm.SSL) {"ssl://"})$($Pools.$Algorithm_Norm.Host) --port $($Pool_Port) --user $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" --pass $($Pools.$Algorithm_Norm.Pass)"})$(if ($PersCoin -and ($_.AutoPers -or $PersCoin -ne "auto")) {" --pers $($PersCoin)"}) --gpu-line --nonvml --extra --latency$(if (-not $Session.Config.ShowMinerWindow) {" --nocolor"})$(if ($Pools.$Algorithm_Norm.Name -eq "MiningRigRentals" -and $PersCoin -ne "auto") {" --smart-pers"}) $($_.Params)"
+					Arguments      = "--telemetry `$mport -cd $($DeviceIDsAll) --server $(if ($Pools.$Algorithm_Norm.SSL) {"ssl://"})$($Pools.$Algorithm_Norm.Host) --port $($Pool_Port) --user $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" --pass $($Pools.$Algorithm_Norm.Pass)"})$(if ($PersCoin -and ($_.AutoPers -or $PersCoin -ne "auto")) {" --pers $($PersCoin)"}) --ocX --gpu-line --nonvml --extra --latency$(if (-not $Session.Config.ShowMinerWindow) {" --nocolor"})$(if ($Pools.$Algorithm_Norm.Name -eq "MiningRigRentals" -and $PersCoin -ne "auto") {" --smart-pers"}) $($_.Params)"
 					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $($Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Week)}
 					API            = "MiniZ"
 					Port           = $Miner_Port
