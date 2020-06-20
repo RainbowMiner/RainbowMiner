2bda04348339d1af21e1796a6e5fd888ddf62bd9
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-06-20 12:12:28 +0200

    Update core
    - cleanup Argon2d, now we have two Argon2d500:
      - Argon2dDyn for mining DYN
      - Argon2dNim for mining NIM

--- Cleanup.ps1
+++ Cleanup.ps1
@@ -880,6 +880,10 @@ try {
         }
     }
 
+    if ($Version -le (Get-Version "4.5.9.3")) {
+        $AddAlgorithm += @("Argon2dNim")
+    }
+
     if ($OverridePoolPenalties) {
         if (Test-Path "Data\PoolsConfigDefault.ps1") {
             $PoolsDefault = Get-ChildItemContent "Data\PoolsConfigDefault.ps1" -Quick
