1ea7abe2baee52076a0507d718f5c52281577ab9
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-07-02 12:34:37 +0200

    Add miner Nqminer v0.99.7 (amd,nvidia)
    - NIMQ mining (issue #1087)

---
+++ Miners/Nqminer.ps1
@@ -0,0 +1,98 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+if (-not $IsWindows -and -not $IsLinux) {return}
+
+if ($IsLinux) {
+    $Path = ".\Bin\GPU-Nqminer\nq-miner"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.99.7-nqminer/nq-miner-linux-0.99.7.tar.gz"
+} else {
+    $Path = ".\Bin\GPU-Nqminer\nq-miner.exe"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.99.7-nqminer/nq-miner-windows-0.99.7.zip"
+}
+$ManualUri = "https://github.com/RainbowMiner/miner-binaries/releases"
+$Port = "361{0:d2}"
+$DevFee = 0.0
+$Cuda = "10.0"
+$Version = "0.99.7"
+
+if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "argon2d-nim"; Params = ""; ExtendInterval = 3} #Argon2d-nim
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("AMD","NVIDIA")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+if (-not (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name)) {return}
+
+if ($Global:DeviceCache.DevicesByTypes.NVIDIA) {$Cuda = Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name}
+
+foreach ($Miner_Vendor in @("AMD","NVIDIA")) {
+    $Global:DeviceCache.DevicesByTypes.$Miner_Vendor | Where-Object Type -eq "GPU" | Where-Object {$_.Vendor -ne "NVIDIA" -or $Cuda} | Select-Object Vendor, Model -Unique | ForEach-Object {
+        $First = $true
+        $Miner_Model = $_.Model
+        $Miner_Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)".Where({$_.Model -eq $Miner_Model})
+
+        $DeviceParams = Switch ($Miner_Vendor) {
+            "AMD"    {"-t opencl"}
+            "NVIDIA" {"-t cuda"}
+        }
+
+        $Commands.ForEach({
+
+            $Algorithm_Norm_0 = Get-Algorithm $_.MainAlgorithm
+
+		    foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)","$($Algorithm_Norm_0)-GPU")) {
+			    if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+                    if ($First) {
+                        $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+                        $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+                        $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ' '
+                        $First = $false
+                    }
+				    $Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
+				    [PSCustomObject]@{
+					    Name           = $Miner_Name
+					    DeviceName     = $Miner_Device.Name
+					    DeviceModel    = $Miner_Model
+					    Path           = $Path
+					    Arguments      = "$($DeviceParams) -d $($DeviceIDsAll) -a `"$($Pools.$Algorithm_Norm.Wallet)`" -n `"$($Pools.$Algorithm_Norm.Worker)`" -p $($Pools.$Algorithm_Norm.Host):$($Pool_Port) --api $($Miner_Port) --mode dumb"
+					    HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Week}
+					    API            = "Nqminer"
+					    Port           = $Miner_Port
+					    Uri            = $Uri
+                        FaultTolerance = $_.FaultTolerance
+					    ExtendInterval = $_.ExtendInterval
+                        Penalty        = 0
+					    DevFee         = $DevFee
+					    ManualUri      = $ManualUri
+                        Version        = $Version
+                        PowerDraw      = 0
+                        BaseName       = $Name
+                        BaseAlgorithm  = $Algorithm_Norm_0
+                        EnvVars        = @("UV_THREADPOOL_SIZE=32")
+				    }
+			    }
+		    }
+        })
+    }
+}
