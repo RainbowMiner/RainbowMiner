c9c237f96dffcd0a7236fcffe0091c61c74f7d51
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-07-02 11:09:55 +0200

    Update miner Gminer to v2.12 (amd,nvidia)
    - improved performance for BeamHashIII algorithm (+2%-10% dependent on GPU)
    - removed auto-switching to BeamHashIII

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -9,16 +9,16 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.11-gminer/gminer_2_11_linux64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.12-gminer/gminer_2_12_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.11-gminer/gminer_2_11_windows64.7z"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.12-gminer/gminer_2_12_windows64.7z"
 }
 $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "2.11"
+$Version = "2.12"
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
 
