1b8182a2240ea9ae0898601dbf887cf11a522669
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-07-05 09:50:24 +0200

    Update miner Lolminer to v1.03 (amd,nvidia)
    - New Beam Hash III 4G solver, replaces the 6G solver on AMD Vega GPUs and earlier (+5 - 8% performance on RX 580 & Vega GPUs)
    - New Beam Hash III 6G solver on ROCm and for AMD Navi GPUs (+8-10% performance on Navi cards)
    - Fixed a bug with the API (--apiport) crashing in Windows

--- Miners/lolminer.ps1
+++ Miners/lolminer.ps1
@@ -9,16 +9,16 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\Equihash-lolMiner\lolMiner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.02-lolminer/lolMiner_v1.02_Lin64.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.03-lolminer/lolMiner_v1.03_Lin64.tar.gz"
 } else {
     $Path = ".\Bin\Equihash-lolMiner\lolMiner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.02-lolminer/lolMiner_v1.02_Win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.03-lolminer/lolMiner_v1.03_Win64.zip"
 }
 $ManualUri = "https://bitcointalk.org/index.php?topic=4724735.0"
 $Port = "317{0:d2}"
 $Cuda = "10.0"
 $DevFee = 1.0
-$Version = "1.02"
+$Version = "1.03"
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
 
