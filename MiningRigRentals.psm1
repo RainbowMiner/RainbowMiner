a52784e653b649290a7a3aa6aee8fd5d21a79c46
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-09-19 19:51:21 +0200

    Update pool MiningRigRentals
    - second part of automatic benchmark missing algorithms

--- MiningRigRentals.psm1
+++ MiningRigRentals.psm1
@@ -448,7 +448,24 @@ Param(
 }
 
 function Get-MiningRigRentalsPoolsData {
-    try {$PoolsData = Invoke-RestMethodAsync "https://rbminer.net/api/data/mrrpools.json" -Tag "MiningRigRentals" -cycletime 86400;Set-ContentJson -PathToFile ".\Data\mrrpools.json" -Data $PoolsData -Compress > $null} catch {if ($Error.Count){$Error.RemoveAt(0)};Write-Log -Level Warn "Rbminer.net/api/data/mrrpools.json could not be reached"}
-    if (-not $PoolsData) {try {$PoolsData = Get-ContentByStreamReader ".\Data\mrrpools.json" | ConvertFrom-Json -ErrorAction Stop} catch {if ($Error.Count){$Error.RemoveAt(0)}}}
+[cmdletbinding()]
+Param(   
+    [Parameter(Mandatory = $False)]
+    [Switch]$UpdateLocalCopy
+)
+    try {
+        $PoolsData = Invoke-RestMethodAsync "https://rbminer.net/api/data/mrrpools.json" -Tag "MiningRigRentals" -cycletime 86400
+        if ($UpdateLocalCopy) {
+            Set-ContentJson -PathToFile ".\Data\mrrpools.json" -Data $PoolsData -Compress > $null
+        }
+    } catch {
+        if ($Error.Count){$Error.RemoveAt(0)}
+        Write-Log -Level Warn "Rbminer.net/api/data/mrrpools.json could not be reached"
+    }
+    if (-not $PoolsData) {
+        try {
+            $PoolsData = Get-ContentByStreamReader ".\Data\mrrpools.json" | ConvertFrom-Json -ErrorAction Stop
+        } catch {if ($Error.Count){$Error.RemoveAt(0)}}
+    }
     $PoolsData
 }
