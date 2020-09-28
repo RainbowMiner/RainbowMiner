0c996de35dec2e0201283535c57370547331cc06
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-09-28 14:13:38 +0200

    Update linux
    - better add libcurl-nss.so.4 as linux package library

--- IncludesLinux/bash/libcurl4.sh
+++ IncludesLinux/bash/libcurl4.sh
@@ -3,7 +3,7 @@
 PKG_MANAGER=$( command -v yum || command -v apt-get || command -v pacman)
 if [ $PKG_MANAGER == 'pacman' ]
  then
-  sudo $PKG_MANAGER -S libcurl4 libcurl4-openssl-dev libcurl4-nss-dev --noconfirm
+  sudo $PKG_MANAGER -S libcurl4 libcurl4-openssl-dev --noconfirm
  else
-  sudo $PKG_MANAGER install libcurl4 libcurl4-openssl-dev libcurl4-nss-dev -y
+  sudo $PKG_MANAGER install libcurl4 libcurl4-openssl-dev -y
 fi
