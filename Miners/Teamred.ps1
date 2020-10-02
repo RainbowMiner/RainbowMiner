f5b766fff62f9057c83638acb890ab7888b3bd50
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-10-02 14:52:39 +0200

    Update miner Teamred to v0.7.12 (amd)
    Bugfix release: v0.7.11 could crash with rejected ethash shares.
    - Fixed print format bug on ethash rejected shares.

--- Miners/Teamred.ps1
+++ Miners/Teamred.ps1
@@ -9,15 +9,15 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\AMD-Teamred\teamredminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7.11-teamred/teamredminer-v0.7.11-linux.tgz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7.12-teamred/teamredminer-v0.7.12-linux.tgz"
 } else {
     $Path = ".\Bin\AMD-Teamred\teamredminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7.11-teamred/teamredminer-v0.7.11-win.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7.12-teamred/teamredminer-v0.7.12-win.zip"
 }
 $Port = "409{0:d2}"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5059817.0"
 $DevFee = 3.0
-$Version = "0.7.11"
+$Version = "0.7.12"
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No AMD present in system
 
