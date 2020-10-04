9111912ad620b060e00ec7c969a67f659341be5b
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-10-04 16:48:31 +0200

    Update install.sh
    - linux runs perfect on Powershell Core 7.0.3

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ cd "$(dirname "$0")"
 command="& {./Install.ps1; exit \$lastexitcode}"
 
 if ! [ -x "$(command -v pwsh)" ]; then
-wget https://github.com/PowerShell/PowerShell/releases/download/v6.2.7/powershell-6.2.7-linux-x64.tar.gz -O /tmp/powershell.tar.gz
-sudo mkdir -p /opt/microsoft/powershell/6
-sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/6
-sudo chmod +x /opt/microsoft/powershell/6/pwsh
-sudo ln -s /opt/microsoft/powershell/6/pwsh /usr/bin/pwsh
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.3/powershell-7.0.3-linux-x64.tar.gz -O /tmp/powershell.tar.gz
+sudo mkdir -p /opt/microsoft/powershell/7
+sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+sudo chmod +x /opt/microsoft/powershell/7/pwsh
+sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 sudo rm -rf /tmp/powershell.tar.gz
 fi
 
