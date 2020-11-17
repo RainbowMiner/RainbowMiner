f2dcf808c759cbc85173dc81e2ed4a60145690ac
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-11-17 12:07:33 +0100

    Update install.sh
    - make installer more interactive: parameters added
      - -pv, --pwsh_version   shows version of currently installed powershell
      - -pu, --pwsh_update    updates powershell to version 7.1.0
      - -h, --help            displays this help page

--- install.sh
+++ install.sh
@@ -6,22 +6,70 @@ command="& {./Scripts/Install.ps1; exit \$lastexitcode}"
 
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
+pwsh_major_version="7"
+pwsh_minor_version="1"
+pwsh_build_version="0"
+
+pwsh_version="${pwsh_major_version}.${pwsh_minor_version}.${pwsh_build_version}"
+
+function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
+
 if [ -x "$(command -v pwsh)" ]; then
-  pwsh_version="$(pwsh --version | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')"
-  if [ $(version ${pwsh_version}) -lt $(version "7.1.0") ]; then
-    sudo rm -f /usr/bin/pwsh
-  fi
+  pwsh_version_current="$(pwsh --version | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')"
 fi
 
+for arg in "$@"
+do
+  if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]; then
+    cat << EOF
+
+RainbowMiner Installer v1.1
+
+Commandline options:
+
+  -pv, --pwsh_version   shows version of currently installed powershell
+  -pu, --pwsh_update    updates powershell to version ${pwsh_version}
+  -h, --help            displays this help page
+
+EOF
+    exit
+  elif [ "$arg" == "--pwsh_version" ] || [ "$arg" == "-pv" ]; then
+    if [ "${pwsh_version_current}" != "" ]; then
+      echo ${pwsh_version_current}
+    else
+      echo "Powershell not installed"
+    fi
+    exit
+  elif [ "$arg" == "--pwsh_update" ] || [ "$arg" == "-pu" ]; then
+    if [ "${pwsh_version_current}" != "" ]; then
+      if [ $(version ${pwsh_version_current}) -lt $(version ${pwsh_version}) ]; then
+        sudo rm -f /usr/bin/pwsh
+        printf "\nPowershell will be updated from ${pwsh_version_current} -> ${pwsh_version}\n\n"
+      else
+        printf "\nPowershell ${pwsh_version_current} already up to date\n\n"
+      fi
+    else
+      printf "\nPowershell not installed, yet\n\n"
+    fi
+    pwsh_update="1"
+  fi
+done
+
 if ! [ -x "$(command -v pwsh)" ]; then
-  wget https://github.com/PowerShell/PowerShell/releases/download/v7.1.0/powershell-7.1.0-linux-x64.tar.gz -O /tmp/powershell.tar.gz
-  sudo mkdir -p /opt/microsoft/powershell/7
-  sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7 --overwrite
-  sudo chmod +x /opt/microsoft/powershell/7/pwsh
-  sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+  if [ -L "/usr/bin/pwsh" ]; then
+    sudo rm -f /usr/bin/pwsh
+  fi
+  wget https://github.com/PowerShell/PowerShell/releases/download/v${pwsh_version}/powershell-${pwsh_version}-linux-x64.tar.gz -O /tmp/powershell.tar.gz
+  sudo mkdir -p /opt/microsoft/powershell/${pwsh_major_version}
+  sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/${pwsh_major_version} --overwrite
+  sudo chmod +x /opt/microsoft/powershell/${pwsh_major_version}/pwsh
+  sudo ln -s /opt/microsoft/powershell/${pwsh_major_version}/pwsh /usr/bin/pwsh
   sudo rm -rf /tmp/powershell.tar.gz
 fi
 
+if [ "${pwsh_update}" == "1" ]; then
+  exit
+fi
 
 if ! [ -d "/opt/rainbowminer" ]; then
   sudo mkdir -p /opt/rainbowminer
