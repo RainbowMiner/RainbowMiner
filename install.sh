6bf08abe3f22fce0a0d696def16718d4e938b1e3
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-11-17 11:20:54 +0100

    Update install.sh
    - add upgrade for powershell: just run ./install.sh if you want to upgrade to PowerShell v7.1.0

--- install.sh
+++ install.sh
@@ -4,17 +4,27 @@ cd "$(dirname "$0")"
 
 command="& {./Scripts/Install.ps1; exit \$lastexitcode}"
 
-if ! [ -x "$(command -v pwsh)" ]; then
-wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.3/powershell-7.0.3-linux-x64.tar.gz -O /tmp/powershell.tar.gz
-sudo mkdir -p /opt/microsoft/powershell/7
-sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
-sudo chmod +x /opt/microsoft/powershell/7/pwsh
-sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
-sudo rm -rf /tmp/powershell.tar.gz
+function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
+
+if [ -x "$(command -v pwsh)" ]; then
+  pwsh_version="$(pwsh --version | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')"
+  if [ $(version ${pwsh_version}) -lt $(version "7.1.0") ]; then
+    sudo rm -f /usr/bin/pwsh
+  fi
 fi
 
+if ! [ -x "$(command -v pwsh)" ]; then
+  wget https://github.com/PowerShell/PowerShell/releases/download/v7.1.0/powershell-7.1.0-linux-x64.tar.gz -O /tmp/powershell.tar.gz
+  sudo mkdir -p /opt/microsoft/powershell/7
+  sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7 --overwrite
+  sudo chmod +x /opt/microsoft/powershell/7/pwsh
+  sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+  sudo rm -rf /tmp/powershell.tar.gz
+fi
+
+
 if ! [ -d "/opt/rainbowminer" ]; then
-sudo mkdir -p /opt/rainbowminer
+  sudo mkdir -p /opt/rainbowminer
   if ! [ -d "/opt/rainbowminer/ocdcmd" ]; then
     sudo mkdir -p /opt/rainbowminer/ocdcmd
     sudo chmod 777 /opt/rainbowminer/ocdcmd
@@ -28,15 +38,15 @@ sudo ln -nfs /opt/rainbowminer/bin/ocdaemon /usr/bin/ocdaemon
 sudo /opt/rainbowminer/bin/ocdaemon reinstall
 
 if ! [ -x "$(command -v amdmeminfo)" ]; then
-sudo ln -nfs /opt/rainbowminer/bin/amdmeminfo /usr/bin/amdmeminfo
+  sudo ln -nfs /opt/rainbowminer/bin/amdmeminfo /usr/bin/amdmeminfo
 fi
 
 if ! [ -x "$(command -v wolfamdctrl)" ]; then
-sudo ln -nfs /opt/rainbowminer/bin/wolfamdctrl /usr/bin/wolfamdctrl
+  sudo ln -nfs /opt/rainbowminer/bin/wolfamdctrl /usr/bin/wolfamdctrl
 fi
 
 if ! [ -x "$(command -v rbmtail)" ]; then
-sudo ln -nfs /opt/rainbowminer/bin/rbmtail /usr/bin/rbmtail
+  sudo ln -nfs /opt/rainbowminer/bin/rbmtail /usr/bin/rbmtail
 fi
 
 sudo pwsh -ExecutionPolicy bypass -Command ${command}
@@ -44,5 +54,5 @@ exitcode=$?
 sudo chmod 777 -R $HOME/.local/share/powershell
 
 if [ "$exitcode" == "10" ]; then
-./start.sh
+  ./start.sh
 fi
