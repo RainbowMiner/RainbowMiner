248cbb14b6789015a18469404575d6744ab67a6b
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-11-21 13:49:29 +0100

    Update miner TTminer to v6.0.1 (nvidia)
    - removed algos: EAGLESONG, LYRA2
    - removed cuda version: cuda 10.0 and cuda 10.1
    - added support for cuda 11.1
    - added support for:
           - VeriBlock
           - Zelantus
           - Veil (ProgPoW)
    - added fees again. Solo mining & EPIC: 2%
             all others 1%
    - support for solomining for:
             - EPIC
             - Raven
             - Veil
             - Zelantus

--- Miners/TTminer.ps1
+++ Miners/TTminer.ps1
@@ -7,38 +7,37 @@ param(
 
 if (-not $IsWindows -and -not $IsLinux) {return}
 
+$ManualUri = "https://bitcointalk.org/index.php?topic=5025783.0"
+$Port = "333{0:d2}"
+$DevFee = 1.0
+$Cuda = "9.2"
+$Version = "6.0.1"
+
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-TTminer\TT-Miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.1-ttminer/TT-Miner-5.0.1.tar.xz"
-    $Version = "5.0.1"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v6.0.1-ttminer/TT-Miner-6.0.1.tar.xz"
 
 } else {
     $Path = ".\Bin\NVIDIA-TTminer\TT-Miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.3-ttminer/TT-Miner-5.0.3.zip"
-    $Version = "5.0.3"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v6.0.1-ttminer/TT-Miner-6.0.1.7z"
 }
-$ManualUri = "https://bitcointalk.org/index.php?topic=5025783.0"
-$Port = "333{0:d2}"
-$DevFee = 0.0
-$Cuda = "9.2"
 
 if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{MainAlgorithm = "BLAKE2S"       ;              MinMemGB = 2.4; Params = "-A BLAKE2S%CUDA% -coin KDA";        ExtendInterval = 2; Coins = @("KDA"); ExcludePoolName = "^Nicehash"} #Kadena
-    #[PSCustomObject]@{MainAlgorithm = "EAGLESONG"     ;              MinMemGB = 0.1; Params = "-A EAGLESONG%CUDA% -coin CKB";      ExtendInterval = 2} #Eaglesong
-    [PSCustomObject]@{MainAlgorithm = "ETHASH"        ; DAG = $true; MinMemGB = 3;   Params = "-A ETHASH%CUDA%";                   ExtendInterval = 2; ExcludePoolName = "^MiningRigRentals"} #Ethash 
-    [PSCustomObject]@{MainAlgorithm = "KAWPOW"        ; DAG = $true; MinMemGB = 3;   Params = "-A PROGPOW%CUDA% -coin RVN";        ExtendInterval = 2; ExcludePoolName = "^MiningPoolHub"} #KAWPOW (RVN)
-    [PSCustomObject]@{MainAlgorithm = "LYRA2V3"       ;              MinMemGB = 1.5; Params = "-A LYRA2V3%CUDA%";                  ExtendInterval = 2; ExcludePoolName = "^Nicehash"} #LYRA2V3
+    [PSCustomObject]@{MainAlgorithm = "Ethash"        ; DAG = $true; MinMemGB = 3;   Params = "-A ETHASH%CUDA%";                   ExtendInterval = 2; ExcludePoolName = "^MiningRigRentals"} #Ethash 
+    [PSCustomObject]@{MainAlgorithm = "KawPow"        ; DAG = $true; MinMemGB = 3;   Params = "-A PROGPOW%CUDA%";                  ExtendInterval = 2; ExcludePoolName = "^MiningPoolHub"} #KAWPOW (RVN,ZELS)
     [PSCustomObject]@{MainAlgorithm = "MTP"           ;              MinMemGB = 5;   Params = "-A MTP%CUDA%";                      ExtendInterval = 2} #MTP
-    [PSCustomObject]@{MainAlgorithm = "MTP-TCR"       ;              MinMemGB = 5;   Params = "-A MTP%CUDA% -coin TCR";            ExtendInterval = 2} #MTP-TCR
-    [PSCustomObject]@{MainAlgorithm = "PROGPOW"       ; DAG = $true; MinMemGB = 3;   Params = "-A PROGPOW%CUDA% -coin EPIC";       ExtendInterval = 2; Coins = @("EPIC"); ExcludePoolName = "^Nicehash"} #ProgPoW (only EPIC left)
-    [PSCustomObject]@{MainAlgorithm = "PROGPOWSERO"   ; DAG = $true; MinMemGB = 3;   Params = "-A PROGPOW%CUDA% -coin SERO";       ExtendInterval = 2; ExcludePoolName = "^Nicehash"} #ProgPoWSero (SERO)
-    [PSCustomObject]@{MainAlgorithm = "PROGPOWZ"      ; DAG = $true; MinMemGB = 3;   Params = "-A PROGPOWZ%CUDA%";                 ExtendInterval = 2; ExcludePoolName = "^Nicehash"} #ProgPoWZ (ZANO)
-    [PSCustomObject]@{MainAlgorithm = "UBQHASH"       ;              MinMemGB = 2.4; Params = "-A UBQHASH%CUDA%";                  ExtendInterval = 2; ExcludePoolName = "^Nicehash"} #Ubqhash 
+    [PSCustomObject]@{MainAlgorithm = "MTPTcr"        ;              MinMemGB = 5;   Params = "-A MTP%CUDA% -coin TCR";            ExtendInterval = 2} #MTP-TCR
+    [PSCustomObject]@{MainAlgorithm = "ProgPoW"       ; DAG = $true; MinMemGB = 3;   Params = "-A PROGPOW%CUDA% -coin EPIC";       ExtendInterval = 2; Coins = @("EPIC"); ExcludePoolName = "^Nicehash"; DevFee = 2.0} #ProgPoW (only EPIC left)
+    [PSCustomObject]@{MainAlgorithm = "ProgPoWSERO"   ; DAG = $true; MinMemGB = 3;   Params = "-A PROGPOW%CUDA% -coin SERO";       ExtendInterval = 2; ExcludePoolName = "^Nicehash"} #ProgPoWSero (SERO)
+    [PSCustomObject]@{MainAlgorithm = "ProgPoWVEIL"   ; DAG = $true; MinMemGB = 3;   Params = "-A PROGPOW%CUDA% -coin VEIL";       ExtendInterval = 2; ExcludePoolName = "^Nicehash"} #ProgPoWSero (VEIL)
+    [PSCustomObject]@{MainAlgorithm = "ProgPoWZ"      ; DAG = $true; MinMemGB = 3;   Params = "-A PROGPOWZ%CUDA%";                 ExtendInterval = 2; ExcludePoolName = "^Nicehash"} #ProgPoWZ (ZANO)
+    [PSCustomObject]@{MainAlgorithm = "UbqHash"       ;              MinMemGB = 2.4; Params = "-A UBQHASH%CUDA%";                  ExtendInterval = 2; ExcludePoolName = "^Nicehash"} #Ubqhash
+    [PSCustomObject]@{MainAlgorithm = "vProgPoW"      ; DAG = $true; MinMemGB = 3;   Params = "-A PROGPOW%CUDA% -coin VBK";        ExtendInterval = 2; ExcludePoolName = "^Nicehash"} #ProgPoWSero (VBK)
 )
 
-$CoinSymbols = @("EPIC","SERO","ZANO","ZCOIN","ETC","ETH","CLO","PIRL","MUSIC","EXP","ETP","CKB","KDA","VTC","UBQ","ERE","HANA","TCR","RVN","RVNt","VEIL")
+$CoinSymbols = @("EPIC","SERO","ZANO","ZCOIN","ETC","ETH","CLO","PIRL","MUSIC","EXP","ETP","UBQ","TCR","ZELS","VBK","RVN","VEIL")
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 
@@ -58,22 +57,24 @@ if ($InfoOnly) {
 
 if (-not (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name)) {return}
 
-$Cuda = ""
-if ($IsLinux) {
-    $Cuda = "-$(if (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion "10.2") {"102"} elseif (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion "10.1") {"101"} elseif (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion "10.0") {"100"} else {"92"})"
-}
-
 $Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
     $Miner_Model = $_.Model
     $Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)".Where({$_.Model -eq $Miner_Model})
 
-    $Commands.Where({$_.Cuda -eq $null -or (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $_.Cuda -Warning "$($Name)-$($_.MainAlgorithm)")}).ForEach({
+    $Commands.ForEach({
         $First = $True
         $Algorithm_Norm_0 = Get-Algorithm $_.MainAlgorithm
+        $Miner_Fee = if ($_.DevFee -ne $null) {$_.DevFee} else {$DevFee}
 
         $MinMemGB = if ($_.DAG) {if ($Pools.$Algorithm_Norm_0.EthDAGSize) {$Pools.$Algorithm_Norm_0.EthDAGSize} else {Get-EthDAGSize $Pools.$Algorithm_Norm_0.CoinSymbol}} else {$_.MinMemGB}
 
         $Miner_Device = $Device | Where-Object {Test-VRAM $_ $MinMemGB}
+
+        $IsMTP = $_.MainAlgorithm -match "^MTP"
+
+        $Cuda = "$(if ($IsLinux -or $IsMTP) {
+            "-$(if (-not $IsMTP -and (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion "11.1")) {"111"} elseif (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion "10.2") {"102"} else {"92"})"
+        })"
         
 		foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)","$($Algorithm_Norm_0)-GPU")) {
 			if ($Pools.$Algorithm_Norm.Host -and (-not $_.ExcludePoolName -or $Pools.$Algorithm_Norm.Name -notmatch $_.ExcludePoolName) -and (-not $_.Coins -or $_.Coins -icontains $Pools.$Algorithm_Norm.CoinSymbol) -and $Miner_Device) {
@@ -104,9 +105,8 @@ $Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique 
                     FaultTolerance = $_.FaultTolerance
 					ExtendInterval = $_.ExtendInterval
                     Penalty        = 0
-					DevFee         = $DevFee
+					DevFee         = $Miner_Fee
 					ManualUri      = $ManualUri
-                    StopCommand    = "Sleep 5"
                     Version        = $Version
                     PowerDraw      = 0
                     BaseName       = $Name
