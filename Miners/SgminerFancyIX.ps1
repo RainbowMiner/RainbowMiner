1c7e35db25b17fc981d0cce7e0d50c21986281c9
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-11-22 12:20:16 +0100

    Update miner SgminerFancyIX to v0.7.1 (amd)
    [yescrypt] Added optimized binary kernels for rx 580 and Vega cards, 30% ~ 37% faster.
    [yescrypt] Added GCN cross lane instructions in Navi kernel, over 3x faster.

--- Miners/SgminerFancyIX.ps1
+++ Miners/SgminerFancyIX.ps1
@@ -9,15 +9,15 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\AMD-FancyIX\sgminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.6.1.3.b7a-fancyix/sgminer-fancyIX-linux-amd64-0.7.0.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.6.1.3.b7b-fancyix/sgminer-fancyIX-linux-amd64-0.7.1.zip"
 } else {
     $Path = ".\Bin\AMD-FancyIX\sgminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.6.1.3.b7a-fancyix/sgminer-fancyIX-win64-0.7.0.7z"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v5.6.1.3.b7b-fancyix/sgminer-fancyIX-win64-0.7.1.zip"
 }
 $Port = "409{0:d2}"
 $ManualUri = "https://github.com/fancyIX/sgminer-phi2-branch/releases"
 $DevFee = 0.0
-$Version = "v5.6.1.3-beta7a"
+$Version = "v5.6.1.3-beta7b"
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No AMD present in system
 
