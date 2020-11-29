2d8214bd8540c0224b17137e727eafddd4602e15
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-11-29 14:57:45 +0100

    Update miner NBminer to v34.2 (amd,nvidia)
    - fix: ethash Reduce stale share ratio.
    - 修复: ethash 降低延迟和拒绝率

--- Miners/NBminer.ps1
+++ Miners/NBminer.ps1
@@ -11,14 +11,14 @@ $ManualURI = "https://github.com/NebuTech/NBMiner/releases"
 $Port = "340{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.1"
-$Version = "34.1"
+$Version = "34.2"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-NBMiner\nbminer"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v34.1-nbminer/NBMiner_34.1_Linux.tgz"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v34.2-nbminer/NBMiner_34.2_Linux.tgz"
 } else {
     $Path = ".\Bin\GPU-NBMiner\nbminer.exe"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v34.1-nbminer/NBMiner_34.1_Win.zip"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v34.2-nbminer/NBMiner_34.2_Win.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
