bbf59ccac3fd363c5a0837f805df3cc634817990
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-12-01 16:50:08 +0100

    Update miner NBminer to 34.4 (amd,nvidia)
    - optimize: octopus Improve hashrate 1-5% on Nvidia 16, 20, 30 series GPUs, 29.2M on 1660s
    - 优化: octopus 16、20、30系列N卡提高算力1-5%, 1660s 默认参数下可达 29.2M

--- Miners/NBminer.ps1
+++ Miners/NBminer.ps1
@@ -11,14 +11,14 @@ $ManualURI = "https://github.com/NebuTech/NBMiner/releases"
 $Port = "340{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.1"
-$Version = "34.3"
+$Version = "34.4"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-NBMiner\nbminer"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v34.3-nbminer/NBMiner_34.3_Linux.tgz"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v34.4-nbminer/NBMiner_34.4_Linux.tgz"
 } else {
     $Path = ".\Bin\GPU-NBMiner\nbminer.exe"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v34.3-nbminer/NBMiner_34.3_Win.zip"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v34.4-nbminer/NBMiner_34.4_Win.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
