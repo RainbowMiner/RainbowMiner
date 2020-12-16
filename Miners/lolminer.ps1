407fcf55fee0fbde229d49717e3d9ee3e6519688
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-12-17 00:48:03 +0100

    Update miner Lolminer to v1.17 (amd,nvidia)
    #### Changes (Over 1.16a)
    - Significantly reduced Ethash power draw on Navi GPUs, Slightly improved performance of 6800 (XT) / 6900
    - Added Cuckoo-29, Cuckaroo-30 CTX, Cuckatoo-31 (MWC) and Cuckatoo-32 (Grin) for RX 6800 family of GPUs
    - Reduced number of stale shares on Cortex algorithm. This will result in a minimally lower displayed hash rate, but higher pool side hash.
    - Added a basic temperature protection mechanism. See notes below for usage.
    - Added parameter --singlethread to work with Ethash and Equihash algorithm. This will disable the 2nd mining thread and slightly reduce performance of the involved cards. Use this option to reduce stumbles when a card does graphic output in parallel. Use --singlethread (equivalent to --singlethread -1) to enable single thread mode for all GPUs, use --singlethread to set the mode for one single card.
    - Added reading of junction temperature on AMD GPUs.
    - The API is now bound to the local host, causing less issues with firewalls.
    - Windows: use --computemode to automatically enable the compute mode on all detected AMD GPUs. Requires higher privileges and a driver restart, see example files.
    - lolMiner.exe Windows executable is now digitally signed.
    
    #### Fixed bugs:
    - Ethash Ethproxy stratum mode some times loosing worker name.
    - Beam Hash III not starting up in Linux on RX 5000 & RX 6000 series card on amdgpu-pro 20.45 driver.
    - Ethash & Beam not starting up on Radeon R9 380
    - Ethash not starting up on some 6G Nvidia cards
    - Ethash mining frequently trying to start up a card if there was an error during mining.
    - "DEVICES" parameter not working when configuring the miner from json file.
    
    #### Known issues:
    - ETC mining is currently not working for Nvidia GTX cards with 3G of memory.
    - On some Linux kernels there is a memory leak in the GPU driver component that effects lolMiner quite hard when mining with Navi cards. Keep system updated. (Note that this bug also affected earlier versions of lolMiner - so if it worked fine in last version it can be expected to do so further.)
    
    #### Basic temperature management / overheating protection.
    Use --tstop to stop any mining operation on a GPU at the given temperature. Use --tstart to allow a restart of the card below a lower temperature. Further you can use --tmode edge/junction/memory to apply the scheme to edge (chip), junction (hotspot) or memory temperature. If a GPU does not have the required sensors the chip temperature will be used as a back up - if no sensors are available at all the parameters will be ignored.
    
    Note that at the moment the miner has no fan control module and also no throttling to keep a target temperature. This may be included in a future version. Thus you should put the limit high enough so the operation system or the driver has a chance to ramp up the fan speed itself. Currently tstop is supposed to be a overheat protection to prevent hardware damage in extreme cases, e.g. broken fans.

--- Miners/lolminer.ps1
+++ Miners/lolminer.ps1
@@ -11,14 +11,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=4724735.0"
 $Port = "317{0:d2}"
 $Cuda = "10.0"
 $DevFee = 1.0
-$Version = "1.16a"
+$Version = "1.17"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-lolMiner\lolMiner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.16a-lolminer/lolMiner_v1.16a_Lin64.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.17-lolminer/lolMiner_v1.17_Lin64.tar.gz"
 } else {
     $Path = ".\Bin\GPU-lolMiner\lolMiner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.16a-lolminer/lolMiner_v1.16a_Win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.17-lolminer/lolMiner_v1.17_Win64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
