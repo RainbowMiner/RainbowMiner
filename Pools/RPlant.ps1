a8d012bfb19c93c5aeace22ecd5cf94c673596aa
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2020-12-21 16:34:21 +0100

    Update pool RPlant
    - add CIRC

--- Pools/RPlant.ps1
+++ Pools/RPlant.ps1
@@ -34,6 +34,7 @@ $Pools_Data = [PSCustomObject]@{
     "BTX"   = [PSCustomObject]@{port = 7066; region = $Pool_Regions}
     "ZNY"   = [PSCustomObject]@{port = 7054; region = $Pool_Regions}
     "BLAS"  = [PSCustomObject]@{port = 7065; region = $Pool_Regions}
+    "CIRC"  = [PSCustomObject]@{port = 7050; region = $Pool_Regions}
     "CPU"   = [PSCustomObject]@{port = 7029; region = $Pool_Regions}
     "CRP"   = [PSCustomObject]@{port = 3335; region = $Pool_Regions}
     "CURVE" = [PSCustomObject]@{port = 7058; region = $Pool_Regions}
