e140b559b2a95656e6acfe981dcdc36cc515ae58
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-01-22 23:03:12 +0100

    Update pool Userpools
    - allow four variables in "User" and "Pass":
      - `$Wallet` will be replaced with your currency's wallet (as defined in pools.config.txt)
      - `$WorkerName` will be replaced with your rig's workername (or the value in pools.config.txt)
      - `$CoinSymbol` will be replaced with the CoinSymbol
      - `$Currency` will be replaced with the Currency

--- Pools/Userpools.ps1
+++ Pools/Userpools.ps1
@@ -14,30 +14,49 @@ param(
 )
 
 $Session.Config.Userpools | Where-Object {$_.Name -eq $Name -and $_.Enable -and ($Wallets."$($_.Currency)" -or $InfoOnly)} | ForEach-Object {
-    $Pool_Coin      = Get-Coin $_.CoinSymbol
-    $Pool_Algorithm_Norm = Get-Algorithm "$(if ($Pool_Coin) {Get-Algorithm $Pool_Coin.Algo} else {$_.Algorithm})"
-    $Pool_Wallet    = $Wallets."$($_.Currency)"
 
+    $Pool_Params = [ordered]@{
+        Wallet     = $Wallets."$($_.Currency)"
+        WorkerName = "{workername:$Worker}"
+        Currency   = "$($_.Currency)".ToUpper()
+        CoinSymbol = "$($_.CoinSymbol)".ToUpper()
+    }
+
+    $Pool_Coin     = Get-Coin $Pool_Params["CoinSymbol"]
+    $Pool_User     = "$(if ($_.User) {$_.User} else {"`$Wallet.`$WorkerName"})"
+    $Pool_Pass     = "$(if ($_.Pass) {$_.Pass} else {"x"})"
     $Pool_EthProxy = if ($Pool_Algorithm_Norm -match $Global:RegexAlgoHasEthproxy) {"ethproxy"} elseif ($Pool_Algorithm_Norm -eq "KawPOW") {"stratum"} else {$null}
 
+    $Pool_Algorithm_Norm = Get-Algorithm "$(if ($_.Algorithm) {$_.Algorithm} else {$Pool_Coin.Algo})"
+
+    if (-not $Pool_Algorithm_Norm) {
+        Write-Log -Level Warn "Userpool $Name has no algorithm for coin $($Pool_Params["CoinSymbol"])"
+        return
+    }
+
     if (-not $InfoOnly) {
-        $Stat = Set-Stat -Name "$($_.Name)_$($Pool_Algorithm_Norm)_$($_.CoinSymbol)_Profit" -Value 0 -Duration $StatSpan -ChangeDetection $false -Quiet
+        $Stat = Set-Stat -Name "$($_.Name)_$($Pool_Algorithm_Norm)_$($Pool_Params["CoinSymbol"])_Profit" -Value 0 -Duration $StatSpan -ChangeDetection $false -Quiet
+    }
+
+    $Pool_Params.GetEnumerator() | Foreach-Object {
+        $Pool_User = $Pool_User.Replace("`$$($_.Name)",$_.Value)
+        $Pool_Pass = $Pool_Pass.Replace("`$$($_.Name)",$_.Value)
     }
 
     [PSCustomObject]@{
         Algorithm     = $Pool_Algorithm_Norm
 		Algorithm0    = $Pool_Algorithm_Norm
         CoinName      = "$(if ($Pool_Coin) {$Pool_Coin.Name} elseif ($_.CoinName) {$_.CoinName} else {$_.CoinSymbol})"
-        CoinSymbol    = "$($_.CoinSymbol)".ToUpper()
-        Currency      = "$($_.Currency)".ToUpper()
+        CoinSymbol    = $Pool_Params["CoinSymbol"]
+        Currency      = $Pool_Params["Currency"]
         Price         = 0
         StablePrice   = 0
         MarginOfError = 0
         Protocol      = "$(if ($_.Protocol) {$_.Protocol} else {"stratum+$(if ($_.SSL) {"ssl"} else {"tcp"})"})"
         Host          = $_.Host
         Port          = $_.Port
-        User          = "$(if ($_.User) {$_.User} else {"`$Wallet.`$WorkerName"})".Replace("`$Wallet",$Pool_Wallet).Replace("`$WorkerName","{workername:$Worker}")
-        Pass          = "$(if ($_.Pass) {$_.Pass} else {"x"})"
+        User          = $Pool_User
+        Pass          = $Pool_Pass
         Region        = "$(if ($_.Region) {Get-Region $_.Region} else {"US"})"
         SSL           = $_.SSL
         WTM           = $true
