410e4e98b90eef9a1ddb72af736d964e16f4a595
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-02-07 15:22:55 +0100

    Update miner CryptoDredge to v0.26.0 (nvidia)
    - Add KawPow Algorithm
    - Add Chukwa-v2 Algorithm
    - Fix MTP issue related to 'invalid device symbol'
    - Support NVIDIA Ampere (RTX 30XX)
    - Add --temperature-limit and --temperature-start options
    - Add off flag to Nimiq optimizer (--optimizer off)
    - Avoid c-ares dependency on libnsl
    - Remove no longer supported algorithms. Check Readme for more information

--- Miners/CryptoDredge.ps1
+++ Miners/CryptoDredge.ps1
@@ -10,31 +10,23 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://github.com/technobyl/CryptoDredge/releases"
 $Port = "313{0:d2}"
 $DevFee = 1.0
-$Version = "0.25.1"
+$Version = "0.26.0"
 $Enable_Logfile = $false
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-CryptoDredge\CryptoDredge"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.25.1-cryptodredge/CryptoDredge_0.25.1_cuda_10.2_linux.tar.gz"
-            Cuda = "10.2"
-        },
-        [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.25.1-cryptodredge/CryptoDredge_0.25.1_cuda_9.2_linux.tar.gz"
-            Cuda = "9.2"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.26.0-cryptodredge/CryptoDredge_0.26.0_cuda_11.2_linux.tar.gz"
+            Cuda = "11.2"
         }
     )
 } else {
     $Path = ".\Bin\NVIDIA-CryptoDredge\CryptoDredge.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.25.1-cryptodredge/CryptoDredge_0.25.1_cuda_10.2_windows.zip"
-            Cuda = "10.2"
-        },
-        [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.25.1-cryptodredge/CryptoDredge_0.25.1_cuda_9.2_windows.zip"
-            Cuda = "9.2"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.26.0-cryptodredge/CryptoDredge_0.26.0_cuda_11.2_windows.zip"
+            Cuda = "11.2"
         }
     )
 }
@@ -42,48 +34,24 @@ if ($IsLinux) {
 if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
-    #[PSCustomObject]@{MainAlgorithm = "aeternity";   MinMemGb = 5; Params = ""; ExcludePoolName = "^Nicehash"} #Aeternity / Cuckoocycle (bad rounding, see https://github.com/technobyl/CryptoDredge/issues/62)
-    #[PSCustomObject]@{MainAlgorithm = "allium";      MinMemGb = 1; Params = ""} #Allium (CD 0.16.0 faster)
     [PSCustomObject]@{MainAlgorithm = "argon2d-dyn"; MinMemGb = 1; Params = ""} #Argon2d-Dyn
     [PSCustomObject]@{MainAlgorithm = "argon2d-nim"; MinMemGb = 1; Params = ""} #Argon2d-Nim
     [PSCustomObject]@{MainAlgorithm = "argon2d250";  MinMemGb = 1; Params = ""} #Argon2d250
     [PSCustomObject]@{MainAlgorithm = "argon2d4096"; MinMemGb = 3.3; Params = ""} #Argon2d4096
-    #[PSCustomObject]@{MainAlgorithm = "bcd";         MinMemGb = 1; Params = ""} #BCD (trex faster)
     [PSCustomObject]@{MainAlgorithm = "chukwa";      MinMemGb = 1.5; Params = ""} #Chukwa, new with v0.21.0
-    [PSCustomObject]@{MainAlgorithm = "chukwa-wrkz"; MinMemGb = 1.5; Params = ""} #Chukwa-Wrkz, new with v0.21.0
+    [PSCustomObject]@{MainAlgorithm = "chukwa2";     MinMemGb = 1.5; Params = ""} #ChukwaV2, new with v0.26.0
     [PSCustomObject]@{MainAlgorithm = "cnconceal";   MinMemGb = 1.5; Params = ""} #CryptonighConceal, new with v0.21.0
     [PSCustomObject]@{MainAlgorithm = "cnfast2";     MinMemGb = 1.5; Params = ""} #CryptonightFast2 / Masari
     [PSCustomObject]@{MainAlgorithm = "cngpu";       MinMemGb = 3.3; Params = ""} #CryptonightGPU
     [PSCustomObject]@{MainAlgorithm = "cnhaven";     MinMemGb = 3.3; Params = ""} #Cryptonighthaven
     [PSCustomObject]@{MainAlgorithm = "cnheavy";     MinMemGb = 3.3; Params = ""} #Cryptonightheavy
-    [PSCustomObject]@{MainAlgorithm = "cnsaber";     MinMemGb = 3.3; Params = ""} #Cryptonightheavytube
-    [PSCustomObject]@{MainAlgorithm = "cntlo";       MinMemGb = 3.3; Params = ""; Version = "0.24.0"} #CryptonightTLO, new with v0.24.0
+    [PSCustomObject]@{MainAlgorithm = "cntlo";       MinMemGb = 3.3; Params = ""} #CryptonightTLO, new with v0.24.0
     [PSCustomObject]@{MainAlgorithm = "cnturtle";    MinMemGb = 3.3; Params = ""} #Cryptonightturtle
     [PSCustomObject]@{MainAlgorithm = "cnupx2";      MinMemGb = 1.5; Params = ""} #CryptoNightLiteUpx2, new with v0.23.0
     [PSCustomObject]@{MainAlgorithm = "cnzls";       MinMemGb = 3.3; Params = ""} #CryptonightZelerius, new with v0.23.0
-    #[PSCustomObject]@{MainAlgorithm = "cuckaroo29";  MinMemGb = 3.3; Params = ""; ExtendInterval = 2} #Cuckaroo29
-    [PSCustomObject]@{MainAlgorithm = "hmq1725";     MinMemGb = 1; Params = ""} #HMQ1725 (new in 0.10.0)
-    [PSCustomObject]@{MainAlgorithm = "lux";         MinMemGb = 1; Params = ""; Algorithm = "phi2"} #Lux/PHI2
-    [PSCustomObject]@{MainAlgorithm = "lyra2v3";     MinMemGb = 1; Params = ""} #Lyra2Re3
-    [PSCustomObject]@{MainAlgorithm = "lyra2vc0ban"; MinMemGb = 1; Params = ""} #Lyra2vc0banHash
-    [PSCustomObject]@{MainAlgorithm = "lyra2z";      MinMemGb = 1; Params = ""} #Lyra2z
-    [PSCustomObject]@{MainAlgorithm = "lyra2zz";     MinMemGb = 1; Params = ""} #Lyra2zz
-    [PSCustomObject]@{MainAlgorithm = "mtp";         MinMemGb = 5; Params = ""; ExtendInterval = 2; DevFee = 2.0} #MTP
+    [PSCustomObject]@{MainAlgorithm = "kawpow";      MinMemGb = 3;   Params = ""; DAG = $true} #CryptonightZelerius, new with v0.26.0
+    #[PSCustomObject]@{MainAlgorithm = "mtp";         MinMemGb = 5; Params = ""; ExtendInterval = 2; DevFee = 2.0} #MTP
     #[PSCustomObject]@{MainAlgorithm = "mtp-tcr";     MinMemGb = 5; Params = ""; ExtendInterval = 2} #MTP-TCR
-    [PSCustomObject]@{MainAlgorithm = "neoscrypt";   MinMemGb = 1; Params = ""} #Neoscrypt
-    #[PSCustomObject]@{MainAlgorithm = "phi2";        MinMemGb = 1; Params = ""} #PHI2 (CD 0.16.0 faster)
-    [PSCustomObject]@{MainAlgorithm = "pipe";        MinMemGb = 1; Params = ""} #Pipe
-    [PSCustomObject]@{MainAlgorithm = "sha256csm";   MinMemGb = 1; Params = ""; Version = "0.24.0"} #Sha256csm, new with v0.24.0
-    [PSCustomObject]@{MainAlgorithm = "skunk";       MinMemGb = 1; Params = ""} #Skunk
-    [PSCustomObject]@{MainAlgorithm = "tribus";      MinMemGb = 1; Params = ""; ExtendInterval = 2} #Tribus
-    #[PSCustomObject]@{MainAlgorithm = "veil";        MinMemGb = 1; Params = ""; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"; Algorithm = "x16rt"} #X16rt-VEIL
-    #[PSCustomObject]@{MainAlgorithm = "x16r";        MinMemGb = 1; Params = ""; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"} #X16r
-    #[PSCustomObject]@{MainAlgorithm = "x16rt";       MinMemGb = 1; Params = ""; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"} #X16rt
-    #[PSCustomObject]@{MainAlgorithm = "x16rv2";      MinMemGb = 1; Params = ""; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"} #X16rv2 (fastest, but too high CPU usage)
-    #[PSCustomObject]@{MainAlgorithm = "x16s";        MinMemGb = 1; Params = ""} #X16s
-    #[PSCustomObject]@{MainAlgorithm = "x17";         MinMemGb = 1; Params = ""; ExtendInterval = 2} #X17
-    #[PSCustomObject]@{MainAlgorithm = "x21s";        MinMemGb = 1; Params = ""; ExtendInterval = 3; FaultTolerance = 0.7; HashrateDuration = "Day"} #X21s
-    #[PSCustomObject]@{MainAlgorithm = "x22i";        MinMemGb = 1; Params = ""; ExtendInterval = 2} #X22i (Trex faster)
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -120,8 +88,8 @@ $Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique 
 
     $Commands.Where({-not $_.Version -or (Compare-Version $Version $_.Version) -ge 0}).ForEach({
         $First = $true
-        $MinMemGb = $_.MinMemGb
-        $Miner_Device = $Device | Where-Object {(Test-VRAM $_ $MinMemGb) -and ($_.OpenCL.Architecture -in @("Other","Pascal","Turing"))}
+        $MinMemGb = if ($_.DAG) {Get-EthDAGSize -CoinSymbol $Pools.$Algorithm_Norm_0.CoinSymbol -Algorithm $Algorithm_Norm_0 -Minimum $_.MinMemGb} else {$_.MinMemGb}
+        $Miner_Device = $Device | Where-Object {Test-VRAM $_ $MinMemGb}
 
         $Algorithm = if ($_.Algorithm) {$_.Algorithm} else {$_.MainAlgorithm}
         $Algorithm_Norm_0 = Get-Algorithm $_.MainAlgorithm
