4cbd09108e02383b4f4301192cba6f7cde572e9a
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-02-09 12:51:08 +0100

    Update miner SrbMinerMulti to v0.6.6 (amd,cpu)
    - Performance increase on 'autolykos2' algorithm on GPU's
    - Reduced power consumption on 'autolykos2' algorithm on GPU's
    - Faster dataset creation on 'autolykos2' algorithm on GPU's
    - Fixed miner crashing when creating dataset on 'autolykos2' algorithm
    - Fixed miner crashing when running --gpu-auto-tune on 'autolykos2' algorithm
    - Possible fix for 'duplicate share' issue on 'autolykos2' algorithm
    - Fixed 'k12' algorithm
    - Minor bug fixes

--- Miners/SrbMinerMulti.ps1
+++ Miners/SrbMinerMulti.ps1
@@ -10,14 +10,14 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://bitcointalk.org/index.php?topic=5190081.0"
 $Port = "349{0:d2}"
 $DevFee = 0.85
-$Version = "0.6.5"
+$Version = "0.6.6"
 
 if ($IsLinux) {
     $Path = ".\Bin\ANY-SRBMinerMulti\SRBMiner-MULTI"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.6.5-srbminermulti/SRBMiner-Multi-0-6-5-Linux.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.6.6-srbminermulti/SRBMiner-Multi-0-6-6-Linux.tar.xz"
 } else {
     $Path = ".\Bin\ANY-SRBMinerMulti\SRBMiner-MULTI.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.6.5-srbminermulti/SRBMiner-Multi-0-6-5-win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.6.6-srbminermulti/SRBMiner-Multi-0-6-6-win64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No AMD nor CPU present in system
