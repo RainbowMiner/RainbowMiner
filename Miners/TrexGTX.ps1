7cc1b4c1c09623985f4338e6ef6a56bcf1b36c4f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-02-12 13:17:03 +0100

    Update miner Trex to v0.19.11 (nvidia)
    - (API) Bind API servers to 127.0.0.1 by default to prevent unauthorised access to the API
    - (API) Miner pause functionality is broken (regression)

--- Miners/TrexGTX.ps1
+++ Miners/TrexGTX.ps1
@@ -10,7 +10,7 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://github.com/trexminer/T-Rex/releases"
 $Port = "326{0:d2}"
 $DevFee = 1.0
-$Version = "0.19.10"
+$Version = "0.19.11"
 
 # use cuda 10, if cuda 11.1 is installed. Otherwise do not use this miner module.
 
@@ -18,7 +18,7 @@ if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-TrexGTX\t-rex"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.19.10-trex/t-rex-0.19.10-linux-cuda10.0.tar.gz"
+            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.19.11-trex/t-rex-0.19.11-linux-cuda10.0.tar.gz"
             Cuda   = "11.1"
         }
     )
@@ -26,7 +26,7 @@ if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-TrexGTX\t-rex.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.19.10-trex/t-rex-0.19.10-win-cuda10.0.zip"
+            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.19.11-trex/t-rex-0.19.11-win-cuda10.0.zip"
             Cuda   = "11.1"
         }
     )
