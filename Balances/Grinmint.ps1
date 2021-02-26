a6c4db88186d9e902d11a98fa77a050b90c7054d
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-02-26 19:47:10 +0100

    Update balance GrinMint
    - make more standardized

--- Balances/Grinmint.ps1
+++ Balances/Grinmint.ps1
@@ -5,27 +5,25 @@ param(
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Pool_Currency = "GRIN"
+
 $PoolConfig = $Config.Pools.$Name
 
-if (!$PoolConfig.GRIN) {
-    Write-Log -Level Verbose "Pool Balance API ($Name) has failed - no wallet address specified."
-    return
-}
-
-if ($Config.ExcludeCoinsymbolBalances.Count -and $Config.ExcludeCoinsymbolBalances -contains "GRIN") {return}
+if (-not $PoolConfig.Wallets.$Pool_Currency -or ($Config.ExcludeCoinsymbolBalances.Count -and $Config.ExcludeCoinsymbolBalances -contains $Pool_Currency)) {return}
 
 $Request = [PSCustomObject]@{}
 
 try {
-    if ($Request = Invoke-RestMethodAsync "https://api.grinmint.com/v2/user/$($PoolConfig.GRIN)/userStats" -cycletime ($Config.BalanceUpdateMinutes*60)) {
+    if ($Request = Invoke-RestMethodAsync "https://api.grinmint.com/v2/user/$($PoolConfig.$Pool_Currency)/userStats" -cycletime ($Config.BalanceUpdateMinutes*60)) {
         if ($Request.status) {
 			$Divisor  = [Decimal]1e9
             $Unpaid   = [Decimal]$Request.unpaid_balance / $Divisor
             $Immature = [Decimal]$Request.immature_balance / $Divisor
             [PSCustomObject]@{
-                    Caption     = "$($Name) (GRIN)"
+                    Caption     = "$($Name) ($($Pool_Currency))"
 					BaseName    = $Name
-                    Currency    = "GRIN"
+                    Currency    = $Pool_Currency
                     Balance     = $Unpaid
                     Pending     = $Immature
                     Total       = $Unpaid + $Immature
