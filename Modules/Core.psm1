340129874c3a958d3087a903341e32facd4f38a8
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-03-02 17:43:18 +0100

    Update core
    - avoid cURL not found message on Linux

--- Modules/Core.psm1
+++ Modules/Core.psm1
@@ -271,7 +271,6 @@ function Start-Core {
                 $Session.Curl = $CurlPath
             }
         }
-        if (-not $IsWindows) {$Session.Curl = $false}
     } catch {
         if ($Error.Count){$Error.RemoveAt(0)}
     }
@@ -279,6 +278,7 @@ function Start-Core {
     if ($Session.Curl) {
         Write-Host "ok" -ForegroundColor Green
         Write-Log -Level Info "Curl found: $($Session.Curl)"
+        if (-not $IsWindows) {$Session.Curl = $false}
     } else {
         Write-Host "not found" -ForegroundColor Red
     }
