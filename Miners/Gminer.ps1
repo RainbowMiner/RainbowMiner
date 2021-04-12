a2b42ab1e2df7c9f2f96c584382fbf21b05d2fb9
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-04-12 12:50:36 +0200

    Update miner Gminer to v2.51 (amd,nvidia)

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -11,15 +11,15 @@ $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "2.51b"
+$Version = "2.51"
 $DeviceCapability = "5.0"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.51-gminer/gminer_2_51_beta_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.51-gminer/gminer_2_51_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.51-gminer/gminer_2_51_beta_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.51-gminer/gminer_2_51_windows64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
