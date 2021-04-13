c6303cac741e49e1636ca7f04ce21bf6e481086c
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-04-13 14:11:55 +0200

    Update miner Lolminer to v1.26 (amd,nvidia)

--- Miners/lolminer.ps1
+++ Miners/lolminer.ps1
@@ -11,14 +11,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=4724735.0"
 $Port = "317{0:d2}"
 $Cuda = "10.0"
 $DevFee = 1.0
-$Version = "1.25"
+$Version = "1.26"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-lolMiner\lolMiner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.25-lolminer/lolMiner_v1.25_Lin64.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.26-lolminer/lolMiner_v1.26_Lin64.tar.gz"
 } else {
     $Path = ".\Bin\GPU-lolMiner\lolMiner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.25-lolminer/lolMiner_v1.25_Win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.26-lolminer/lolMiner_v1.26_Win64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
