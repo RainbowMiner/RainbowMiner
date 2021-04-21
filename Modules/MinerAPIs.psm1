231242e00825ea9c2d75dc5b5971349c02735ebc
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-04-21 16:55:15 +0200

    Update miner Xmrig to v6.12.0 (amd,cpu,nvidia)
    - Added support for Uplexa (cn/upx2 algorithm).
    - Show total hashrate if compiled without OpenCL.
    - RandomX: optimized IMUL_RCP instruction.
    - Added support for --user command line option for online benchmark.

--- Modules/MinerAPIs.psm1
+++ Modules/MinerAPIs.psm1
@@ -2817,6 +2817,166 @@ class Xmrig3 : Miner {
     }
 }
 
+class Xmrig6 : Miner {
+
+    [String]GetArguments() {
+        $Arguments = $this.Arguments -replace "\`$mport",$this.Port
+        if ($Arguments -notlike "{*}") {return $Arguments}
+
+        $ThreadsConfig     = $null
+
+        $Miner_Path        = Split-Path $this.Path
+        $Parameters        = $Arguments | ConvertFrom-Json
+
+        $ConfigName        = "$($this.BaseAlgorithm -join '-')_$($Parameters.HwSig)$(if ($Parameters.Affinity -ne $null) {"_$($Parameters.Affinity)"})_$($Parameters.Threads)"
+
+        $ConfigFN          = "config_$($ConfigName).json"
+        $ThreadsConfigFN   = "threads_$($Parameters.HwSig).json"
+        $RunConfigFN       = "run_$(Get-MD5Hash "$($ConfigName)$(ConvertTo-Json $Parameters.Pools -Compress)").json"
+        $ConfigFile        = Join-Path $Miner_Path $ConfigFN
+        $ThreadsConfigFile = Join-Path $Miner_Path $ThreadsConfigFN
+        $RunConfigFile     = Join-Path $Miner_Path $RunConfigFN
+        $LogFile           = "log_$($ConfigName).txt"
+
+        $Algo              = $Parameters.Algorithm
+        $Algo0             = $Parameters.Algorithm -replace "/.+$"
+        $Device            = Switch($Parameters.Vendor) {"AMD" {"opencl"}; "NVIDIA" {"cuda"}; default {"cpu"}}
+
+        try {
+            if (Test-Path $ThreadsConfigFile) {
+                $ThreadsConfig = Get-Content $ThreadsConfigFile -Raw -ErrorAction Ignore | ConvertFrom-Json -ErrorAction Ignore
+            }
+            if (-not ($ThreadsConfig.$Algo | Measure-Object).Count -and -not ($ThreadsConfig.$Algo0 | Measure-Object).Count) {
+                $InitConfig = $Parameters.Config | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+                $InitConfig | Add-Member pools $Parameters.Pools -Force -PassThru | ConvertTo-Json -Depth 10 | Set-Content $ThreadsConfigFile -Force
+
+                $ArgumentList = ("--algo=$($Parameters.Algorithm) --config=$ThreadsConfigFN $($Parameters.DeviceParams) $($Parameters.Params)" -replace "\s+",' ').Trim()
+                $Job = Start-SubProcess -FilePath $this.Path -ArgumentList $ArgumentList -WorkingDirectory $Miner_Path -LogPath (Join-Path $Miner_Path $LogFile) -Priority ($this.DeviceName | ForEach-Object {if ($_ -like "CPU*") {$this.Priorities.CPU} else {$this.Priorities.GPU}} | Measure-Object -Maximum | Select-Object -ExpandProperty Maximum) -ShowMinerWindow $true -IsWrapper ($this.API -eq "Wrapper") -SetLDLIBRARYPATH:$this.SetLDLIBRARYPATH
+                if ($Job.XJob) {
+                    $wait = 0
+                    While ($wait -lt 60) {
+                        if (($ThreadsConfig = @(Get-Content $ThreadsConfigFile -Raw -ErrorAction Ignore | ConvertFrom-Json -ErrorAction Ignore).$Device | Select-Object)) {
+                            ConvertTo-Json $ThreadsConfig -Depth 10 | Set-Content $ThreadsConfigFile -Force
+                            break
+                        }
+                        Start-Sleep -Milliseconds 500
+                        $MiningProcess = $Job.ProcessId | Foreach-Object {Get-Process -Id $_ -ErrorAction Ignore | Select-Object Id,HasExited}
+                        if ((-not $MiningProcess -and $Job.XJob.State -eq "Running") -or ($MiningProcess -and ($MiningProcess | Where-Object {-not $_.HasExited} | Measure-Object).Count -eq 1)) {$wait++} else {break}
+                    }
+                }
+                if ($Job) {
+                    Stop-SubProcess -Job $Job -Title "Miner $($this.Name) (prerun)"
+                    Remove-Variable "Job"
+                }
+                if ((Test-Path $ThreadsConfigFile) -and -not ($ThreadsConfig.$Algo | Measure-Object).Count -and -not ($ThreadsConfig.$Algo0 | Measure-Object).Count) {
+                    Remove-Item $ThreadsConfigFile -ErrorAction Ignore -Force
+                }
+                $ThreadsConfig = Get-Content $ThreadsConfigFile -Raw -ErrorAction Ignore | ConvertFrom-Json -ErrorAction Ignore
+            }
+
+            $Config = Get-Content $ConfigFile -Raw -ErrorAction Ignore | ConvertFrom-Json -ErrorAction Ignore
+
+            if (-not $Config.$Device -and -not ($Config.$Device.$Algo | Measure-Object).Count -and -not ($Config.$Device.$Algo0 | Measure-Object).Count) {
+                if ($ThreadsConfig.$Algo -or $ThreadsConfig.$Algo0) {
+                    $Parameters.Config | Add-Member $Device ([PSCustomObject]@{}) -Force
+                    $ThreadsConfig.PSObject.Properties | Where-Object {$_.Name -notmatch "/0$" -and $_.Value -isnot [array]} | Foreach-Object {
+                        $n = $_.Name; $v = $_.Value
+                        $Parameters.Config.$Device | Add-Member $n $v -Force
+                    }
+                    $Algo = if ($ThreadsConfig.$Algo) {$Algo} else {$Algo0}
+
+                    if ($Device -eq "cpu") {
+                        $cix = @{}
+                        $ThreadsAffinity = $ThreadsConfig.$Algo | Foreach-Object {if ($_ -is [array] -and $_.Count -eq 2) {$cix["k$($_[1])"] = $_[0];$_[1]} else {$_}}
+
+                        $Parameters.Config.$Device | Add-Member $Algo ([Array]($ThreadsAffinity | Sort-Object {$_ -band 1},{$_} | Select-Object -First $(if ($Parameters.Threads -and $Parameters.Threads -lt $ThreadsConfig.$Algo.Count) {$Parameters.Threads} else {$ThreadsConfig.$Algo.Count}) | Sort-Object)) -Force
+
+                        $Aff = if ($Parameters.Affinity) {ConvertFrom-CPUAffinity $Parameters.Affinity}
+                        if ($AffCount = ($Aff | Measure-Object).Count) {
+                            $AffThreads = @(Compare-Object $Aff $Parameters.Config.$Device.$Algo -IncludeEqual -ExcludeDifferent | Where-Object {$_.SideIndicator -eq "=="} | Foreach-Object {$_.InputObject} | Select-Object)
+                            $ThreadsCount = [Math]::Min($AffCount,$Parameters.Config.$Device.$Algo.Count)
+                            if ($AffThreads.Count -lt $ThreadsCount) {
+                                $Aff | Where-Object {$_ -notin $AffThreads} | Sort-Object {$_ -band 1},{$_} | Select-Object -First ($ThreadsCount-$AffThreads.Count) | Foreach-Object {$AffThreads += $_}
+                            }
+                            $Parameters.Config.$Device.$Algo = @($AffThreads | Sort-Object);
+                        }
+                        if ($cix.Count) {
+                            for ($i=0; $i -lt $Parameters.Config.$Device.$Algo.Count; $i++) {
+                                $thr = $Parameters.Config.$Device.$Algo[$i]
+                                $Parameters.Config.$Device.$Algo[$i] = @($(if ($cix["k$thr"]) {$cix["k$thr"]} else {1}),$thr)
+                            }
+                        }
+                    } else { #device is cuda or opencl
+                        $Parameters.Config.$Device | Add-Member $Algo ([Array](@($ThreadsConfig.$Algo | Where-Object {$Parameters.Devices -contains $_.index} | Select-Object) * $Parameters.Threads)) -Force
+                    }
+                    $Parameters.Config | Add-Member autosave $false -Force
+                    $Parameters.Config | ConvertTo-Json -Depth 10 | Set-Content $ConfigFile -Force
+                }
+                else {
+                    Write-Log -Level Warn "Error parsing threads config file - cannot create miner config file ($($this.Name) {$($this.BaseAlgorithm -join '-')@$($this.Pool -join '-')})$(if ($Error.Count){"[Error: '$($Error[0])'].";$Error.RemoveAt(0)})"
+                }                
+            }
+
+            $LastModified = (Get-Item $ConfigFile -ErrorAction Ignore).LastWriteTime
+
+            if (-not (Test-Path $RunConfigFile) -or (Test-Path $RunConfigFile -OlderThan $LastModified)) {
+                $RunConfig = Get-Content $ConfigFile -Raw -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
+                $RunConfig | Add-Member pools $Parameters.Pools -Force
+
+                Set-ContentJson -PathToFile $RunConfigFile -Data $RunConfig > $null
+            }
+        }
+        catch {
+            if ($Error.Count){$Error.RemoveAt(0)}
+            Write-Log -Level Warn "Creating miner config files failed ($($this.BaseName) $($this.BaseAlgorithm -join '-')@$($this.Pool -join '-')}) [Error: '$($_.Exception.Message)']."
+        }
+
+
+        return ("--algo=$($Parameters.Algorithm) --config=$RunConfigFN $($Parameters.DeviceParams) $($Parameters.APIParams) $($Parameters.Params)" -replace "\s+",' ').Trim()
+    }
+
+    [Void]UpdateMinerData () {
+        if ($this.GetStatus() -ne [MinerStatus]::Running) {return}
+
+        $Server = "localhost"
+        $Timeout = 10 #seconds
+
+        $Request = ""
+        $Response = ""
+
+        $HashRate   = [PSCustomObject]@{}
+        $Difficulty = [PSCustomObject]@{}
+
+        try {
+            $Data = Invoke-GetUrl "http://$($Server):$($this.Port)/api.json" -Timeout $Timeout
+        }
+        catch {
+            if ($Error.Count){$Error.RemoveAt(0)}
+            Write-Log -Level Info "Failed to connect to miner ($($this.Name)). "
+            return
+        }
+
+        $HashRate_Name = $this.Algorithm[0]
+        $HashRate_Value = [Double]$Data.hashrate.total[0]
+        if (-not $HashRate_Value) {$HashRate_Value = [Double]$Data.hashrate.total[1]} #fix
+        if (-not $HashRate_Value) {$HashRate_Value = [Double]$Data.hashrate.total[2]} #fix
+
+        if ($HashRate_Name -and $HashRate_Value -gt 0) {
+            $HashRate   | Add-Member @{$HashRate_Name = $HashRate_Value}
+
+            $Difficulty_Value = [Double]$Data.results.diff_current
+            $Difficulty | Add-Member @{$HashRate_Name = $Difficulty_Value}
+
+            $Accepted_Shares  = [Double]$Data.results.shares_good
+            $Rejected_Shares  = [Double]($Data.results.shares_total - $Data.results.shares_good)
+            $this.UpdateShares(0,$Accepted_Shares,$Rejected_Shares)
+        }
+
+        $this.AddMinerData($Response,$HashRate,$Difficulty)
+
+        $this.CleanupMinerData()
+    }
+}
 
 class XmrigWrapper : Miner {
 
