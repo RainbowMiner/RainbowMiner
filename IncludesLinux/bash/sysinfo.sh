6591bb449f44e1751de8ea4fd6738c43573f0fb1
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-05-11 12:29:44 +0200

    Update sysinfo.sh
    - show local disks, only
    - fix diskspace, if TB disks are used

--- IncludesLinux/bash/sysinfo.sh
+++ IncludesLinux/bash/sysinfo.sh
@@ -36,14 +36,14 @@ awk 'BEGIN {
 
         printf "  \"Disks\": [\n"
         i = 0
-        while("df -hP " | getline) {
+        while("df -lPm " | getline) {
                 if ( $NF == "/" ) {
                         if (i>0) {printf ",\n"}
                         printf "    {\n"
                         printf "      \"Drive\": \"%s\",\n", $1
                         printf "      \"Name\": \"\",\n"
-                        printf "      \"TotalGB\": %.1f,\n", $2
-                        printf "      \"UsedGB\": %.1f,\n", $3
+                        printf "      \"TotalGB\": %.1f,\n", $2/1024
+                        printf "      \"UsedGB\": %.1f,\n", $3/1024
                         printf "      \"UsedPercent\": %.2f\n", $3*100/$2
                         printf "    }"
                         i++
