0e0d2bcc27a285943d4fdb79ee3723b3fea7f0ae
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-05-13 10:45:27 +0200

    Update miner SgminerFancyIx
    - get rid of invalid object warning message (issues #1581 and #1590)

--- Miners/SgminerFancyIX.ps1
+++ Miners/SgminerFancyIX.ps1
@@ -80,7 +80,6 @@ $Global:DeviceCache.DevicesByTypes.AMD | Select-Object Vendor, Model -Unique | F
                     $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
                     $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
                     $Miner_PlatformId = $Miner_Device | Select -Property Platformid -Unique -ExpandProperty PlatformId
-                    $Miner_UseNavi 
                     $First = $false
                 }
 				$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
