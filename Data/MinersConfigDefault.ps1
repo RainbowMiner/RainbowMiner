cbab4c7b4f24ed178e2689d37ea747a70217083b
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-05-23 11:15:23 +0200

    Update MinersConfigDefault.ps1
    - remove deprecated entries

--- Data/MinersConfigDefault.ps1
+++ Data/MinersConfigDefault.ps1
@@ -27,78 +27,21 @@
             [PSCustomObject]@{MainAlgorithm = "x13"; SecondaryAlgorithm = ""; Params = "-i 20"; OCprofile = ""; MSIAprofile = ""}
             [PSCustomObject]@{MainAlgorithm = "x17"; SecondaryAlgorithm = ""; Params = "-i 20.5"; OCprofile = ""; MSIAprofile = ""}
         )
-        "CcminerAlexis78x64" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "c11"; SecondaryAlgorithm = ""; Params = "-i 21"; OCprofile = ""; MSIAprofile = ""}
-            [PSCustomObject]@{MainAlgorithm = "keccak"; SecondaryAlgorithm = ""; Params = "-m 2 -i 29"; OCprofile = ""; MSIAprofile = ""}
-            [PSCustomObject]@{MainAlgorithm = "keccakc"; SecondaryAlgorithm = ""; Params = "-i 29"; OCprofile = ""; MSIAprofile = ""}
-            [PSCustomObject]@{MainAlgorithm = "veltor"; SecondaryAlgorithm = ""; Params = "-i 23"; OCprofile = ""; MSIAprofile = ""}
-            [PSCustomObject]@{MainAlgorithm = "x11evo"; SecondaryAlgorithm = ""; Params = "-i 21"; OCprofile = ""; MSIAprofile = ""}
-            [PSCustomObject]@{MainAlgorithm = "x13"; SecondaryAlgorithm = ""; Params = "-i 20"; OCprofile = ""; MSIAprofile = ""}
-            [PSCustomObject]@{MainAlgorithm = "x17"; SecondaryAlgorithm = ""; Params = "-i 20.5"; OCprofile = ""; MSIAprofile = ""}
-        )
-        "CcminerCool" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "lyra2z"; SecondaryAlgorithm = ""; Params = "-i 20"; OCprofile = "Profile4"; MSIAprofile = "4"}
-        )
-        "CcminerDumax" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "phi2"; SecondaryAlgorithm = ""; Params = "-i 19"; OCprofile = ""; MSIAprofile = ""}
-        )
         "CcminerKlausT" = [PSCustomObject[]]@(            
             [PSCustomObject]@{MainAlgorithm = "lyra2v3"; SecondaryAlgorithm = ""; Params = "-i 24"; OCprofile = "Profile4"; MSIAprofile = "4"}
-        )
-        "CcminerPolytimos" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "keccak"; SecondaryAlgorithm = ""; Params = "-i auto"; OCprofile = ""; MSIAprofile = ""}
-            [PSCustomObject]@{MainAlgorithm = "nist5"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile4"; MSIAprofile = "4"}
-            [PSCustomObject]@{MainAlgorithm = "x17"; SecondaryAlgorithm = ""; Params = "-i 20.5"; OCprofile = ""; MSIAprofile = ""}
-        )
-        "CcminerRaven" = [PSCustomObject[]]@(        
-            [PSCustomObject]@{MainAlgorithm = "x16r"; SecondaryAlgorithm = ""; Params = "-i 20"; OCprofile = "Profile4"; MSIAprofile = "4"}
-        )
-        "CcminerSkunk" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "skunk"; SecondaryAlgorithm = ""; Params = "-i 25"; OCprofile = ""; MSIAprofile = ""}
-        )
-        "CcminerSpModSkunk" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "skunk"; SecondaryAlgorithm = ""; Params = "-i 25"; OCprofile = ""; MSIAprofile = ""}
-        )
-        "CcminerSpModTribus" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "tribus"; SecondaryAlgorithm = ""; Params = "-i auto"; OCprofile = ""; MSIAprofile = ""}
         )
         "CcminerTpruvot" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "lyra2z"; SecondaryAlgorithm = ""; Params = "-i 21"; OCprofile = "Profile4"; MSIAprofile = "4"}
             [PSCustomObject]@{MainAlgorithm = "x13"; SecondaryAlgorithm = ""; Params = "-i 20"; OCprofile = ""; MSIAprofile = ""}
-        )
-        "CcminerTpruvotx64" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "allium"; SecondaryAlgorithm = ""; Params = "-i 22.125"; OCprofile = ""; MSIAprofile = ""}
-            [PSCustomObject]@{MainAlgorithm = "groestl"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
-            [PSCustomObject]@{MainAlgorithm = "hmq1725"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile3"; MSIAprofile = "3"}
-            [PSCustomObject]@{MainAlgorithm = "keccak"; SecondaryAlgorithm = ""; Params = "-i 29"; OCprofile = ""; MSIAprofile = ""}
-            [PSCustomObject]@{MainAlgorithm = "keccakc"; SecondaryAlgorithm = ""; Params = "-i 29"; OCprofile = ""; MSIAprofile = ""}
-            [PSCustomObject]@{MainAlgorithm = "lyra2z"; SecondaryAlgorithm = ""; Params = "-i 21"; OCprofile = "Profile4"; MSIAprofile = "4"}
-            [PSCustomObject]@{MainAlgorithm = "sha256t"; SecondaryAlgorithm = ""; Params = "-i 29"; OCprofile = ""; MSIAprofile = ""}
-            [PSCustomObject]@{MainAlgorithm = "x13"; SecondaryAlgorithm = ""; Params = "-i 20"; OCprofile = ""; MSIAprofile = ""}
-        )
-        "CcminerX16r" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "groestl"; SecondaryAlgorithm = ""; Params = "-i 21"; OCprofile = "Profile4"; MSIAprofile = "4"}
-            [PSCustomObject]@{MainAlgorithm = "lyra2z"; SecondaryAlgorithm = ""; Params = "-i 21"; OCprofile = "Profile4"; MSIAprofile = "4"}
-        )
-        "CcminerX16s" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "hmq1725"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile3"; MSIAprofile = "3"}
         )
         "CcminerXevan" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "c11"; SecondaryAlgorithm = ""; Params = "-i 21"; OCprofile = ""; MSIAprofile = ""}
             [PSCustomObject]@{MainAlgorithm = "skein"; SecondaryAlgorithm = ""; Params = "-i auto"; OCprofile = ""; MSIAprofile = ""}
             [PSCustomObject]@{MainAlgorithm = "xevan"; SecondaryAlgorithm = ""; Params = "-i 21"; OCprofile = ""; MSIAprofile = ""}
         )
-        "CcminerXevan75" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "skein"; SecondaryAlgorithm = ""; Params = "-i auto"; OCprofile = ""; MSIAprofile = ""}
-        )
         "CcminerYescrypt" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "neoscrypt"; SecondaryAlgorithm = ""; Params = "-i 17.6"; OCprofile = "Profile3"; MSIAprofile = "3"}
             [PSCustomObject]@{MainAlgorithm = "yescryptR32"; SecondaryAlgorithm = ""; Params = "-i 12.5"; OCprofile = ""; MSIAprofile = ""}
-        )
-        "ClaymoreEthash" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "ethash"; SecondaryAlgorithm = "pascal"; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
-            [PSCustomObject]@{MainAlgorithm = "ethash"; SecondaryAlgorithm = "keccak"; Params = ""; OCprofile = "Profile3"; MSIAprofile = "3"}
-            [PSCustomObject]@{MainAlgorithm = "ethash"; SecondaryAlgorithm = "blake2s"; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
         )
         "EWBF" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "equihash24x5"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
@@ -115,9 +58,6 @@
         )
         "NBminer" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "ethash"; SecondaryAlgorithm = "tensority"; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
-        )
-        "Nsgminer" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "neoscrypt"; SecondaryAlgorithm = ""; Params = "--worksize 64 --intensity d"; OCprofile = ""; MSIAprofile = ""}
         )
         "Phoenix" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "progpow"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = ""}
@@ -149,9 +89,6 @@
         "SgminerLyra2z" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "lyra2z"; SecondaryAlgorithm = ""; Params = "--worksize 32 --intensity 18"; OCprofile = "Profile4"; MSIAprofile = "4"}
         )
-        "SgminerSkein" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "skeincoin"; SecondaryAlgorithm = ""; Params = "--gpu-threads 2 --worksize 256 --intensity d"; OCprofile = ""; MSIAprofile = ""}
-        )
         "SgminerXevan" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "xevan-mod"; SecondaryAlgorithm = ""; Params = "--intensity 15"; OCprofile = ""; MSIAprofile = ""}
         )        
@@ -160,14 +97,6 @@
             [PSCustomObject]@{MainAlgorithm = "x17"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile4"; MSIAprofile = "4"}
         )
         "TTminer" = [PSCustomObject[]]@(
-            [PSCustomObject]@{MainAlgorithm = "progpow"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
-            [PSCustomObject]@{MainAlgorithm = "progpow2gb"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
-            [PSCustomObject]@{MainAlgorithm = "progpow3gb"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
-            [PSCustomObject]@{MainAlgorithm = "progpowz"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
-            [PSCustomObject]@{MainAlgorithm = "progpowz2gb"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
-            [PSCustomObject]@{MainAlgorithm = "progpowz3gb"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
-        )
-        "TTminer3" = [PSCustomObject[]]@(
             [PSCustomObject]@{MainAlgorithm = "progpow"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
             [PSCustomObject]@{MainAlgorithm = "progpow2gb"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
             [PSCustomObject]@{MainAlgorithm = "progpow3gb"; SecondaryAlgorithm = ""; Params = ""; OCprofile = "Profile5"; MSIAprofile = "5"}
