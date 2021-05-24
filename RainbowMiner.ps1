cf608906dee0d6374ec59dff7e3fa1074cf1d010
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-05-24 10:59:47 +0200

    Bump version to v4.7.2.0

--- RainbowMiner.ps1
+++ RainbowMiner.ps1
@@ -320,7 +320,7 @@ if ($MyInvocation.MyCommand.Path) {Set-Location (Split-Path $MyInvocation.MyComm
 
 Initialize-Session
 
-$Session.Version         = "4.7.1.9"
+$Session.Version         = "4.7.2.0"
 $Session.MainWindowTitle = "RainbowMiner v$($Session.Version)"
 $Session.SetupOnly       = $SetupOnly
 $Session.LogLevel        = $LogLevel
