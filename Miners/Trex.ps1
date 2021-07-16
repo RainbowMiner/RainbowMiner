c994e706755f749ef802a9522472956b7814b60c
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-07-16 20:56:31 +0200

    Update miner Trex to v0.21.4 (nvidia)
    - (autolykos2) Minor performance improvements (mainly Pascal GPUs)
    - (Windows) Increase fan speed to 100% in auto-fan mode
    
    Bug fixes:
    - (autolykos2) Invalid shares when mining ERGO at NiceHash
    - (mtp) ntime out of range and Low difficulty share errors

--- Miners/Trex.ps1
+++ Miners/Trex.ps1
@@ -10,14 +10,14 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://bitcointalk.org/index.php?topic=4432704.0"
 $Port = "316{0:d2}"
 $DevFee = 1.0
-$Version = "0.21.3"
+$Version = "0.21.4"
 $DeviceCapability = "5.0"
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Trex\t-rex"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.21.3-trex/t-rex-0.21.3-linux.tar.gz"
+            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.21.4-trex/t-rex-0.21.4-linux.tar.gz"
             Cuda   = "9.2"
         }
     )
@@ -25,7 +25,7 @@ if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Trex\t-rex.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.21.3-trex/t-rex-0.21.3-win.zip"
+            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.21.4-trex/t-rex-0.21.4-win.zip"
             Cuda   = "9.2"
         }
     )
