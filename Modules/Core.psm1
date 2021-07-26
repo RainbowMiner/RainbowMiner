5825cc23fc362190c0934afaff41152df680c3e7
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-07-26 17:02:40 +0200

    Update core
    - fix Fastlane for Benchmark not saved

--- Modules/Core.psm1
+++ Modules/Core.psm1
@@ -2128,14 +2128,14 @@ function Invoke-Core {
                     $Miner_Models = @{}
                     $Global:DeviceCache.Devices.ForEach({$Miner_Models[$_.Name] = $_.Model})
                     $AllMiners.Where({$_.HashRates.PSObject.Properties.Value -contains $null -and $_.HashRates.PSObject.Properties.Name.Count -eq 1}).ForEach({
-                        $Miner_Name = $_.Name
-                        $Miner_Algo = $_.HashRates.PSObject.Properties.Name -replace '\-.*$'
+                        $Miner_Name = $_.BaseName
+                        $Miner_Algo = "$($_.HashRates.PSObject.Properties.Name -replace '\-.*$')"
                         $Miner_HR   = ($DeviceName | Foreach-Object {$Response.data."$($Miner_Models[$_])".$Miner_Name.$Miner_Algo.hr} | Measure-Object -Sum).Sum
 
                         if ($Miner_HR -gt 0 -or -not $Session.Config.EnableFastlaneBenchmarkMissing) {
                             $_.HashRates."$($_.HashRates.PSObject.Properties.Name)" = $Miner_HR
                             $_.PowerDraw             = ($DeviceName | Foreach-Object {$Response.data."$($Miner_Models[$_])".$Miner_Name.$Miner_Algo.pd} | Measure-Object -Sum).Sum
-                            Set-Stat -Name "$($_.Name)_$($Miner_Algo)_HashRate" -Value $Miner_HR -Duration (New-TimeSpan -Seconds 10) -FaultDetection $false -PowerDraw $_.PowerDraw -Sub $Global:DeviceCache.DevicesToVendors[$_.DeviceModel] -Quiet > $null
+                            Set-Stat -Name "$($_.Name)_$($Miner_Algo)_HashRate" -Value $Miner_HR -Duration (New-TimeSpan -Seconds 1) -FaultDetection $false -PowerDraw $_.PowerDraw -Sub $Global:DeviceCache.DevicesToVendors[$_.DeviceModel] -Quiet > $null
                         }
                         if ($Miner_HR -gt 0) {$Fastlane_Success++} else {$Fastlane_Failed++}
                     })
