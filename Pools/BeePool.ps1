ca46e9010a7ae454c7a57f00966f594d50bfce2a
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-08-08 15:33:05 +0200

    Update pool BeePool
    - remove BEAM, MOAC
    - add missing SSL ports

--- Pools/BeePool.ps1
+++ Pools/BeePool.ps1
@@ -21,14 +21,12 @@ $Pool_Region_Default = "asia"
 @("asia") | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
 
 $Pools_Data = @(
-    [PSCustomObject]@{symbol = "ETH";  port = @(9530);      fee = 1.0; fee_pplns = 1.0}
-    [PSCustomObject]@{symbol = "ETC";  port = @(9518);      fee = 1.0; fee_pplns = 1.0}
-    [PSCustomObject]@{symbol = "RVN";  port = @(9531);      fee = 2.0; fee_pplns = 1.0}
+    [PSCustomObject]@{symbol = "ETH";  port = @(9530,9531); fee = 1.0; fee_pplns = 1.0}
+    [PSCustomObject]@{symbol = "ETC";  port = @(9518,9519); fee = 1.0; fee_pplns = 1.0}
+    [PSCustomObject]@{symbol = "RVN";  port = @(9531,9532); fee = 2.0; fee_pplns = 1.0}
     [PSCustomObject]@{symbol = "CFX";  port = @(9555,9556); fee = 2.0; fee_pplns = $null}
-    [PSCustomObject]@{symbol = "SERO"; port = @(9515);      fee = 2.0; fee_pplns = 1.0}
-    [PSCustomObject]@{symbol = "AE";   port = @(9505);      fee = 2.0; fee_pplns = 1.0}
-    [PSCustomObject]@{symbol = "BEAM"; port = @(9507);      fee = 2.0; fee_pplns = 1.0}
-    [PSCustomObject]@{symbol = "MOAC"; port = @(9540);      fee = 1.0; fee_pplns = $null}
+    [PSCustomObject]@{symbol = "SERO"; port = @(9515,9516); fee = 2.0; fee_pplns = $null}
+    [PSCustomObject]@{symbol = "AE";   port = @(9505,9506); fee = 2.0; fee_pplns = 1.0}
 )
 
 $Pool_Request = [PSCustomObject]@{}
