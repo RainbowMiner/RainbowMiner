3c2cf1c41e39e08e0d32d55f2acd01282a8579b3
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-08-09 21:43:45 +0200

    Add miner Suprminer v2.3.1 (nvidia)
    - added for mining OBTC/HeavyHash (pull request #1665)

---
+++ Miners/Suprminer.ps1
@@ -0,0 +1,119 @@
+﻿using module ..\Modules\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+if (-not $IsWindows -and -not $IsLinux) {return}
+
+$ManualUri = "https://github.com/ocminer/suprminer-releases"
+$Port = "107{0:d2}"
+$DevFee = 1.0
+$Version = "2.3.1"
+
+if ($IsLinux) {
+    $Path = ".\Bin\NVIDIA-Suprminer\suprminer"
+    $UriCuda = @(
+        [PSCustomObject]@{
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.3.1-suprminer/suprminer-cuda-11-3.tar.gz"
+            Cuda = "11.3"
+        },
+        [PSCustomObject]@{
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.3.1-suprminer/suprminer-cuda-11-2.tar.gz"
+            Cuda = "11.2"
+        },
+        [PSCustomObject]@{
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.3.1-suprminer/suprminer-cuda-9.1.tar.gz"
+            Cuda = "9.1"
+        }
+    )
+} else {
+    $Path = ".\Bin\NVIDIA-Suprminer\suprminer.exe"
+    $UriCuda = @(
+        [PSCustomObject]@{
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.3.1-suprminer/suprminer-winx86_64_cuda11_1.zip"
+            Cuda = "11.1"
+        }
+    )
+}
+
+if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "heavyhash"; MinMemGB = 2; Params = "-a obtc"; ExtendInterval = 2} #HeavyHash/OBTC
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("NVIDIA")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $UriCuda.Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Cuda = $null
+for($i=0;$i -lt $UriCuda.Count -and -not $Cuda;$i++) {
+    if (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $UriCuda[$i].Cuda -Warning $(if ($i -lt $UriCuda.Count-1) {""}else{$Name})) {
+        $Uri  = $UriCuda[$i].Uri
+        $Cuda = $UriCuda[$i].Cuda
+        if ($UriCuda[$i].Version -ne $null) {$Version = $UriCuda[$i].Version}
+    }
+}
+
+if (-not $Cuda) {return}
+
+$Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Model = $_.Model
+    $Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)".Where({$_.Model -eq $Miner_Model})
+
+    $Commands.ForEach({
+        $First = $true
+        $Algorithm_Norm_0 = Get-Algorithm $_.MainAlgorithm
+
+        $MinMemGB = $_.MinMemGB
+        $Miner_Device = $Device | Where-Object {Test-VRAM $_ $MinMemGB}
+
+		foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)","$($Algorithm_Norm_0)-GPU")) {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and (-not $_.ExcludePoolName -or $Pools.$Algorithm_Norm.Name -notmatch $_.ExcludePoolName)) {
+                if ($First) {
+                    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+                    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+                    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
+                    $First = $false
+                }
+				$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
+				[PSCustomObject]@{
+					Name           = $Miner_Name
+					DeviceName     = $Miner_Device.Name
+					DeviceModel    = $Miner_Model
+					Path           = $Path
+					Arguments      = "-b `$mport -d $($DeviceIDsAll) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) $($_.Params)"
+					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Week}
+					API            = "Ccminer"
+					Port           = $Miner_Port
+					Uri            = $Uri
+                    FaultTolerance = $_.FaultTolerance
+					ExtendInterval = $_.ExtendInterval
+                    Penalty        = 0
+					DevFee         = 0.0
+					ManualUri      = $ManualUri
+                    MiningPriority = 2
+                    Version        = $Version
+                    PowerDraw      = 0
+                    BaseName       = $Name
+                    BaseAlgorithm  = $Algorithm_Norm_0
+                    ExcludePoolName= $_.ExcludePoolName
+				}
+			}
+		}
+    })
+}
