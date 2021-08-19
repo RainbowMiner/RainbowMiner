47042893d19cf90fbd6e58ad3fb18c50bb3365e9
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-08-19 09:10:50 +0200

    Update miner Gminer to v2.65 (amd,nvidia)
    - fixed floating hashrate reporting (appeared in v2.64)

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -11,15 +11,15 @@ $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "2.64"
+$Version = "2.65"
 $DeviceCapability = "5.0"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.64-gminer/gminer_2_64_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.65-gminer/gminer_2_65_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.64-gminer/gminer_2_64_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.65-gminer/gminer_2_65_windows64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
