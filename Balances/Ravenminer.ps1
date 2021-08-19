6d98383b2ac75232a6943ec9adaee863a11a6f32
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-08-19 15:37:27 +0200

    Update pool Ravenminer
    - apply new API
    - allow PPS selection (add "pps" to "RVN-Params" in pools.config.txt, section "Ravenminer")

--- Balances/Ravenminer.ps1
+++ Balances/Ravenminer.ps1
@@ -5,9 +5,8 @@ param(
 )
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
-$PoolConfig = $Config.Pools.$Name
 
-if (!$PoolConfig.RVN) {
+if (-not $Config.Pools.$Name.Wallets.RVN) {
     Write-Log -Level Verbose "Pool Balance API ($Name) has failed - no wallet address specified."
     return
 }
@@ -16,16 +15,15 @@ if ($Config.ExcludeCoinsymbolBalances.Count -and $Config.ExcludeCoinsymbolBalanc
 
 $Request = [PSCustomObject]@{}
 
-$Ravenminer_Host = "www.ravenminer.com"
-
-#https://www.ravenminer.com/api/wallet?address=RFtHhp8S43JDnzAJz9GDvups6pdJjBA7nM
+#https://new.ravenminer.com/api/v1/wallet/RFV5WxTdbQEQCdgESiMLRBj5rwXyFHokmC
+#old:https://www.ravenminer.com/api/wallet?address=RFV5WxTdbQEQCdgESiMLRBj5rwXyFHokmC
 $Success = $true
 try {
-    if (-not ($Request = Invoke-RestMethodAsync "https://$($Ravenminer_Host)/api/wallet?address=$($PoolConfig.RVN)" -cycletime ($Config.BalanceUpdateMinutes*60))){$Success = $false}
+    if (-not ($Request = Invoke-RestMethodAsync "https://new.ravenminer.com/api/v1/wallet/$($Config.Pools.$Name.Wallets.RVN)" -cycletime ($Config.BalanceUpdateMinutes*60))){$Success = $false}
 }
 catch {
     if ($Error.Count){$Error.RemoveAt(0)}
-    $Success=$false
+    $Success = $_.Exception.Message -match "404"
 }
 
 if (-not $Success) {
@@ -42,12 +40,11 @@ if (($Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Measur
         Caption     = "$($Name) (RVN)"
 		BaseName    = $Name
         Currency    = $Request.currency
-        Balance     = [Decimal]$Request.balance
-        Pending     = [Decimal]$Request.unsold
-        Total       = [Decimal]$Request.unpaid
+        Balance     = [Decimal]$Request.balance.cleared
+        Pending     = [Decimal]$Request.balance.pending
+        Total       = [Decimal]$Request.balance.cleared + [Decimal]$Request.balance.pending
         #Paid        = [Decimal]$Request.total - [Decimal]$Request.unpaid
-        Paid24h     = [Decimal]$Request.paid24h
-        Earned      = [Decimal]$Request.total
-        Payouts     = @()
+        Paid24h     = [Decimal]$Request.earnings."1d"
+        Payouts     = @(Get-BalancesPayouts $Request.payouts | Select-Object)
         LastUpdated = (Get-Date).ToUniversalTime()
 }
--- Pools/Ravenminer.ps1
+++ Pools/Ravenminer.ps1
@@ -16,9 +16,8 @@ param(
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 
 $Pool_Request = [PSCustomObject]@{}
-
 try {
-    $Pool_Request = Invoke-RestMethodAsync "https://www.ravenminer.com/api/status" -tag $Name -cycletime 120
+    $Pool_Request = Invoke-RestMethodAsync "https://new.ravenminer.com/api/v1/dashboard" -tag $Name -cycletime 120
 }
 catch {
     if ($Error.Count){$Error.RemoveAt(0)}
@@ -26,16 +25,16 @@ catch {
     return
 }
 
+$PoolBlocks_Request = [PSCustomObject]@{}
 try {
-    $PoolCoins_Request = Invoke-RestMethodAsync "https://www.ravenminer.com/api/currencies" -tag $Name -cycletime 120
+    $PoolBlocks_Request = Invoke-RestMethodAsync "https://new.ravenminer.com/api/v1/blocks" -tag $Name -cycletime 120
 }
 catch {
     if ($Error.Count){$Error.RemoveAt(0)}
-    Write-Log -Level Info "Pool Currency API ($Name) has failed. "
+    Write-Log -Level Warn "Pool blocks API ($Name) has failed. "
 }
 
 $Pool_Currency = "RVN"
-$Pool_Algorithm = if ($PoolCoins_Request.$Pool_Currency) {$PoolCoins_Request.$Pool_Currency.algo} else {"kawpow"}
 
 $Pool_Coin = Get-Coin $Pool_Currency
 $Pool_Host = "ravenminer.com"
@@ -47,66 +46,68 @@ $Pool_Ports = @(3838,13838)
 $Pool_Regions = @("us","eu","asia")
 $Pool_Regions | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
 
-$Pool_Request.PSObject.Properties.Name | Where-Object {$_ -eq $Pool_Algorithm} | Foreach-Object {
-    $Pool_PoolFee = [Double]$Pool_Request.$_.fees
-    $Pool_User = $Wallets.$Pool_Currency
+$Pool_Fee_PPLNS = 0.5
+$Pool_Fee_PPS   = 2.0 #add "pps" to "RVN-Params" in pools.config.txt
 
-    $Pool_Factor = $Pool_Request.$_.mbtc_mh_factor
+$Pool_User = $Wallets.$Pool_Currency
 
-    $Pool_TSL = if ($PoolCoins_Request) {$PoolCoins_Request.$Pool_Currency.timesincelast}else{$null}
-    $Pool_BLK = $PoolCoins_Request.$Pool_Currency."24h_blocks"
+$Pool_TSL = if ($PoolBlocks_Request.blocks) {(Get-UnixTimestamp) - ($PoolBlocks_Request.blocks | Sort-Object -Property time -Descending | Select-Object -First 1).time} else {$null}
+$Pool_BLK = ($Pool_Request.history | Where-Object day -eq 1).blocks
 
-    if (-not $InfoOnly) {
-        $NewStat = $false; if (-not (Test-Path "Stats\Pools\$($Name)_$($Pool_Algorithm_Norm)_Profit.txt")) {$NewStat = $true; $DataWindow = "actual_last24h"}
-        $Pool_Price = Get-YiiMPValue $Pool_Request.$_ -DataWindow $DataWindow -Factor $Pool_Factor
-        $Stat = Set-Stat -Name "$($Name)_$($Pool_Algorithm_Norm)_Profit" -Value $Pool_Price -Duration $(if ($NewStat) {New-TimeSpan -Days 1} else {$StatSpan}) -ChangeDetection $false -Actual24h $($Pool_Request.$_.actual_last24h/1000) -Estimate24h $($Pool_Request.$_.estimate_last24h) -HashRate $Pool_Request.$_.hashrate -BlockRate $Pool_BLK -Quiet
-        if (-not $Stat.HashRate_Live -and -not $AllowZero) {return}
-    }
+$Pool_WTM = $false
 
-    $Pool_Params = if ($Params.$Pool_Currency) {",$($Params.$Pool_Currency)"}
+if (-not $InfoOnly) {
+    $Pool_Price = if ($Pool_Request.hashrate -and $Pool_Request.poolTtfSec) {(86400/$Pool_Request.poolTtfSec)*$Pool_Request.coin.reward*$Pool_Request.coin.priceBTC/$Pool_Request.hashrate} else {0;$Pool_WTM = $true}
+    $Stat = Set-Stat -Name "$($Name)_$($Pool_Algorithm_Norm)_Profit" -Value $Pool_Price -Duration $StatSpan -ChangeDetection $false -HashRate $Pool_Request.hashrate -BlockRate $Pool_BLK -Quiet
+    if (-not $Stat.HashRate_Live -and -not $AllowZero) {return}
+}
 
-    if ($Pool_User -or $InfoOnly) {
-        foreach($Pool_Region in $Pool_Regions) {
-            $Pool_SSL = $false
-            foreach($Pool_Port in $Pool_Ports) {
-                [PSCustomObject]@{
-                    Algorithm     = $Pool_Algorithm_Norm
-                    Algorithm0    = $Pool_Algorithm_Norm
-                    CoinName      = $Pool_Coin.Name
-                    CoinSymbol    = $Pool_Currency
-                    Currency      = $Pool_Currency
-                    Price         = $Stat.$StatAverage #instead of .Live
-                    StablePrice   = $Stat.$StatAverageStable
-                    MarginOfError = $Stat.Week_Fluctuation
-                    Protocol      = "stratum+$(if ($Pool_SSL) {"ssl"} else {"tcp"})"
-                    Host          = "$($Pool_Region).$($Pool_Host)"
-                    Port          = $Pool_Port
-                    User          = $Pool_User
-                    Pass          = "{workername:$Worker},c=$Pool_Currency{diff:,d=`$difficulty}$Pool_Params"
-                    Region        = $Pool_Regions.$Pool_Region
-                    SSL           = $Pool_SSL
-                    Updated       = $Stat.Updated
-                    PoolFee       = $Pool_PoolFee
-                    DataWindow    = $DataWindow
-                    Workers       = $Pool_Request.$_.workers
-                    Hashrate      = $Stat.HashRate_Live
-                    BLK           = $Stat.BlockRate_Average
-                    TSL           = $Pool_TSL
-				    ErrorRatio    = $Stat.ErrorRatio
-                    EthMode       = "stratum"
-                    Name          = $Name
-                    Penalty       = 0
-                    PenaltyFactor = 1
-                    Disabled      = $false
-                    HasMinerExclusions = $false
-                    Price_Bias    = 0.0
-                    Price_Unbias  = 0.0
-                    Wallet        = $Wallets.$Pool_Currency
-                    Worker        = "{workername:$Worker}"
-                    Email         = $Email
-                }
-                $Pool_SSL = $true
+$Pool_Params = "$(if ($Params.$Pool_Currency) {",$($Params.$Pool_Currency)"})"
+
+$Pool_PoolFee = if ($Pool_Params -match ",pps(,|$)") {$Pool_Fee_PPS} else {$Pool_Fee_PPLNS}
+
+if ($Pool_User -or $InfoOnly) {
+    foreach($Pool_Region in $Pool_Regions) {
+        $Pool_SSL = $false
+        foreach($Pool_Port in $Pool_Ports) {
+            [PSCustomObject]@{
+                Algorithm     = $Pool_Algorithm_Norm
+                Algorithm0    = $Pool_Algorithm_Norm
+                CoinName      = $Pool_Coin.Name
+                CoinSymbol    = $Pool_Currency
+                Currency      = $Pool_Currency
+                Price         = $Stat.$StatAverage #instead of .Live
+                StablePrice   = $Stat.$StatAverageStable
+                MarginOfError = $Stat.Week_Fluctuation
+                Protocol      = "stratum+$(if ($Pool_SSL) {"ssl"} else {"tcp"})"
+                Host          = "$($Pool_Region).$($Pool_Host)"
+                Port          = $Pool_Port
+                User          = $Pool_User
+                Pass          = "{workername:$Worker}$Pool_Params"
+                Region        = $Pool_Regions.$Pool_Region
+                SSL           = $Pool_SSL
+                Updated       = $Stat.Updated
+                PoolFee       = $Pool_PoolFee
+                DataWindow    = $DataWindow
+                Workers       = $Pool_Request.workersNum
+                Hashrate      = $Stat.HashRate_Live
+                BLK           = $Stat.BlockRate_Average
+                TSL           = $Pool_TSL
+                WTM           = $Pool_WTM
+				ErrorRatio    = $Stat.ErrorRatio
+                EthMode       = "stratum"
+                Name          = $Name
+                Penalty       = 0
+                PenaltyFactor = 1
+                Disabled      = $false
+                HasMinerExclusions = $false
+                Price_Bias    = 0.0
+                Price_Unbias  = 0.0
+                Wallet        = $Wallets.$Pool_Currency
+                Worker        = "{workername:$Worker}"
+                Email         = $Email
             }
+            $Pool_SSL = $true
         }
     }
 }
