5f7549c32d5eca9036a97e3c5df9a631d2309b65
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-08-20 15:05:23 +0200

    Update miner SgminerFancyIX
    - enable INTEL GPU mining

--- Miners/SgminerFancyIX.ps1
+++ Miners/SgminerFancyIX.ps1
@@ -20,7 +20,7 @@ if ($IsLinux) {
     $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7.5-fancyix/sgminer-fancyIX-win64-0.7.5.zip"
 }
 
-if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $InfoOnly) {return} # No AMD present in system
+if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.INTEL -and -not $InfoOnly) {return} # No AMD/INTEL present in system
 
 $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "allium";         Params = "--gpu-threads 1 --worksize 256 -I 22 --difficulty-multiplier 0.5"; ExcludePoolName = "^Nicehash"}
@@ -48,7 +48,7 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 
 if ($InfoOnly) {
     [PSCustomObject]@{
-        Type      = @("AMD")
+        Type      = @("AMD","INTEL")
         Name      = $Name
         Path      = $Path
         Port      = $Miner_Port
@@ -60,53 +60,55 @@ if ($InfoOnly) {
     return
 }
 
-$Global:DeviceCache.DevicesByTypes.AMD | Select-Object Vendor, Model -Unique | ForEach-Object {
-    $First = $true
-    $Miner_Model = $_.Model
-    $Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)".Where({$_.Model -eq $Miner_Model})
+foreach ($Miner_Vendor in @("AMD","INTEL")) {
+    $Global:DeviceCache.DevicesByTypes.$Miner_Vendor | Where-Object Type -eq "GPU" | Select-Object Vendor, Model -Unique | ForEach-Object {
+        $First = $true
+        $Miner_Model = $_.Model
+        $Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)".Where({$_.Model -eq $Miner_Model})
 
-    $Commands.ForEach({
+        $Commands.ForEach({
 
-        $MainAlgorithm = $_.MainAlgorithm -replace "_navi"
+            $MainAlgorithm = $_.MainAlgorithm -replace "_navi"
 
-        $Algorithm_Norm_0 = Get-Algorithm $MainAlgorithm
+            $Algorithm_Norm_0 = Get-Algorithm $MainAlgorithm
 
-        $Miner_Device = $Device | Where-Object {($_.Model -notmatch "^RX[56]\d\d\d" -and $MainAlgorithm -eq $_.MainAlgorithm) -or ($_.Model -match "^RX[56]\d\d\d" -and $MainAlgorithm -ne $_.MainAlgorithm)}
+            $Miner_Device = $Device | Where-Object {($_.Model -notmatch "^RX[56]\d\d\d" -and $MainAlgorithm -eq $_.MainAlgorithm) -or ($_.Model -match "^RX[56]\d\d\d" -and $MainAlgorithm -ne $_.MainAlgorithm)}
 
-		foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)","$($Algorithm_Norm_0)-GPU")) {
-			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and (-not $_.ExcludePoolName -or $Pools.$Algorithm_Norm.Name -notmatch $_.ExcludePoolName)) {
-                if ($First) {
-                    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
-                    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-                    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
-                    $Miner_PlatformId = $Miner_Device | Select -Property Platformid -Unique -ExpandProperty PlatformId
-                    $First = $false
-                }
-				$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
-				[PSCustomObject]@{
-					Name           = $Miner_Name
-					DeviceName     = $Miner_Device.Name
-					DeviceModel    = $Miner_Model
-					Path           = $Path
-					Arguments      = "--device $($DeviceIDsAll) --api-port `$mport --api-listen -k $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) --text-only --gpu-platform $($Miner_PlatformId) $($_.Params)"
-					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Week}
-					API            = "Xgminer"
-					Port           = $Miner_Port
-					Uri            = $Uri
-					DevFee         = $DevFee
-					FaultTolerance = $_.FaultTolerance
-					ExtendInterval = $_.ExtendInterval
-                    Penalty        = 0
-					ManualUri      = $ManualUri
-					EnvVars        = @("GPU_FORCE_64BIT_PTR=0")
-                    Version        = $Version
-                    PowerDraw      = 0
-                    BaseName       = $Name
-                    BaseAlgorithm  = $Algorithm_Norm_0
-                    Benchmarked    = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Benchmarked
-                    LogFile        = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".LogFile
-				}
-			}
-		}
-    })
+		    foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)","$($Algorithm_Norm_0)-GPU")) {
+			    if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and (-not $_.ExcludePoolName -or $Pools.$Algorithm_Norm.Name -notmatch $_.ExcludePoolName)) {
+                    if ($First) {
+                        $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+                        $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+                        $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
+                        $Miner_PlatformId = $Miner_Device | Select -Property Platformid -Unique -ExpandProperty PlatformId
+                        $First = $false
+                    }
+				    $Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
+				    [PSCustomObject]@{
+					    Name           = $Miner_Name
+					    DeviceName     = $Miner_Device.Name
+					    DeviceModel    = $Miner_Model
+					    Path           = $Path
+					    Arguments      = "--device $($DeviceIDsAll) --api-port `$mport --api-listen -k $($_.MainAlgorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"}) --text-only --gpu-platform $($Miner_PlatformId) $($_.Params)"
+					    HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Week}
+					    API            = "Xgminer"
+					    Port           = $Miner_Port
+					    Uri            = $Uri
+					    DevFee         = $DevFee
+					    FaultTolerance = $_.FaultTolerance
+					    ExtendInterval = $_.ExtendInterval
+                        Penalty        = 0
+					    ManualUri      = $ManualUri
+					    EnvVars        = @("GPU_FORCE_64BIT_PTR=0")
+                        Version        = $Version
+                        PowerDraw      = 0
+                        BaseName       = $Name
+                        BaseAlgorithm  = $Algorithm_Norm_0
+                        Benchmarked    = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Benchmarked
+                        LogFile        = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".LogFile
+				    }
+			    }
+		    }
+        })
+    }
 }
