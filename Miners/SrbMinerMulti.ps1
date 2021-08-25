5e60aae14424b6e76a50d738f118752ca3472699
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-08-25 16:29:32 +0200

    Update miner SrbminerMulti to v0.8.0 (amd,cpu)
    - Added algorithm 'randomgrft' (GRFT - Graft coin) for CPU mining, fee 0.85%
    - Performance increase on 'ethash' algorithm for RX6800/6900 GPU's
    - Performance increase on 'etchash' algorithm for RX6800/6900 GPU's
    - Performance increase on 'ubqhash' algorithm for RX6800/6900 GPU's
    - Performance increase on 'verthash' algorithm for RX6800/6900 GPU's
    - Performance increase on 'heavyhash' algorithm for RX6800/6900 GPU's
    - Performance increase on 'autolykos2' algorithm on ZEN2+ AMD CPU's (up to ~90%) (*)
    - Fixed 'cryptonight_gpu' algorithm for RDNA/RDNA2 GPU's
    - Fixed 'cryptonight_xhv' algorithm for RDNA/RDNA2 GPU's
    - Added GPU power consumption details to stats & api
    - Decreased time needed for algorithm switch when using --multi-algorithm-job-mode 3 with cpu mining
    - Minor bug fixes

--- Miners/SrbMinerMulti.ps1
+++ Miners/SrbMinerMulti.ps1
@@ -10,14 +10,14 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://bitcointalk.org/index.php?topic=5190081.0"
 $Port = "349{0:d2}"
 $DevFee = 0.85
-$Version = "0.7.9"
+$Version = "0.8.0"
 
 if ($IsLinux) {
     $Path = ".\Bin\ANY-SRBMinerMulti\SRBMiner-MULTI"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7.9-srbminermulti/SRBMiner-Multi-0-7-9-Linux.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.0-srbminermulti/SRBMiner-Multi-0-8-0-Linux.tar.xz"
 } else {
     $Path = ".\Bin\ANY-SRBMinerMulti\SRBMiner-MULTI.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.7.9-srbminermulti/SRBMiner-Multi-0-7-9-win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.0-srbminermulti/SRBMiner-Multi-0-8-0-win64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No AMD nor CPU present in system
@@ -33,6 +33,7 @@ $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "panthera"         ;              Params = ""; Fee = 0.85;               Vendor = @("CPU")} #Panthera
     [PSCustomObject]@{MainAlgorithm = "randomarq"        ;              Params = "--randomx-use-1gb-pages"; Fee = 0.85; Vendor = @("CPU")} #RandomArq
     [PSCustomObject]@{MainAlgorithm = "randomepic"       ;              Params = "--randomx-use-1gb-pages"; Fee = 0.85; Vendor = @("CPU")} #RandomEPIC
+    [PSCustomObject]@{MainAlgorithm = "randomgrft"       ;              Params = "--randomx-use-1gb-pages"; Fee = 0.85; Vendor = @("CPU")} #RandomGRFT
     [PSCustomObject]@{MainAlgorithm = "randomhash2"      ;              Params = ""; Fee = 0.85;               Vendor = @("CPU")} #RandomHash2/PASC
     [PSCustomObject]@{MainAlgorithm = "randomkeva"       ;              Params = "--randomx-use-1gb-pages"; Fee = 0.85; Vendor = @("CPU")} #RandomKEVA
     [PSCustomObject]@{MainAlgorithm = "randomsfx"        ;              Params = "--randomx-use-1gb-pages"; Fee = 0.85; Vendor = @("CPU")} #RandomSFX
