4acb8f71e239fb2467b8568936bcf043bb2967b4
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-09-06 10:08:53 +0200

    Update miner Teamred to v0.8.5 (amd)
    - General: added Windows clocks/voltage/built-in timings control (beta functionality). See section in --help and CLOCKS_VOLTAGE_CONTROL.txt.
    - General: fixed some issues with Windows fan control, mostly for Big Navi gpus.
    - General: added mem temp limits (stop/resume), see --mem_temp_limit and --mem_temp_resume.
    - Ethash: added support for forcing ethash pool hashrate reports (see --eth_hash_report).
    - Ethash: fixed hashrate reports for Crazypool when using failover pools.
    - Autolykos2: added extranonce subscription support for e.g. Nicehash.

--- Miners/Teamred.ps1
+++ Miners/Teamred.ps1
@@ -10,15 +10,15 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $Port = "409{0:d2}"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5059817.0"
 $DevFee = 3.0
-$Version = "0.8.4"
+$Version = "0.8.5"
 
 if ($IsLinux) {
     $Path = ".\Bin\AMD-Teamred\teamredminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.4-teamred/teamredminer-v0.8.4-linux.tgz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.5-teamred/teamredminer-v0.8.5-linux.tgz"
     $DatFile = "$env:HOME/.vertcoin/verthash.dat"
 } else {
     $Path = ".\Bin\AMD-Teamred\teamredminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.4-teamred/teamredminer-v0.8.4-win.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.5-teamred/teamredminer-v0.8.5-win.zip"
     $DatFile = "$env:APPDATA\Vertcoin\verthash.dat"
 }
 
