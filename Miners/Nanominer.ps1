9307f1d730b985dc6dfbd83e66641114d6944e3f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-09-18 11:25:09 +0200

    Update miner Nanominer to v3.3.11 (amd,cpu,nvidia)
    Fixed invalid ETH shares issue on some Nvidia RTX30xx.

--- Miners/Nanominer.ps1
+++ Miners/Nanominer.ps1
@@ -11,17 +11,17 @@ $ManualURI = "https://github.com/nanopool/nanominer/releases"
 $Port = "534{0:d2}"
 $Cuda = "10.0"
 $DevFee = 3.0
-$Version = "3.3.10"
+$Version = "3.3.11"
 
 if ($IsLinux) {
     $Path = ".\Bin\ANY-Nanominer\nanominer"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.3.10-nanominer/nanominer-linux-3.3.10-cuda11.tar.gz"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.3.11-nanominer/nanominer-linux-3.3.11-cuda11.tar.gz"
             Cuda = "11.1"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.3.10-nanominer/nanominer-linux-3.3.10.tar.gz"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.3.11-nanominer/nanominer-linux-3.3.11.tar.gz"
             Cuda = "10.0"
         }
     )
@@ -29,11 +29,11 @@ if ($IsLinux) {
     $Path = ".\Bin\ANY-Nanominer\nanominer.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.3.10-nanominer/nanominer-windows-3.3.10-cuda11.zip"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.3.11-nanominer/nanominer-windows-3.3.11-cuda11.zip"
             Cuda = "11.1"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.3.10-nanominer/nanominer-windows-3.3.10.zip"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.3.11-nanominer/nanominer-windows-3.3.11.zip"
             Cuda = "10.0"
         }
     )
