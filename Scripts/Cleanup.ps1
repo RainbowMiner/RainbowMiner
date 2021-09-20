32c723e74f528c73b91de0299c81548721425988
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-09-20 15:51:55 +0200

    Update Cleanup.ps1

--- Scripts/Cleanup.ps1
+++ Scripts/Cleanup.ps1
@@ -1281,6 +1281,10 @@ try {
         }
     }
 
+    if ($Version -le (Get-Version "4.7.4.6")) {
+        $AddAlgorithm += @("FiroPoW")
+    }
+
     # remove mrrpools.json from cache
     Get-ChildItem "Cache\9FB0DC7AA798CEB4B4B7CB39F6E0CD9C.asy" -ErrorAction Ignore | Foreach-Object {$ChangesTotal++;Remove-Item $_.FullName -Force -ErrorAction Ignore}
 
