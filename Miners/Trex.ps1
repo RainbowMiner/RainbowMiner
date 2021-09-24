b43fa8229ab8f8914dda531abd1549247a95f10f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-09-24 14:00:03 +0200

    Update miner Trex to v0.23.1 (nvidia)
    NOTE: this version includes a couple of breaking changes: GPU ordering change and telnet API removal. Make sure you remove your GPU-order specific OC settings from *.bat/*.sh files before updating.
    Auto-updates won't be turned on for this version for the same reason.
    Users of HiveOS and other mining operating systems that force PCI bus GPU order are unaffected by the GPU-order change.
    
    - New WebUI and API (refer to https://github.com/trexminer/T-Rex/wiki/WebUI on how to set it up):
      1. Ability to generate config files
      2. Improved API security: optional password and certificate based authentication, https
      3. GPU/memory clock and share difficulty reporting
      4. Log viewer
      5. Ability to pause GPUs
    
    If you don't like the new WebUI and would prefer to use the old one for any reason, download webui.zip archive, put it next to t-rex executable, and restart the miner.
    
    - Order GPUs by PCI bus id. You may need to change the settings that relied on GPU order.
    - Remove telnet API
    - Multiple bug fixes

--- Miners/Trex.ps1
+++ Miners/Trex.ps1
@@ -10,14 +10,14 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://bitcointalk.org/index.php?topic=4432704.0"
 $Port = "316{0:d2}"
 $DevFee = 1.0
-$Version = "0.22.1"
+$Version = "0.23.1"
 $DeviceCapability = "5.0"
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Trex\t-rex"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.22.1-trex/t-rex-0.22.1-linux.tar.gz"
+            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.23.1-trex/t-rex-0.23.1-linux.tar.gz"
             Cuda   = "9.2"
         }
     )
@@ -25,7 +25,7 @@ if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Trex\t-rex.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.22.1-trex/t-rex-0.22.1-win.zip"
+            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.23.1-trex/t-rex-0.23.1-win.zip"
             Cuda   = "9.2"
         }
     )
@@ -120,7 +120,7 @@ $Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique 
 					DeviceName     = $Miner_Device.Name
 					DeviceModel    = $Miner_Model
 					Path           = $Path
-					Arguments      = "-N 10 -r 5 --api-bind-http 127.0.0.1:`$mport -d $($DeviceIDsAll) -a $($Algorithm) -o $($Pool_Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Wallet -and $Pools.$Algorithm_Norm.Worker) {" -w $($Pools.$Algorithm_Norm.Worker)"})$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"})$($Pools.$Algorithm_Norm.Failover | Select-Object | Foreach-Object {" -o $($_.Protocol)://$($_.Host):$($_.Port) -u $($_.User)$(if ($_.Pass) {" -p $($_.Pass)"})"})$(if ($Pools.$Algorithm_Norm.SSL) {" --no-strict-ssl"})$(if (-not $Session.Config.ShowMinerWindow){" --no-color"}) --no-watchdog -b 0 $($_.Params)"
+					Arguments      = "-N 10 -r 5 --api-bind-http 127.0.0.1:`$mport -d $($DeviceIDsAll) -a $($Algorithm) -o $($Pool_Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pool_Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Wallet -and $Pools.$Algorithm_Norm.Worker) {" -w $($Pools.$Algorithm_Norm.Worker)"})$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"})$($Pools.$Algorithm_Norm.Failover | Select-Object | Foreach-Object {" -o $($_.Protocol)://$($_.Host):$($_.Port) -u $($_.User)$(if ($_.Pass) {" -p $($_.Pass)"})"})$(if ($Pools.$Algorithm_Norm.SSL) {" --no-strict-ssl"})$(if (-not $Session.Config.ShowMinerWindow){" --no-color"}) --no-watchdog $($_.Params)"
 					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate"."$(if ($_.HashrateDuration){$_.HashrateDuration}else{"Week"})"}
 					API            = "Trex"
 					Port           = $Miner_Port
