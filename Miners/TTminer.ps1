f0d422e31c4aa7cce3bc9c2979ff37d03c778be3
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-09-29 12:23:19 +0200

    Update miner TTminer to v6.2.0 (nvidia)
    - CUDA 11.2
    - fix multiple TTminer instance crash

--- Miners/TTminer.ps1
+++ Miners/TTminer.ps1
@@ -11,21 +11,22 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5025783.0"
 $Port = "333{0:d2}"
 $DevFee = 1.0
 $Cuda = "9.2"
-$Version = "6.0.1"
+$Version = "6.2.0"
 
 if ($IsLinux) {
-    $Path = ".\Bin\NVIDIA-TTminer\TT-Miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v6.0.1-ttminer/TT-Miner-6.0.1.tar.xz"
+    $Path = ".\Bin\NVIDIA-TTminer-%MODEL%\TT-Miner"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v6.2.0-ttminer/TT-Miner-6.2.0-linux.tar.xz"
 
 } else {
-    $Path = ".\Bin\NVIDIA-TTminer\TT-Miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v6.0.1-ttminer/TT-Miner-6.0.1.7z"
+    $Path = ".\Bin\NVIDIA-TTminer-%MODEL%\TT-Miner.exe"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v6.2.0-ttminer/TT-Miner-6.2.0-win.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
 
 $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "Ethash"        ; DAG = $true; MinMemGB = 3;   Params = "-A ETHASH%CUDA%";                   ExtendInterval = 2; ExcludePoolName = "^MiningRigRentals"} #Ethash 
+    [PSCustomObject]@{MainAlgorithm = "Etchash"       ; DAG = $true; MinMemGB = 3;   Params = "-A ETHASH%CUDA% -coin ETC";         ExtendInterval = 2; ExcludePoolName = "^MiningRigRentals"} #Etchash 
     [PSCustomObject]@{MainAlgorithm = "EthashLowMemory" ; DAG = $true; MinMemGB = 2;   Params = "-A ETHASH%CUDA%";                 ExtendInterval = 2; ExcludePoolName = "^MiningRigRentals"} #Ethash for low memory coins
     #[PSCustomObject]@{MainAlgorithm = "KawPow"        ; DAG = $true; MinMemGB = 3;   Params = "-A PROGPOW%CUDA%";                  ExtendInterval = 2; ExcludePoolName = "^MiningPoolHub"} #KAWPOW (RVN,ZELS)
     [PSCustomObject]@{MainAlgorithm = "MTP"           ;              MinMemGB = 5;   Params = "-A MTP%CUDA%";                      ExtendInterval = 2} #MTP
@@ -62,6 +63,8 @@ $Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique 
     $Miner_Model = $_.Model
     $Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)".Where({$_.Model -eq $Miner_Model})
 
+    $Miner_Path = $Path -replace "%MODEL%",$Miner_Model
+
     $Commands.ForEach({
         $First = $True
         $Algorithm_Norm_0 = Get-Algorithm $_.MainAlgorithm
@@ -74,7 +77,7 @@ $Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique 
         $IsMTP = $_.MainAlgorithm -match "^MTP"
 
         $Cuda = "$(if ($IsLinux -or $IsMTP) {
-            "-$(if (-not $IsMTP -and (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion "11.1")) {"111"} elseif (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion "10.2") {"102"} else {"92"})"
+            "-$(if (-not $IsMTP -and (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion "11.2")) {"112"} elseif (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion "10.2") {"102"} else {"92"})"
         })"
         
 		foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)","$($Algorithm_Norm_0)-GPU")) {
@@ -97,7 +100,7 @@ $Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique 
 					Name           = $Miner_Name
 					DeviceName     = $Miner_Device.Name
 					DeviceModel    = $Miner_Model
-					Path           = $Path
+					Path           = $Miner_Path
 					Arguments      = "--api-bind 127.0.0.1:`$mport -d $($DeviceIDsAll) -P $($Miner_Protocol)$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pass) {" -p $Pass"}) -w $($Pools.$Algorithm_Norm.Worker) -PRHRI 1 -nui $($_.Params -replace '%CUDA%',$Cuda)$(if ($_.Params -notmatch "-coin" -and $Pools.$Algorithm_Norm.CoinSymbol -and $CoinSymbols -icontains $Pools.$Algorithm_Norm.CoinSymbol) {" -coin $($Pools.$Algorithm_Norm.CoinSymbol)"}) -work-timeout 500000"
 					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $($Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Week)}
 					API            = "Claymore"
