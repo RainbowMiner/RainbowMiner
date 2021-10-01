84cddccdf64756e86affa8caeaffe80b70cca03c
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-10-01 23:05:32 +0200

    Update miner Lolminer to v1.33 (amd,nvidia)
    #### Core Feature: Complete rework of LHR semi-unlock feature.
    
    - Better performance of LHR semi-unlock, about 70%+ on GDDR6x cards (3070 Ti & 3080 (Ti) ), about 71-72%+ on GDDR6 (3060 (Ti) & 3070), 81%+ on 3060 LHR V1 with the right drivers (earlier then 460.39). Recommended driver for LHR v2: 470.74 (Linux), 472.12 (Windows)
    **Important hint:** We saw some calibration bugs with 460.93 driver (Linux). Please do not use it!
    - Less performance difference between low and high core clock then earlier versions
    - Cards are automatically detected if they are LHR **- no more --mode switch required**
    - In the beginning the miner will calibrate to your exact core & memory clocks. The total process takes 3-4 minutes total, one with rather low speed and the remaining time with speed closer to the final value. Try not to change any overclock after or during calibration, else the performance might be lower then expected!
    - **--lhrtune** has now default value of auto for an automatic tuning. Results of automatic tuning will be displayed in stats after calibration is completed. Note: re-tuning of values used in 1.32a is required!
    
    #### Other feature changes
    - Default --shortstats interval lowered to 15 seconds, default --longstats interval lowered to 60 seconds
    - More stable displayed hashrate on Nvidia cards when mining Eth
    - Changes in Api: On supported algorithms the miner now exposes the number of stale shares in API (stales and defect shares are no longer collapsed into one value)
    
    #### Fixes:
    - Fixed a bug with processing old style --dualmode etc dualmine settings
    - Fixed some minor bugs of the API
    - Fixed a bug causing a non-realistic high hash rate to be displayed on LHR cards when overclocked after start

--- Miners/lolminer.ps1
+++ Miners/lolminer.ps1
@@ -11,14 +11,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=4724735.0"
 $Port = "317{0:d2}"
 $Cuda = "10.0"
 $DevFee = 1.0
-$Version = "1.32a"
+$Version = "1.33"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-lolMiner\lolMiner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.32a-lolminer/lolMiner_v1.32a_Lin64.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.33-lolminer/lolMiner_v1.33_Lin64.tar.gz"
 } else {
     $Path = ".\Bin\GPU-lolMiner\lolMiner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.32a-lolminer/lolMiner_v1.32a_Win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.33-lolminer/lolMiner_v1.33_Win64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
