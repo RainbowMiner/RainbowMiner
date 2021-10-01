d8703edf4b09c9b8419481d58041a3aa2ee49b7f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-10-01 23:05:02 +0200

    Update miner Gminer to v2.70 (amd,nvidia)
    - improved auto-tune for LHR GPUs, now miner speedup performance when GPU is steady
    - removed --lhr_tune1 / --lhr_tune2 parameters, use --lhr_tune to tune LHR GPUs (value range is -10 - 10, old parameters ignored for compatibility)
    - display current --lhr_tune value and current kernel in statistics table

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -11,15 +11,15 @@ $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "2.69"
+$Version = "2.70"
 $DeviceCapability = "5.0"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.69-gminer/gminer_2_69_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.70-gminer/gminer_2_70_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.69-gminer/gminer_2_69_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.70-gminer/gminer_2_70_windows64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
