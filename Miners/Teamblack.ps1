3a85bb25804e280b075ddd929600c096f391ac33
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-10-22 14:38:08 +0200

    Update miner Teamblack to v1.18 (amd,intel,nvidia)
    - Added poolside hashrate calculated from the time, accepted shares, difficulty.
    - Added xintensity and kernel in the stats.
    - Added shares per minute to stats.
    - Remove crash in dag generation on high clocks.
    - Fix submit hashrate at ethermine.
    - Fix average hashrate/W calculation in stats.
    - Added kucoin pool.

--- Miners/Teamblack.ps1
+++ Miners/Teamblack.ps1
@@ -15,9 +15,9 @@ if ($IsLinux) {
 
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.16-teamblack/TeamBlackMiner_1_16_Ubuntu_18_04_Cuda_11_4.zip"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.18-teamblack/TeamBlackMiner_1_18_Ubuntu_18_04_Cuda_11_4.zip"
             Cuda = "11.4"
-            Version = "1.16"
+            Version = "1.18"
         }
     )
 } else {
@@ -25,21 +25,21 @@ if ($IsLinux) {
 
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.17-teamblack/TeamBlackMiner_1_17_cuda_11_4.7z"
-            Cuda = "11.4"
-            Version = "1.17"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.18-teamblack/TeamBlackMiner_1_18_cuda_11_5.7z"
+            Cuda = "11.5"
+            Version = "1.18"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.17-teamblack/TeamBlackMiner_1_17_cuda_11_2.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.18-teamblack/TeamBlackMiner_1_18_cuda_11_2.7z"
             Cuda = "11.2"
-            Version = "1.17"
+            Version = "1.18"
         }
     )
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.INTEL -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
 
-$ExcludePools = "^666Pool|^FlexPool|^KuCoin|^MiningDutch|^MiningRigRentals|^MoneroOcean|^Nicehash|^PoolSexy|^SuprNova|^Zpool"
+$ExcludePools = "^666Pool|^FlexPool|^MiningDutch|^MiningRigRentals|^MoneroOcean|^Nicehash|^PoolSexy|^SuprNova|^Zpool"
 
 $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "ethash";     DAG = $true; Params = ""; MinMemGb = 3;  Vendor = @("AMD","NVIDIA"); ExtendInterval = 3; DevFee = 0.5; ExcludePoolName = $ExcludePools} #Ethash
