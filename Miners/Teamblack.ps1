7c77be822a1ac06906c3cab1f0f386d710e71007
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-11-07 12:36:12 +0100

    Update miner Teamblack to v1.23 (amd,intel,nvidia)
    - Tiny speedup on Nvidia cards. Faster on lower intensity than before.
    - More accurate kernel autotune. Now running twice as long as before.
    - Reduced stales on ethermine.org, flexpool.org, crazypool and nicehash on amd (default --xintensity 24)
    - Console stats are rewritten to work with all terminals.
    - Fixed zil mining at rustpool.
    - Fix index problem in set memclock / coreclock / powerlimit (nvidia)
    - Added --tweak parameter for nvidia 0-7
    - Added more pools.

--- Miners/Teamblack.ps1
+++ Miners/Teamblack.ps1
@@ -15,8 +15,8 @@ if ($IsLinux) {
 
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.22-teamblack/TeamBlackMiner_1_22_Ubuntu_18_04_Cuda_11_4.tar.gz"
-            Cuda = "11.4"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.23-teamblack/TeamBlackMiner_1_23_Ubuntu_18_04_Cuda_11_5.tar.gz"
+            Cuda = "11.5"
             Version = "1.22"
         }
     )
@@ -25,19 +25,19 @@ if ($IsLinux) {
 
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.22-teamblack/TeamBlackMiner_1_22_cuda_11_5.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.23-teamblack/TeamBlackMiner_1_23_cuda_11_5.7z"
             Cuda = "11.5"
-            Version = "1.22"
+            Version = "1.23"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.22-teamblack/TeamBlackMiner_1_22_cuda_11_4.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.23-teamblack/TeamBlackMiner_1_23_cuda_11_4.7z"
             Cuda = "11.4"
-            Version = "1.22"
+            Version = "1.23"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.22-teamblack/TeamBlackMiner_1_22_cuda_11_2.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.23-teamblack/TeamBlackMiner_1_23_cuda_11_2.7z"
             Cuda = "11.2"
-            Version = "1.22"
+            Version = "1.23"
         }
     )
 }
