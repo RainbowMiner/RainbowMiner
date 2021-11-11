d95ba885089c0d02a2e2ea37d5a69043532bb266
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-11-11 13:43:29 +0100

    Update miner Teamblack to v1.24 (amd,intel,nvidia)
    - Fixed issue with multiple set core, mem, power.
    - Fixed issue with dag cache when switching back from ZIL pow.
    - Always verify dag on regeneration. Not only on the first run.
    - Log date time format is now DD:HH:MM.
    - API bumped to v1.3: Added port, versioning in url and summary.

--- Miners/Teamblack.ps1
+++ Miners/Teamblack.ps1
@@ -15,9 +15,9 @@ if ($IsLinux) {
 
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.23-teamblack/TeamBlackMiner_1_23_Ubuntu_18_04_Cuda_11_5.tar.gz"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.24-teamblack/TeamBlackMiner_1_24_Ubuntu_18_04_Cuda_11_4.tar.gz"
             Cuda = "11.5"
-            Version = "1.22"
+            Version = "1.24"
         }
     )
 } else {
@@ -25,19 +25,19 @@ if ($IsLinux) {
 
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.23-teamblack/TeamBlackMiner_1_23_cuda_11_5.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.24-teamblack/TeamBlackMiner_1_24_cuda_11_5.7z"
             Cuda = "11.5"
-            Version = "1.23"
+            Version = "1.24"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.23-teamblack/TeamBlackMiner_1_23_cuda_11_4.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.24-teamblack/TeamBlackMiner_1_24_cuda_11_4.7z"
             Cuda = "11.4"
-            Version = "1.23"
+            Version = "1.24"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.23-teamblack/TeamBlackMiner_1_23_cuda_11_2.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.24-teamblack/TeamBlackMiner_1_24_cuda_11_2.7z"
             Cuda = "11.2"
-            Version = "1.23"
+            Version = "1.24"
         }
     )
 }
