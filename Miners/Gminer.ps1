448fb1b8165f0f1cada9610c3b21a6b5539d9cc0
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-11-17 16:40:08 +0100

    Update miner Gminer to v2.72 (amd,nvidia)
    - fixed memory leaks on AMD GPUs
    - fixed compatibility with latest linux distributions
    - fixed crashes appeared in v2.71

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -11,15 +11,15 @@ $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "2.71"
+$Version = "2.72"
 $DeviceCapability = "5.0"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.71-gminer/gminer_2_71_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.72-gminer/gminer_2_72_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.71-gminer/gminer_2_71_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.72-gminer/gminer_2_72_windows64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
