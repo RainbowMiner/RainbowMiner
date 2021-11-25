0f50c6bc9cea46c37c07b9bc3c1ff105d7306601
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-11-25 14:34:37 +0100

    Update miner SrbminerMulti to v0.8.4 (amd,cpu)
    - Performance increase on 'heavyhash' algorithm for GPU's
    - Fixed display of hashrate while doing gpu auto tune process
    - Changed GPU temperature to show 'edge' value instead of 'hotspot'

--- Miners/SrbMinerMulti.ps1
+++ Miners/SrbMinerMulti.ps1
@@ -10,14 +10,14 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://bitcointalk.org/index.php?topic=5190081.0"
 $Port = "349{0:d2}"
 $DevFee = 0.85
-$Version = "0.8.3"
+$Version = "0.8.4"
 
 if ($IsLinux) {
     $Path = ".\Bin\ANY-SRBMinerMulti\SRBMiner-MULTI"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.3-srbminermulti/SRBMiner-Multi-0-8-3-Linux.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.4-srbminermulti/SRBMiner-Multi-0-8-4-Linux.tar.xz"
 } else {
     $Path = ".\Bin\ANY-SRBMinerMulti\SRBMiner-MULTI.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.3-srbminermulti/SRBMiner-Multi-0-8-3-win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.4-srbminermulti/SRBMiner-Multi-0-8-4-win64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.CPU -and -not $InfoOnly) {return} # No AMD nor CPU present in system
