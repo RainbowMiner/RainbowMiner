a47dc1b02c6ad89871c89ce17800982b59c57590
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-11-27 20:06:58 +0100

    Update miner Wildrig to v0.30.8.1 beta (amd,nvidia)
    heavyhash nightmare xD Because of a compiler bug of VC++ releasing this fix
    - fixed rejects on Vega64 for heavyhash

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -11,14 +11,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 1.0
 $Cuda = "8.0"
-$Version = "0.30.8"
+$Version = "0.30.8.1"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-WildRig\wildrig-multi"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.30.8-wildrigmulti/wildrig-multi-linux-0.30.8.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.30.8.1-wildrigmulti/wildrig-multi-linux-0.30.8.1.tar.xz"
 } else {
     $Path = ".\Bin\GPU-WildRig\wildrig.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.30.8-wildrigmulti/wildrig-multi-windows-0.30.8.7z"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.30.8.1-wildrigmulti/wildrig-multi-windows-0.30.8.1.7z"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
