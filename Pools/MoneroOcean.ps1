a2fe91a05ad395ebf1bd7f670658a07390af38ad
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-12-06 12:35:19 +0100

    Update pool MoneroOceaon
    - fix mining to multiple coins with one algorithm

--- Pools/MoneroOcean.ps1
+++ Pools/MoneroOcean.ps1
@@ -35,57 +35,64 @@ $Pool_CPUPort_Base = if ($Pool_CPUPort_Test -gt 0) {[Math]::Min([Math]::Pow(2,1+
 $Pool_GPUPost_Test = $Global:DeviceCache.Devices.Where({$_.type -eq "gpu"}).Count
 $Pool_GPUPort_Base = if ($Pool_GPUPost_Test -gt 1) {8192} else {1024}
 
-$Pool_Request | Where-Object {($_.profit -gt 0.00 -and ($AllowZero -or $_.hashrate -gt 0)) -or $InfoOnly} | ForEach-Object {
-    $Pool_Port = $_.port
-    $Pool_CoinSymbol = $_.coin
-    $Pool_Coin = Get-Coin $Pool_CoinSymbol
-    $Pool_Algorithm_Norm = if ($Pool_Coin) {$Pool_Coin.Algo} else {Get-Algorithm $_.algo}
+$Pool_Request | Group-Object -Property algo | ForEach-Object {
+    $IsFirst = $true
+    $_.Group | Sort-Object -Property profit -Descending | Foreach-Object {
+        $Pool_Port = $_.port
+        $Pool_CoinSymbol = $_.coin
+        $Pool_Coin = Get-Coin $Pool_CoinSymbol
+        $Pool_Algorithm_Norm = if ($Pool_Coin) {$Pool_Coin.Algo} else {Get-Algorithm $_.algo}
 
-    if (-not $InfoOnly) {
-        $Pool_TSL = if ($_.tsl -ge 0) {$_.tsl} else {$null}
-        $Stat = Set-Stat -Name "$($Name)_$($Pool_CoinSymbol)_Profit" -Value $_.profit -Duration $StatSpan -ChangeDetection $false -Difficulty $_.diff -HashRate $_.hashrate -BlockRate $(if ($_.ttf -gt 0) {86400/$_.ttf} else {0}) -Quiet
-    }
+        if (-not $InfoOnly) {
+            $Pool_TSL = if ($_.tsl -ge 0) {$_.tsl} else {$null}
+            if ($_.profit -eq 0.0) {return}
+            $Stat = Set-Stat -Name "$($Name)_$($Pool_CoinSymbol)_Profit" -Value $_.profit -Duration $StatSpan -ChangeDetection $false -Difficulty $_.diff -HashRate $_.hashrate -BlockRate $(if ($_.ttf -gt 0) {86400/$_.ttf} else {0}) -Quiet
+            if (-not $IsFirst -or ($_.hashrate -eq 0 -and -not $AllowZero)) {return}
+        }
 
-    $CPUPort_Base = if ($Pool_Algorithm_Norm -match "^Cucka") {1} else {$Pool_CPUPort_Base}
-    $GPUPort_Base = if ($Pool_Algorithm_Norm -match "^Cucka") {1} else {$Pool_GPUPort_Base}
+        $IsFirst = $false
 
-    foreach($Pool_Protocol in @("stratum+tcp","stratum+ssl")) {
-        $Port = if ($Pool_Protocol -match "ssl") {20000} else {10000}
-        [PSCustomObject]@{
-            Algorithm     = $Pool_Algorithm_Norm
-			Algorithm0    = $Pool_Algorithm_Norm
-            CoinName      = if ($Pool_Coin) {$Pool_Coin.Name} else {$Pool_CoinSymbol}
-            CoinSymbol    = $Pool_CoinSymbol
-            Currency      = "XMR"
-            Price         = $Stat.$StatAverage
-            StablePrice   = $Stat.$StatAverageStable
-            MarginOfError = $Stat.Week_Fluctuation
-            Protocol      = $Pool_Protocol
-            Host          = "gulf.moneroocean.stream"
-            Port          = $Port+$Pool_CPUPort_Base
-            Ports         = [PSCustomObject]@{CPU=$Port+$CPUPort_Base; GPU=$Port+$GPUPort_Base; RIG=$Port+8192}
-            User          = "$($Wallets.XMR)"
-            Pass          = "{workername:$Worker}:$($Password)~$($_.algo)"
-            Region        = Get-Region "US"
-            SSL           = $Pool_Protocol -match "ssl"
-            Updated       = $Stat.Updated
-            #WTM           = $true
-            PoolFee       = $Pool_PoolFee + $_.fee
-            Workers       = $_.worker
-            Hashrate      = $Stat.HashRate_Live
-            TSL           = $Pool_TSL
-            BLK           = $Stat.BlockRate_Average
-            EthMode       = if ($Pool_Algorithm_Norm -match $Global:RegexAlgoIsEthash) {"ethstratumnh"} else {$null}
-            Name          = $Name
-            Penalty       = 0
-            PenaltyFactor = 1
-            Disabled      = $false
-            HasMinerExclusions = $false
-            Price_Bias    = 0.0
-            Price_Unbias  = 0.0
-            Wallet        = $Wallets.XMR
-            Worker        = "{workername:$Worker}"
-            Email         = $Email
+        $CPUPort_Base = if ($Pool_Algorithm_Norm -match "^Cucka") {1} else {$Pool_CPUPort_Base}
+        $GPUPort_Base = if ($Pool_Algorithm_Norm -match "^Cucka") {1} else {$Pool_GPUPort_Base}
+
+        foreach($Pool_Protocol in @("stratum+tcp","stratum+ssl")) {
+            $Port = if ($Pool_Protocol -match "ssl") {20000} else {10000}
+            [PSCustomObject]@{
+                Algorithm     = $Pool_Algorithm_Norm
+			    Algorithm0    = $Pool_Algorithm_Norm
+                CoinName      = if ($Pool_Coin) {$Pool_Coin.Name} else {$Pool_CoinSymbol}
+                CoinSymbol    = $Pool_CoinSymbol
+                Currency      = "XMR"
+                Price         = $Stat.$StatAverage
+                StablePrice   = $Stat.$StatAverageStable
+                MarginOfError = $Stat.Week_Fluctuation
+                Protocol      = $Pool_Protocol
+                Host          = "gulf.moneroocean.stream"
+                Port          = $Port+$Pool_CPUPort_Base
+                Ports         = [PSCustomObject]@{CPU=$Port+$CPUPort_Base; GPU=$Port+$GPUPort_Base; RIG=$Port+8192}
+                User          = "$($Wallets.XMR)"
+                Pass          = "{workername:$Worker}:$($Password)~$($_.algo)"
+                Region        = Get-Region "US"
+                SSL           = $Pool_Protocol -match "ssl"
+                Updated       = $Stat.Updated
+                #WTM           = $true
+                PoolFee       = $Pool_PoolFee + $_.fee
+                Workers       = $_.worker
+                Hashrate      = $Stat.HashRate_Live
+                TSL           = $Pool_TSL
+                BLK           = $Stat.BlockRate_Average
+                EthMode       = if ($Pool_Algorithm_Norm -match $Global:RegexAlgoIsEthash) {"ethstratumnh"} else {$null}
+                Name          = $Name
+                Penalty       = 0
+                PenaltyFactor = 1
+                Disabled      = $false
+                HasMinerExclusions = $false
+                Price_Bias    = 0.0
+                Price_Unbias  = 0.0
+                Wallet        = $Wallets.XMR
+                Worker        = "{workername:$Worker}"
+                Email         = $Email
+            }
         }
     }
 }
