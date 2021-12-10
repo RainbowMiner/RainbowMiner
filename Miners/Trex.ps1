fd33de377df0bc4412e77d02cf5bbc33049db803
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-12-10 10:17:54 +0100

    Update miner Trex to v0.24.8 (nvidia)
    - Add DNS-over-HTTPS support when resolving mining pool domain names (see --dns-https-server for details)
    - Add an option not to set mining pool domain name in SNI header for SSL connections (see --no-sni)
    - Resolve domain names through SOCKS5 proxy if --proxy is set
    - Display mining pool IP address in console
    - (WebUI) Make the monitoring page layout more compact on mobile phones
    
    Bug fixes:
    - Miner is not creating a log file when --log-path is specified
    - GPU does not get turned off when overheated while mining in dual mode
    - (WebUI) Graph zooming is not scaling the charts correctly

--- Miners/Trex.ps1
+++ Miners/Trex.ps1
@@ -10,14 +10,14 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://bitcointalk.org/index.php?topic=4432704.0"
 $Port = "316{0:d2}"
 $DevFee = 1.0
-$Version = "0.24.7"
+$Version = "0.24.8"
 $DeviceCapability = "5.0"
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Trex\t-rex"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.24.7-trex/t-rex-0.24.7-linux.tar.gz"
+            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.24.8-trex/t-rex-0.24.8-linux.tar.gz"
             Cuda   = "9.2"
         }
     )
@@ -25,7 +25,7 @@ if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Trex\t-rex.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.24.7-trex/t-rex-0.24.7-win.zip"
+            Uri    = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.24.8-trex/t-rex-0.24.8-win.zip"
             Cuda   = "9.2"
         }
     )
