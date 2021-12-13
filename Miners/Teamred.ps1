9d3f9ae29ea58a646cd7c8f0a52298b1f21738c2
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-12-13 09:25:15 +0100

    Update miner Teamred to v0.9.0 (amd)
    - Added initial FPGA ethash support. See FPGA_GUIDE.txt for details.

--- Miners/Teamred.ps1
+++ Miners/Teamred.ps1
@@ -10,15 +10,15 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $Port = "409{0:d2}"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5059817.0"
 $DevFee = 3.0
-$Version = "0.8.7"
+$Version = "0.9.0"
 
 if ($IsLinux) {
     $Path = ".\Bin\AMD-Teamred\teamredminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.7-teamred/teamredminer-v0.8.7-linux.tgz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.9.0-teamred/teamredminer-v0.9.0-linux.tgz"
     $DatFile = "$env:HOME/.vertcoin/verthash.dat"
 } else {
     $Path = ".\Bin\AMD-Teamred\teamredminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.8.7-teamred/teamredminer-v0.8.7-win.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.9.0-teamred/teamredminer-v0.9.0-win.zip"
     $DatFile = "$env:APPDATA\Vertcoin\verthash.dat"
 }
 
