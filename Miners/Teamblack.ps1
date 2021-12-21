abe1404d5b9e3fff4d4e24007a278d84b66d031d
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-12-21 13:52:08 +0100

    Update miner Teamblack to v1.35 (amd,intel,nvidia,win only)
    - Improved LHR unlock

--- Miners/Teamblack.ps1
+++ Miners/Teamblack.ps1
@@ -9,7 +9,7 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 
 $ManualURI = "https://github.com/sp-hash/TeamBlackMiner"
 $Port = "365{0:d2}"
-$Version = "1.34"
+$Version = "1.35"
 
 if ($IsLinux) {
     $Path     = ".\Bin\GPU-Teamblack\TBMiner"
@@ -29,15 +29,15 @@ if ($IsLinux) {
 
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.34-teamblack/TeamBlackMiner_1_34_cuda_11_5_fix.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.35-teamblack/TeamBlackMiner_1_35_cuda_11_5.7z"
             Cuda = "11.5"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.34-teamblack/TeamBlackMiner_1_34_cuda_11_4_fix.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.35-teamblack/TeamBlackMiner_1_35_cuda_11_4.7z"
             Cuda = "11.4"
         },
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.34-teamblack/TeamBlackMiner_1_34_cuda_11_2_fix.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.35-teamblack/TeamBlackMiner_1_35_cuda_11_2.7z"
             Cuda = "11.2"
         }
     )
