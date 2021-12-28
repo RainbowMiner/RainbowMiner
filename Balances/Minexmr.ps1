0a6cafee3d29e39ff1bd2aa69d7c0339032e8ad7
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2021-12-28 11:11:56 +0100

    Update pool Minexmr
    - update to new API (issue #1908)

--- Balances/Minexmr.ps1
+++ Balances/Minexmr.ps1
@@ -18,24 +18,21 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 $Request = [PSCustomObject]@{}
 
 try {
-    $Request = Invoke-RestMethodAsync "https://minexmr.com/api/pool/stats_address?address=$(Get-WalletWithPaymentId $Config.Pools.$Name.Wallets.$Pool_Currency -pidchar '.')" -delay 100 -cycletime ($Config.BalanceUpdateMinutes*60)
+    $Request = Invoke-RestMethodAsync "https://minexmr.com/api/main/user/stats?address=$($Config.Pools.$Name.Wallets.$Pool_Currency)" -delay 100 -cycletime ($Config.BalanceUpdateMinutes*60)
     if (-not $Request.stats) {
         Write-Log -Level Info "Pool Balance API ($Name) for $($Pool_Currency) returned nothing. "
     } else {
-        $Pending = ($Request.blocks | Where-Object {$_ -match "^\d+?:\d+?:\d+?:\d+?:\d+?:(\d+?):"} | Foreach-Object {[int64]$Matches[1]} | Measure-Object -Sum).Sum / $coinUnits
-		$Payouts = @($i=0;$Request.payments | Where-Object {$_ -match "^(.+?):(\d+?):"} | Foreach-Object {[PSCustomObject]@{time=$Request.payments[$i+1];amount=[Decimal]$Matches[2] / $coinUnits;txid=$Matches[1]};$i+=2})
         [PSCustomObject]@{
             Caption     = "$($Name) ($Pool_Currency)"
 			BaseName    = $Name
             Currency    = $Pool_Currency
-            Balance     = [Decimal]$Request.stats.balance / $coinUnits
-            Pending     = [Decimal]$Pending
-            Total       = [Decimal]$Request.stats.balance / $coinUnits + [Decimal]$Pending
-            Paid        = [Decimal]$Request.stats.paid / $coinUnits
-            Payouts     = @(Get-BalancesPayouts $Payouts | Select-Object)
+            Balance     = [Decimal]$Request.balance / 1e12
+            Pending     = 0
+            Total       = [Decimal]$Request.balance / 1e12
+            Paid        = [Decimal]$Request.paid / 1e12
+            Payouts     = @()
             LastUpdated = (Get-Date).ToUniversalTime()
         }
-		Remove-Variable "Payouts"
     }
 }
 catch {
--- Pools/Minexmr.ps1
+++ Pools/Minexmr.ps1
@@ -24,81 +24,33 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 
 [hashtable]$Pool_RegionsTable = @{}
 
-$Pool_Regions = @("eu","ca","sg")
+$Pool_Regions = @("pool","fr","de","ca","sg","us-west")
 $Pool_Regions | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
+$Pool_RegionsTable.pool = Get-Region "Eu"
+
+$Pool_Ports = @(4444,443)
 
 $Pool_Request = [PSCustomObject]@{}
-$Pool_Ports   = @([PSCustomObject]@{})
 
 if (-not $InfoOnly) {
-    $Pool_Ports_Ok = $false
     try {
-        $Pool_Request = Invoke-RestMethodAsync "https://minexmr.com/api/pool/stats" -tag $Name -timeout 15 -cycletime 120
-        @("CPU","GPU","RIG","CPU-SSL","GPU-SSL","RIG-SSL") | Foreach-Object {
-            $PortType = $_ -replace '-.*$'
-            $Ports = if ($_ -match 'SSL') {$Pool_Request.config.ports | Where-Object {$_.ssl -or $_.desc -match "SSL"}} else {$Pool_Request.config.ports | Where-Object {-not $_.ssl -and $_.desc -notmatch "SSL"}}
-            if ($Ports) {
-                $PortIndex = if ($_ -match 'SSL') {1} else {0}
-                $Port = Switch ($PortType) {
-                    "GPU" {$Ports | Where-Object desc -match 'Mid' | Select-Object -First 1}
-                    "RIG" {$Ports | Where-Object desc -match 'High' | Select-Object -First 1}
-                }
-                if (-not $Port) {$Port = $Ports | Select-Object -First 1}
-                if ($Pool_Ports.Count -eq 1 -and $PortIndex -eq 1) {$Pool_Ports += [PSCustomObject]@{}}
-                $Pool_Ports[$PortIndex] | Add-Member $PortType ($Port.port -replace ',.+$') -Force
-                $Pool_Ports_Ok = $true
-            }
-        }
+        $Pool_Request = Invoke-RestMethodAsync "https://minexmr.com/api/main/pool/stats" -tag $Name -timeout 15 -cycletime 120
     }
     catch {
         if ($Error.Count){$Error.RemoveAt(0)}
         Write-Log -Level Warn "Pool API ($Name) for $Pool_Currency has failed. "
         return
     }
-    if (-not $Pool_Ports_Ok) {
-        $Pool_Ports = @([PSCustomObject]@{CPU=4444;GPU=7777;RIG=3333},[PSCustomObject]@{CPU=6666;GPU=6666;RIG=6666})
-    }
-    $Pool_Fee = $Pool_Request.config.fee + $(if ($Pool_Request.config.doDonations) {$Pool_Request.config.coreDonation} else {0})
 }
 
 if (-not $InfoOnly) {
-
-    $timestamp    = Get-UnixTimestamp
-    $timestamp24h = $timestamp - 24*3600
-
-    $coinUnits    = 1e12
-    $Divisor      = 1e8
-
-    $lastSatPrice = if ($Global:Rates.$Pool_Currency) {1/$Global:Rates.$Pool_Currency*1e8} elseif ($Pool_Request.charts.price) {[Double]($Pool_Request.charts.price | Select-Object -Last 1)[1]} else {0}
-
-    $diffLive     = $Pool_Request.network.difficulty
-    $reward       = $Pool_Request.network.reward
-    $profitLive   = 86400/$diffLive * $reward/$coinUnits
-    $satRewardLive= $profitLive * $lastSatPrice
-
-    $blocks = $Pool_Request.pool.blocks | Where-Object {$_ -match '^.*?\:(\d+?)\:(\d+?)\:.*?(\d+?)\:(\d+?)$'} | Foreach-Object {[PSCustomObject]@{Time=[int64]$Matches[1];Diff=[int64]$Matches[2];Reward=if($Matches[3] -ne "0"){[int64]$Matches[4]} else {$reward}}} | Where-Object {$_.Time -gt $timestamp24h} | Sort-Object Time -Descending
-    $blocks_measure = $blocks | Select-Object -ExpandProperty Time | Measure-Object -Minimum -Maximum
-    $Pool_BLK = [int]$(if ($blocks_measure.Maximum - $blocks_measure.Minimum) {24*3600/($blocks_measure.Maximum - $blocks_measure.Minimum)*$blocks_measure.Count})
-    $Pool_TSL = if ($blocks.Count) {$timestamp - $blocks[0].Time}
-    
-    if (-not (Test-Path "Stats\Pools\$($Name)_$($Pool_Currency)_Profit.txt")) {
-        $satRewardDay = 0.0
-        $diffDay = ($blocks | Select-Object -ExpandProperty Diff | Measure-Object -Average).Average
-        if ($diffDay) {
-            $reward      = ($blocks | Select-Object -ExpandProperty Reward | Measure-Object -Average).Average
-            $profitDay   = 86400/$diffDay * $reward/$coinUnits
-            $satRewardDay= $profitDay * $lastSatPrice
-        }
-        if (-not $satRewardDay) {$satRewardDay=$satRewardLive}
-        $Stat = Set-Stat -Name "$($Name)_$($Pool_Currency)_Profit" -Value ($satRewardDay/$Divisor) -Duration (New-TimeSpan -Days 1) -HashRate $Pool_Request.pool.hashrate -BlockRate $Pool_BLK -Quiet
-    } else {
-        $Stat = Set-Stat -Name "$($Name)_$($Pool_Currency)_Profit" -Value ($satRewardLive/$Divisor) -Duration $StatSpan -ChangeDetection $false -HashRate $Pool_Request.pool.hashrate -BlockRate $Pool_BLK -Quiet
-    }
+    $Pool_TSL   = [int]((Get-UnixTimestamp) - $Pool_Request.pool.lastBlockFound/1000)
+    $Pool_Price = if ($Global:Rates.XMR) {$Pool_Request.network.reward/($Pool_Request.network.difficulty*1e12)*86400/$Global:Rates.XMR} else {0}
+    $Stat = Set-Stat -Name "$($Name)_$($Pool_Currency)_Profit" -Value $Pool_Price -Duration $StatSpan -ChangeDetection $false -HashRate $Pool_Request.pool.hashrate -BlockRate $Pool_Request.pool.blocksDay -Quiet
 }
 
 if ($AllowZero -or $Pool_Request.pool.hashrate -gt 0 -or $InfoOnly) {
     $Pool_SSL = $false
-    $Pool_Wallet = Get-WalletWithPaymentId $Wallets.$Pool_Currency -pidchar '.' -asobject
     foreach ($Pool_Port in $Pool_Ports) {
         foreach($Pool_Region in $Pool_Regions) {
             [PSCustomObject]@{
@@ -107,23 +59,23 @@ if ($AllowZero -or $Pool_Request.pool.hashrate -gt 0 -or $InfoOnly) {
                 CoinName      = $Pool_CoinName
                 CoinSymbol    = $Pool_Currency
                 Currency      = $Pool_Currency
-                Price         = $Stat.$StatAverage #instead of .Live
-                StablePrice   = $Stat.$StatAverageStable
-                MarginOfError = $Stat.Week_Fluctuation
+                Price         = if ($Pool_Price -gt 0) {$Stat.$StatAverage} else {0}
+                StablePrice   = if ($Pool_Price -gt 0) {$Stat.$StatAverageStable} else {0}
+                MarginOfError = if ($Pool_Price -gt 0) {$Stat.Week_Fluctuation} else {0}
                 Protocol      = "stratum+$(if ($Pool_SSL) {"ssl"} else {"tcp"})"
-                Host          = "$(if ($Pool_Region -eq "eu") {"pool"} else {$Pool_Region}).minexmr.com"
-                Port          = $Pool_Port.CPU
-                Ports         = $Pool_Port
-                User          = "$($Pool_Wallet.wallet).{workername:$Worker}$(if ($Pool_Wallet.difficulty) {"+$($Pool_Wallet.difficulty)"} else {"{diff:+`$difficulty}"})"
+                Host          = "$($Pool_Region).minexmr.com"
+                Port          = $Pool_Port
+                User          = "$($Wallets.$Pool_Currency).{workername:$Worker}"
                 Pass          = "x"
                 Region        = $Pool_RegionsTable.$Pool_Region
                 SSL           = $Pool_SSL
                 Updated       = $Stat.Updated
                 PoolFee       = $Pool_Fee
-                Workers       = $Pool_Request.pool.miners
+                Workers       = $Pool_Request.pool.activeMiners
                 Hashrate      = $Stat.HashRate_Live
                 TSL           = $Pool_TSL
                 BLK           = $Stat.BlockRate_Average
+                WTM           = $Pool_Price -eq 0
                 Name          = $Name
                 Penalty       = 0
                 PenaltyFactor = 1
@@ -131,7 +83,7 @@ if ($AllowZero -or $Pool_Request.pool.hashrate -gt 0 -or $InfoOnly) {
                 HasMinerExclusions = $false
                 Price_Bias    = 0.0
                 Price_Unbias  = 0.0
-                Wallet        = $Pool_Wallet.wallet
+                Wallet        = $Wallets.$Pool_Currency
                 Worker        = "{workername:$Worker}"
                 Email         = $Email
             }
