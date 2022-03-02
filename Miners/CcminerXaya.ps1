97a95b81e0706c0446e93097acea43c43acb8713
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-03-02 11:44:32 +0100

    Update miner CcminerXaya
    - set intensity to 21 for all GPUs with at least 2GB memory, else set to 20

--- Miners/CcminerXaya.ps1
+++ Miners/CcminerXaya.ps1
@@ -54,7 +54,7 @@ $Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique 
                     $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
                     $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
                     $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ','
-                    $Intensities = @($Miner_Device | Foreach-Object {if ($_.OpenCL.GlobalMemsize -ge 8Gb) {"21"} else {"13"}}) -join ','
+                    $Intensities = @($Miner_Device | Foreach-Object {if ($_.OpenCL.GlobalMemsize -ge 2Gb) {"21"} else {"20"}}) -join ','
                     $First = $false
                 }
 				$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
