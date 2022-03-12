1f7917564199a0e51a3d840132943cf8d1c4a732
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-03-12 21:13:28 +0100

    Update miner Wildrig to v0.31.3 (amd,nvidia)
    - add support for 0x10 algorithm (coin ChainOX)

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -11,19 +11,20 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 1.0
 $Cuda = "8.0"
-$Version = "0.31.2"
+$Version = "0.31.3"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-WildRig\wildrig-multi"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.31.2-wildrigmulti/wildrig-multi-linux-0.31.2.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.31.3-wildrigmulti/wildrig-multi-linux-0.31.3.tar.xz"
 } else {
     $Path = ".\Bin\GPU-WildRig\wildrig.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.31.2-wildrigmulti/wildrig-multi-windows-0.31.2.7z"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.31.3-wildrigmulti/wildrig-multi-windows-0.31.3.7z"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
 
 $Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "0x10";                      Vendor = @("AMD","NVIDIA"); Params = ""} #0x10/CHOX
     [PSCustomObject]@{MainAlgorithm = "aergo";                     Vendor = @("AMD");          Params = ""} #Aergo
     [PSCustomObject]@{MainAlgorithm = "anime";                     Vendor = @("AMD");          Params = ""} #Anime
     [PSCustomObject]@{MainAlgorithm = "bcd";                       Vendor = @("AMD");          Params = ""} #BCD
