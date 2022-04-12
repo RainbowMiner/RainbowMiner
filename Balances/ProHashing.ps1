ee4de697377c735878ca868b7566064a6cc5e043
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-04-12 11:47:06 +0200

    Update balance ProHashing
    - look for API_Keys in ProHashingCoins & ProHashingCoinsSolo, too

--- Balances/ProHashing.ps1
+++ Balances/ProHashing.ps1
@@ -6,33 +6,37 @@ param(
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 
-$Request = [PSCustomObject]@{}
+$Pool_Users = @([PSCustomObject]@{User=$Config.Pools.$Name.User;API_Key=$Config.Pools.$Name.API_Key}) + [PSCustomObject]@{User=$Config.Pools."$($Name)Coins".User;API_Key=$Config.Pools."$($Name)Coins".API_Key} + [PSCustomObject]@{User=$Config.Pools."$($Name)CoinsSolo".User;API_Key=$Config.Pools."$($Name)CoinsSolo".API_Key} | Where-Object {$_.User -and $_.API_Key} | Select-Object User,API_Key -Unique | Sort-Object User 
 
-if (-not $Config.Pools.$Name.User -or -not $Config.Pools.$Name.API_Key) {return}
+$Pool_Users | Foreach-Object {
 
-$Request = [PSCustomObject]@{}
-try {
-    $Request = Invoke-RestMethodAsync "https://prohashing.com/api/v1/wallet?apiKey=$($Config.Pools.$Name.API_Key)" -tag $Name -cycletime 120
-}
-catch {
-    if ($Error.Count){$Error.RemoveAt(0)}
-}
+    $Pool_User = $_.User
 
-if ($Request.code -ne 200) {
-    Write-Log -Level Warn "Pool API ($Name) has failed. "
-    return
-}
+    $Request = [PSCustomObject]@{}
+    try {
+        $Request = Invoke-RestMethodAsync "https://prohashing.com/api/v1/wallet?apiKey=$($_.API_Key)" -tag $Name -cycletime 120
+    }
+    catch {
+        if ($Error.Count){$Error.RemoveAt(0)}
+    }
 
- $Request.data.balances.PSObject.Properties | Where-Object {-not $Config.ExcludeCoinsymbolBalances.Count -or $Config.ExcludeCoinsymbolBalances -notcontains $_.Name} | Foreach-Object {
-    [PSCustomObject]@{
-        Caption     = "$($Name) ($($_.Name))"
-        Currency    = $_.Name
-        Balance     = [decimal]$_.Value.balance
-        Pending     = 0
-        Total       = [decimal]$_.Value.total
-        Paid        = [decimal]$_.Value.paid
-        Paid24h     = [decimal]$_.Value.paid24h
-        Payouts     = @()
-        LastUpdated = (Get-Date).ToUniversalTime()
+    if ($Request.code -ne 200) {
+        Write-Log -Level Warn "Pool API ($Name) has failed for user $($Pool_User). "
+        return
+    }
+
+     $Request.data.balances.PSObject.Properties | Where-Object {-not $Config.ExcludeCoinsymbolBalances.Count -or $Config.ExcludeCoinsymbolBalances -notcontains $_.Name} | Foreach-Object {
+        [PSCustomObject]@{
+            Caption     = "$($Name) $($Pool_User) ($($_.Name))"
+            Info        = "$(if (($Pool_Users | Measure-Object).Count -gt 1) {$Pool_User})"
+            Currency    = $_.Name
+            Balance     = [decimal]$_.Value.balance
+            Pending     = 0
+            Total       = [decimal]$_.Value.total
+            Paid        = [decimal]$_.Value.paid
+            Paid24h     = [decimal]$_.Value.paid24h
+            Payouts     = @()
+            LastUpdated = (Get-Date).ToUniversalTime()
+        }
     }
 }
