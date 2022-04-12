9e90980c00bd93e3ba9904a1470192fdcdec5bcf
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-04-12 11:45:39 +0200

    Update pool ProHashing
    - allow zero hashrate during
    - add Workers to records
    - base on AECurrency (remove coins-loop for InfoOnly)

--- Pools/ProHashing.ps1
+++ Pools/ProHashing.ps1
@@ -22,6 +22,8 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 
 if (-not $User -and -not $InfoOnly) {return}
 
+$AllowZero = $true
+
 $Pool_Request = [PSCustomObject]@{}
 $PoolCoins_Request = [PSCustomObject]@{}
 
@@ -58,18 +60,17 @@ $Pool_Host = "prohashing.com"
 $Pool_Regions = @("us","eu")
 $Pool_Regions | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
 
-$Pool_Currencies = @("BTC") + @($Wallets.PSObject.Properties.Name | Select-Object) + @($PoolCoins_Request.data.PSObject.Properties.Name | Where-Object {$_ -notmatch "_"}) | Select-Object -Unique
+$Pool_Currencies = @("BTC") + @($PoolCoins_Request.data.PSObject.Properties.Name | Where-Object {$_ -notmatch "_"}) | Select-Object -Unique
 
-if (-not $InfoOnly -and $Pool_Currencies.Count -gt 1) {
-    if ($AECurrency -eq "" -or $AECurrency -notin $Pool_Currencies) {$AECurrency = $Pool_Currencies | Select-Object -First 1}
-    $Pool_Currencies = $Pool_Currencies | Where-Object {$_ -eq $AECurrency}
-}
+if ($InfoOnly) {$AECurrency = "BTC"}
+elseif ($AECurrency -eq "" -or $AECurrency -notin $Pool_Currencies) {$AECurrency = $Pool_Currencies | Select-Object -First 1}
 
 $PoolCoins_Overview = @{}
 $PoolCoins_Request.data.PSObject.Properties.Value | Where-Object {$_.port -and $_.enabled -and $_.lastblock} | Group-Object -Property algo | Foreach-Object {
     $PoolCoins_Overview[$_.Name] = [PSCustomObject]@{
         "24h_blocks"  = ($_.Group."24h_blocks" | Measure-Object -Maximum).Maximum
         timesincelast = ($_.Group.timesincelast | Measure-Object -Minimum).Minimum
+        workers       = ($_.Group.workers | Measure-Object -Sum).Sum
     }
 }
 
@@ -82,6 +83,7 @@ $Pool_Request.data.PSObject.Properties.Name | Where-Object {$PoolCoins_Overview.
     $Pool_Factor    = [double]$Pool_Request.data.$_.mbtc_mh_factor
     $Pool_TSL       = [int]$PoolCoins_Overview[$_].timesincelast
     $Pool_BLK       = [int]$PoolCoins_Overview[$_]."24h_blocks"
+    $Pool_Workers   = [int]$PoolCoins_Overview[$_].workers
 
     if ($Pool_Factor -le 0) {
         Write-Log -Level Info "$($Name): Unable to determine divisor for algorithm $Pool_Algorithm. "
@@ -99,48 +101,46 @@ $Pool_Request.data.PSObject.Properties.Name | Where-Object {$PoolCoins_Overview.
     }
 
     foreach($Pool_Region in $Pool_Regions) {
-        foreach($Pool_Currency in $Pool_Currencies) {
-            $Pool_Params = if ($Params.$Pool_Currency) {
-                $Pool_ParamsCurrency = "$(if ($Pool_APIKey) {$Params.$Pool_Currency -replace "k=[0-9a-f]+" -replace ",+","," -replace "^,+" -replace ",+$"} else {$Params.$Pool_Currency})"
-                if ($Pool_ParamsCurrency) {",$($Pool_ParamsCurrency)"}
-            }
-            [PSCustomObject]@{
-                Algorithm     = $Pool_Algorithm_Norm
-                Algorithm0    = $Pool_Algorithm_Norm
-                CoinName      = ""
-                CoinSymbol    = ""
-                Currency      = $Pool_Currency
-                Price         = $Stat.$StatAverage #instead of .Live
-                StablePrice   = $Stat.$StatAverageStable
-                MarginOfError = $Stat.Week_Fluctuation
-                Protocol      = "stratum+tcp"
-                Host          = "$(if ($Pool_Region -eq "eu") {"eu."})$Pool_Host"
-                Port          = $Pool_Port
-                User          = $User
-                Pass          = "a=$($_),n={workername:$Worker}{diff:,d=`$difficulty}$($Pool_APIKey)$($Pool_Params)"
-                Region        = $Pool_RegionsTable.$Pool_Region
-                SSL           = $false
-                Updated       = $Stat.Updated
-                PoolFee       = $Pool_PoolFee
-                PaysLive      = $true
-                DataWindow    = $DataWindow
-                Hashrate      = $Stat.HashRate_Live
-                Workers       = $null
-                BLK           = $Stat.BlockRate_Average
-                TSL           = $Pool_TSL
-				ErrorRatio    = $Stat.ErrorRatio
-                Name          = $Name
-                Penalty       = 0
-                PenaltyFactor = 1
-                Disabled      = $false
-                HasMinerExclusions = $false
-                Price_0       = 0.0
-                Price_Bias    = 0.0
-                Price_Unbias  = 0.0
-                Wallet        = $User
-                Worker        = "{workername:$Worker}"
-                Email         = $Email
-            }
+        $Pool_Params = if ($Params.$AECurrency) {
+            $Pool_ParamsCurrency = "$(if ($Pool_APIKey) {$Params.$AECurrency -replace "k=[0-9a-f]+" -replace ",+","," -replace "^,+" -replace ",+$"} else {$Params.$AECurrency})"
+            if ($Pool_ParamsCurrency) {",$($Pool_ParamsCurrency)"}
+        }
+        [PSCustomObject]@{
+            Algorithm     = $Pool_Algorithm_Norm
+            Algorithm0    = $Pool_Algorithm_Norm
+            CoinName      = ""
+            CoinSymbol    = ""
+            Currency      = $AECurrency
+            Price         = $Stat.$StatAverage #instead of .Live
+            StablePrice   = $Stat.$StatAverageStable
+            MarginOfError = $Stat.Week_Fluctuation
+            Protocol      = "stratum+tcp"
+            Host          = "$(if ($Pool_Region -eq "eu") {"eu."})$Pool_Host"
+            Port          = $Pool_Port
+            User          = $User
+            Pass          = "a=$($_),n={workername:$Worker}{diff:,d=`$difficulty}$($Pool_APIKey)$($Pool_Params)"
+            Region        = $Pool_RegionsTable.$Pool_Region
+            SSL           = $false
+            Updated       = $Stat.Updated
+            PoolFee       = $Pool_PoolFee
+            PaysLive      = $true
+            DataWindow    = $DataWindow
+            Hashrate      = $Stat.HashRate_Live
+            Workers       = $Pool_Workers
+            BLK           = $Stat.BlockRate_Average
+            TSL           = $Pool_TSL
+			ErrorRatio    = $Stat.ErrorRatio
+            Name          = $Name
+            Penalty       = 0
+            PenaltyFactor = 1
+            Disabled      = $false
+            HasMinerExclusions = $false
+            Price_0       = 0.0
+            Price_Bias    = 0.0
+            Price_Unbias  = 0.0
+            Wallet        = $User
+            Worker        = "{workername:$Worker}"
+            Email         = $Email
         }
     }
 }
