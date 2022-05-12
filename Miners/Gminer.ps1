467036b8363bd445fecff296f0d93ce5288245d5
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-05-12 17:18:14 +0200

    Update miner Gminer to v2.93 (amd,nvidia)
    - LHR 100% Unlock
    
    Unlocker requirements:
    - driver version: 512.xx for Windows / 510.xx for Linux
    - admin rights

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -11,16 +11,16 @@ $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "2.91"
+$Version = "2.93"
 $DeviceCapability = "5.0"
 $EnableContest = $false
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.91-gminer/gminer_2_91_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.93-gminer/gminer_2_93_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.91-gminer/gminer_2_91_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.93-gminer/gminer_2_93_windows64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
