05d32d8946afee09d962d01c69969c9f920973a8
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-05-13 18:20:36 +0200

    Update miner NBminer to v41.5 (amd,nvidia)
    - feature: ethash Add 90% LHR unlocker for 3080 12G & 3050.
    - feature: ergo Add LHR unlocker support.
    - fix: ethash LHR unlocker improve stability.

--- Miners/NBminer.ps1
+++ Miners/NBminer.ps1
@@ -11,14 +11,14 @@ $ManualURI = "https://github.com/NebuTech/NBMiner/releases"
 $Port = "340{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.1"
-$Version = "41.3"
+$Version = "41.5"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-NBMiner\nbminer"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v41.3-nbminer/NBMiner_41.3_Linux.tgz"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v41.5-nbminer/NBMiner_41.5_Linux.tgz"
 } else {
     $Path = ".\Bin\GPU-NBMiner\nbminer.exe"
-    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v41.3-nbminer/NBMiner_41.3_Win.zip"
+    $URI = "https://github.com/RainbowMiner/miner-binaries/releases/download/v41.5-nbminer/NBMiner_41.5_Win.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
