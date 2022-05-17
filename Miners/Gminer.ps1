1424af774211177574cb97914b69de418774ff1b
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-05-17 16:11:41 +0200

    Update miner Gminer to v2.96 (amd,nvidia)
    - LHR unlock for oldest drivers (fixed "LHR unlock failed" error on old drivers)

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -11,16 +11,16 @@ $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "2.95"
+$Version = "2.96"
 $DeviceCapability = "5.0"
 $EnableContest = $false
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.95-gminer/gminer_2_95_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.96-gminer/gminer_2_96_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.95-gminer/gminer_2_95_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.96-gminer/gminer_2_96_windows64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
