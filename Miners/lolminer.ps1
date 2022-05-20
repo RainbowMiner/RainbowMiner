fb33d62f23a2f85270bad48259c544f6d8e12fb0
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-05-20 18:53:49 +0200

    Update miner Lolminer to v1.51a (amd,nvidia)
    - Improved / Fixed Nvidia memory management (allowing dual mining in Windows to run for more epochs on 6G cards)
    - Updated Web Gui - showing LHR unlock percentage and dual factor
    - "LHR_Unlock_Pct" and "Dual_Factor" now provided per GPU in the API

--- Miners/lolminer.ps1
+++ Miners/lolminer.ps1
@@ -11,14 +11,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=4724735.0"
 $Port = "317{0:d2}"
 $Cuda = "10.0"
 $DevFee = 1.0
-$Version = "1.51"
+$Version = "1.51a"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-lolMiner\lolMiner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.51-lolminer/lolMiner_v1.51_Lin64.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.51a-lolminer/lolMiner_v1.51a_Lin64.tar.gz"
 } else {
     $Path = ".\Bin\GPU-lolMiner\lolMiner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.51-lolminer/lolMiner_v1.51_Win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.51a-lolminer/lolMiner_v1.51a_Win64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
