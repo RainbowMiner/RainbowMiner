d8cc16767ba963fe5c96c472d272ec169c87c6a5
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-06-11 19:47:33 +0200

    Update miner MiniZ to v1.8z3 (amd,nvidia)
    * Added support for AMD GPUs RX 6600/6800/6900 (XT).
    * Added support for AMD RX 590.
    * Reduced stale shares on Aion.
    * Improved stability.
    * Minor bug fixes.

--- Miners/MiniZ.ps1
+++ Miners/MiniZ.ps1
@@ -10,13 +10,13 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://bitcointalk.org/index.php?topic=4767892.0"
 $Port = "330{0:d2}"
 $DevFee = 2.0
-$Version = "1.8z2"
+$Version = "1.8z3"
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-MiniZ\miniZ"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.8z2-miniz/miniZ_v1.8z2_linux-x64.tar.gz"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.8z3-miniz/miniZ_v1.8z3_linux-x64.tar.gz"
             Cuda = "8.0"
         }
     )
@@ -24,7 +24,7 @@ if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-MiniZ\miniZ.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.8z2-miniz/miniZ_v1.8z2_win-x64.7z"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.8z3-miniz/miniZ_v1.8z3_win-x64.7z"
             Cuda = "8.0"
         }
     )
