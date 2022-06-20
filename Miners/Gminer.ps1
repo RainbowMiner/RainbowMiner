715262c1f3524844852f2e229bfc9252ba4f7123
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-06-20 14:26:15 +0200

    Update miner Gminer to v3.01 (amd,nvidia)
    - fixed RavenCoin mining errors
    - fixed displaying of pool hashrate for RavenCoin

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -11,16 +11,16 @@ $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "3.00"
+$Version = "3.01"
 $DeviceCapability = "5.0"
 $EnableContest = $false
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.00-gminer/gminer_3_00_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.01-gminer/gminer_3_01_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.00-gminer/gminer_3_00_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.01-gminer/gminer_3_01_windows64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
