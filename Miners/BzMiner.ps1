5deba001396eefd5da2a814ea8183de945123d1f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-08-15 21:52:52 +0200

    Update miner BzMiner to v10.0.3 (amd,nvidia)
    - Further Improvements to Improved Eth/Etc + Kaspa hashrate (Nvidia)
    - Improvements to dual autotune
    - Added --max_dual_autotune_drop option to control how much eth hashrate your willing to lose when autotuning (default 0.92)
    - Added --disable_opencl option. useful for rigs where bzminer crashes due to amd drivers
    - Added --zil_only option (do not mine eth part of eth + zil)
    - Added --log_file_verbosity option to log different verbosity to log file vs console
    - Added download config button to gui
    - Updated download log in gui to download log file (not just text in console)
    - Increased gui console update frequency
    - Fixed opencl enumerating device 0:0

--- Miners/BzMiner.ps1
+++ Miners/BzMiner.ps1
@@ -11,14 +11,14 @@ $ManualUri = "https://github.com/bzminer/bzminer/releases"
 $Port = "332{0:d2}"
 $DevFee = 0.5
 $Cuda = "11.2"
-$Version = "10.0.3b2"
+$Version = "10.0.3"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-BzMiner\bzminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v10.0.3b2-bzminer/bzminer_v10.0.3b2_linux.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v10.0.3-bzminer/bzminer_v10.0.3_linux.tar.gz"
 } else {
     $Path = ".\Bin\GPU-BzMiner\bzminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v10.0.3b2-bzminer/bzminer_v10.0.3b2_windows.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v10.0.3-bzminer/bzminer_v10.0.3_windows.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
