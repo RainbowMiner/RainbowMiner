d1a8b1e4a808d1c0ec098c4d8d8d7a0de9148af0
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-09-09 21:39:15 +0200

    Update miner Bzminer to v11.0.3 (amd,nvidia)
    - Fixed dual Kaspa duplicate shares
    - Fixed Ergo on AMD taking too long to generate DAG
    - Fixed empty worker name not using a default
    - Lowered default time for hung gpu, hung_gpu_ms
    - Fixed Rejeced shares watchdog
    - Added oc_reset_on_exit option
    - Added --nc 1 to all script files (do not write to config by default)
    - Fixed BzMiner exit taking too long

--- Miners/BzMiner.ps1
+++ Miners/BzMiner.ps1
@@ -11,14 +11,14 @@ $ManualUri = "https://github.com/bzminer/bzminer/releases"
 $Port = "332{0:d2}"
 $DevFee = 0.5
 $Cuda = "11.2"
-$Version = "11.0.2"
+$Version = "11.0.3"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-BzMiner\bzminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v11.0.2-bzminer/bzminer_v11.0.2_linux.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v11.0.3-bzminer/bzminer_v11.0.3_linux.tar.gz"
 } else {
     $Path = ".\Bin\GPU-BzMiner\bzminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v11.0.2-bzminer/bzminer_v11.0.2_windows.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v11.0.3-bzminer/bzminer_v11.0.3_windows.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
