6781a12dcab97c265dd15905481a1b7d983591bc
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-09-14 10:23:00 +0200

    Update GetOpenCL.ps1
    - duplicate platform detection disabled for Linux+AMD, only

--- Scripts/GetOpenCL.ps1
+++ Scripts/GetOpenCL.ps1
@@ -10,8 +10,7 @@ $Result = [PSCustomObject]@{
 try {
     $PlatformId = 0
     $Result.Platform_Devices = [OpenCl.Platform]::GetPlatformIDs() | ForEach-Object {
-        $AllPlatforms.Add("$($_.Name) $($_.Version)") > $null
-        $Device_Index = 0
+
         $PlatformVendor = switch -Regex ([String]$_.Vendor) { 
                                 "Advanced Micro Devices" {"AMD"}
                                 "Intel"  {"INTEL"}
@@ -19,30 +18,37 @@ try {
                                 "AMD"    {"AMD"}
                                 default {$_.Vendor -replace '\(R\)|\(TM\)|\(C\)' -replace '[^A-Z0-9]'}
                     }
-        [PSCustomObject]@{
-            PlatformId=$PlatformId
-            Vendor=$PlatformVendor
-            Name="$($_.Name) $($_.Version)"
-            Devices=[OpenCl.Device]::GetDeviceIDs($_, [OpenCl.DeviceType]::All) | Foreach-Object {
-                [PSCustomObject]@{
-                    DeviceIndex      = $Device_Index
-                    Name             = $_.Name
-                    Architecture     = $_.Architecture
-                    Type             = "$($_.Type)"
-                    Vendor           = $_.Vendor
-                    GlobalMemSize    = $_.GlobalMemSize
-                    GlobalMemSizeGB  = [int]($_.GlobalMemSize/1GB)
-                    MaxComputeUnits  = $_.MaxComputeUnits
-                    PlatformVersion  = $_.Platform.Version
-                    DriverVersion    = $_.DriverVersion
-                    PCIBusId         = $_.PCIBusId
-                    DeviceCapability = $_.DeviceCapability
-                    CardId           = -1
+
+        if (($Global:IsLinux -and $PlatformVendor -eq "AMD") -or $AllPlatforms -inotcontains "$($_.Name) $($_.Version)") {
+
+            $AllPlatforms.Add("$($_.Name) $($_.Version)") > $null
+            $Device_Index = 0
+            [PSCustomObject]@{
+                PlatformId=$PlatformId
+                Vendor=$PlatformVendor
+                Name="$($_.Name) $($_.Version)"
+                Devices=[OpenCl.Device]::GetDeviceIDs($_, [OpenCl.DeviceType]::All) | Foreach-Object {
+                    [PSCustomObject]@{
+                        DeviceIndex      = $Device_Index
+                        Name             = $_.Name
+                        Architecture     = $_.Architecture
+                        Type             = "$($_.Type)"
+                        Vendor           = $_.Vendor
+                        GlobalMemSize    = $_.GlobalMemSize
+                        GlobalMemSizeGB  = [int]($_.GlobalMemSize/1GB)
+                        MaxComputeUnits  = $_.MaxComputeUnits
+                        PlatformVersion  = $_.Platform.Version
+                        DriverVersion    = $_.DriverVersion
+                        PCIBusId         = $_.PCIBusId
+                        DeviceCapability = $_.DeviceCapability
+                        CardId           = -1
+                    }
+                    $Device_Index++
                 }
-                $Device_Index++
             }
+
+            $PlatformId++
         }
-        $PlatformId++
     }
 } catch {
     if ($Error.Count){$Error.RemoveAt(0)}
