2525511ad789e6cb7e01706fc4afa2f71d5510cb
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-12-07 10:45:45 +0100

    Update balance MiningPoolHub
    - add workaround for API error (missing coin, issue #2217)

--- Balances/MiningPoolHub.ps1
+++ Balances/MiningPoolHub.ps1
@@ -45,7 +45,13 @@ if (($Pool_Request.return | Measure-Object).Count -le 1) {
     return
 }
 
+if (-not ($Request.getuserallbalances.data | Measure-Object).Count) {return}
+
+$CoinsEmpty = $true
+
 $Request.getuserallbalances.data | Where-Object {$_.coin} | Foreach-Object {
+
+    $CoinsEmpty = $false
 
     $Pool_CoinName = $_.coin
     $Currency = ($Pool_Request.return | Where-Object {$_.coin_name -eq $Pool_CoinName}).symbol
@@ -66,4 +72,33 @@ $Request.getuserallbalances.data | Where-Object {$_.coin} | Foreach-Object {
             Lastupdated = (Get-Date).ToUniversalTime()
         }
     }
+}
+
+# Workaround for empty coin parameter, error in MPH API
+if ($CoinsEmpty) {
+    $PoolConfigCoins = $Config.Pools."$($Name)Coins"
+    $PoolConfig.CoinSymbol + $PoolConfigCoins.CoinSymbol | Where-Object {$_} | Select-Object -Unique | Where-Object {$Currency = $_;$Pool_Data = $Pool_Request.return | Where-Object {$_.symbol -eq $Currency};$Pool_Data -and (-not $Config.ExcludeCoinsymbolBalances.Count -or $Config.ExcludeCoinsymbolBalances -notcontains $Currency)} | Foreach-Object {
+        try {
+            $Request = Invoke-RestMethodAsync "https://$($Pool_Data.coin_name).miningpoolhub.com/index.php?page=api&action=getuserbalance&api_key=$($PoolConfig.API_Key)&id=$($PoolConfig.API_ID)" -cycletime ($Config.BalanceUpdateMinutes*60)
+        }
+        catch {
+            if ($Error.Count){$Error.RemoveAt(0)}
+            Write-Warning "Pool Balance API ($Name) for coin $($Currency) has failed. "
+            return
+        }
+
+        [PSCustomObject]@{
+            Caption     = "$($Name) ($($Currency))"
+		    BaseName    = $Name
+            Currency    = $Currency
+            Balance     = [Decimal]$_.confirmed
+            Pending     = [Decimal]$_.unconfirmed
+            Total       = [Decimal]$_.confirmed + [Decimal]$_.unconfirmed
+            Paid        = [Decimal]0
+		    Payouts     = @()
+            Lastupdated = (Get-Date).ToUniversalTime()
+        }
+
+    }
+
 }
