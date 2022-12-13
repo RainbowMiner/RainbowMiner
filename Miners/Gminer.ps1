79d4a8280e9196ee50fd223d78e200ba72dfef99
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-12-13 10:59:09 +0100

    Update miner Gminer to v3.17 (amd,nvidia)
    - improved Kaspa hashrate in single mining mode +5 - +7%
    - improved Kaspa hashrate in dual mining mode up to +10% (ETC + KASPA, ERGO + KASPA)

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -11,16 +11,16 @@ $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "3.16"
+$Version = "3.17"
 $DeviceCapability = "5.0"
 $EnableContest = $false
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.16-gminer/gminer_3_16_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.17-gminer/gminer_3_17_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.16-gminer/gminer_3_16_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.17-gminer/gminer_3_17_windows64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
