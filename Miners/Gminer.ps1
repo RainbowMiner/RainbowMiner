2640e044e2618b8022123d7d19f7d1318049af9e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-12-29 15:56:58 +0100

    Update miner Gminer to v3.20 (amd,nvidia)
    - improved compatibility with Kaspa pools (supports KStratum to support mining on Kaspa Node)
    - improved Kaspa, ETC+Kaspa, Ergo+Kaspa mining performance for GTX 10xx GPUs (up to +5% in single mining mode)

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -11,16 +11,16 @@ $ManualUri = "https://github.com/develsoftware/GMinerRelease/releases"
 $Port = "329{0:d2}"
 $DevFee = 2.0
 $Cuda = "9.0"
-$Version = "3.19"
+$Version = "3.20"
 $DeviceCapability = "5.0"
 $EnableContest = $false
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-Gminer\miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.19-gminer/gminer_3_19_linux64.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.20-gminer/gminer_3_20_linux64.tar.xz"
 } else {
     $Path = ".\Bin\GPU-Gminer\miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.19-gminer/gminer_3_19_windows64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.20-gminer/gminer_3_20_windows64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD, NVIDIA present in system
