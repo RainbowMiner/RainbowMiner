f17acba61baa874ea181d2f3263d862fdcf3c645
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-02-06 22:41:04 +0100

    Update miner TTminer to v2023.1.5 (nvidia)
    - add a Web-Interface to access the Miner-Statistics through a Browser. Ypu can define which port TT should use - default is 8081. (-web [port])
    - add an option to enable 'auto-refresh' for the statistics page. You can define the refesh interval as parameter in seconds. Default is 15 seconds. (-webrfsh [int])
    - fixes DAG epoch calculation for Veriblock (VBK) algo

--- Miners/TTminer.ps1
+++ Miners/TTminer.ps1
@@ -11,15 +11,15 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5025783.0"
 $Port = "333{0:d2}"
 $DevFee = 1.0
 $Cuda = "11.8"
-$Version = "2023.1.4"
+$Version = "2023.1.5"
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-TTminer\TT-Miner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2023.1.4-ttminer/TT-Miner-2023.1.4.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2023.1.5-ttminer/TT-Miner-2023.1.5.tar.gz"
 
 } else {
     $Path = ".\Bin\NVIDIA-TTminer\TT-Miner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2023.1.4-ttminer/TT-Miner-2023.1.4.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2023.1.5-ttminer/TT-Miner-2023.1.5.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
