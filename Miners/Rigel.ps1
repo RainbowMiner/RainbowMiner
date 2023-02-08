b2b4f276e64a1e083a5431e572816812528a3a42
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-02-08 23:01:31 +0100

    Update miner Rigel to v1.3.6 (nvidia)
    - (Nexa) Minor performance improvements
    - (Nexa) Fix performance regression for 40xx GPUs

--- Miners/Rigel.ps1
+++ Miners/Rigel.ps1
@@ -10,13 +10,13 @@ if (-not $IsWindows -and -not $IsLinux) {return}
 $ManualUri = "https://github.com/rigelminer/rigel/releases"
 $Port = "324{0:d2}"
 $DevFee = 0.7
-$Version = "1.3.5"
+$Version = "1.3.6"
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Rigel\rigel"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.3.5-rigel/rigel-1.3.5-linux.tar.gz"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.3.6-rigel/rigel-1.3.6-linux.tar.gz"
             Cuda = "8.0"
         }
     )
@@ -24,7 +24,7 @@ if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Rigel\rigel.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.3.5-rigel/rigel-1.3.5-win.zip"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.3.6-rigel/rigel-1.3.6-win.zip"
             Cuda = "8.0"
         }
     )
