f786fcbba18596ec3608e631454987a135beb32e
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-02-24 17:10:27 +0100

    Update miner Lolminer to v1.68 (amd,nvidia)
    - Significantly improved Nexa mining performance on supported GPUs (e.g. up to +25% on 8G Ampere GPUs)
    - Fixed a bug that can make the miner crash with error message "Authorization problem on all configured pools 1" during Nexa mining

--- Miners/lolminer.ps1
+++ Miners/lolminer.ps1
@@ -11,14 +11,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=4724735.0"
 $Port = "317{0:d2}"
 $Cuda = "10.0"
 $DevFee = 1.0
-$Version = "1.67"
+$Version = "1.68"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-lolMiner\lolMiner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.67-lolminer/lolMiner_v1.67_Lin64.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.68-lolminer/lolMiner_v1.68_Lin64.tar.gz"
 } else {
     $Path = ".\Bin\GPU-lolMiner\lolMiner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.67-lolminer/lolMiner_v1.67_Win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.68-lolminer/lolMiner_v1.68_Win64.zip"
 }
 
 if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.INTEL -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No GPU present in system
