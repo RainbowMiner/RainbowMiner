7d4c6ce190389a7e12d47b1c5c25d4c17ca188c9
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-03-13 19:32:49 +0100

    Add EwbfZcash
    - old Equihash / by request

---
+++ Miners/EwbfZcash.ps1
@@ -0,0 +1,93 @@
+﻿using module ..\Modules\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+if (-not $IsWindows -and -not $IsLinux) {return}
+
+if ($IsLinux) {
+    $Path = ".\Bin\NVIDIA-EWBFZcash\miner"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.3.4b-ewbf/EWBF-nvidia-linux.0.3.4b.7z"
+} else {
+    $Path = ".\Bin\NVIDIA-EWBFZcash\miner.exe"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.3.4b-ewbf/EWBF-nvidia-win.0.3.4b.7z"
+}
+$ManualUri = "https://bitcointalk.org/index.php?topic=1707546.0"
+$Port = "351{0:d2}"
+$DevFee = 0.0
+$Cuda = "8.0"
+$Version = "0.3.4b"
+
+if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No NVIDIA present in system
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "Equihash";  MinMemGB = 2; Params = ""; ExcludePoolName = "Nicehash"}  #Equihash
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("NVIDIA")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+if (-not (Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name)) {return}
+
+$Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Miner_Model = $_.Model
+    $Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)".Where({$_.Model -eq $Miner_Model})
+
+    $Commands.ForEach({
+        $First = $true
+        $Algorithm_Norm_0 = Get-Algorithm $_.MainAlgorithm
+
+        $MinMemGB = $_.MinMemGB        
+        $Miner_Device = $Device | Where-Object {Test-VRAM $_ $MinMemGB}
+        
+		foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)","$($Algorithm_Norm_0)-GPU")) {
+			if ($Pools.$Algorithm_Norm.Host -and (-not $_.ExcludePoolName -or $Pools.$Algorithm_Norm.Host -notmatch $_.ExcludePoolName) -and $Miner_Device) {
+                if ($First) {
+                    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+                    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+                    $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ' '
+                    $First = $false
+                }
+				$Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
+				[PSCustomObject]@{
+					Name           = $Miner_Name
+					DeviceName     = $Miner_Device.Name
+					DeviceModel    = $Miner_Model
+					Path           = $Path
+					Arguments      = "--api 127.0.0.1:`$mport --cuda_devices $($DeviceIDsAll) --server $($Pools.$Algorithm_Norm.Host) --port $($Pool_Port) --fee 0 --eexit 1 --user $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" --pass $($Pools.$Algorithm_Norm.Pass)"}) $($_.Params)"
+					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $($Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Week)}
+					API            = "DSTM"
+					Port           = $Miner_Port
+                    FaultTolerance = $_.FaultTolerance
+					ExtendInterval = 2
+                    Penalty        = 0
+					DevFee         = $DevFee
+					Uri            = $Uri
+					ManualUri      = $ManualUri
+                    Version        = $Version
+                    PowerDraw      = 0
+                    BaseName       = $Name
+                    BaseAlgorithm  = $Algorithm_Norm_0
+                    Benchmarked    = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Benchmarked
+                    LogFile        = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".LogFile
+                    ExcludePoolName = $_.ExcludePoolName
+				}
+			}
+		}
+    })
+}
