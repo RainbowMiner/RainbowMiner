247ef25d0f8b3274dbe91690cbd8d9a85ffd2784
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-04-20 17:29:24 +0200

    Update miner BzMiner to v14.2.1 (amd,intel,nvidia)
    - Improved Ironfish hashrate
    - Improved dual Octa + Ironfish hashrate
    - Added show_pending option to see shares that were found but have not received a response from the pool
    - Fixed dual +Ironfish percentages
    - Fixed rejected shares on ethstratum protocol for ethw, etc, and octa

--- Miners/BzMiner.ps1
+++ Miners/BzMiner.ps1
@@ -15,14 +15,14 @@ $ManualUri = "https://github.com/bzminer/bzminer/releases"
 $Port = "332{0:d2}"
 $DevFee = 0.5
 $Cuda = "11.2"
-$Version = "14.2.0"
+$Version = "14.2.1"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-BzMiner\bzminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v14.1.1-bzminer/bzminer_v14.1.1_linux.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v14.2.1-bzminer/bzminer_v14.2.1_linux.tar.gz"
 } else {
     $Path = ".\Bin\GPU-BzMiner\bzminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v14.1.1-bzminer/bzminer_v14.1.1_windows.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v14.2.1-bzminer/bzminer_v14.2.1_windows.zip"
 }
 
 $ExcludePoolName = "prohashing|miningrigrentals"
