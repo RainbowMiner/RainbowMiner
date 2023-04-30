1ffc3ead7ac945bd6de0cd0ddb12d0582446011c
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-04-30 12:52:59 +0200

    Update miner SrbminerMulti to v2.2.5 (amd,cpu,intel,nvidia)
    - Significantly improved mining performance on algorithm 'dynex' for AMD/NVIDIA/INTEL
    - Improved mining performance on algorithm 'blake3_ironfish' for AMD/NVIDIA/INTEL in single and dual modes
    - Added support for algorithm 'sha3d' on NVIDIA/INTEL GPU's
    - Ironfish is now compatible with pools using stratum v2
    - Added optimised kernel for DUAL mining : DYNEX/SHA512_256D_RADIANT
    - Added auto tune for algorithm 'dynex' that will try to find an optimal gpu intensity value
    - Removed CPU POW [algorithm 'dynex']
    - Algorithm 'xdag' is now compatible with xdagj 0.6.1
    - Fixed broken RXD/ALEPH mining on RDNA gpus [linux]
    - If using '--gpu-reset-oc' parameter, it will get applied also on miner shutdown
    - Bug fixes

--- Miners/SrbMinerMulti.ps1
+++ Miners/SrbMinerMulti.ps1
@@ -11,15 +11,15 @@ if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.De
 $ManualUri = "https://bitcointalk.org/index.php?topic=5190081.0"
 $Port = "349{0:d2}"
 $DevFee = 0.85
-$Version = "2.2.4"
+$Version = "2.2.5"
 $Cuda = "11.7"
 
 if ($IsLinux) {
     $Path = ".\Bin\ANY-SRBMinerMulti\SRBMiner-MULTI"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.2.4-srbminermulti/SRBMiner-Multi-2-2-4-Linux.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.2.5-srbminermulti/SRBMiner-Multi-2-2-5-Linux.tar.xz"
 } else {
     $Path = ".\Bin\ANY-SRBMinerMulti\SRBMiner-MULTI.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.2.4-srbminermulti/SRBMiner-Multi-2-2-4-win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.2.5-srbminermulti/SRBMiner-Multi-2-2-5-win64.zip"
 }
 
 $Commands = [PSCustomObject[]]@(
@@ -109,7 +109,7 @@ $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "progpow_zano"     ; DAG = $true; Params = ""; Fee = 0.85; MinMemGb = 2; Vendor = @("AMD","CPU","NVIDIA")} #ProgPowZANO/ZANO
     [PSCustomObject]@{MainAlgorithm = "pufferfish2bmb"   ;              Params = ""; Fee = 1.00;               Vendor = @("AMD","CPU")} #Pufferfishbmb/BMB
     [PSCustomObject]@{MainAlgorithm = "sha256dt";                       Params = ""; Fee = 0.85;               Vendor = @("AMD","CPU","NVIDIA")} #SHA256dt/NOVO
-    [PSCustomObject]@{MainAlgorithm = "sha3d"            ;              Params = ""; Fee = 0.85;               Vendor = @("AMD","CPU")} #SHA3d/KCN,YCN
+    [PSCustomObject]@{MainAlgorithm = "sha3d"            ;              Params = ""; Fee = 0.85;               Vendor = @("AMD","CPU","NVIDIA")} #SHA3d/KCN,YCN
     [PSCustomObject]@{MainAlgorithm = "sha512_256d_radiant";            Params = ""; Fee = 0.85;               Vendor = @("AMD","CPU","NVIDIA")} #SHA512256d/RAD
     [PSCustomObject]@{MainAlgorithm = "ubqhash"          ;              Params = ""; Fee = 0.65; MinMemGb = 3; Vendor = @("AMD","INTEL","NVIDIA")} #ubqhash
     [PSCustomObject]@{MainAlgorithm = "verthash"         ;              Params = ""; Fee = 1.00;               Vendor = @("AMD","CPU")} #Verthash
