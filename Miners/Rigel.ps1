56c3275eccef03d244a680babf476bc0c8f91233
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-07-03 21:23:52 +0200

    Update miner Rigel to v1.6.1 (nvidia)
    - (Ergo) Add ERG+KAS and ERG+KAS+ZIL mining support (see dual-erg-kas bat/sh script)
    - (Ergo) Add --autolykos2-prebuild parameter to enable/disable dataset prebuild (see readme)
    - Improve pool re-connection logic (no delay if previous connection attempt was less than 10s ago)
    - Make troubleshooting overclock issues easier by only logging the error from the first GPU that crashed - most often this is the GPU that needs further OC tweaking

--- Miners/Rigel.ps1
+++ Miners/Rigel.ps1
@@ -11,13 +11,13 @@ if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return}
 $ManualUri = "https://github.com/rigelminer/rigel/releases"
 $Port = "324{0:d2}"
 $DevFee = 0.7
-$Version = "1.6.0"
+$Version = "1.6.1"
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Rigel\rigel"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.6.0-rigel/rigel-1.6.0-linux.tar.gz"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.6.1-rigel/rigel-1.6.1-linux.tar.gz"
             Cuda = "8.0"
         }
     )
@@ -25,7 +25,7 @@ if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Rigel\rigel.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.6.0-rigel/rigel-1.6.0-win.zip"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.6.1-rigel/rigel-1.6.1-win.zip"
             Cuda = "8.0"
         }
     )
@@ -48,6 +48,7 @@ $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "sha512256d";                   MinMemGB = 2; Params = "";                     Vendor = @("NVIDIA"); Fee = 1.0} #SHA512256d/RAD
 
     # Dual mining
+    [PSCustomObject]@{MainAlgorithm = "autolykos2";      DAG = $true; MinMemGB = 2; Params = "";                       Vendor = @("NVIDIA"); SecondaryAlgorithm = "kheavyhash"; Fee = 1.0} #Autolykos2/ERG + KAS
     [PSCustomObject]@{MainAlgorithm = "autolykos2";      DAG = $true; MinMemGB = 2; Params = "";                       Vendor = @("NVIDIA"); SecondaryAlgorithm = "sha512256d"; Fee = 1.0} #Autolykos2/ERG + RXD
 
     [PSCustomObject]@{MainAlgorithm = "etchash";         DAG = $true; MinMemGB = 2; Params = ""; ExtendInterval = 2;   Vendor = @("NVIDIA"); SecondaryAlgorithm = "alephium"} #Etchash (ETC)
