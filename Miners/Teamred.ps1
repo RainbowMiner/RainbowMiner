26a52d3cdb73c332a56badc6cc3686430db4085d
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-07-08 16:56:45 +0200

    Update miner Teamred to v0.10.14 (amd)
    - GPU: Fixed ZIL mining on epoch 1 (instead of epoch 0).

--- Miners/Teamred.ps1
+++ Miners/Teamred.ps1
@@ -11,15 +11,15 @@ if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $InfoOnly) {return} # 
 $Port = "409{0:d2}"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5059817.0"
 $DevFee = 3.0
-$Version = "0.10.13"
+$Version = "0.10.14"
 
 if ($IsLinux) {
     $Path = ".\Bin\AMD-Teamred\teamredminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.10.13-teamred/teamredminer-v0.10.13-linux.tgz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.10.14-teamred/teamredminer-v0.10.14-linux.tgz"
     $DatFile = "$env:HOME/.vertcoin/verthash.dat"
 } else {
     $Path = ".\Bin\AMD-Teamred\teamredminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.10.13-teamred/teamredminer-v0.10.13-win.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.10.14-teamred/teamredminer-v0.10.14-win.zip"
     $DatFile = "$env:APPDATA\Vertcoin\verthash.dat"
 }
 
