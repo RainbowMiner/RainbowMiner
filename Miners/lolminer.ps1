270e20b633d2752945a790d23b18b17a2798d4f0
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-11-11 14:30:24 +0100

    Update miner Lolminer to v1.77b (amd,nvidia)
    - Added support to mine SHA512_256D used for Radiant (RXD). Use -a RADIANT to mine it, fee is 0.75%. Supported GPUs: AMD RX 470 - 590, Vega (requires rocm drivers) and all Navi and newer, Nvidia Pascal and newer.
    - Added support to mine Ethash B3 used for Rethereum (RETH). Use -a ETHASHB3 to mine it, fee is 1.0%. Supported GPUs: AMD Navi and newer, Nvidia Pascal and newer.
    - Added ethash dag correction tables for epochs 13-29 (PowBlocks, XPB), 112-129 (OCTA) and 608 - 619 (ETHW)
    - Fixed a bug in Ethash solvers for AMD Navi and newer and Nvidia GPUs that caused a crash when mining at some low epochs. This fix will allow to mine PowBlocks (XPB).
    - Fixed a bug introduced in 1.76a causing CTXC mining to not work.
    - (1.77a): Fixed a bug causing RXD mining to not work (bug was new in 1.77)
    - (1.77b): Fixed a bug causing Alephium and Ironfish mining to not work (bug was new in 1.77)

--- Miners/lolminer.ps1
+++ Miners/lolminer.ps1
@@ -12,14 +12,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=4724735.0"
 $Port = "317{0:d2}"
 $Cuda = "10.0"
 $DevFee = 1.0
-$Version = "1.76a"
+$Version = "1.77b"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-lolMiner\lolMiner"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.76a-lolminer/lolMiner_v1.76a_Lin64.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.77b-lolminer/lolMiner_v1.77b_Lin64.tar.gz"
 } else {
     $Path = ".\Bin\GPU-lolMiner\lolMiner.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.76a-lolminer/lolMiner_v1.76a_Win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.77b-lolminer/lolMiner_v1.77b_Win64.zip"
 }
 
 $Commands = [PSCustomObject[]]@(
