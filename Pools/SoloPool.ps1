3b368a1fed2250763198922297474d3968eec7dd
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-11-21 12:30:52 +0100

    Update pool SoloPool
    - fix: adapt to API changes (issue #2537)

--- Pools/SoloPool.ps1
+++ Pools/SoloPool.ps1
@@ -32,6 +32,9 @@ if (($Pool_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | M
     return
 }
 
+[hashtable]$Pool_RegionsTable = @{}
+@("eu","us") | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
+
 [hashtable]$Pool_Algorithms = @{}
 
 $Pool_Request.PSObject.Properties.Name | Where-Object {$Pool_Currency = $_.ToUpper();($Wallets.$Pool_Currency -or ($Pool_Currency -eq "FIRO" -and $Wallets.XZC) -or $InfoOnly) -and $_ -notmatch "^dgb-"} | ForEach-Object {
@@ -48,12 +51,12 @@ $Pool_Request.PSObject.Properties.Name | Where-Object {$Pool_Currency = $_.ToUpp
     $ok = $false
     try {
         $Pool_HelpPage = Invoke-WebRequestAsync "https://$($_).solopool.org/help" -tag $Name -cycletime 86400
-        if ($Pool_HelpPage -match 'meta\s+name="arts-pool/config/environment"\s+content="(.+?)"') {
+        if ($Pool_HelpPage -match 'meta\s+name="\w+/config/environment"\s+content="(.+?)"') {
             $Pool_MetaVars = [System.Web.HttpUtility]::UrlDecode($Matches[1]) | ConvertFrom-Json -ErrorAction Stop
             $ok = $true
             if (-not $Pool_Coin) {
-                $Pool_Algorithm = $Pool_MetaVars.TEMPLATE.algorithmTitle
-                $Pool_CoinName  = $Pool_MetaVars.COIN.Name
+                $Pool_Algorithm = $Pool_MetaVars.TEMPLATE.algoTitle
+                $Pool_CoinName  = $Pool_MetaVars.TEMPLATE.title
             }
         }
     } catch {
@@ -65,13 +68,8 @@ $Pool_Request.PSObject.Properties.Name | Where-Object {$Pool_Currency = $_.ToUpp
         return
     }
 
-    if ($_ -eq "xwp" -or $_ -eq "xmr") {
-        $Pool_User = $Pool_Wallet
-        $Pool_Pass = "{workername:$Worker}"
-    } else {
-        $Pool_User = "$Pool_Wallet.{workername:$Worker}"
-        $Pool_Pass = "x"
-    }
+    $Pool_User = "$Pool_Wallet.{workername:$Worker}"
+    $Pool_Pass = "x"
 
     $Pool_PoolFee   = $Pool_Request.$_.fee
 
@@ -79,53 +77,52 @@ $Pool_Request.PSObject.Properties.Name | Where-Object {$Pool_Currency = $_.ToUpp
     $Pool_Algorithm_Norm = $Pool_Algorithms.$Pool_Algorithm
     $Pool_EthProxy  = if ($Pool_Algorithm_Norm -match $Global:RegexAlgoHasEthproxy) {"ethproxy"} elseif ($Pool_Algorithm_Norm -match $Global:RegexAlgoIsProgPow) {"stratum"} else {$null}
 
-    if ($ok) {
-        $Pool_Host      = $Pool_MetaVars.APP.StratumHost
-        $Pool_Port      = if ($Pool_MetaVars.APP.StratumPortVar) {$Pool_MetaVars.APP.StratumPortVar} else {$Pool_MetaVars.APP.StratumPortLow}
+    $Pool_Hosts = $Pool_RegionsTable.Keys | Where-Object {$Pool_MetaVars.TEMPLATE."stratumHost$($_.toUpper())"} | Foreach-Object {[PSCustomObject]@{region=$_;host=$Pool_MetaVars.TEMPLATE."stratumHost$($_.toUpper())";port=$Pool_MetaVars.TEMPLATE."stratumPort$(if ($Pool_MetaVars.TEMPLATE.stratumPortGpu) {"Gpu"} else {"Low"})"}}
 
-        if (-not $InfoOnly) {
-            $Stat = Set-Stat -Name "$($Name)_$($Pool_Currency)_Profit" -Value 0 -Duration $StatSpan -ChangeDetection $false -Difficulty $Pool_Request.$_.difficulty -Quiet
-        }
+    if ($ok -and -not $InfoOnly) {
+        $Stat = Set-Stat -Name "$($Name)_$($Pool_Currency)_Profit" -Value 0 -Duration $StatSpan -ChangeDetection $false -Difficulty $Pool_Request.$_.difficulty -Quiet
     }
 
     if ($ok -or $InfoOnly) {
-        [PSCustomObject]@{
-            Algorithm     = $Pool_Algorithm_Norm
-            Algorithm0    = $Pool_Algorithm_Norm
-            CoinName      = $Pool_CoinName
-            CoinSymbol    = $Pool_Currency
-            Currency      = $Pool_Currency
-            Price         = 0
-            StablePrice   = 0
-            MarginOfError = 0
-            Protocol      = "stratum+$(if ($_ -eq "beam") {"ssl"} else {"tcp"})"
-            Host          = $Pool_Host
-            Port          = $Pool_Port
-            User          = $Pool_User
-            Pass          = $Pool_Pass
-            Region        = "US"
-            SSL           = $_ -eq "beam"
-            Updated       = (Get-Date).ToUniversalTime()
-            PoolFee       = $Pool_PoolFee
-            Workers       = $null
-            Hashrate      = $null
-            BLK           = $null
-            TSL           = $null
-            Difficulty    = $Stat.Diff_Average
-            SoloMining    = $true
-            WTM           = $true
-            EthMode       = $Pool_EthProxy
-            Name          = $Name
-            Penalty       = 0
-            PenaltyFactor = 1
-            Disabled      = $false
-            HasMinerExclusions = $false
-            Price_0       = 0.0
-            Price_Bias    = 0.0
-            Price_Unbias  = 0.0
-            Wallet        = $Pool_Wallet
-            Worker        = "{workername:$Worker}"
-            Email         = $Email
+        foreach($Pool_Host in $Pool_Hosts) {
+            [PSCustomObject]@{
+                Algorithm     = $Pool_Algorithm_Norm
+                Algorithm0    = $Pool_Algorithm_Norm
+                CoinName      = $Pool_CoinName
+                CoinSymbol    = $Pool_Currency
+                Currency      = $Pool_Currency
+                Price         = 0
+                StablePrice   = 0
+                MarginOfError = 0
+                Protocol      = "stratum+tcp"
+                Host          = $Pool_Host.host
+                Port          = $Pool_Host.port
+                User          = $Pool_User
+                Pass          = $Pool_Pass
+                Region        = $Pool_RegionsTable[$Pool_Host.region]
+                SSL           = $false
+                Updated       = (Get-Date).ToUniversalTime()
+                PoolFee       = $Pool_PoolFee
+                Workers       = $null
+                Hashrate      = $null
+                BLK           = $null
+                TSL           = $null
+                Difficulty    = $Stat.Diff_Average
+                SoloMining    = $true
+                WTM           = $true
+                EthMode       = $Pool_EthProxy
+                Name          = $Name
+                Penalty       = 0
+                PenaltyFactor = 1
+                Disabled      = $false
+                HasMinerExclusions = $false
+                Price_0       = 0.0
+                Price_Bias    = 0.0
+                Price_Unbias  = 0.0
+                Wallet        = $Pool_Wallet
+                Worker        = "{workername:$Worker}"
+                Email         = $Email
+            }
         }
     }
 }
