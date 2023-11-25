ab0cdbb47eec98eb04055e7f71a5452d0554ccba
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-11-25 16:46:48 +0100

    Update miner Wildrig to v0.39.4 (amd,intel,nvidia)
    - improved memehash, skydoge and other x-like algorithms on NVIDIA Pascal and Turing gpu's
    - slightly improved rwahash on RDNA3
    - fixed rejects on some pools like mining4people that sends same job with changed diff
    - fixed Polaris and Vega support on Linux for x-like algorithms
    - fixed --zil-another-miner on Linux

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -12,14 +12,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 0.75
 $Cuda = "8.0"
-$Version = "0.39.2"
+$Version = "0.39.4"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-WildRig\wildrig-multi"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.39.2-wildrigmulti/wildrig-multi-linux-0.39.2.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.39.4-wildrigmulti/wildrig-multi-linux-0.39.4.tar.xz"
 } else {
     $Path = ".\Bin\GPU-WildRig\wildrig.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.39.2-wildrigmulti/wildrig-multi-windows-0.39.2.7z"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.39.4-wildrigmulti/wildrig-multi-windows-0.39.4.7z"
 }
 
 $Commands = [PSCustomObject[]]@(
