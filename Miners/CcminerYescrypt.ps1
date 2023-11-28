34c00bc04307599f9a33c45ab79e4dfab2496699
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2023-11-28 12:32:05 +0100

    Update miner CcminerYescrypt
    - remove GPU arch lock (issue #2543)

--- Miners/CcminerYescrypt.ps1
+++ Miners/CcminerYescrypt.ps1
@@ -70,7 +70,7 @@ if (-not $Cuda) {return}
 $Global:DeviceCache.DevicesByTypes.NVIDIA | Select-Object Vendor, Model -Unique | ForEach-Object {
     $First = $true
     $Miner_Model = $_.Model
-    $Miner_Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)".Where({$_.Model -eq $Miner_Model -and ($_.OpenCL.Architecture -in @("Other","Pascal","Turing"))})
+    $Miner_Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)".Where({$_.Model -eq $Miner_Model})
 
     $Commands.ForEach({
 
