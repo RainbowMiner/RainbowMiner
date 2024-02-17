c72a9f7cc412727fcac7ee3afa62a2bf4d03b80f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2024-02-17 15:54:45 +0100

    Update miner Teamred to v0.10.18 (amd)
    - GPU: Added TON/GRAM for RDNA3 gpus.
    - GPU: Fixed TON/GRAM dual mining with abel and autolykos2.
    - GPU: Fixed broken kawpow on RDNA3 gpus.
    - GPU: Fixed loading Radeon VII kernels in general on newer drivers.
    - GPU: Fixed broken abel+ironfish dual mining.
    - GPU: Fixed old bug for ironfish mining on pools like Kryptex using email as wallet/username.
    - GPU: Fixed broken power read for some gpus on newer linux drivers.
    - GPU: Fixed broken kawpow mining on vultaic.
    - GPU: Patched broken job messages for new TON/GRAM pools.
    - GPU: Added --debug logging of network traffic for out-of-band stats for ABEL pools.

--- Miners/Teamred.ps1
+++ Miners/Teamred.ps1
@@ -11,15 +11,15 @@ if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $InfoOnly) {return} # 
 $Port = "409{0:d2}"
 $ManualUri = "https://bitcointalk.org/index.php?topic=5059817.0"
 $DevFee = 3.0
-$Version = "0.10.17"
+$Version = "0.10.18"
 
 if ($IsLinux) {
     $Path = ".\Bin\AMD-Teamred\teamredminer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.10.17-teamred/teamredminer-v0.10.17-linux.tgz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.10.18-teamred/teamredminer-v0.10.18-linux.tgz"
     $DatFile = "$env:HOME/.vertcoin/verthash.dat"
 } else {
     $Path = ".\Bin\AMD-Teamred\teamredminer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.10.17-teamred/teamredminer-v0.10.17-win.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.10.18-teamred/teamredminer-v0.10.18-win.zip"
     $DatFile = "$env:APPDATA\Vertcoin\verthash.dat"
 }
 
