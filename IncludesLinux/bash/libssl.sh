5d7c8784985e624ed21ffae085b27c91060c6033
RetroMike <michaeldlesk@gmail.com>
2024-03-24 11:30:45 -0500

    Update libssl.sh

--- IncludesLinux/bash/libssl.sh
+++ IncludesLinux/bash/libssl.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 arch=$(lscpu | grep Architecture | awk {'print $2'});
-if [ "${arch}" == "aarch64" ]; then
-  wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-  sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-  rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-fi
+
+if [ "${arch}" == "aarch64"] && [  -n "$(uname -a | grep Ubuntu)" ]; then
+    wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+    sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+    rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+fi 
