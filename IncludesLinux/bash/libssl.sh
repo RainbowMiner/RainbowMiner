b9a7250e902443070ea0ddd73bdd0d450ca3f91f
RetroMike <michaeldlesk@gmail.com>
2024-03-24 14:27:29 -0500

    Update libssl.sh

--- IncludesLinux/bash/libssl.sh
+++ IncludesLinux/bash/libssl.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 arch=$(lscpu | grep Architecture | awk {'print $2'});
-osname=$(./etc/os-release && echo $NAME)
+osname=$(. /etc/os-release && echo $NAME)
 
 if [ "${arch}" == "aarch64"] && [ "${osname} == "Ubuntu" ]; then
     wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
