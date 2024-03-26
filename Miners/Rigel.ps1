428d5a7cd4ea3db1d9713eb57d8711c779375c14
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2024-03-26 15:22:31 +0100

    Update miner Rigel to v1.15.1 (nvidia)
    - (IRON) Add ironfish -> fishhash auto-switch, see this guide on how to set it up
    - (RVN) Report hashrate to mining pools

--- Miners/Rigel.ps1
+++ Miners/Rigel.ps1
@@ -11,13 +11,13 @@ if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return}
 $ManualUri = "https://github.com/rigelminer/rigel/releases"
 $Port = "324{0:d2}"
 $DevFee = 0.7
-$Version = "1.15.0"
+$Version = "1.15.1"
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Rigel\rigel"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.15.0-rigel/rigel-1.15.0-linux.tar.gz"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.15.1-rigel/rigel-1.15.1-linux.tar.gz"
             Cuda = "8.0"
         }
     )
@@ -25,7 +25,7 @@ if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-Rigel\rigel.exe"
     $UriCuda = @(
         [PSCustomObject]@{
-            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.15.0-rigel/rigel-1.15.0-win.zip"
+            Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.15.1-rigel/rigel-1.15.1-win.zip"
             Cuda = "8.0"
         }
     )
