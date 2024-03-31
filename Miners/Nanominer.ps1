ac82df736438f259f3fa447be5bbd4e93e4efcd2
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2024-03-31 17:22:25 +0200

    Update miner Nanominer to v3.9.0 (amd,cpu,intel,nvidia)
    - Supported IronFish on Nvidia and AMD GPUs: new FishHash algorithm and old IronFish algorithm too. Switch to FishHash should occur automatically when hardfork comes;
    - Added support new AMD GPUs on all algorithms;
    - New config examples for EVR due to EVR delisting on rplant;
    - New config examples for ZIL due to Ezil's death.

--- Miners/Nanominer.ps1
+++ Miners/Nanominer.ps1
@@ -12,14 +12,14 @@ $ManualURI = "https://github.com/nanopool/nanominer/releases"
 $Port = "234{0:d2}"
 $Cuda = "10.0"
 $DevFee = 3.0
-$Version = "3.8.12"
+$Version = "3.9.0"
 
 if ($IsLinux) {
     $Path = ".\Bin\ANY-Nanominer\nanominer"
-    $Uri  = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.8.12-nanominer/nanominer-linux-3.8.12.tar.gz"
+    $Uri  = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.9.0-nanominer/nanominer-linux-3.9.0.tar.gz"
 } else {
     $Path = ".\Bin\ANY-Nanominer\nanominer.exe"
-    $Uri  = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.8.12-nanominer/nanominer-windows-3.8.12.zip"
+    $Uri  = "https://github.com/RainbowMiner/miner-binaries/releases/download/v3.9.0-nanominer/nanominer-windows-3.9.0.zip"
 }
 
 $Commands = [PSCustomObject[]]@(
@@ -35,6 +35,7 @@ $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "EtcHash";         DAG = $true; Params = ""; MinMemGb = 3;  Vendor = @("AMD","NVIDIA");                  ExtendInterval = 2; DevFee = 1.0; DualZIL = $true} #EtcHash
     [PSCustomObject]@{MainAlgorithm = "EvrProgPow";      DAG = $true; Params = ""; MinMemGb = 2;  Vendor = @("AMD","INTEL","NVIDIA"); ExtendInterval = 2; DevFee = 2.0; DualZIL = $true} #EvrProgPow/EVR
     [PSCustomObject]@{MainAlgorithm = "FiroPow";         DAG = $true; Params = ""; MinMemGb = 2;  Vendor = @("AMD","INTEL","NVIDIA"); ExtendInterval = 2; DevFee = 1.0; DualZIL = $true; ZombieMode = $true} #FiroPOW
+    [PSCustomObject]@{MainAlgorithm = "fishhash";                     Params = ""; MinMemGb = 2;  Vendor = @("AMD","NVIDIA");         ExtendInterval = 2; DevFee = 1.0} #FishHash
     [PSCustomObject]@{MainAlgorithm = "heavyhash";                    Params = ""; MinMemGb = 2;  Vendor = @("AMD","NVIDIA");         ExtendInterval = 2; DevFee = 1.0} #kHeavyHash/KAS
     [PSCustomObject]@{MainAlgorithm = "karlsenhash";                  Params = ""; MinMemGb = 2;  Vendor = @("AMD","NVIDIA");         ExtendInterval = 2; DevFee = 1.0} #Karlsenhash/KLS
     [PSCustomObject]@{MainAlgorithm = "KawPow";          DAG = $true; Params = ""; MinMemGb = 3;  Vendor = @("AMD","INTEL","NVIDIA"); ExtendInterval = 2; DevFee = 2.0; DualZIL = $true} #KawPOW
