48188ccbbb9efae0895af2ee13ba74adc7a5ff18
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2024-04-03 15:24:37 +0200

    Update miner SrbMinerMulti to v2.5.1 (amd,cpu,intel,nvidia)
    - Added support for AMD BC-250 Mining GPU's
    - DevFee for 'fishhash' algorithm lowered to 0.85%
    - AMD BC-250 tested on MMPOS, works out of the box.
    - On other Linux based systems you might need to add parameter '--bc250'

--- Miners/SrbMinerMulti.ps1
+++ Miners/SrbMinerMulti.ps1
@@ -11,15 +11,15 @@ if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.De
 $ManualUri = "https://bitcointalk.org/index.php?topic=5190081.0"
 $Port = "349{0:d2}"
 $DevFee = 0.85
-$Version = "2.5.0"
+$Version = "2.5.1"
 $Cuda = "11.7"
 
 if ($IsLinux) {
     $Path = ".\Bin\ANY-SRBMinerMulti\SRBMiner-MULTI"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.5.0-srbminermulti/SRBMiner-Multi-2-5-0-Linux.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.5.1-srbminermulti/SRBMiner-Multi-2-5-1-Linux.tar.gz"
 } else {
     $Path = ".\Bin\ANY-SRBMinerMulti\SRBMiner-MULTI.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.5.0-srbminermulti/SRBMiner-Multi-2-5-0-win64.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v2.5.1-srbminermulti/SRBMiner-Multi-2-5-1-win64.zip"
 }
 
 $Commands = [PSCustomObject[]]@(
