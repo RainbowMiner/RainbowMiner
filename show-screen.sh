57365ad5d554ea0dbd8d6b7af0ab8ebe5adaca5a
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2024-06-25 13:54:21 +0200

    Update linux
    - add script `show-screen.sh` for easy screen access to a running RainbowMiner instance

---
+++ show-screen.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+! command -v screen >/dev/null 2>&1 && { echo "screen command not installed!"; exit 1; }
+
+if ! test -f "/opt/rainbowminer/lib"
+then
+        screen_dir="$HOME/.screen"
+        if ! test -d "$screen_dir"
+        then
+                mkdir "$screen_dir"
+                chmod 700 "$screen_dir"
+        fi
+        export SCREENDIR="$screen_dir"
+fi
+
+echo "Note: Press Ctrl+A then D to exit the screen"
+echo "Hit Enter to start the screen now"
+pause
+
+screen -R RainbowMiner
