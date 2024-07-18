1f84c4e4171a350177e34e72cc86b987efbab4a5
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2024-07-18 16:13:46 +0200

    Update miner OneZeroMiner to v1.3.7 (nvidia)
    - Xelis: Minor performance improvement on some cards
    - Zil: Reduce the switching delay with applying the OC settings in parallel

--- Miners/OneZeroMiner.ps1
+++ Miners/OneZeroMiner.ps1
@@ -10,16 +10,16 @@ if (-not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return}
 
 if ($IsLinux) {
     $Path = ".\Bin\NVIDIA-OneZero\onezerominer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.3.6-onezerominer/onezerominer-linux-1.3.6.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.3.7-onezerominer/onezerominer-linux-1.3.7.tar.gz"
 } else {
     $Path = ".\Bin\NVIDIA-OneZero\onezerominer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.3.6-onezerominer/onezerominer-win64-1.3.6.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.3.7-onezerominer/onezerominer-win64-1.3.7.zip"
 }
 $ManualUri = "https://github.com/OneZeroMiner/onezerominer/releases"
 $Port = "370{0:d2}"
 $DevFee = 3.0
 $Cuda = "11.8"
-$Version = "1.3.6"
+$Version = "1.3.7"
 
 $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "dynex"; Params = ""; ExtendInterval = 5; DualZIL = $true} #DynexSolve/DNX
