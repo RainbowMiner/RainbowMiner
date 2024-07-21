8eb1057b85946656ae600167221d4f33ea441031
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2024-07-21 17:13:23 +0200

    Add miner SccMiner v1.1.0 (amd,nvidia)
    - Update DAG Epoch Length to match PoW for stakecube coin
    - Update application executable and naming for scc
    - issue #2824

---
+++ Miners/SccMiner.ps1
@@ -0,0 +1,112 @@
+﻿using module ..\Modules\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+if (-not $IsWindows -and -not $IsLinux) {return}
+if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.DevicesByTypes.NVIDIA -and -not $InfoOnly) {return} # No AMD/NVIDIA present in system
+
+$Port = "374{0:d2}"
+$ManualURI = "https://github.com/stakecube/sccminer/releases"
+$DevFee = 0.0
+$Version = "1.1.0"
+
+if ($IsLinux) {
+    $Path = ".\Bin\GPU-SccMiner\sccminer"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1.0-sccminer/sccminer-1.1.0-linux.zip"
+    $Cuda = "12.0"
+} else {
+    $Path = ".\Bin\GPU-SccMiner\sccminer.exe"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.1.0-sccminer/sccminer-1.1.0-Windows.zip"
+    $Cuda = "12.0"
+}
+
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "sccpow"; Params = ""; ExtendInterval = 2; MinMemGB = 3} #SCCPow
+)
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("AMD","NVIDIA")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+if ($Global:DeviceCache.DevicesByTypes.NVIDIA) {$Cuda = Confirm-Cuda -ActualVersion $Session.Config.CUDAVersion -RequiredVersion $Cuda -Warning $Name}
+
+foreach ($Miner_Vendor in @("AMD","NVIDIA")) {
+	$Global:DeviceCache.DevicesByTypes.$Miner_Vendor | Where-Object Type -eq "GPU" | Where-Object {$_.Vendor -ne "NVIDIA" -or $Cuda} | Select-Object Vendor, Model -Unique | ForEach-Object {
+        $First = $true
+        $Miner_Model = $_.Model
+        $Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)".Where({$_.Model -eq $Miner_Model})
+
+		switch($_.Vendor) {
+			"NVIDIA" {$Miner_Deviceparams = "--cuda --cu-devices"}
+			"AMD" {$Miner_Deviceparams = "--opencl --cl-devices"}
+			Default {$Miner_Deviceparams = ""}
+		}
+
+        $Commands.ForEach({
+
+            $Algorithm_Norm_0 = Get-Algorithm $_.MainAlgorithm
+
+            $MinMemGB = Get-EthDAGSize -CoinSymbol "SCC" -Algorithm $Algorithm_Norm_0 -Minimum $_.MinMemGb
+
+            $Miner_Device = $Device.Where({Test-VRAM $_ $MinMemGB})
+
+		    foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)","$($Algorithm_Norm_0)-GPU")) {
+			    if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+                    if ($First) {
+                        $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+                        $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+                        $DeviceIDsAll = $Miner_Device.Type_Vendor_Index -join ' '
+                        $First = $false
+                    }
+
+                    $Miner_Protocol = Switch ($Pools.$Algorithm_Norm.EthMode) {
+                        "stratum"          {"stratum+$(if ($Pools.$Algorithm_Norm.SSL) {"ssl"} else {"tcp"})"}
+                        "ethproxy"         {"stratum1+$(if ($Pools.$Algorithm_Norm.SSL) {"ssl"} else {"tcp"})"}
+					    "ethstratumnh"     {"stratum2+$(if ($Pools.$Algorithm_Norm.SSL) {"ssl"} else {"tcp"})"}
+					    default            {"stratum$(if ($Pools.$Algorithm_Norm.SSL) {"s"})"}
+				    }
+
+				    $Pool_Port = if ($Pools.$Algorithm_Norm.Ports -ne $null -and $Pools.$Algorithm_Norm.Ports.GPU) {$Pools.$Algorithm_Norm.Ports.GPU} else {$Pools.$Algorithm_Norm.Port}
+				    [PSCustomObject]@{
+					    Name           = $Miner_Name
+					    DeviceName     = $Miner_Device.Name
+					    DeviceModel    = $Miner_Model
+					    Path           = $Path
+					    Arguments      = "--api-port -`$mport -P $($Miner_Protocol)://$(Get-UrlEncode $Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {":$(Get-UrlEncode $Pools.$Algorithm_Norm.Pass)"})@$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) $($Miner_Deviceparams) $($DeviceIDsAll) $($_.Params)"
+					    HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Week }
+					    API            = "Claymore"
+					    Port           = $Miner_Port
+					    Uri            = $Uri
+					    FaultTolerance = $_.FaultTolerance
+					    ExtendInterval = $_.ExtendInterval
+                        Penalty        = 0
+					    DevFee         = $DevFee
+					    ManualUri      = $ManualUri
+                        Version        = $Version
+                        PowerDraw      = 0
+                        BaseName       = $Name
+                        BaseAlgorithm  = $Algorithm_Norm_0
+                        Benchmarked    = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Benchmarked
+                        LogFile        = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".LogFile
+				    }
+			    }
+		    }
+        })
+    }
+}
