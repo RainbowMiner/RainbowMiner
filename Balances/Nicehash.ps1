b4b0596af172a07939c1e9699469b7265a8c1786
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2024-12-01 12:30:07 +0100

    Update pool Nicehash
    - disable external wallet mining
    - move to different stats API

--- Balances/Nicehash.ps1
+++ Balances/Nicehash.ps1
@@ -14,8 +14,6 @@ if (-not $PoolConfig.BTC) {
 
 $Request = [PSCustomObject]@{}
 
-if (-not (Test-Path "Variable:Global:NHWallets")) {$Global:NHWallets = [hashtable]@{}}
-
 if ($PoolConfig.API_Key -and $PoolConfig.API_Secret -and $PoolConfig.OrganizationID) {
 
     try {
@@ -36,8 +34,6 @@ if ($PoolConfig.API_Key -and $PoolConfig.API_Secret -and $PoolConfig.Organizatio
 
     if ($Request.active) {
 
-        $Global:NHWallets[$PoolConfig.BTC] = $false
-
         [PSCustomObject]@{
             Caption     = "$($Name) ($($Request.currency))"
             BaseName    = $Name
@@ -49,29 +45,4 @@ if ($PoolConfig.API_Key -and $PoolConfig.API_Secret -and $PoolConfig.Organizatio
             LastUpdated = (Get-Date).ToUniversalTime()
         }        
     }
-}
-
-if (-not $Request.active) {
-
-    try {
-        $Request = Invoke-RestMethodAsync "https://api2.nicehash.com/main/api/v2/mining/external/$($PoolConfig.BTC)/rigs2/" -cycletime ($Config.BalanceUpdateMinutes*60) -tag $Name
-        $Global:NHWallets[$PoolConfig.BTC] = $Request.externalAddress
-    }
-    catch {
-        if ($Error.Count){$Error.RemoveAt(0)}
-        Write-Log -Level Warn "Pool Mining API ($Name) has failed. "
-        return
-    }
-
-    [PSCustomObject]@{
-        Caption     = "$($Name) (BTC)"
-        BaseName    = $Name
-        Currency    = "BTC"
-        Balance     = [Decimal]$Request.externalBalance
-        Pending     = [Decimal]$Request.unpaidAmount
-        Total       = [Decimal]$Request.externalBalance + [Decimal]$Request.unpaidAmount
-        Payouts     = @()
-        LastUpdated = (Get-Date).ToUniversalTime()
-    }
-
 }
--- Pools/NiceHash.ps1
+++ Pools/NiceHash.ps1
@@ -18,28 +18,11 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 $Pool_Request = [PSCustomObject]@{}
 $Pool_MiningRequest = [PSCustomObject]@{}
 
-if (-not (Test-Path "Variable:Global:NHWallets")) {$Global:NHWallets = [hashtable]@{}}
-
-if (-not $InfoOnly) {
-    if (-not $Wallets.BTC) {return}
-
-    if (-not $Global:NHWallets.ContainsKey($Wallets.BTC)) {
-        $Request = [PSCustomObject]@{}
-        try {
-            $Request = Invoke-GetUrl "https://api2.nicehash.com/main/api/v2/mining/external/$($Wallets.BTC)/rigs2/"
-            $Global:NHWallets[$Wallets.BTC] = $Request.externalAddress
-        }
-        catch {
-            if ($Error.Count){$Error.RemoveAt(0)}
-            Write-Log -Level Info "Pool Mining API ($Name) has failed. "
-        }
-    }
-}
-
 $Pool_Wallet = $Wallets.BTC
 
 try {
-    $Pool_Request = Invoke-RestMethodAsync "https://api2.nicehash.com/main/api/v2/public/simplemultialgo/info/" -tag $Name -timeout 20
+    $Pool_Request = Invoke-RestMethodAsync "https://api2.nicehash.com/main/api/v2/public/stats/global/current" -tag $Name -timeout 20
+    #$Pool_Request = Invoke-RestMethodAsync "https://api2.nicehash.com/main/api/v2/public/simplemultialgo/info" -tag $Name -timeout 20
     $Pool_MiningRequest = Invoke-RestMethodAsync "https://api2.nicehash.com/main/api/v2/mining/algorithms/" -tag $Name -cycle 3600 -timeout 20
 }
 catch {
@@ -55,14 +38,17 @@ if (($Pool_Request.miningAlgorithms | Measure-Object).Count -le 10 -or ($Pool_Mi
 
 [hashtable]$Pool_Algorithms = @{}
 
-$Pool_PoolFee = if (-not $InfoOnly -and $Global:NHWallets[$Pool_Wallet]) {5.0} else {2.0}
+$Pool_PoolFee = 2.0
 
 $Grin29_Algorithm = (Get-Coin "GRIN").algo
 
-$Pool_Request.miningAlgorithms | Where-Object {([Double]$_.paying -gt 0.00 -and [Double]$_.speed -gt 0) -or $InfoOnly} | ForEach-Object {
-    $Pool_Algorithm = $_.algorithm
-    $Pool_Data      = $Pool_MiningRequest.miningAlgorithms | Where-Object {$_.Enabled -and $_.algorithm -eq $Pool_Algorithm}
-    $Pool_Algorithm = $Pool_Algorithm.ToLower()
+$Pool_Request.algos | Where-Object {([Double]$_.p -gt 0.00 -and [Double]$_.s -gt 0 -and [int]$_.o -gt 0) -or $InfoOnly} | ForEach-Object {
+    $Pool_Algo_Id   = $_.a
+    $Pool_Data      = $Pool_MiningRequest.miningAlgorithms | Where-Object {$_.Enabled -and $_.order -eq $Pool_Algo_Id}
+
+    if (-not $Pool_Data) {return}
+
+    $Pool_Algorithm = $Pool_Data.algorithm.ToLower()
     $Pool_Host      = "$($Pool_Algorithm).auto.nicehash.com"
 
     if (-not $Pool_Algorithms.ContainsKey($Pool_Algorithm)) {$Pool_Algorithms.$Pool_Algorithm = Get-Algorithm $Pool_Algorithm}
@@ -97,7 +83,7 @@ $Pool_Request.miningAlgorithms | Where-Object {([Double]$_.paying -gt 0.00 -and 
     $Pool_IsEthash = $Pool_Algorithm_Norm -match "^Etc?hash"
 
     if (-not $InfoOnly) {
-        $Stat = Set-Stat -Name "$($Name)_$($Pool_Algorithm_Norm)_Profit" -Value ([Double]$_.paying / 1e8) -Duration $StatSpan -ChangeDetection $true -Quiet
+        $Stat = Set-Stat -Name "$($Name)_$($Pool_Algorithm_Norm)_Profit" -Value ([Double]$_.p / 1e8) -Duration $StatSpan -HashRate ([Double]$_.s) -ChangeDetection $true -Quiet
     }
 
     if ($Pool_Wallet -or $InfoOnly) {
@@ -128,6 +114,10 @@ $Pool_Request.miningAlgorithms | Where-Object {([Double]$_.paying -gt 0.00 -and 
                 SSL           = $Pool_SSL
                 Updated       = $Stat.Updated
                 PoolFee       = $Pool_PoolFee
+                Workers       = $null
+                Hashrate      = $Stat.HashRate_Live
+                TSL           = $null
+                BLK           = $null
                 PaysLive      = $true
                 EthMode       = $Pool_EthProxy
                 Name          = $Name
@@ -161,6 +151,10 @@ $Pool_Request.miningAlgorithms | Where-Object {([Double]$_.paying -gt 0.00 -and 
                     SSL           = $Pool_SSL
                     Updated       = $Stat.Updated
                     PoolFee       = $Pool_PoolFee
+                    Workers       = $null
+                    Hashrate      = $Stat.HashRate_Live
+                    TSL           = $null
+                    BLK           = $null
                     PaysLive      = $true
                     EthMode       = $Pool_EthProxy
                     Name          = $Name
