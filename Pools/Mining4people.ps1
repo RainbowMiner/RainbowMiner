8168b49452bedcb5c6efec40030ceef43d8bd753
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2024-12-01 12:28:49 +0100

    Update pool Mining4people
    - always call the calcapi

--- Pools/Mining4people.ps1
+++ Pools/Mining4people.ps1
@@ -56,27 +56,27 @@ $Pool_Request | Where-Object {$_.feeType -eq "PPLNSBF" -and ($Wallets."$($_.coin
     $Pool_EthProxy = if ($Pool_Algorithm_Norm -match $Global:RegexAlgoHasEthproxy) {"ethstratumnh"} elseif ($Pool_Algorithm_Norm -match $Global:RegexAlgoIsProgPow) {"stratum"} else {$null}
     $Pool_Ports = @(0,0)
         
+    $Pool_CoinRequest = [PSCustomObject]@{}
+    try {
+        $Pool_CoinRequest = Invoke-RestMethodAsync "https://mining4people.com/calcapi/pools/$($_.id)" -retry 3 -retrywait 500 -tag $Name -cycletime 120 -delay 200
+    }
+    catch {
+        if ($Error.Count){$Error.RemoveAt(0)}
+        Write-Log -Level Warn "Pool coin API ($Name) has failed for $($Pool_Currency) "
+        return
+    }
+
+    $Pool_CoinRequest = $Pool_CoinRequest.pool | Select-Object -First 1
+
+    $Pool_CoinRequest.ports.PSObject.Properties | Sort-Object {[double]$_.Value.difficulty},{[int]$_.Name},{[bool]$_.Value.tls} | Foreach-Object {
+        if ([bool]$_.Value.tls) {
+            if (-not $Pool_Ports[1]) {$Pool_Ports[1] = $_.Name}
+        } else {
+            if (-not $Pool_Ports[0]) {$Pool_Ports[0] = $_.Name}
+        }
+    }
+
     if (-not $InfoOnly) {
-        $Pool_CoinRequest = [PSCustomObject]@{}
-        try {
-            $Pool_CoinRequest = Invoke-RestMethodAsync "https://mining4people.com/calcapi/pools/$($_.id)" -retry 3 -retrywait 500 -tag $Name -cycletime 120 -delay 200
-        }
-        catch {
-            if ($Error.Count){$Error.RemoveAt(0)}
-            Write-Log -Level Warn "Pool coin API ($Name) has failed for $($Pool_Currency) "
-            return
-        }
-
-        $Pool_CoinRequest = $Pool_CoinRequest.pool | Select-Object -First 1
-
-        $Pool_CoinRequest.ports.PSObject.Properties | Sort-Object {[double]$_.Value.difficulty},{[int]$_.Name},{[bool]$_.Value.tls} | Foreach-Object {
-            if ([bool]$_.Value.tls) {
-                if (-not $Pool_Ports[1]) {$Pool_Ports[1] = $_.Name}
-            } else {
-                if (-not $Pool_Ports[0]) {$Pool_Ports[0] = $_.Name}
-            }
-        }
-
         $hashrate      = $_.poolHashrate
         $avgTime       = if ($_.poolHashrate -gt 0) {$Pool_CoinRequest.networkStats.networkDifficulty * [Math]::Pow(2,32) / $hashrate} else {0}
         $Pool_BLK      = [int]$(if ($avgTime -gt 0) {86400/$avgTime})
