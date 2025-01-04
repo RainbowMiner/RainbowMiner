e3ce3cc7c90e63c0e1db0cbd4f51d17b9397b762
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2025-01-04 16:05:54 +0100

    Update miner CpuminerJayddee to v25.1 (cpu)
    - MacOS ARM64: m7m algo is now working.
    - MacOS x86_64: is now working compiled with GCC.
    - Fixed some minor bugs & removed some obsolete code.
    - v24.8 ARM: Apple MacOS on M series CPU is now supported compiled from source code, see Wiki for details.
    - v24.8 ARM: Fix incorrect compiler version display when using clang.
    - v24.8 build.sh can now be used to compile all targets, arm_build.sh & build_msys2.sh have been removed.
    - v24.8 Windows: MSys2 build now enables CPU groups by default, prebuilt binaries continue to be compiled with CPU groups disabled.
    - v24.7 ARM: compile works for Windows using MSys2 & MingW, see wiki for details.

--- Miners/CpuminerJayddee.ps1
+++ Miners/CpuminerJayddee.ps1
@@ -11,7 +11,7 @@ if (-not $Global:DeviceCache.DevicesByTypes.CPU -and -not $InfoOnly) {return} # 
 $ManualUri = "https://github.com/JayDDee/cpuminer-opt/releases"
 $Port = "200{0:d2}"
 $DevFee = 0.0
-$Version = "24.6"
+$Version = "25.1"
 
 $Path = $null
 
@@ -19,18 +19,18 @@ if ($IsLinux) {
     if ($Global:GlobalCPUInfo.Vendor -eq "ARM" -or $Global:GlobalCPUInfo.Features.ARM) {
         if ($Global:GlobalCPUInfo.Architecture -eq 8) {
             $Path = ".\Bin\CPU-JayDDee\cpuminer-armv8$($f=$Global:GlobalCPUInfo.Features;$(if($f.sha3 -and $f.sve2 -and $f.aes){'.5-crypto-sha3-sve2'}elseif($f.sha3 -and $f.aes){'.4-crypto-sha3'}elseif($f.sha2 -and $f.aes){'-crypto'}))"
-            $Uri  = "https://github.com/RainbowMiner/miner-binaries/releases/download/v24.6-jayddee/cpuminer-opt-24.6-arm.7z"
+            $Uri  = "https://github.com/RainbowMiner/miner-binaries/releases/download/v25.1-jayddee/cpuminer-opt-25.1-arm.7z"
         } elseif ($Global:GlobalCPUInfo.Architecture -eq 9) {
             $Path = ".\Bin\CPU-JayDDee\cpuminer-armv9$($f=$Global:GlobalCPUInfo.Features;$(if($f.sha3 -and $f.aes){'-crypto-sha3'}elseif($f.sha2 -and $f.aes){'-crypto'}))"
-            $Uri  = "https://github.com/RainbowMiner/miner-binaries/releases/download/v24.6-jayddee/cpuminer-opt-24.6-arm.7z"
+            $Uri  = "https://github.com/RainbowMiner/miner-binaries/releases/download/v25.1-jayddee/cpuminer-opt-25.1-arm.7z"
         }
     } else {
         $Path = ".\Bin\CPU-JayDDee\cpuminer-$($f=$Global:GlobalCPUInfo.Features;$v=$Global:GlobalCPUInfo.Vendor;$(if($f.avx512 -and $f.sha -and $f.vaes) {if ($v -eq "AMD"){"zen4"}else{'avx512-sha-vaes'}}elseif($f.avx512){'avx512'}elseif($f.avx2 -and $f.sha -and $f.vaes) {if ($v -eq "AMD"){"zen3"}else{"avx2-sha-vaes"}}elseif($f.avx2 -and $f.sha -and $f.aes){'avx2-sha'}elseif($f.avx2 -and $f.aes){'avx2'}elseif($f.avx -and $f.aes){'avx'}elseif($f.sse42 -and $f.aes){'aes-sse42'}elseif($f.sse42){'sse42'}elseif($f.ssse3){"ssse3"}else{'sse2'}))"
-        $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v24.6-jayddee/cpuminer-opt-24.6-linux.7z"
+        $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v25.1-jayddee/cpuminer-opt-25.1-linux.7z"
     }
 } else {
     $Path = ".\Bin\CPU-JayDDee\cpuminer-$($f=$Global:GlobalCPUInfo.Features;$(if($f.avx512 -and $f.sha -and $f.vaes){'avx512-sha-vaes'}elseif($f.avx512){'avx512'}elseif($f.avx2 -and $f.sha -and $f.vaes){'avx2-sha-vaes'}elseif($f.avx2 -and $f.sha -and $f.aes){'avx2-sha'}elseif($f.avx2 -and $f.aes){'avx2'}elseif($f.avx -and $f.aes){'avx'}elseif($f.sse42 -and $f.aes){'aes-sse42'}else{'sse2'})).exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v24.6-jayddee/cpuminer-opt-24.6-windows.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v25.1-jayddee/cpuminer-opt-25.1-windows.zip"
 }
 
 if ($Path -eq $null) {return}
@@ -51,7 +51,7 @@ $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "jha"; Params = ""} #JHA
     [PSCustomObject]@{MainAlgorithm = "lyra2rev3"; Params = ""} #Lyra2v3
     [PSCustomObject]@{MainAlgorithm = "lyra2z330"; Params = ""} #lyra2z330, CpuminerRplant faster
-    #[PSCustomObject]@{MainAlgorithm = "m7m"; Params = ""} #m7m, (CpuminerRKZ faster)
+    [PSCustomObject]@{MainAlgorithm = "m7m"; Params = ""} #m7m, (CpuminerRKZ faster)
     [PSCustomObject]@{MainAlgorithm = "minotaur"; Params = ""} #Minotaur/RNG
     [PSCustomObject]@{MainAlgorithm = "minotaurx"; Params = ""} #MinotaurX/AVN
     [PSCustomObject]@{MainAlgorithm = "power2b"; Params = "--param-n 2048 --param-r 32 --param-key `"Now I am become Death, the destroyer of worlds`""; Algorithm = "yespower-b2b"} #power2b
