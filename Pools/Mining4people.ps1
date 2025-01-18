00794618ff08e0ad637a074d0e3bf706b84a0e10
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2025-01-18 16:46:08 +0100

    Update pool Mining4people
    - remove "fi" stratum region

--- Pools/Mining4people.ps1
+++ Pools/Mining4people.ps1
@@ -33,7 +33,7 @@ if (-not $Pool_Request -or ($Pool_Request | Measure-Object).Count -le 5) {
 
 [hashtable]$Pool_RegionsTable = @{}
 
-$Pool_Regions = @("au","br","eu","fi","in","jp","us-west","us-east","us-cent")
+$Pool_Regions = @("au","br","eu","in","jp","us-west","us-east","us-cent")
 $Pool_Regions | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
 
 $Pool_Fee = 1
