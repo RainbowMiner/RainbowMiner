e83a6c1b1cc88f6e579b1bbb3aeb27fed0025df2
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2025-01-31 17:38:23 +0100

    Update miner Wildrig to v0.42.2 (amd,intel,nvidia)
    - fixed NVIDIA Blackwell support

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -13,14 +13,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 0.75
 $Cuda = "11.0"
-$Version = "0.42.1"
+$Version = "0.42.2"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-WildRig\wildrig-multi"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.42.1-wildrigmulti/wildrig-multi-linux-0.42.1.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.42.2-wildrigmulti/wildrig-multi-linux-0.42.2.tar.xz"
 } else {
     $Path = ".\Bin\GPU-WildRig\wildrig.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.42.1-wildrigmulti/wildrig-multi-windows-0.42.1.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.42.2-wildrigmulti/wildrig-multi-windows-0.42.2.zip"
 }
 
 #removed aergo, bcd, blake2b-btcc, blake2b-glt, blake3, dedal, glt family algorithms, lyra2tdc, lyra2v3, lyra2vc0ban, phi5, rwahash, x7, xevan
