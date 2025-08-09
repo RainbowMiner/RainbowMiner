e19b8ba135f0d1a40b7421ef68c4a00025af0133
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2025-08-09 16:50:33 +0200

    Update miner Wildrig to v0.43.8 (amd,intel,nvidia)
    - improved qhash for AMD, Intel and NVIDA(4000/5000 series only)
    - improved auto-tune for qhash and added parameter --qhash-blocks
    
    NOTE:
    - on weak gpu's auto-tuning may take few minutes
    - CPU load can be still high for NVIDIA gpu's

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -14,14 +14,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 0.00
 $Cuda = "11.0"
-$Version = "0.43.7"
+$Version = "0.43.8"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-WildRig\wildrig-multi"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.43.7-wildrigmulti/wildrig-multi-linux-0.43.7.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.43.8-wildrigmulti/wildrig-multi-linux-0.43.8.tar.xz"
 } else {
     $Path = ".\Bin\GPU-WildRig\wildrig.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.43.7-wildrigmulti/wildrig-multi-windows-0.43.7.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.43.8-wildrigmulti/wildrig-multi-windows-0.43.8.zip"
 }
 
 $Commands = [PSCustomObject[]]@(
