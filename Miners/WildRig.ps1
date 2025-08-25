d65092d6dab8bd9ab4174378e6479d9cec8334e7
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2025-08-25 10:52:34 +0200

    Update miner Wildrig to v0.44.5 (amd,intel,nvidia)
    - up to 20% qhash speed improvements for AMD and NVIDIA gpu's
    - added --qhash-kernel 2 for CMP50HX and other Volta/Turings
    - fixed support AMD RX4x0/RX5x0/Vega gpu's(but Vega still better to use on Linux with ROCm drivers)

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -14,14 +14,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 0.00
 $Cuda = "11.0"
-$Version = "0.44.4"
+$Version = "0.44.5"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-WildRig\wildrig-multi"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.44.4-wildrigmulti/wildrig-multi-linux-0.44.4.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.44.5-wildrigmulti/wildrig-multi-linux-0.44.5.tar.xz"
 } else {
     $Path = ".\Bin\GPU-WildRig\wildrig.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.44.4-wildrigmulti/wildrig-multi-windows-0.44.4.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.44.5-wildrigmulti/wildrig-multi-windows-0.44.5.zip"
 }
 
 $Commands = [PSCustomObject[]]@(
@@ -142,7 +142,7 @@ foreach ($Miner_Vendor in @("AMD","INTEL","NVIDIA")) {
                         $NvCompute = $_.ExcludeNvCompute
                         $Miner_Device = $Miner_Device | Where-Object {$_.OpenCL.Architecture -notmatch $NvCompute}
                     }
-                    if ($_.MainAlgorithm -eq "qhash" -and $Miner_Device.OpenCL.Architecture -eq "Ampere" -and $Miner_Device.Model_Base -eq "CMP170HX") {
+                    if ($_.MainAlgorithm -eq "qhash" -and $Miner_Device.OpenCL.Architecture -in @("Volta","Turing","Ampere") -and $Miner_Device.Model_Base -match "^CMP") {
                         $QhashParams = " --qhash-kernel 2"
                     }
                 } elseif ($Miner_Vendor -eq "AMD") {
