389b6dcf9287cd7e4ea537949e55ced9fb068ae4
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2025-09-08 15:35:00 +0200

    Update miner Wildrig to v0.44.8 (amd,intel,nvidia)
    - further speed bump for qhash across all gpu's
    - improved rejects ratio for qhash
    - fixed AMD Vega kernels for qhash on Linux
    - fixed some connection issues and crash on start
    - fixed a bug that potentially can harm pool hashrate(qhash)

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -14,14 +14,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 0.00
 $Cuda = "11.0"
-$Version = "0.44.6"
+$Version = "0.44.8"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-WildRig\wildrig-multi"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.44.6-wildrigmulti/wildrig-multi-linux-0.44.6.tar.xz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.44.8-wildrigmulti/wildrig-multi-linux-0.44.8.tar.xz"
 } else {
     $Path = ".\Bin\GPU-WildRig\wildrig.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.44.6-wildrigmulti/wildrig-multi-windows-0.44.6.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.44.8-wildrigmulti/wildrig-multi-windows-0.44.8.zip"
 }
 
 $Commands = [PSCustomObject[]]@(
