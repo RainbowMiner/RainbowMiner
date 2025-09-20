fc86e37140f099556097a955d0d169c2290bffa4
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2025-09-20 22:21:37 +0200

    Update miner OneZeroMiner to v1.6.0 (amd,nvidia)
    - Performance improvement for Qubitcoin (~100%)
    
    Note:
    The rejected shares of this version is higher than the previous one. It should settle at around 5% over time but could have higher peaks.

--- Miners/OneZeroMiner.ps1
+++ Miners/OneZeroMiner.ps1
@@ -12,17 +12,17 @@ if (-not $Global:DeviceCache.DevicesByTypes.AMD -and -not $Global:DeviceCache.De
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-OneZero\onezerominer"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.5.9-onezerominer/onezerominer-linux-1.5.9.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.6.0-onezerominer/onezerominer-linux-1.6.0.tar.gz"
 } else {
     $Path = ".\Bin\GPU-OneZero\onezerominer.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.5.9-onezerominer/onezerominer-win64-1.5.9.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.6.0-onezerominer/onezerominer-win64-1.6.0.zip"
 }
 
 $ManualUri = "https://github.com/OneZeroMiner/onezerominer/releases"
 $Port = "370{0:d2}"
 $DevFee = 3.0
 $Cuda = "11.8"
-$Version = "1.5.9"
+$Version = "1.6.0"
 
 $Commands = [PSCustomObject[]]@(
     [PSCustomObject]@{MainAlgorithm = "cryptix"; Params = ""; ExtendInterval = 3; Fee = @{NVIDIA=2.0}; Vendor = @("NVIDIA")} #CryptixOX8/CPAY
