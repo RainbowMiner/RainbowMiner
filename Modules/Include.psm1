0887b3b1c1611135ef37bc24165430173bed9e22
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2025-10-12 18:43:30 +0200

    Update core
    - re-add constant declaration

--- Modules/Include.psm1
+++ Modules/Include.psm1
@@ -35,7 +35,7 @@ function Initialize-Session {
         $Session.UnixEpoch          = [DateTime]::new(1970, 1, 1, 0, 0, 0, 0, ([System.DateTimeKind]::Utc))
 
         Set-Variable RegexAlgoHasEthproxy -Option Constant -Scope Global -Value "^Etc?hash|ProgPow|^Meraki|UbqHash"
-        Set-Variable RegexAlgoHasDAGSize -Scope Global -Value "^Etc?hash|^KawPow|ProgPow|^FiroPow|^MeowPow|^Meraki|^NexaPow|^SccPow|UbqHash|Octopus|^Abelian|Autolykos2|^FishHash|^PhiHash"
+        Set-Variable RegexAlgoHasDAGSize -Option Constant -Scope Global -Value "^Etc?hash|^KawPow|ProgPow|^FiroPow|^MeowPow|^Meraki|^NexaPow|^SccPow|UbqHash|Octopus|^Abelian|Autolykos2|^FishHash|^PhiHash"
         Set-Variable RegexAlgoIsEthash -Option Constant -Scope Global -Value "^Etc?hash|UbqHash"
         Set-Variable RegexAlgoIsProgPow -Option Constant -Scope Global -Value "^KawPow|ProgPow|^FiroPow|^MeowPow|^Meraki|^NexaPow|^SccPow|Octopus"
     }
