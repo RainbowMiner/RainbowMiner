15f1882e546a6c9bea72160a0edc1a5b6fd61dbc
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2025-10-27 22:46:06 +0100

    Update miner Wildrig to v0.46.4 (amd,intel,nvidia)
    - significantly increased qhash hashrate for all NVIDIA and AMD Vega/RDNA gpu's
    - improved power efficiency of qhash for all NVIDIA gpu's and AMD Vega/RDNA gpu's

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -14,14 +14,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 0.00
 $Cuda = "11.0"
-$Version = "0.46.2"
+$Version = "0.46.4"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-WildRig\wildrig-multi"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.46.2-wildrigmulti/wildrig-multi-linux-0.46.2.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.46.4-wildrigmulti/wildrig-multi-linux-0.46.4.tar.gz"
 } else {
     $Path = ".\Bin\GPU-WildRig\wildrig.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.46.2-wildrigmulti/wildrig-multi-windows-0.46.2.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.46.4-wildrigmulti/wildrig-multi-windows-0.46.4.zip"
 }
 
 $Commands = [PSCustomObject[]]@(
