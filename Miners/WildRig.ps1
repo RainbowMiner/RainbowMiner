ced37f8158bbd10d7b435b373eb2b527ea895de6
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2025-12-07 16:44:33 +0100

    Update miner Wildrig to v0.47.7 (amd,intel,nvidia)
    - improved speed and power efficiency of qhash for NVIDIA and AMD gpu's
    - fixed support AMD RDNA3 and RDNA4 gpu's for qhash

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -14,14 +14,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 0.00
 $Cuda = "11.0"
-$Version = "0.47.7"
+$Version = "0.47.8"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-WildRig\wildrig-multi"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.47.7-wildrigmulti/wildrig-multi-linux-0.47.7.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.47.8-wildrigmulti/wildrig-multi-linux-0.47.8.tar.gz"
 } else {
     $Path = ".\Bin\GPU-WildRig\wildrig.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.47.7-wildrigmulti/wildrig-multi-windows-0.47.7.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.47.8-wildrigmulti/wildrig-multi-windows-0.47.8.zip"
 }
 
 $Commands = [PSCustomObject[]]@(
