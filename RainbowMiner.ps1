6f5685638568f67d97959784fc30944480cf5464
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2026-01-26 19:38:48 +0100

    Bump version to v5.0.1.2

--- RainbowMiner.ps1
+++ RainbowMiner.ps1
@@ -405,7 +405,7 @@ Import-Module .\Modules\Include.psm1
 
 Initialize-Session
 
-$Session.Version         = "5.0.1.1"
+$Session.Version         = "5.0.1.2"
 $Session.MainWindowTitle = "RainbowMiner v$($Session.Version)"
 $Session.SetupOnly       = $SetupOnly
 $Session.LogLevel        = $LogLevel
