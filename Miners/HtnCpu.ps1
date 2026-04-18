747181522631f806a420f53ae99029b7eeb13d07
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2026-04-18 15:47:51 +0200

    Add miner HtnCpu v1.4.9 (cpu)
    - add miner issue #3198
    - hoo_gpu hoo_cpu release PEPEPOW ONLY
    - Now correctly supports xtranonce2
    - DevFee Stratum is selected from candidates at startup to prevent stalls during strata outage
    - Duplicate Solutions are no longer a thing

---
+++ Miners/HtnCpu.ps1
@@ -0,0 +1,97 @@
+using module ..\Modules\Include.psm1
+
+param(
+    [String]$Name,
+    [PSCustomObject]$Pools,
+    [Bool]$InfoOnly
+)
+
+if (-not $IsLinux) {return}
+if ((-not $Global:DeviceCache.DevicesByTypes.CPU -or -not $Global:GlobalCPUInfo.Features.avx2) -and -not $InfoOnly) {return} # No CPU present in system
+if ($Session.LibCVersion -and $Session.LibCVersion -lt (Get-Version "2.30")) {return}
+
+$ManualUri = "https://htn.foztor.net/"
+$Port = "237{0:d2}"
+$DevFee = 1.5
+$Version = "1.4.9"
+
+if ($Global:GlobalCPUInfo.Vendor -eq "ARM" -or $Global:GlobalCPUInfo.Features.ARM) {
+    if ($Global:GlobalCPUInfo.Architecture -ne 8) {return}
+    $Path = ".\Bin\CPU-Htn\hoo_cpu_arm"
+    $Uri  = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.4.9-htm/hoo_cpu_arm-1.4.9.tar.gz"
+} else {
+    $Path = ".\Bin\CPU-Htn\hoo_cpu"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.4.9-htm/hoo_cpu-1.4.9.tar.gz"
+}
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "hoohashpepew"; MinMemGB = 2; Params = "--pepepow"; Vendor = @("CPU")} #PEPEW/HoohashPepeW
+)
+
+# $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($InfoOnly) {
+    [PSCustomObject]@{
+        Type      = @("CPU")
+        Name      = $Name
+        Path      = $Path
+        Port      = $Miner_Port
+        Uri       = $Uri
+        DevFee    = $DevFee
+        ManualUri = $ManualUri
+        Commands  = $Commands
+    }
+    return
+}
+
+$Global:DeviceCache.DevicesByTypes.CPU | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $First = $true
+    $Miner_Model = $_.Model
+    $Miner_Device = $Global:DeviceCache.DevicesByTypes."$($_.Vendor)" | Where-Object {$_.Model -eq $Miner_Model}
+
+    $Commands | ForEach-Object {
+        $Algorithm_Norm_0 = Get-Algorithm $_.MainAlgorithm
+
+        $CPUThreads = if ($Session.Config.Miners."$Name-CPU-$Algorithm_Norm_0".Threads)  {$Session.Config.Miners."$Name-CPU-$Algorithm_Norm_0".Threads}  elseif ($Session.Config.Miners."$Name-CPU".Threads)  {$Session.Config.Miners."$Name-CPU".Threads}  elseif ($Session.Config.CPUMiningThreads)  {$Session.Config.CPUMiningThreads}
+        $CPUAffinity= if ($Session.Config.Miners."$Name-CPU-$Algorithm_Norm_0".Affinity) {$Session.Config.Miners."$Name-CPU-$Algorithm_Norm_0".Affinity} elseif ($Session.Config.Miners."$Name-CPU".Affinity) {$Session.Config.Miners."$Name-CPU".Affinity} elseif ($Session.Config.CPUMiningAffinity) {$Session.Config.CPUMiningAffinity}
+
+        $DeviceParams = "$(if ($CPUThreads){" -t $CPUThreads"})$(if ($CPUAffinity){" --cpu-affinity $CPUAffinity"})"
+
+		foreach($Algorithm_Norm in @($Algorithm_Norm_0,"$($Algorithm_Norm_0)-$($Miner_Model)")) {
+			if ($Pools.$Algorithm_Norm.Host -and $Miner_Device -and (-not $_.ExcludePoolName -or $Pools.$Algorithm_Norm.Host -notmatch $_.ExcludePoolName)) {
+                if ($First) {
+                    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
+                    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+                    $First = $false
+                }
+
+				[PSCustomObject]@{
+					Name           = $Miner_Name
+					DeviceName     = $Miner_Device.Name
+					DeviceModel    = $Miner_Model
+					Path           = if ($_.Path) {$_.Path} else {$Path}
+					Arguments      = "-b 127.0.0.1:`$mport -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User)$(if ($Pools.$Algorithm_Norm.Pass) {" -p $($Pools.$Algorithm_Norm.Pass)"})$($DeviceParams) -q $($_.Params)"
+					HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Week}
+					API            = "Ccminer"
+					Port           = $Miner_Port
+					Uri            = $Uri
+                    FaultTolerance = $_.FaultTolerance
+					ExtendInterval = if ($_.ExtendInterval -ne $null) {$_.ExtendInterval} else {2}
+                    Penalty        = 0
+					DevFee         = $DevFee
+					ManualUri      = $ManualUri
+                    Version        = $Version
+                    PowerDraw      = 0
+                    BaseName       = $Name
+                    BaseAlgorithm  = $Algorithm_Norm_0
+                    Benchmarked    = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".Benchmarked
+                    LogFile        = $Global:StatsCache."$($Miner_Name)_$($Algorithm_Norm_0)_HashRate".LogFile
+                    PrerequisitePath = $null
+                    PrerequisiteURI  = $null
+                    PrerequisiteMsg  = $null
+                    ExcludePoolName = $_.ExcludePoolName
+				}
+			}
+		}
+    }
+}
