8d9327b84f031b0b0138bba04dd0fbe91bcc3a6f
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2026-05-08 17:02:54 +0200

    Update pool SuprNova
    - improve readbility
    - remove rpc from data

--- Pools/SuprNova.ps1
+++ Pools/SuprNova.ps1
@@ -44,26 +44,26 @@ $Pool_Regions = @("eu","us-west","us-east","asia")
 $Pool_Regions | Foreach-Object {$Pool_RegionsTable.$_ = Get-Region $_}
 
 $Pools_Data = @(
-    [PSCustomObject]@{symbol = "C64"      ; rpc = "c64"     ; port = @(6464,6469); regions = @("eu","us-east","asia")}
-    [PSCustomObject]@{symbol = "DCR"      ; rpc = "dcr"     ; port = @(9332,9336); regions = @("eu","us-east","asia")}
-    [PSCustomObject]@{symbol = "DGB";     ; rpc = "dgbq"    ; port = @(8531); regions = @("eu")}
-    [PSCustomObject]@{symbol = "FAIR"     ; rpc = "fair"    ; port = @(3833,3834); regions = @("eu","asia")}
-	[PSCustomObject]@{symbol = "GAP"      ; rpc = "gap"     ; port = @(2433); regions = @("eu")}
-	[PSCustomObject]@{symbol = "GRS"      ; rpc = "grs"     ; port = @(5544); regions = @("eu","us-east")}
-    [PSCustomObject]@{symbol = "JUNO"     ; rpc = "juno"    ; port = @(8383); regions = @("eu","us-east")}
-    [PSCustomObject]@{symbol = "LPEPE"    ; rpc = "lpepe"   ; port = @(3633,3634); regions = @("eu","asia")}
-    [PSCustomObject]@{symbol = "NPT"      ; rpc = "xnt"     ; port = @([PSCustomObject]@{CPU=@(3832);GPU=@(3833)}); regions = @("eu")}
-    [PSCustomObject]@{symbol = "OBTC"     ; rpc = "obtc"    ; port = @([PSCustomObject]@{CPU=@(4074);GPU=@(4075)}); regions = @("eu")}
-    [PSCustomObject]@{symbol = "PXC"      ; rpc = "pxc"     ; port = @(2026,2027); regions = @("eu")}
-    [PSCustomObject]@{symbol = "QTC"      ; rpc = "qtc"     ; port = @(5555,5557); regions = @("eu","us-east","asia")}
-	[PSCustomObject]@{symbol = "RIC"      ; rpc = "ric"     ; port = @(5000); regions = @("eu")}
-    [PSCustomObject]@{symbol = "RTM"      ; rpc = "rtm"     ; port = @(6273); regions = @("eu")}
-    [PSCustomObject]@{symbol = "RVN"      ; rpc = "rvn"     ; port = @(8888,8889); regions = @("eu")}
-    [PSCustomObject]@{symbol = "VTC"      ; rpc = "vtc"     ; port = @(1777,1780); regions = @("eu")}
-    [PSCustomObject]@{symbol = "XEL"      ; rpc = "xel"     ; port = @(3333); regions = @("eu","us-east","asia")}
-    [PSCustomObject]@{symbol = "XNT"      ; rpc = "xnt"     ; port = @([PSCustomObject]@{CPU=@(3832);GPU=@(3833)}); regions = @("eu")}
-	[PSCustomObject]@{symbol = "ZEC"      ; rpc = "zec"     ; port = @(3732); regions = @("eu")}
-    [PSCustomObject]@{symbol = "XMR"      ; rpc = "xmr"     ; port = @(6665,6666); regions = @("eu","us-east")}
+    [PSCustomObject]@{symbol = "C64"    ; port = @(6464,6469)                                   ; regions = @("eu","us-east","asia")}
+    [PSCustomObject]@{symbol = "DCR"    ; port = @(9332,9336)                                   ; regions = @("eu","us-east","asia")}
+    [PSCustomObject]@{symbol = "DGB";   ; port = @(8531)                                        ; regions = @("eu")}
+    [PSCustomObject]@{symbol = "FAIR"   ; port = @(3833,3834)                                   ; regions = @("eu","asia")}
+	[PSCustomObject]@{symbol = "GAP"    ; port = @(2433)                                        ; regions = @("eu")}
+	[PSCustomObject]@{symbol = "GRS"    ; port = @(5544)                                        ; regions = @("eu","us-east")}
+    [PSCustomObject]@{symbol = "JUNO"   ; port = @(8383)                                        ; regions = @("eu","us-east")}
+    [PSCustomObject]@{symbol = "LPEPE"  ; port = @(3633,3634)                                   ; regions = @("eu","asia")}
+    [PSCustomObject]@{symbol = "NPT"    ; port = @([PSCustomObject]@{CPU=@(3832);GPU=@(3833)})  ; regions = @("eu")}
+    [PSCustomObject]@{symbol = "OBTC"   ; port = @([PSCustomObject]@{CPU=@(4074);GPU=@(4075)})  ; regions = @("eu")}
+    [PSCustomObject]@{symbol = "PXC"    ; port = @(2026,2027)                                   ; regions = @("eu")}
+    [PSCustomObject]@{symbol = "QTC"    ; port = @(5555,5557)                                   ; regions = @("eu","us-east","asia")}
+	[PSCustomObject]@{symbol = "RIC"    ; port = @(5000)                                        ; regions = @("eu")}
+    [PSCustomObject]@{symbol = "RTM"    ; port = @(6273)                                        ; regions = @("eu")}
+    [PSCustomObject]@{symbol = "RVN"    ; port = @(8888,8889)                                   ; regions = @("eu")}
+    [PSCustomObject]@{symbol = "VTC"    ; port = @(1777,1780)                                   ; regions = @("eu")}
+    [PSCustomObject]@{symbol = "XEL"    ; port = @(3333)                                        ; regions = @("eu","us-east","asia")}
+    [PSCustomObject]@{symbol = "XNT"    ; port = @([PSCustomObject]@{CPU=@(3832);GPU=@(3833)})  ; regions = @("eu")}
+	[PSCustomObject]@{symbol = "ZEC"    ; port = @(3732)                                        ; regions = @("eu")}
+    [PSCustomObject]@{symbol = "XMR"    ; port = @(6665,6666)                                   ; regions = @("eu","us-east")}
 )
 
 $Pool_Request.pools | Where-Object {-not $_.coming_soon} | Where-Object {$Wallets."$($_.coin.symbol)" -or $InfoOnly} | ForEach-Object {
@@ -88,7 +88,7 @@ $Pool_Request.pools | Where-Object {-not $_.coming_soon} | Where-Object {$Wallet
     $Pool_Workers  = $_.stats.workers
 
     foreach ($Pool_Region in $Pool_Data.regions) {
-        $Pool_Stratum = "$(if ($Pools_Region_Stratums[$Pool_Region]) {$Pools_Region_Stratums[$Pool_Region]} else {$Pool_Data.rpc}).suprnova.cc"
+        $Pool_Stratum = "$(if ($Pools_Region_Stratums[$Pool_Region]) {$Pools_Region_Stratums[$Pool_Region]} else {$_.id}).suprnova.cc"
         $Pool_SSL = $false
         foreach ($Port in @($Pool_Data.port | Select-Object)) {
             [PSCustomObject]@{
