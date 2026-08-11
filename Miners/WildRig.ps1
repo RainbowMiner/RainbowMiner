4acf7cd3792e95b134f08ee6f1e730f4f120b735
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2026-08-11 17:15:00 +0200

    Update miner Wildrig to v0.50.1 (amd,intel,nvidia)
    - implemented support hardfork for Pearl on pearlhash.xyz pool
    - tuned pearlhash kernels for NVIDIA RTX 3000/4000 to be efficient instead of max hashrate

--- Miners/WildRig.ps1
+++ Miners/WildRig.ps1
@@ -14,14 +14,14 @@ $ManualUri = "https://bitcointalk.org/index.php?topic=5023676.0"
 $Port = "407{0:d2}"
 $DevFee = 0.00
 $Cuda = "11.0"
-$Version = "0.49.9"
+$Version = "0.50.1"
 
 if ($IsLinux) {
     $Path = ".\Bin\GPU-WildRig\wildrig-multi"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.49.9-wildrigmulti/wildrig-multi-linux-0.49.9.tar.gz"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.50.1-wildrigmulti/wildrig-multi-linux-0.50.1.tar.gz"
 } else {
     $Path = ".\Bin\GPU-WildRig\wildrig.exe"
-    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.49.9-wildrigmulti/wildrig-multi-windows-0.49.9.zip"
+    $Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v0.50.1-wildrigmulti/wildrig-multi-windows-0.50.1.zip"
 }
 
 $Commands = [PSCustomObject[]]@(
