efa77a6d6f7c244e57bc995786e0cbcbf9995d49
Decho Kocharin <sourcedecho@outlook.com>
2026-09-13 10:15:24 +0700

    Update CcminerTpfuemp.ps1

--- Miners/CcminerTpfuemp.ps1
+++ Miners/CcminerTpfuemp.ps1
@@ -25,15 +25,33 @@ if ($IsLinux) {
 }
 
 $Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{MainAlgorithm = "allium"; Params = "-a allium "; ExtendInterval = 2} #allium
     [PSCustomObject]@{MainAlgorithm = "argon2d500"; Params = "-a argon2d500 "; ExtendInterval = 2} #argon2d500
+    [PSCustomObject]@{MainAlgorithm = "argon2d1000"; Params = "-a argon2d1000 "; ExtendInterval = 2} #argon2d1000
     [PSCustomObject]@{MainAlgorithm = "argon2d4096"; Params = "-a argon2d4096 "; ExtendInterval = 2} #argon2d4096
+    [PSCustomObject]@{MainAlgorithm = "argon2d16000"; Params = "-a argon2d16000 "; ExtendInterval = 2} #argon2d16000
+    [PSCustomObject]@{MainAlgorithm = "balloon"; Params = "-a balloon "; ExtendInterval = 2} #balloon
+    [PSCustomObject]@{MainAlgorithm = "bmw"; Params = "-a bmw "; ExtendInterval = 2} #bmw
+    [PSCustomObject]@{MainAlgorithm = "bmw512"; Params = "-a bmw512 "; ExtendInterval = 2} #bmw512
     [PSCustomObject]@{MainAlgorithm = "curvehash"; Params = "-a curvehash "; ExtendInterval = 2} #curvehash
+    [PSCustomObject]@{MainAlgorithm = "evrprogpow"; Params = "-a evrprogpow "; ExtendInterval = 2} #evrprogpow
+    [PSCustomObject]@{MainAlgorithm = "firopow"; Params = "-a firopow "; ExtendInterval = 2} #firopow
     [PSCustomObject]@{MainAlgorithm = "hoohashpepew"; Params = "-a pepew "; ExtendInterval = 2} #hoohashpepew
+    [PSCustomObject]@{MainAlgorithm = "kawpow"; Params = "-a kawpow "; ExtendInterval = 2} #kawpow
+    [PSCustomObject]@{MainAlgorithm = "meowpow"; Params = "-a meowpow "; ExtendInterval = 2} #meowpow
+    [PSCustomObject]@{MainAlgorithm = "meraki"; Params = "-a meraki "; ExtendInterval = 2} #meraki
+    [PSCustomObject]@{MainAlgorithm = "rinhash"; Params = "-a rinhash "; ExtendInterval = 2} #rinhash
+    [PSCustomObject]@{MainAlgorithm = "sha256csm"; Params = "-a sha256csm "; ExtendInterval = 2} #sha256csm
     [PSCustomObject]@{MainAlgorithm = "sha3t"; Params = "-a sha3t "; ExtendInterval = 2} #SHA3t
     [PSCustomObject]@{MainAlgorithm = "sha512256d"; Params = "-a sha512256d "; ExtendInterval = 2} #sha512256d
+    [PSCustomObject]@{MainAlgorithm = "skydoge"; Params = "-a skydoge "; ExtendInterval = 2} #skydoge
     [PSCustomObject]@{MainAlgorithm = "soterg"; Params = "-a soterg "; ExtendInterval = 2} #soterg
     [PSCustomObject]@{MainAlgorithm = "whirlpoolx2"; Params = "-a whirlpoolx2 "; ExtendInterval = 2} #whirlpoolx2
     [PSCustomObject]@{MainAlgorithm = "x25x"; Params = "-a x25x "; ExtendInterval = 2} #x25x
+    [PSCustomObject]@{MainAlgorithm = "yescrypt"; Params = "-a yescrypt "; ExtendInterval = 2} #yescrypt
+    [PSCustomObject]@{MainAlgorithm = "yescryptr8"; Params = "-a yescryptr8 "; ExtendInterval = 2} #yescryptr8
+    [PSCustomObject]@{MainAlgorithm = "yescryptr16"; Params = "-a yescryptr16 "; ExtendInterval = 2} #yescryptr16
+    [PSCustomObject]@{MainAlgorithm = "yescryptr32"; Params = "-a yescryptr32 "; ExtendInterval = 2} #yescryptr32
 )
 
 if (-not $InfoOnly) {
